63d57e4a314a582e6b01e2ec0ad52242afcef6c5
Bob den Otter <bobdenotter@gmail.com>
2012-06-15 10:07:14 +0200

    Added view/ for the backend

---
+++ app/view/js/silex.js
@@ -0,0 +1,11 @@
+
+// Don't break on browsers without console.log();
+if (typeof(console) === 'undefined') { console = { log: function() {}, assert: function() {} }; }
+
+
+jQuery(function($) {
+
+    // Do stuff.
+
+});
+
