082e4a8d7dd3c927fb7fa6aea8a2174f94badf99
Bob den Otter <bob@pivotx.net>
2012-06-22 14:13:46 +0200

    More cleanup..

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -7,8 +7,8 @@ if (!file_exists(__DIR__.'/../vendor/autoload.php')) {
 
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/lib.php';
-require_once __DIR__.'/storage.php';
-require_once __DIR__.'/users.php';
+require_once __DIR__.'/classes/storage.php';
+require_once __DIR__.'/classes/users.php';
 require_once __DIR__.'/config.php';
 
 
---
+++ app/config.php
@@ -0,0 +1,42 @@
+<?php
+
+$config = array();
+
+// Read the config
+$yamlparser = new Symfony\Component\Yaml\Parser();
+$config['general'] = $yamlparser->parse(file_get_contents(__DIR__.'/config/config.yml'));
+$config['taxonomy'] = $yamlparser->parse(file_get_contents(__DIR__.'/config/taxonomy.yml'));
+$config['contenttypes'] = $yamlparser->parse(file_get_contents(__DIR__.'/config/contenttypes.yml'));
+
+$configdb = $config['general']['database'];
+
+if (isset($configdb['driver']) && ($configdb['driver'] == "pdo_sqlite") ) {
+    
+    $basename = basename($configdb['database']);
+    if (getExtension($basename)!="db") { $basename .= ".db"; };
+    
+    $dboptions = array(
+        'driver' => 'pdo_sqlite',
+        'dbname' => __DIR__ . "/" . $basename
+    );
+    
+} else {
+    // Assume we configured it correctly. Yeehaa!
+    $dboptions = array(
+        'driver'    => (isset($configdb['driver']) ? $configdb['driver'] : 'pdo_mysql'),
+        'host'      => (isset($configdb['host']) ? $configdb['host'] : 'localhost'),
+        'dbname'    => $configdb['databasename'],
+        'user'      => $configdb['username'],
+        'password'  => $configdb['password'],
+        'port'      => (isset($configdb['host']) ? $configdb['host'] : '3306'),
+    );
+    
+}
+
+// I don't think i can set Twig's path in runtime, so we have to resort to hackishness to set the 
+// path.. if the request URI starts with '/pilex' in the URL, we assume we're in the Backend.. Yeah..
+if (strpos($_SERVER['REQUEST_URI'], "/pilex") === 0) {
+    $config['twigpath'] = __DIR__.'/view';
+} else {
+    $$config['twigpath'] = __DIR__.'/../view';
+}
