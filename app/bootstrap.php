b98ae872e20b2f8ca31e2284045129d806fe1912
Bob den Otter <bob@twokings.nl>
2012-08-29 17:39:38 +0200

    Moved twig_pilex.php to classes/

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -78,6 +78,9 @@ $app['users'] = new Users($app);
 $app['editlink'] = "";
 
 // Add the Pilex Twig functions, filters and tags.
-require_once __DIR__.'/twig_pilex.php';
+require_once __DIR__.'/classes/twig_pilex.php';
+$app['twig']->addExtension(new Pilex_Twig_Extension());
+
+
 require_once __DIR__.'/app_backend.php';
 require_once __DIR__.'/app_frontend.php';
