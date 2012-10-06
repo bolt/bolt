536ae35187b01a8584a2228ac4ff1d5585438477
Bob den Otter <bobdenotter@gmail.com>
2012-10-06 14:10:15 +0200

    Split Setcontent, as it should

---
+++ app/src/Bolt/SetcontentNode.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bolt;
+
+class SetcontentNode extends \Twig_Node
+{
+    public function __construct($name, $contenttype, $arguments, $lineno, $tag = null)
+    {
+        parent::__construct(array(), array('name' => $name, 'contenttype' => $contenttype, 'arguments' => $arguments),  $lineno, $tag);
+    }
+
+    public function compile(\Twig_Compiler $compiler)
+    {
+        $arguments = $this->getAttribute('arguments');
+
+        $compiler
+            ->addDebugInfo($this)
+            ->write('$template_storage = new Bolt\Storage($context[\'app\']);' . "\n")
+            ->write('$context[\'' . $this->getAttribute('name') . '\'] = ')
+            ->write('$template_storage->getContent(')
+            ->subcompile($this->getAttribute('contenttype'))
+            ->raw(", " . var_export($arguments,true) )
+            ->raw(" );\n")
+        ;
+
+    }
+}
--- app/src/Bolt/SetcontentTokenParser.php
+++ app/src/Bolt/SetcontentTokenParser.php
@@ -68,7 +68,7 @@ class SetcontentTokenParser extends \Twig_TokenParser
 
         $this->parser->getStream()->expect(\Twig_Token::BLOCK_END_TYPE);
 
-        return new Bolt_Setcontent_Node($name, $contenttype, $arguments, $lineno, $this->getTag());
+        return new SetcontentNode($name, $contenttype, $arguments, $lineno, $this->getTag());
     }
 
     public function getTag()
@@ -77,27 +77,3 @@ class SetcontentTokenParser extends \Twig_TokenParser
     }
 }
 
-
-class Bolt_Setcontent_Node extends \Twig_Node
-{
-    public function __construct($name, $contenttype, $arguments, $lineno, $tag = null)
-    {
-        parent::__construct(array(), array('name' => $name, 'contenttype' => $contenttype, 'arguments' => $arguments),  $lineno, $tag);
-    }
-
-    public function compile(\Twig_Compiler $compiler)
-    {
-        $arguments = $this->getAttribute('arguments');
-
-        $compiler
-            ->addDebugInfo($this)
-            ->write('$template_storage = new Bolt\Storage($context[\'app\']);' . "\n")
-            ->write('$context[\'' . $this->getAttribute('name') . '\'] = ')
-            ->write('$template_storage->getContent(')
-            ->subcompile($this->getAttribute('contenttype'))
-            ->raw(", " . var_export($arguments,true) )
-            ->raw(" );\n")
-        ;
-
-    }
-}
