e44c63079dbd0f65c2c30a19e633846bb5eed34c
Bob den Otter <bob@twokings.nl>
2012-11-08 17:52:06 +0100

    Fixed table headers in overview.

--- app/view/js/jquery.plugins.js
+++ app/view/js/jquery.plugins.js
@@ -126,3 +126,140 @@
     d.moveStart("character",a);d.moveEnd("character",c-a);d.select()}else{this[0].selectionStart=a;this[0].selectionEnd=c}this[0].focus();return this}else{if(d){c=document.selection;if(this[0].tagName.toLowerCase()!="textarea"){d=this.val();a=c[i]()[j]();a.moveEnd("character",d[e]);var g=a.text==""?d[e]:d.lastIndexOf(a.text);a=c[i]()[j]();a.moveStart("character",-d[e]);var h=a.text[e]}else{a=c[i]();c=a[j]();c.moveToElementText(this[0]);c.setEndPoint("EndToEnd",a);g=c.text[e]-a.text[e];h=g+a.text[e]}}else{g=
     f.selectionStart;h=f.selectionEnd}a=f.value.substring(g,h);return{start:g,end:h,text:a,replace:function(m){return f.value.substring(0,g)+m+f.value.substring(h,f.value[e])}}}}})(jQuery,"length","createRange","duplicate");
 */
+
+
+/*! Copyright (c) 2011 by Jonas Mosbech - https://github.com/jmosbech/StickyTableHeaders
+ MIT license info: https://github.com/jmosbech/StickyTableHeaders/blob/master/license.txt */
+
+;(function ($, window, undefined) {
+    'use strict';
+
+    var pluginName = 'stickyTableHeaders';
+    var defaults = {
+        fixedOffset: 0
+    };
+
+    function Plugin (el, options) {
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var base = this;
+
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
+
+        // Cache DOM refs for performance reasons
+        base.$window = $(window);
+        base.$clonedHeader = null;
+        base.$originalHeader = null;
+
+        // Keep track of state
+        base.isCloneVisible = false;
+        base.leftOffset = null;
+        base.topOffset = null;
+
+        base.init = function () {
+            base.options = $.extend({}, defaults, options);
+
+            base.$el.each(function () {
+                var $this = $(this);
+
+                // remove padding on <table> to fix issue #7
+                $this.css('padding', 0);
+
+                base.$originalHeader = $('thead:first', this);
+                base.$clonedHeader = base.$originalHeader.clone();
+
+                base.$clonedHeader.addClass('tableFloatingHeader');
+                base.$clonedHeader.css({
+                    'position': 'fixed',
+                    'top': 0,
+                    'z-index': 1, // #18: opacity bug
+                    'display': 'none'
+                });
+
+                base.$originalHeader.addClass('tableFloatingHeaderOriginal');
+
+                base.$originalHeader.after(base.$clonedHeader);
+
+                // enabling support for jquery.tablesorter plugin
+                // forward clicks on clone to original
+                $('th', base.$clonedHeader).click(function (e) {
+                    var index = $('th', base.$clonedHeader).index(this);
+                    $('th', base.$originalHeader).eq(index).click();
+                });
+                $this.bind('sortEnd', base.updateWidth);
+            });
+
+            base.updateWidth();
+            base.toggleHeaders();
+
+            base.$window.scroll(base.toggleHeaders);
+            base.$window.resize(base.toggleHeaders);
+            base.$window.resize(base.updateWidth);
+        };
+
+        base.toggleHeaders = function () {
+            base.$el.each(function () {
+                var $this = $(this);
+
+                var newTopOffset = isNaN(base.options.fixedOffset) ?
+                    base.options.fixedOffset.height() : base.options.fixedOffset;
+
+                var offset = $this.offset();
+                var scrollTop = base.$window.scrollTop() + newTopOffset;
+                var scrollLeft = base.$window.scrollLeft();
+
+                if ((scrollTop > offset.top) && (scrollTop < offset.top + $this.height())) {
+                    var newLeft = offset.left - scrollLeft;
+                    if (base.isCloneVisible && (newLeft === base.leftOffset) && (newTopOffset === base.topOffset)) {
+                        return;
+                    }
+
+                    base.$clonedHeader.css({
+                        'top': newTopOffset,
+                        'margin-top': 0,
+                        'left': newLeft,
+                        'display': 'block'
+                    });
+                    base.$originalHeader.css('visibility', 'hidden');
+                    base.isCloneVisible = true;
+                    base.leftOffset = newLeft;
+                    base.topOffset = newTopOffset;
+                }
+                else if (base.isCloneVisible) {
+                    base.$clonedHeader.css('display', 'none');
+                    base.$originalHeader.css('visibility', 'visible');
+                    base.isCloneVisible = false;
+                }
+            });
+        };
+
+        base.updateWidth = function () {
+            // Copy cell widths and classes from original header
+            $('th', base.$clonedHeader).each(function (index) {
+                var $this = $(this);
+                var $origCell = $('th', base.$originalHeader).eq(index);
+                this.className = $origCell.attr('class') || '';
+                $this.css('width', $origCell.width());
+            });
+
+            // Copy row width from whole table
+            base.$clonedHeader.css('width', base.$originalHeader.width());
+        };
+
+        // Run initializer
+        base.init();
+    }
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    };
+
+})(jQuery, window);
