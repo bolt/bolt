341951da1b5b00bd3937501899fcd7409cf72d6f
Richard <richard@besitegroup.nl>
2013-01-09 13:23:19 +0100

    Fix warning

--- app/app_backend.php
+++ app/app_backend.php
@@ -227,7 +227,10 @@ $app->error(function (\Exception $e) use ($app) {
         }
 
         // Don't display the full path..
-        $trace[$key]['file'] = str_replace($paths['rootpath'], "[root]", $trace[$key]['file']);
+        if ( isset( $trace[$key]['file'] ) )
+        {
+            $trace[$key]['file'] = str_replace(BOLT_BASE_DIR, "[root]", $trace[$key]['file']);
+        }
 
     }
 
