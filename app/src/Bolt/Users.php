0374da462751807d8129418cf047bcce047aaf6d
Juan Treminio <jtreminio@gmail.com>
2013-01-21 19:10:29 -0600

    Trying to get config options before $this->config is set!

--- app/src/Bolt/Users.php
+++ app/src/Bolt/Users.php
@@ -25,14 +25,14 @@ class Users
 
     public function __construct(Silex\Application $app)
     {
-        $prefix = isset($this->config['general']['database']['prefix']) ? $this->config['general']['database']['prefix'] : "bolt_";
-
         $this->db = $app['db'];
         $this->app = $app;
         $this->config = $app['config'];
         $this->usertable = $prefix . "users";
         $this->users = array();
         $this->session = $app['session'];
+
+        $prefix = isset($this->config['general']['database']['prefix']) ? $this->config['general']['database']['prefix'] : "bolt_";
 
         $this->checkValidSession();
 
