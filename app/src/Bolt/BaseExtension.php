ae38d30201f158abd6ca3182d104be7fc340be30
Bob den Otter <bobdenotter@gmail.com>
2013-01-28 19:18:34 +0100

    Minor fixes and tweaks: set $this->config, don't just return it.

--- app/src/Bolt/BaseExtension.php
+++ app/src/Bolt/BaseExtension.php
@@ -75,14 +75,14 @@ abstract class BaseExtension extends \Twig_Extension implements BaseExtensionInt
         // If it's readable, we're cool
         if (is_readable($configfile)) {
             $yamlparser = new \Symfony\Component\Yaml\Parser();
-            $config = $yamlparser->parse(file_get_contents($configfile));
-            return $config;
+            $this->config = $yamlparser->parse(file_get_contents($configfile));
+            return $this->config;
         }
 
         // Otherwise, check if there's a config.yml.dist
         if (is_readable($configdistfile)) {
             $yamlparser = new \Symfony\Component\Yaml\Parser();
-            $config = $yamlparser->parse(file_get_contents($configdistfile));
+            $this->config = $yamlparser->parse(file_get_contents($configdistfile));
 
             // If config.yml.dist exists, attempt to copy it to config.yml.
             if (copy($configdistfile, $configfile)) {
@@ -95,7 +95,7 @@ abstract class BaseExtension extends \Twig_Extension implements BaseExtensionInt
                 $this->app['session']->setFlash('error', $message);
             }
 
-            return $config;
+            return $this->config;
         }
 
         // Nope. No config.
