36b9dcd2538a93c24123478ef9ee17504f278ca6
Bob den Otter <bobdenotter@gmail.com>
2013-01-29 12:26:07 +0100

    Added Twig_SimpleFunction to list of ignored classes in util::var_dump()

--- app/classes/util.php
+++ app/classes/util.php
@@ -340,10 +340,13 @@ if ( ! class_exists( 'util' ) ) {
             // Don't traverse into Closures or Silex / Symfony objects..
             if (is_object($var)) {
                 list($root) = explode("\\", get_class($var));
-                if ( (get_class($var) == "Bolt\\Application") ||in_array($root, array('Closure', 'Silex', 'Symfony'))) {
+                // echo "[ " .$root . " - " .get_class($var)." ]";
+                if ( (get_class($var) == "Bolt\\Application") || in_array($root, array('Closure', 'Silex', 'Symfony', 'Twig_SimpleFunction'))) {
                     $html = '<span style="color:#588bff;">object</span>(' . get_class( $var ) . ') ';
+                    // echo "[return]\n";
                     return $html;
                 }
+                // echo "[stay]\n";
             }
 
             $html = '';
