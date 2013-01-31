8d18feab2b8f65468a652620874e7c8213479af2
pvankouteren <pvankouteren@gmail.com>
2013-01-31 09:32:06 +0100

    Final fix for trimText with use of a recursive function.

--- app/tests/Bolt/Tests/classes/libTest.php
+++ app/tests/Bolt/Tests/classes/libTest.php
@@ -46,8 +46,11 @@ class libTest extends \PHPUnit_Framework_TestCase {
             array("123<i>4567890<b>123456789012345</i>67</b>890", 20,
                 false, false, false, "123<i>4567890<b>1234567890</b></i>"),
             // tags with attributes
-            array('The quick brown <a href="http://bolt.cm">fox</a> jumps over the lazy dog',
+            array('The quick brown <a href="http://www.bolt.cm">fox</a> jumps over the lazy dog',
                 19, false, false, false, 'The quick brown <a href="http://www.bolt.cm">fox</a>'),
+            // two tags
+            array('The <a href="http://bolt.cm">quick</a> brown <a href="http://bolt.cm">fox</a> jumps over the lazy dog',
+                19, false, false, false, 'The <a href="http://bolt.cm">quick</a> brown <a href="http://bolt.cm">fox</a>'),
             // http:// shouldn't get stripped
             array('http://bolt.cm', 11, false, false, false, 'http://bolt'),
             // add dots in links when link text is trimmed
