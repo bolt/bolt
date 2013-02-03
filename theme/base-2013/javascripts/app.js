7d02057213287ccd367616c530178ec4e11b1f6e
Bob den Otter <bobdenotter@gmail.com>
2013-02-03 17:39:48 +0100

    More cleanup with theme.

--- theme/base-2013/javascripts/app.js
+++ theme/base-2013/javascripts/app.js
@@ -5,37 +5,22 @@
       Modernizr = window.Modernizr;
 
     $(document).ready(function() {
-        $.fn.foundationAlerts           ? $doc.foundationAlerts() : null;
-        $.fn.foundationButtons          ? $doc.foundationButtons() : null;
-        $.fn.foundationAccordion        ? $doc.foundationAccordion() : null;
-        $.fn.foundationNavigation       ? $doc.foundationNavigation() : null;
         $.fn.foundationTopBar           ? $doc.foundationTopBar() : null;
-        $.fn.foundationCustomForms      ? $doc.foundationCustomForms() : null;
-        $.fn.foundationMediaQueryViewer ? $doc.foundationMediaQueryViewer() : null;
-        $.fn.foundationTabs             ? $doc.foundationTabs({callback : $.foundation.customForms.appendCustomMarkup}) : null;
-        $.fn.foundationTooltips         ? $doc.foundationTooltips() : null;
-        $.fn.foundationMagellan         ? $doc.foundationMagellan() : null;
-        $.fn.foundationClearing         ? $doc.foundationClearing() : null;
-
-        $.fn.placeholder                ? $('input, textarea').placeholder() : null;
     });
-
-    // UNCOMMENT THE LINE YOU WANT BELOW IF YOU WANT IE8 SUPPORT AND ARE USING .block-grids
-    // $('.block-grid.two-up>li:nth-child(2n+1)').css({clear: 'both'});
-    // $('.block-grid.three-up>li:nth-child(3n+1)').css({clear: 'both'});
-    // $('.block-grid.four-up>li:nth-child(4n+1)').css({clear: 'both'});
-    // $('.block-grid.five-up>li:nth-child(5n+1)').css({clear: 'both'});
 
     // Hide address bar on mobile devices (except if #hash present, so we don't mess up deep linking).
     if (Modernizr.touch && !window.location.hash) {
-        $(window).load(function () {
-          setTimeout(function () {
-            window.scrollTo(0, 1);
-          }, 0);
+        $(window).load(function() {
+            setTimeout(function() { window.scrollTo(0, 1); }, 0);
         });
     }
 
-    // Initialize the Fancybox shizzle.
-    $('.fancybox, div.imageholder a').fancybox({ });
+    // Initialize the Fancybox shizzle, if present.
+    if(jQuery().fancybox) {
+        $('.fancybox, div.imageholder a').fancybox({ });
+    }
 
 })(jQuery, this);
+
+// Don't break on browsers without console.log();
+try { console.assert(1); } catch(e) { console = { log: function() {}, assert: function() {} } }
