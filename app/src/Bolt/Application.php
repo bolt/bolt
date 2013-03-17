05c0543d446e579746b1b85e636a2e6976a59874
Bob den Otter <bobdenotter@gmail.com>
2013-03-17 13:23:00 +0100

    Bumping version number to 1.0.3.

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -8,7 +8,7 @@ class Application extends BaseApplication
 {
     public function __construct(array $values = array())
     {
-        $values['bolt_version'] = '1.0.2';
+        $values['bolt_version'] = '1.0.3';
         $values['bolt_name'] = 'prerelease';
 
         parent::__construct($values);
