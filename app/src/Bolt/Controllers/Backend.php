82a310ffacfc0fec6542bd1018c5e38f344fd9fd
Bob den Otter <bobdenotter@gmail.com>
2013-03-17 09:40:21 +0100

    Updated Dutch translations, and replaced extra space before comment in yml.

--- app/src/Bolt/Controllers/Backend.php
+++ app/src/Bolt/Controllers/Backend.php
@@ -955,7 +955,7 @@ class Backend implements ControllerProviderInterface
                 if ($cnt) {
                     $content .= sprintf("# %d untranslated strings\n\n",$cnt);
                     foreach($msg['not_translated'] as $key) {
-                        $content .= "$key: #\n";
+                        $content .= "$key:  #\n";
                     }
                     $content .= "\n#-----------------------------------------\n";
                 } else {
@@ -971,7 +971,7 @@ class Backend implements ControllerProviderInterface
                 if ($cnt) {
                     $content .= sprintf("# %d untranslated strings\n\n",$cnt);
                     foreach($ctype['not_translated'] as $key) {
-                        $content .= "$key: #\n";
+                        $content .= "$key:  #\n";
                     }
                     $content .= "\n#-----------------------------------------\n";
                 } else {
