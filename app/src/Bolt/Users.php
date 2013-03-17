81b6bd2f8c0781ad9ab42ec3e30968f74867f1d8
Bob den Otter <bobdenotter@gmail.com>
2013-03-17 13:02:46 +0100

    Additional tweak for #240. Only 'Developer' can edit translations

--- app/src/Bolt/Users.php
+++ app/src/Bolt/Users.php
@@ -63,7 +63,7 @@ class Users
             'files:config' => self::DEVELOPER,
             'files:theme' => self::DEVELOPER,
             'files:uploads' => self::ADMIN,
-            'translation' => self::EDITOR,
+            'translation' => self::DEVELOPER,
             'activitylog' => self::ADMIN,
             'fileedit' => self::ADMIN
         );
