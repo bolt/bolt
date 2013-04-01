6506af8a0b9b969ded0f33b58430472aec7c0827
Miguel Angel Gabriel <magabriel@gmail.com>
2013-04-01 08:34:13 +0200

    Add language option to TwitterButton extension

--- app/extensions/TwitterButton/extension.php
+++ app/extensions/TwitterButton/extension.php
@@ -36,7 +36,12 @@ class Extension extends \Bolt\BaseExtension
         if (empty($this->config['url'])) {
             $this->config['url'] = $this->app['paths']['canonicalurl'];
         }
-
+        if (empty($this->config['language'])) {
+            $this->config['language'] = 'en_US';
+        }
+        if (empty($this->config['label'])) {
+            $this->config['label'] = 'Tweet';
+        }
         $this->addTwigFunction('twitterbutton', 'twitterButton');
 
     }
@@ -48,14 +53,16 @@ class Extension extends \Bolt\BaseExtension
         // code from: https://twitter.com/about/resources/buttons#tweet
 
         $html = <<< EOM
-    <a href="https://twitter.com/share" class="twitter-share-button" data-via="%via%" %count% data-url="%url%" data-dnt="true">Tweet</a>
+    <a href="https://twitter.com/share" class="twitter-share-button" data-via="%via%" %count% data-url="%url%" data-dnt="true" data-lang="%language%">%label%</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 EOM;
 
         $html = str_replace("%url%", $this->config['url'], $html);
         $html = str_replace("%via%", $this->config['via'], $html);
         $html = str_replace("%count%", $this->config['count'], $html);
-
+        $html = str_replace("%language%", $this->config['language'], $html);
+        $html = str_replace("%label%", $this->config['label'], $html);
+        
         return new \Twig_Markup($html, 'UTF-8');
 
     }
