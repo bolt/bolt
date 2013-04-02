999948f3d07b0a5c6227b558e598c59a9ab9de39
Felipe Martin <fmanaya@gmail.com>
2013-04-03 00:37:46 +0300

    Internet access through proxy.

--- app/src/Bolt/Controllers/Async.php
+++ app/src/Bolt/Controllers/Async.php
@@ -76,7 +76,19 @@ class Async implements ControllerProviderInterface
                 base64_encode($name)
             );
 
-            $guzzleclient = new \Guzzle\Http\Client($url);
+            // If there's a proxy ...
+    		if (!empty($app['config']['general']['httpProxy'])) {
+	            $options = array (
+	                   'curl.options' => array (
+	                          'CURLOPT_PROXY'          => $app['config']['general']['httpProxy']['host'],     
+	                          'CURLOPT_PROXYTYPE'      => 'CURLPROXY_HTTP',
+	                          'CURLOPT_PROXYUSERPWD'   => $app['config']['general']['httpProxy']['user'] . ':' . $app['config']['general']['httpProxy']['password']
+	                   )
+	            );
+				$guzzleclient = new \Guzzle\Http\Client($url, $options);
+			} else {
+				$guzzleclient = new \Guzzle\Http\Client($url);	
+			}
 
             $news = $guzzleclient->get("/")->send()->getBody(true);
             $news = json_decode($news);
