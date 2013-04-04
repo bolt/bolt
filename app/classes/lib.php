233a3907b15d22a23068ac0d002f964afd345ed1
Richard <richard@hinkamp.nl>
2013-04-04 15:06:03 +0200

    Restore css from CKEditor download Move custom Bolt CSS for CKEditor to own file Fix default config for WYSIWYG part, needs recursive merge CKEditor custom CSS file will be loaded instead of the Bolt CSS, after the stock CKEditor CSS is loaded Fixes #230

--- app/classes/lib.php
+++ app/classes/lib.php
@@ -685,6 +685,8 @@ function getConfig()
 
     // @todo: If no config files can be found, get them from bolt.cm/files/default/
 
+    $paths = getPaths($app['config']);
+
     // Assume some sensible defaults for some options
     $defaultconfig = array(
         'sitename' => 'Default Bolt site',
@@ -703,11 +705,22 @@ function getConfig()
         'listing_template' => 'listing.twig',
         'listing_records' => '5',
         'listing_sort' => 'datepublish DESC',
-        'wysiwyg_images' => false,
-        'wysiwyg_tables' => false,
-        'wysiwyg_embed' => false,
-        'wysiwyg_fontcolor' => false,
-        'wysiwyg_align' => false,
+        'wysiwyg' => array(
+            'images' => false,
+            'tables' => false,
+            'embed' => false,
+            'fontcolor' => false,
+            'align' => false,
+            'subsuper' => false,
+            'embed' => false,
+            'ck' => array(
+                'contentsCss' => array(
+                    $paths['app'] . 'view/lib/ckeditor/contents.css',
+                    $paths['app'] . 'view/css/ckeditor.css',
+                ),
+            ),
+            'filebrowser' => array(),
+        ),
         'canonical' => !empty($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : "",
         'developer_notices' => false,
         'cookies_use_remoteaddr' => true,
@@ -719,7 +732,11 @@ function getConfig()
         'hash_strength' => 10
     );
 
-    $config['general'] = array_merge($defaultconfig, $config['general']);
+    if (isset($config['general']['wysiwyg']) && isset($config['general']['wysiwyg']['ck']) &&
+        isset($config['general']['wysiwyg']['ck']['contentsCss'])) {
+        $config['general']['wysiwyg']['ck']['contentsCss'] = array(1 => $config['general']['wysiwyg']['ck']['contentsCss']);
+    }
+    $config['general'] = array_merge_recursive_distinct($defaultconfig, $config['general']);
 
     // Make sure the cookie_domain for the sessions is set properly.
     if (empty($config['general']['cookies_domain'])) {
@@ -1047,7 +1064,6 @@ function getPaths($original = array())
 
     // Set the root
     $path_prefix = dirname($_SERVER['PHP_SELF'])."/";
-    $path_prefix = preg_replace("/^[a-z]:/i", "", $path_prefix);
     $path_prefix = str_replace("//", "/", str_replace("\\", "/", $path_prefix));
     if (empty($path_prefix)) {
         $path_prefix = "/";
@@ -1298,6 +1314,45 @@ function str_replace_first($search, $replace, $subject) {
         $subject = substr_replace($subject, $replace, $pos, strlen($search));
     }
     return $subject;
+}
+
+/**
+ * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
+ * keys to arrays rather than overwriting the value in the first array with the duplicate
+ * value in the second array, as array_merge does. I.e., with array_merge_recursive,
+ * this happens (documented behavior):
+ *
+ * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('org value', 'new value'));
+ *
+ * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
+ * Matching keys' values in the second array overwrite those in the first array, as is the
+ * case with array_merge, i.e.:
+ *
+ * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
+ *     => array('key' => array('new value'));
+ *
+ * Parameters are passed by reference, though only for performance reasons. They're not
+ * altered by this function.
+ *
+ * @param array $array1
+ * @param array $array2
+ * @return array
+ * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
+ * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
+ */
+function array_merge_recursive_distinct (array &$array1, array &$array2) {
+    $merged = $array1;
+
+    foreach($array2 as $key => &$value) {
+        if (is_array($value) && isset($merged[$key]) && is_array($merged[$key])) {
+            $merged[$key] = array_merge_recursive_distinct($merged [$key], $value);
+        } else {
+            $merged[$key] = $value;
+        }
+    }
+
+    return $merged;
 }
 
 /**
