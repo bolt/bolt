b09ddff93b5d2c6e63c29a42da5ee6131636a96b
pvankouteren <pvankouteren@gmail.com>
2013-04-05 17:05:07 +0200

    Replaced mysql and puppi modules

--- support/puppet/modules/bolt/manifests/sql.pp
+++ support/puppet/modules/bolt/manifests/sql.pp
@@ -1,11 +1,12 @@
 class bolt::sql {
     # should be switchable later on
-    include mysql
+    class { "mysql":
+        puppi => true,
+    }
 
     exec { 'create-db':
         unless => "/usr/bin/mysql -u${params::dbuser} -p${params::dbpass} ${params::dbname}",
         command => "/usr/bin/mysql -e \"create database ${params::dbname}; grant all on ${params::dbname}.* to ${params::dbuser}@localhost identified by '${params::dbpass}';\"",
         require => Service["mysql"],
     }
-
 }
--- support/puppet/modules/puppi/manifests/is_installed.pp
+++
@@ -1,3 +0,0 @@
-class puppi::is_installed {
-
-} 
