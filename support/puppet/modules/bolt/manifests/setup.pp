bb38e9e5041cb5293e028cc18523a4b9abbbcb89
pvankouteren <pvankouteren@gmail.com>
2013-04-06 16:20:55 +0200

    Some cleaning

--- support/puppet/modules/bolt/manifests/setup.pp
+++ support/puppet/modules/bolt/manifests/setup.pp
@@ -16,15 +16,4 @@ class bolt::setup {
 
     class { "epel": }
 
-    # Setup a EPEL repo, the default one is disabled.
-    /*
-    file { "EpelRepo" :
-        path   => "/etc/yum.repos.d/epel.repo",
-        source => "${params::filepath}/bolt/files/epel.repo",
-        owner  => "root",
-        group  => "root",
-        mode  => 0644,
-    }
-    */
-
 }
