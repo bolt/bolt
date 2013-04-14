8d9e10c78c3cbd9a5ac8672276b171c61dd17b05
Bob den Otter <bobdenotter@gmail.com>
2013-04-14 14:33:07 +0200

    Bumping version number

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -8,7 +8,7 @@ class Application extends BaseApplication
 {
     public function __construct(array $values = array())
     {
-        $values['bolt_version'] = '1.0.4';
+        $values['bolt_version'] = '1.0.5';
         $values['bolt_name'] = '';
 
         parent::__construct($values);
