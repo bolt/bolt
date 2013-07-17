4aa9756d2f4113a19ad629b12e1e883bb66115f6
Bob den Otter <bobdenotter@gmail.com>
2013-07-17 17:27:41 +0200

    Bumping version number for Simpleforms to 1.5

--- app/extensions/SimpleForms/extension.php
+++ app/extensions/SimpleForms/extension.php
@@ -20,7 +20,7 @@ class Extension extends \Bolt\BaseExtension
             'description' => "This extension will allow you to insert simple forms on your site, for users to get in touch, send you a quick note or something like that. To use, configure the required fields in config.yml, and place <code>{{ simpleform('contact') }}</code> in your templates.",
             'author' => "Bob den Otter",
             'link' => "http://bolt.cm",
-            'version' => "1.4",
+            'version' => "1.5",
             'required_bolt_version' => "1.1",
             'highest_bolt_version' => "1.1",
             'type' => "Twig function",
