4e68f51dd0d7255df6e0e844f7bcf521d4fdc573
Bob den Otter <bobdenotter@gmail.com>
2013-09-10 20:33:39 +0200

    Bumping version number to "1.2 RC"

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -9,7 +9,7 @@ class Application extends BaseApplication
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '1.2';
-        $values['bolt_name'] = 'beta';
+        $values['bolt_name'] = 'RC';
 
         parent::__construct($values);
     }
