8494b4d256c251da306c06adba62f5a564207249
Tobias Dammers <tdammers@gmail.com>
2013-10-07 15:34:51 +0200

    Added missing method to BaseExtensionInterface

--- app/src/Bolt/Extensions/BaseExtensionInterface.php
+++ app/src/Bolt/Extensions/BaseExtensionInterface.php
@@ -9,4 +9,5 @@ interface BaseExtensionInterface
     public function __construct(Application $app);
     public function initialize();
     public function getInfo();
+    public function getConfig();
 }
