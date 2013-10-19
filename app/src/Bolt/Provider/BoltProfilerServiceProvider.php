407b60eee98a704516a3496cc1400b56f18551ee
Bob den Otter <bobdenotter@gmail.com>
2013-10-19 20:16:14 +0200

    Placing "Bolt" at the front of the toolbar, replacing "Symfony"

--- app/src/Bolt/Provider/BoltProfilerServiceProvider.php
+++ app/src/Bolt/Provider/BoltProfilerServiceProvider.php
@@ -11,9 +11,13 @@ class BoltProfilerServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
-        $app['data_collector.templates'] = array_merge(array(
-            array('bolt', '@BoltProfiler/toolbar/bolt.html.twig'),
-        ), $app['data_collector.templates']);
+
+        // Set the 'bolt' toolbar item as the first one, overriding the 'Symfony' one.
+        // Note: we use this workartound, because setting $app['data_collector.templates'][0]
+        // does not work.
+        $templates = $app['data_collector.templates'];
+        $templates[0] = array('bolt', '@BoltProfiler/toolbar/bolt.html.twig');
+        $app['data_collector.templates'] = $templates;
 
         $app['data_collectors'] = array_merge($app['data_collectors'], array(
             'bolt' => $app->share(function ($app) {
