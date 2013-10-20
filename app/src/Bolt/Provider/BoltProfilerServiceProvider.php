c229e8cb896615ebe29e64efc63c962324c4cfa6
bobdenotter <bobdenotter@air.lan>
2013-10-20 12:49:07 +0200

    Tweaking the toolbar.

--- app/src/Bolt/Provider/BoltProfilerServiceProvider.php
+++ app/src/Bolt/Provider/BoltProfilerServiceProvider.php
@@ -12,11 +12,17 @@ class BoltProfilerServiceProvider implements ServiceProviderInterface
     public function register(Application $app)
     {
 
-        // Set the 'bolt' toolbar item as the first one, overriding the 'Symfony' one.
-        // Note: we use this workartound, because setting $app['data_collector.templates'][0]
+        // Set the 'bolt' toolbar item as the first one, and overriding the 'Symfony' one.
+        // Note: we use this workaround, because setting $app['data_collector.templates'][0]
         // does not work.
-        $templates = $app['data_collector.templates'];
-        $templates[0] = array('bolt', '@BoltProfiler/toolbar/bolt.html.twig');
+        $templates = array_merge(
+            array(array('bolt', '@BoltProfiler/toolbar/bolt.html.twig')),
+            $app['data_collector.templates']
+        );
+
+        // Hackishly replace the template for the first toolbar item with our own.
+        $templates[1][1] = '@BoltProfiler/toolbar/config.html.twig';
+
         $app['data_collector.templates'] = $templates;
 
         $app['data_collectors'] = array_merge($app['data_collectors'], array(
