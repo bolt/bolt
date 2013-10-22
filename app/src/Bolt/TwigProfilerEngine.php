3c53b8dde4c7d92a27d1af0ed24b5d2bbdff7390
Simon <blankspace@me.com>
2013-10-22 10:24:27 +0100

    Added used templates to Profiler
    Based on a port of the Twig profiler which is part of
    https://github.com/Elao/WebProfilerExtraBundle All credits to the
    author of this package.

---
+++ app/src/Bolt/TwigProfilerEngine.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Bolt;
+
+use Symfony\Bridge\Twig\TwigEngine;
+
+use Bolt\TwigDataCollector;
+
+class TwigProfilerEngine extends TwigEngine
+{
+    protected $environment;
+    protected $twigEngine;
+    protected $collector;
+
+    public function __construct(\Twig_Environment $environment, TwigEngine $twigEngine, TwigDataCollector $collector)
+    {
+        $this->environment = $environment;
+        $this->twigEngine  = $twigEngine;
+        $this->collector   = $collector;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function render($name, array $parameters = array())
+    {
+        $templatePath = null;
+
+        $loader = $this->environment->getLoader();
+        if ($loader instanceof \Twig_Loader_Filesystem) {
+            $templatePath = $loader->getCacheKey($name);
+        }
+        $this->collector->collectTemplateData($name, $parameters, $templatePath);
+
+        return $this->twigEngine->render($name, $parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function stream($name, array $parameters = array())
+    {
+        $this->twigEngine->stream($name, $parameters);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function exists($name)
+    {
+        return $this->twigEngine->exists($name);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports($name)
+    {
+        return $this->twigEngine->supports($name);
+    }
+}
