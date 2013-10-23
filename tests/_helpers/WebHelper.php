7e71a891c46da3999e7b397c763460a376f28d61
Tobias Dammers <tobias@twokings.nl>
2013-10-23 12:56:24 +0200

    Moved test login to the web helper

--- tests/_helpers/WebHelper.php
+++ tests/_helpers/WebHelper.php
@@ -5,4 +5,16 @@ namespace Codeception\Module;
 
 class WebHelper extends \Codeception\Module
 {
+    /**
+     * Makes sure that WebGuy is logged in.
+     * @param array $user An associative array containing keys 'username'
+     * and 'password'.
+     */
+    public function loginAs($user) {
+        $web = $this->getModule('PhpBrowser');
+        $web->amOnPage('/bolt/login');
+        $web->fillField('username', $user['username']);
+        $web->fillField('password', $user['password']);
+        $web->click('Log on');
+    }
 }
--- tests/acceptance/backendLoginCept.php
+++ tests/acceptance/backendLoginCept.php
@@ -1,8 +1,5 @@
 <?php
 $I = new WebGuy($scenario);
 $I->wantTo('log into the backend as Admin');
-$I->amOnPage('/bolt/login');
-$I->fillField('username', $adminUser['username']);
-$I->fillField('password', $adminUser['password']);
-$I->click('Log on');
+$I->loginAs($adminUser);
 $I->see('Dashboard');
