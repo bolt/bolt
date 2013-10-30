776c4828074aa9ed218bc9300df25a47cd6c53da
Bob den Otter <bobdenotter@gmail.com>
2013-10-30 15:13:37 +0100

    Bumping version to "1.3 beta".

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -9,7 +9,7 @@ class Application extends BaseApplication
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '1.3';
-        $values['bolt_name'] = 'dev';
+        $values['bolt_name'] = 'beta';
 
         parent::__construct($values);
     }
