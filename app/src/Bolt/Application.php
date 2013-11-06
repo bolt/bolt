39ae37a940b1cd63f270ebc4b22422a6facc037f
Bob den Otter <bobdenotter@gmail.com>
2013-11-06 22:21:45 +0100

    Bumping version to 1.3 RC2

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -9,7 +9,7 @@ class Application extends BaseApplication
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '1.3';
-        $values['bolt_name'] = 'RC';
+        $values['bolt_name'] = 'RC2';
 
         parent::__construct($values);
     }
