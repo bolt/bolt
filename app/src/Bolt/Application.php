8eec56f8905aacf75bd9246a8e5be2c3aaa39f05
Bob den Otter <bobdenotter@gmail.com>
2013-11-16 16:27:07 +0100

    Bumping version to 1.4 dev

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -8,8 +8,8 @@ class Application extends BaseApplication
 {
     public function __construct(array $values = array())
     {
-        $values['bolt_version'] = '1.3.1';
-        $values['bolt_name'] = '';
+        $values['bolt_version'] = '1.4';
+        $values['bolt_name'] = 'dev';
 
         parent::__construct($values);
     }
