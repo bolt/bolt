5c47205b758176ca31f641295c1a5f7927a3e267
Tobias Dammers <tobias@twokings.nl>
2013-11-21 15:00:08 +0100

    Added some more acceptance tests for roles

---
+++ tests/acceptance/100-back-basic/502-logoutAsAdminCept.php
@@ -0,0 +1,9 @@
+<?php
+$I = new WebGuy($scenario);
+$I->wantTo('log out of the backend as Admin');
+$I->loginAs($users['admin']);
+$I->see('Dashboard');
+$I->click('Logout Admin');
+$I->see('You have been logged out');
+$I->amOnPage('bolt');
+$I->see('Please log on');
