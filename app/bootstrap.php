13d3546939fecd5a60321406b4f6d5e1426dd6b6
Bob den Otter <bobdenotter@gmail.com>
2013-12-08 14:13:39 +0100

    Major cleanup/refactoring: Cleaned up boostrap.php, removed app.php, moved functionality into Application.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -40,105 +40,16 @@ $checker->doChecks();
 require_once BOLT_PROJECT_ROOT_DIR.'/vendor/autoload.php';
 require_once __DIR__.'/classes/util.php';
 
-// Start the timer:
-$starttime = getMicrotime();
-
+// Initialize the 'Bolt application'.
 $app = new Bolt\Application();
-
-$app->register(new Bolt\Provider\ConfigServiceProvider());
-$app->register(new Silex\Provider\SessionServiceProvider(), array(
-    'session.storage.options' => array(
-        'name' => 'bolt_session',
-        'cookie_secure' => $app['config']->get('general/cookies_https_only'),
-        'cookie_httponly' => true
-    )
-));
-$app->register(new Bolt\Provider\LogServiceProvider());
 
 // Finally, check if the app/database folder is writable, if it needs to be.
 $checker->doDatabaseCheck($app['config']);
 
-$dboptions = $app['config']->getDBOptions();
-
-$app['debug'] = $app['config']->get('general/debug', false);
-$app['debugbar'] = false;
-
-list ($app['locale'], $app['territory']) = explode('_', $app['config']->get('general/locale'));
-
-// Set The Timezone Based on the Config, fallback to UTC
-date_default_timezone_set(
-    $app['config']->get('general/timezone')?:'UTC'
-);
-
-// Set default locale
-$locale = array(
-    $app['config']->get('general/locale') . '.utf8',
-    $app['config']->get('general/locale'),
-    'en_GB.utf8', 'en_GB', 'en'
-);
-setlocale(LC_ALL, $locale);
-
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => $app['config']->get('twigpath'),
-    'twig.options' => array(
-        'debug'=>true,
-        'cache' => __DIR__.'/cache/',
-        'strict_variables' => $app['config']->get('general/strict_variables'),
-        'autoescape' => true )
-));
-
-$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
-    'db.options' => $dboptions
-));
-
-if ($dboptions['driver']=="pdo_sqlite") {
-    $app['db']->query("PRAGMA synchronous = OFF");
-} else if ($dboptions['driver']=="pdo_mysql") {
-    // https://groups.google.com/forum/?fromgroups=#!topic/silex-php/AR3lpouqsgs
-    $app['db']->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
-    // set utf8 on names and connection as all tables has this charset
-    $app['db']->query("SET NAMES 'utf8';");
-    $app['db']->query("SET CHARACTER SET 'utf8';");
-    $app['db']->query("SET CHARACTER_SET_CONNECTION = 'utf8';");
-}
-
-$app->register(new Silex\Provider\HttpCacheServiceProvider(), array(
-    'http_cache.cache_dir' => __DIR__.'/cache/',
-));
-
-
-// Setup Swiftmailer, with optional SMTP settings. If no settings are provided in config.yml, mail() is used.
-$app->register(new Silex\Provider\SwiftmailerServiceProvider());
-if ($app['config']->get('general/mailoptions')) {
-    $app['swiftmailer.options'] = $app['config']->get('general/mailoptions');
-}
-
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-$app->register(new Silex\Provider\FormServiceProvider());
-$app->register(new Silex\Provider\ValidatorServiceProvider());
-$app->register(new Silex\Provider\TranslationServiceProvider(), array());
-
-// Loading stub functions for when intl / IntlDateFormatter isn't available.
-if (!function_exists('intl_get_error_code')) {
-    require_once BOLT_PROJECT_ROOT_DIR.'/vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs/functions.php';
-    require_once BOLT_PROJECT_ROOT_DIR.'/vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs/IntlDateFormatter.php';
-}
-
-$app->register(new Bolt\Provider\TranslationServiceProvider());
-$app->register(new Bolt\Provider\StorageServiceProvider());
-$app->register(new Bolt\Provider\UsersServiceProvider());
-$app->register(new Bolt\Provider\CacheServiceProvider());
-$app->register(new Bolt\Provider\ExtensionServiceProvider());
-$app->register(new Bolt\Provider\StackServiceProvider());
-
-$app['paths'] = getPaths($app['config']);
-$app['twig']->addGlobal('paths', $app['paths']);
-
-// Initialize the 'editlink' and 'edittitle'..
-$app['editlink'] = "";
-$app['edittitle'] = "";
+$app->initialize();
 
 // Set the Krumo default configuration.
+// @todo: Refactor this out. Shouldn't be here..
 \Krumo::setConfig(array(
     'skin' => array(
         'selected' => "stylish"
@@ -155,14 +66,3 @@ $app['edittitle'] = "";
     )
 ));
 
-
-$app->register(new Bolt\Provider\RenderServiceProvider());
-
-// Add the Bolt Twig functions, filters and tags.
-$app['twig']->addExtension(new Bolt\TwigExtension($app));
-$app['twig']->addTokenParser(new Bolt\SetcontentTokenParser());
-
-require __DIR__.'/app.php';
-
-// Initialize enabled extensions.
-$app['extensions']->initialize();
--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -2,9 +2,12 @@
 
 namespace Bolt;
 
-use Silex\Application as BaseApplication;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-class Application extends BaseApplication
+
+class Application extends \Silex\Application
 {
     public function __construct(array $values = array())
     {
@@ -12,6 +15,416 @@ class Application extends BaseApplication
         $values['bolt_name'] = 'dev';
 
         parent::__construct($values);
+
+        // Initialize the config. Note that we do this here, on 'construct'.
+        // All other initialisation is triggered from bootstrap.php
+        $this->initConfig();
+
+        $this['debug'] = $this['config']->get('general/debug', false);
+        $this['debugbar'] = false;
+
+        // Initialize the 'editlink' and 'edittitle'..
+        $this['editlink'] = "";
+        $this['edittitle'] = "";
+
+
+    }
+
+    /**
+     * Initialize the config and session providers.
+     */
+    private function initConfig()
+    {
+        $this->register(new Provider\ConfigServiceProvider());
+        $this->register(new \Silex\Provider\SessionServiceProvider(), array(
+            'session.storage.options' => array(
+                'name' => 'bolt_session',
+                'cookie_secure' => $this['config']->get('general/cookies_https_only'),
+                'cookie_httponly' => true
+            )
+        ));
+        $this->register(new Provider\LogServiceProvider());
+
+    }
+
+    public function initialize()
+    {
+
+        // Set up locale and translations.
+        $this->initLocale();
+
+        // Initialize Twig and our rendering Provider.
+        $this->initRendering();
+
+        // Initialize the Database Providers.
+        $this->initDatabase();
+
+        // Initialize the rest of the Providers.
+        $this->initProviders();
+
+        // Initialise the Mount points for 'frontend', 'backend' and 'async'.
+        $this->initMountpoints();
+
+        // Initialise the global 'before' handler.
+        $this->initBeforeHandler();
+
+        // Initialise the global 'after' handlers.
+        $this->initAfterHandler();
+
+        // Initialise the 'error' handler.
+        $this->initErrorHandler();
+
+
+    }
+
+
+    /**
+     * Initialize the database providers.
+     */
+    public function initDatabase()
+    {
+
+        $dboptions = $this['config']->getDBOptions();
+
+        $this->register(new \Silex\Provider\DoctrineServiceProvider(), array(
+            'db.options' => $dboptions
+        ));
+
+        if ($dboptions['driver']=="pdo_sqlite") {
+            $this['db']->query("PRAGMA synchronous = OFF");
+        } else if ($dboptions['driver']=="pdo_mysql") {
+            // https://groups.google.com/forum/?fromgroups=#!topic/silex-php/AR3lpouqsgs
+            $this['db']->getDatabasePlatform()->registerDoctrineTypeMapping('enum', 'string');
+            // set utf8 on names and connection as all tables has this charset
+            $this['db']->query("SET NAMES 'utf8';");
+            $this['db']->query("SET CHARACTER SET 'utf8';");
+            $this['db']->query("SET CHARACTER_SET_CONNECTION = 'utf8';");
+        }
+
+        $this->register(new \Silex\Provider\HttpCacheServiceProvider(), array(
+            'http_cache.cache_dir' => __DIR__.'/cache/',
+        ));
+
+    }
+
+    /**
+     *
+     */
+    public function initRendering()
+    {
+        $this->register(new \Silex\Provider\TwigServiceProvider(), array(
+            'twig.path' => $this['config']->get('twigpath'),
+            'twig.options' => array(
+                'debug'=>true,
+                'cache' => __DIR__.'/../../cache/',
+                'strict_variables' => $this['config']->get('general/strict_variables'),
+                'autoescape' => true )
+        ));
+
+        $this->register(new Provider\RenderServiceProvider());
+
+
+    }
+
+    /**
+     *
+     */
+    public function initLocale()
+    {
+        list ($this['locale'], $this['territory']) = explode('_', $this['config']->get('general/locale'));
+
+        // Set The Timezone Based on the Config, fallback to UTC
+        date_default_timezone_set(
+            $this['config']->get('general/timezone')?:'UTC'
+        );
+
+        // Set default locale
+        $locale = array(
+            $this['config']->get('general/locale') . '.utf8',
+            $this['config']->get('general/locale'),
+            'en_GB.utf8', 'en_GB', 'en'
+        );
+        setlocale(LC_ALL, $locale);
+
+        $this->register(new \Silex\Provider\TranslationServiceProvider(), array());
+
+        // Loading stub functions for when intl / IntlDateFormatter isn't available.
+        if (!function_exists('intl_get_error_code')) {
+            require_once BOLT_PROJECT_ROOT_DIR.'/vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs/functions.php';
+            require_once BOLT_PROJECT_ROOT_DIR.'/vendor/symfony/locale/Symfony/Component/Locale/Resources/stubs/IntlDateFormatter.php';
+        }
+
+        $this->register(new Provider\TranslationServiceProvider());
+
+    }
+
+    /**
+     *
+     */
+    public function initProviders()
+    {
+
+        // Setup Swiftmailer, with optional SMTP settings. If no settings are provided in config.yml, mail() is used.
+        $this->register(new \Silex\Provider\SwiftmailerServiceProvider());
+        if ($this['config']->get('general/mailoptions')) {
+            $this['swiftmailer.options'] = $this['config']->get('general/mailoptions');
+        }
+
+        $this->register(new \Silex\Provider\UrlGeneratorServiceProvider());
+        $this->register(new \Silex\Provider\FormServiceProvider());
+        $this->register(new \Silex\Provider\ValidatorServiceProvider());
+
+        $this->register(new Provider\StorageServiceProvider());
+        $this->register(new Provider\UsersServiceProvider());
+        $this->register(new Provider\CacheServiceProvider());
+        $this->register(new Provider\ExtensionServiceProvider());
+        $this->register(new Provider\StackServiceProvider());
+
+        $this['paths'] = getPaths($this['config']);
+        $this['twig']->addGlobal('paths', $this['paths']);
+
+        // Add the Bolt Twig functions, filters and tags.
+        $this['twig']->addExtension(new TwigExtension($this));
+        $this['twig']->addTokenParser(new SetcontentTokenParser());
+
+        // Initialize enabled extensions.
+        $this['extensions']->initialize();
+
+    }
+
+
+
+    public function initMountpoints()
+    {
+
+        $request = Request::createFromGlobals();
+        if ($proxies = $this['config']->get('general/trustProxies')) {
+            $request->setTrustedProxies($proxies);
+        }
+
+        // Mount the 'backend' on the branding:path setting. Defaults to '/bolt'.
+        $this->mount($this['config']->get('general/branding/path'), new Controllers\Backend());
+
+        // Mount the 'async' controllers on /async. Not configurable.
+        $this->mount('/async', new Controllers\Async());
+
+        if ($this['config']->get('general/enforce_ssl')) {
+            foreach ($this['routes']->getIterator() as $route) {
+                $route->requireHttps();
+            }
+        }
+
+        // Mount the 'frontend' controllers, ar defined in our Routing.yml
+        $this->mount('', new Controllers\Routing());
+
+    }
+
+    public function initBeforeHandler()
+    {
+
+        // PHP does not allow 'use ($this)' in closures.
+        $app = $this;
+
+        $this->before(function (Request $request) use ($app) {
+
+            // Start the 'stopwatch' for the profiler.
+            $app['stopwatch']->start('bolt.app.before');
+
+            $app['twig']->addGlobal('bolt_name', $app['bolt_name']);
+            $app['twig']->addGlobal('bolt_version', $app['bolt_version']);
+
+            $app['twig']->addGlobal('frontend', false);
+            $app['twig']->addGlobal('backend', false);
+            $app['twig']->addGlobal('async', false);
+            $app['twig']->addGlobal($app['config']->getWhichEnd(), true);
+
+            $app['twig']->addGlobal('user', $app['users']->getCurrentUser());
+            $app['twig']->addGlobal('users', $app['users']->getUsers());
+            $app['twig']->addGlobal('config', $app['config']);
+
+            // Sanity checks for doubles in in contenttypes.
+            // unfortunately this has to be done here, because the 'translator' classes need to be initialised.
+            $app['config']->checkConfig();
+
+            // Stop the 'stopwatch' for the profiler.
+            $app['stopwatch']->stop('bolt.app.before');
+
+        });
+
+
+    }
+
+
+    public function initAfterHandler()
+    {
+
+        // PHP does not allow 'use ($this)' in closures.
+        $app = $this;
+
+        // On 'finish' attach the debug-bar, if debug is enabled..
+        if ($this['debug'] && ($this['session']->has('user') || $this['config']->get('general/debug_show_loggedoff') ) ) {
+
+            // Set the error_reporting to the level specified in config.yml
+            error_reporting($this['config']->get('general/debug_error_level'));
+
+            // Register Whoops, to handle errors for logged in users only.
+            if ($this['config']->get('general/debug_enable_whoops')) {
+                $this->register(new \Whoops\Provider\Silex\WhoopsServiceProvider);
+            }
+
+            $this->register(new \Silex\Provider\ServiceControllerServiceProvider);
+
+            // Register the Silex/Symfony web debug toolbar.
+            $this->register(new \Silex\Provider\WebProfilerServiceProvider(), array(
+                'profiler.cache_dir' => __DIR__.'/../../cache/profiler',
+                'profiler.mount_prefix' => '/_profiler', // this is the default
+            ));
+
+            // Register the toolbar item for our Database query log.
+            $this->register(new Provider\DatabaseProfilerServiceProvider());
+
+            // Register the toolbar item for our bolt nipple.
+            $this->register(new Provider\BoltProfilerServiceProvider());
+
+            // Register the toolbar item for the Twig toolbar item.
+            $this->register(new Provider\TwigProfilerServiceProvider());
+
+            $this['twig.loader.filesystem']->addPath(BOLT_PROJECT_ROOT_DIR . '/vendor/symfony/web-profiler-bundle/Symfony/Bundle/WebProfilerBundle/Resources/views', 'WebProfiler');
+            $this['twig.loader.filesystem']->addPath(__DIR__ . '/../../view', 'BoltProfiler');
+
+            $this->after(function () use ($app) {
+
+                foreach(hackislyParseRegexTemplates($app['twig.loader.filesystem']) as $template) {
+                    $app['twig.logger']->collectTemplateData($template);
+                }
+
+            });
+
+        } else {
+            error_reporting(E_ALL &~ E_NOTICE &~ E_DEPRECATED &~ E_USER_DEPRECATED);
+        }
+
+
+        $this->after(function (Request $request, Response $response) use ($app) {
+
+            // Start the 'stopwatch' for the profiler.
+            $app['stopwatch']->start('bolt.app.after');
+
+            // true if we need to consider adding html snippets
+            if (isset($app['htmlsnippets']) && ($app['htmlsnippets'] === true)) {
+
+                // only add when content-type is text/html
+                if (strpos($response->headers->get('Content-Type'), 'text/html') !== false) {
+
+                    // Add our meta generator tag..
+                    $app['extensions']->insertSnippet(\Bolt\Extensions\Snippets\Location::AFTER_META, '<meta name="generator" content="Bolt">');
+
+                    // Perhaps add a canonical link..
+
+                    if ($app['config']->get('general/canonical')) {
+                        $snippet = sprintf('<link rel="canonical" href="%s">', $app['paths']['canonicalurl']);
+                        $app['extensions']->insertSnippet(\Bolt\Extensions\Snippets\Location::AFTER_META, $snippet);
+                    }
+
+                    // Perhaps add a favicon..
+                    if ($app['config']->get('general/favicon')) {
+                        $snippet = sprintf('<link rel="shortcut icon" href="//%s%s%s">',
+                            $app['paths']['canonical'],
+                            $app['paths']['theme'],
+                            $app['config']->get('general/favicon'));
+                        $app['extensions']->insertSnippet(\Bolt\Extensions\Snippets\Location::AFTER_META, $snippet);
+                    }
+
+                    $html = $response->getContent();
+
+                    $html = $app['extensions']->processSnippetQueue($html);
+
+                    $response->setContent($html);
+                }
+            }
+
+            // Stop the 'stopwatch' for the profiler.
+            $app['stopwatch']->stop('bolt.app.after');
+
+        });
+
+
+    }
+
+
+    public function initErrorHandler()
+    {
+
+        // PHP does not allow 'use ($this)' in closures.
+        $app = $this;
+
+        $this->error(function (\Exception $e) use ($app) {
+
+            // If we are in maintenance mode and current user is not logged in, show maintenance notice.
+            // @see /app/src/Bolt/Controllers/Frontend.php, Frontend::before()
+            if ($app['config']->get('general/maintenance_mode')) {
+                $user = $app['users']->getCurrentUser();
+                if ($user['userlevel'] < 2) {
+                    $template = $app['config']->get('general/maintenance_template');
+                    $body = $app['render']->render($template);
+                    return new Response($body, 503);
+                }
+            }
+
+            $paths = getPaths($app['config']);
+
+            $twigvars = array();
+
+            $twigvars['class'] = get_class($e);
+            $twigvars['message'] = $e->getMessage();
+            $twigvars['code'] = $e->getCode();
+            $twigvars['paths'] = $paths;
+
+            $app['log']->add($twigvars['message'], 2, '', 'abort');
+
+            $end = $app['config']->getWhichEnd();
+
+            $trace = $e->getTrace();
+
+            foreach ($trace as $key=>$value) {
+
+                if (!empty($value['file']) && strpos($value['file'], "/vendor/") > 0 ) {
+                    unset($trace[$key]['args']);
+                }
+
+                // Don't display the full path..
+                if ( isset( $trace[$key]['file'] ) )
+                {
+                    $trace[$key]['file'] = str_replace(BOLT_PROJECT_ROOT_DIR, "[root]", $trace[$key]['file']);
+                }
+
+            }
+
+            $twigvars['trace'] = $trace;
+            $twigvars['title'] = "An error has occured!";
+
+            if ( ($e instanceof HttpException) && ($end == "frontend") ) {
+
+                $content = $app['storage']->getContent($app['config']->get('general/notfound'), array('returnsingle' => true));
+
+                // Then, select which template to use, based on our 'cascading templates rules'
+                if ($content instanceof \Bolt\Content && !empty($content->id)) {
+                    $template = $content->template();
+
+                    return $app['render']->render($template, array(
+                        'record' => $content,
+                        $content->contenttype['singular_slug'] => $content // Make sure we can also access it as {{ page.title }} for pages, etc.
+                    ));
+                } else {
+                    $twigvars['message'] = "The page could not be found, and there is no 'notfound' set in 'config.yml'. Sorry about that.";
+                }
+
+            }
+
+            return $app['render']->render('error.twig', $twigvars);
+
+        });
+
     }
 
     /**
