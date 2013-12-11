707cc9032f44bcd95d341c601ac091b065474fca
Bob den Otter <bobdenotter@gmail.com>
2013-12-11 09:53:08 +0100

    Added Code comments to render.php

--- app/src/Bolt/Render.php
+++ app/src/Bolt/Render.php
@@ -7,7 +7,12 @@ use Symfony\Component\HttpFoundation\Response;
 
 
 /**
- * Wrapper around Twig's render() function.
+ * Wrapper around Twig's render() function. Handles the following responsibilities:
+ *
+ * - Calls twig's render
+ * - Stores a page in cache, if needed
+ * - Store template (partials) in cache, if needed
+ * - Fetches pages or template (partials) from cache
  *
  * @author Bob den Otter, bob@twokings.nl
  *
@@ -17,12 +22,22 @@ class Render
 
     /**
      * Set up the object.
+     *
+     * @param Silex\Application $app
      */
     public function __construct(Silex\Application $app)
     {
         $this->app = $app;
     }
 
+    /**
+     *
+     * Render a template, possibly store it in cache. Or, if applicable, return the cached result
+     *
+     * @param $template
+     * @param array $vars
+     * @return mixed
+     */
     public function render($template, $vars = array())
     {
 
@@ -49,8 +64,13 @@ class Render
 
     }
 
-    
-    public function postProces(Response $response)
+    /**
+     * Postprocess the rendered HTML: insert the snippets, and stuff.
+     *
+     * @param Response $response
+     * @return string
+     */
+    public function postProcess(Response $response)
     {
         $html = $response->getContent();
 
@@ -63,23 +83,39 @@ class Render
     }
 
 
+    /**
+     * Retrieve a  page (or basically, any template) from cache
+     *
+     * @param string $template
+     * @return mixed
+     */
     public function fetchCachedPage($template)
     {
         $key = md5($template . $this->app['request']->getRequestUri());
 
-        return $this->app['cache']->fetch($key, $html);
+        return $this->app['cache']->fetch($key);
 
     }
 
-
+    /**
+     * Retrieve a fully cached page from cache.
+     *
+     * @return mixed
+     */
     public function fetchCachedRequest()
     {
         $key = md5($this->app['request']->getRequestUri());
 
-        return $this->app['cache']->fetch($key, $html);
+        return $this->app['cache']->fetch($key);
 
     }
 
+    /**
+     * Store a page (or basically, any template) to cache.
+     *
+     * @param $template
+     * @param $html
+     */
     public function cacheRenderedPage($template, $html)
     {
 
@@ -93,6 +129,11 @@ class Render
 
     }
 
+    /**
+     * Store a fully rendered (and postprocessed) page to cache.
+     *
+     * @param $html
+     */
     public function cacheRequest($html) {
 
         if ($this->app['end'] == "frontend" && $this->app['config']->get('general/caching/request')) {
