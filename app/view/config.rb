be4fd0b646be02c345761bc2f7b457afb5c3b6ff
Jadwigo <lodewijk@jadwigo.nl>
2014-01-25 15:29:58 +0100

    initial sassified bootstrap 3 styles

---
+++ app/view/config.rb
@@ -0,0 +1,10 @@
+# configuration file for compass with sass
+# this file is used to build the css files from bootstrap-sass and fontawesome etc.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+
