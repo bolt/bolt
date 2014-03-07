99920b08d2459270c799c60c00367d42cb0e2e82
odlex <alexandre@imageinfrance.com>
2014-03-07 15:23:22 +0100

    Extension limitation with fileupload
    Attempt to establish a test on the fileupload (js) to allow only certain
    file type.
    Using the keyword "extensions" word in the declaration of the fields in
    contenttypes.yml

--- app/view/js/bolt.js
+++ app/view/js/bolt.js
@@ -372,9 +372,27 @@ function bindFileUpload(key) {
             var progress = Math.round(100 * data._bitrateTimer.loaded / data.files[0].size);
             $('#progress-' + key).show().addClass('progress-striped active');
             $('#progress-' + key + ' div.bar').css('width', progress+"%");
-        });
+        })
+		.bind('fileuploadsubmit', function (e, data) {
+				var that = this,
+				fileTypes = $('#field-' + key).attr('accept');
+				
+				if( typeof fileTypes !== 'undefined' ) {
+					var pattern = new RegExp( "(\.|\/)(" + fileTypes + ")$", "gi" );
+					$.each( data.files , function (index, file) {
+						if( !pattern.test(file.type) ) {
+							var message = "Oops! There was an error uploading the image. Make sure that the file type is correct."
+                            + "\n\n(accept type was: "
+                            + fileTypes + ")";
 
-
+							alert(message);
+							e.preventDefault();
+							return false;
+						}
+					});
+				}
+		})
+		;
 }
 
 
@@ -903,7 +921,24 @@ var FilelistHolder = Backbone.View.extend({
                         $this.add(filename, filename);
                     });
                 }
-            });
+            }).bind('fileuploadsubmit', function (e, data) {
+				var that = this,
+				fileTypes = $('#fileupload-' + contentkey).attr('accept');
+				
+				if( typeof fileTypes !== 'undefined' ) {
+					var pattern = new RegExp( "(\.|\/)(" + fileTypes + ")$", "i" );
+					$.each( data.files , function (index, file) {
+						if( !pattern.test(file.name) ) {
+							var message = "Oops! There was an error uploading the file. Make sure that the file type is correct."
+											+ "\n\n(accept type was: "
+											+ fileTypes + ")";
+							alert(message);
+							e.preventDefault();
+							return false;
+						}
+					});
+				}
+			});
 
         $holder.find("div.list").on('click', 'a', function(e) {
             e.preventDefault();
@@ -1036,7 +1071,24 @@ var ImagelistHolder = Backbone.View.extend({
                         $this.add(filename, filename);
                     });
                 }
-            });
+            }).bind('fileuploadsubmit', function (e, data) {
+				var that = this,
+				fileTypes = $('#fileupload-' + contentkey).attr('accept');
+				
+				if( typeof fileTypes !== 'undefined' ) {
+					var pattern = new RegExp( "(\.|\/)(" + fileTypes + ")$", "i" );
+					$.each( data.files , function (index, file) {
+						if( !pattern.test(file.name) ) {
+							var message = "Oops! There was an error uploading the image. Make sure that the file type is correct."
+											+ "\n\n(accept type was: "
+											+ fileTypes + ")";
+							alert(message);
+							e.preventDefault();
+							return false;
+						}
+					});
+				}
+			});
 
         $holder.find("div.list").on('click', 'a', function(e) {
             e.preventDefault();
