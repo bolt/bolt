e26b1a376559e7a16da0e9b0a6e850dd4a604162
Gawain Lynch <gawain.lynch@gmail.com>
2014-03-10 08:31:16 +0000

    Add a cron Nut job

---
+++ app/src/Bolt/Nut/CronRunner.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Bolt\Nut;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CronRunner extends BaseCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('cron')
+            ->setDescription('Cron virtual daemon')
+            ->addOption('single', null, InputOption::VALUE_NONE, 'If set, tell Bolt cron to run a single task')
+            ->addArgument('name', InputArgument::OPTIONAL, 'Name of task to run'
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        if ($input->getOption('single')) {
+            $param['single'] = true;
+        } else {
+            $param['single'] = false;
+        }
+
+        $name = $input->getArgument('name');
+        if ($name) {
+            $param['name'] = $name;
+        }
+
+        //$result = $this->app['cron']->execute($param);
+        $result = new \Bolt\Controllers\Cron($this->app);
+
+        if ($result) {
+            $output->writeln("<info>Cron run!</info>");
+        }
+    }
+}
+
