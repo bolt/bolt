f477b3e3cfc93a9896498615d4de576c2355080f
WedgeSama <wedgesama@gmail.com>
2014-03-15 15:30:00 +0100

    [Security] Files browser and editor
    Related to #997. Add some permissions on files to secure some of them
    like PHP file, etc...

---
+++ app/src/Bolt/Provider/FilePermissionsServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Bolt\Provider;
+
+use Bolt\FilePermissions;
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * @author Benjamin Georgeault <benjamin@wedgesama.fr>
+ */
+class FilePermissionsServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+        $app['filepermissions'] = $app->share(function ($app) {
+            $filepermissions = new FilePermissions($app);
+            return $filepermissions;
+        });
+    }
+
+    public function boot(Application $app)
+    {
+    }
+}
