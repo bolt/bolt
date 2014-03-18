7a57d6b6e997cc6ac3c8a6dc137ef37474058a6e
Rix Beck <rixbee@gmail.com>
2014-03-18 18:30:01 +0100

    Wrong paths if BOLT_COMPOSER_INSTALLED

--- app/classes/timthumb.php
+++ app/classes/timthumb.php
@@ -44,11 +44,7 @@ if (empty($matches[1]) || empty($matches[2]) || empty($matches[4])) {
 /**
  * Bolt specific: Set BOLT_PROJECT_ROOT_DIR, and Bolt-specific settings..
  */
-if (substr(__DIR__, -20) == DIRECTORY_SEPARATOR.'bolt-public'.DIRECTORY_SEPARATOR.'classes') { // installed bolt with composer
-    require_once __DIR__ . '/../../../vendor/bolt/bolt/app/bootstrap.php';
-} else {
-    require_once __DIR__ . '/../bootstrap.php';
-}
+require_once __DIR__ . '/../bootstrap.php';
 
 // Let's get on with the rest..
 $yamlparser = new Symfony\Component\Yaml\Parser();
@@ -1364,7 +1360,12 @@ class timthumb {
 	}
 	protected function serveImg($file){
         if (!file_exists($file)) {
-            $file = BOLT_PROJECT_ROOT_DIR . '/app/' . substr($file, 3);
+        	$relfile = substr($file, 3);
+			if (BOLT_COMPOSER_INSTALLED) {
+				$file = BOLT_WEB_DIR . '/bolt_public/' . $relfile;
+			} else {
+				$file = BOLT_PROJECT_ROOT_DIR . '/app/' . $relfile;
+			}
         }
 		$s = getimagesize($file);
 		if(! ($s && $s['mime'])){
