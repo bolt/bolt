641023ffd585c4fbf540e7ea03a64dc435872782
Gawain Lynch <gawain.lynch@gmail.com>
2014-03-27 11:51:02 +0000

    Insert JS very late to better cope if jQuery has been pushed down

--- app/extensions/RateIt/extension.php
+++ app/extensions/RateIt/extension.php
@@ -24,12 +24,12 @@ class Extension extends \Bolt\BaseExtension
             'description' => "An extentions to add RateIt to your site when using <code>{{ rateit('slider_name') }}</code> in your templates.",
             'author' => "Gawain Lynch",
             'link' => "http://bolt.cm",
-            'version' => "1.2",
+            'version' => "1.3",
             'required_bolt_version' => "1.5",
             'highest_bolt_version' => "2.0",
             'type' => "Twig function",
             'first_releasedate' => "2014-03-04",
-            'latest_releasedate' => "2014-03-24",
+            'latest_releasedate' => "2014-03-27",
         );
 
         return $data;
@@ -85,10 +85,10 @@ class Extension extends \Bolt\BaseExtension
         $this->path = $this->app['paths']['app'] . 'extensions/' . $this->namespace;
 
         $html = '
-            <script type="text/javascript" defer="defer" src="' . $this->path . '/js/jquery.rateit.min.js"></script>
-            <script type="text/javascript" defer="defer" src="' . $this->path . '/js/bolt.rateit.js"></script>
+            <script type="text/javascript" src="' . $this->path . '/js/jquery.rateit.min.js"></script>
+            <script type="text/javascript" src="' . $this->path . '/js/bolt.rateit.js"></script>
                 ';
-        $this->insertSnippet(SnippetLocation::END_OF_BODY, $html);
+        $this->insertSnippet(SnippetLocation::END_OF_HTML, $html);
 
         $this->addTwigFunction('rateit', 'twigRateIt');
         $this->app->after(array($this, "afterCallback"), 1);
@@ -218,7 +218,7 @@ class Extension extends \Bolt\BaseExtension
             </script>";
         }
 
-        $this->insertSnippet(SnippetLocation::END_OF_BODY, $js );
+        $this->insertSnippet(SnippetLocation::END_OF_HTML, $js );
     }
 
     /**
