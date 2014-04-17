be83cd6be0bce3835fef881a226e3a1b2c593e74
bobdenotter <bobdenotter@gmail.com>
2014-04-17 19:44:16 +0200

    Bumping version numver to "1.6.0 beta 2"

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -17,7 +17,7 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '1.6.0';
-        $values['bolt_name'] = 'beta';
+        $values['bolt_name'] = 'beta 2';
 
         parent::__construct($values);
 
