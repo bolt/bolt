93ba5d8cd6a4fba3c81b48142bd0c627d4bf845f
bobdenotter <bob@twokings.nl>
2014-05-06 11:07:03 +0200

    Bumping version to "1.6.0 RC 3"

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -17,7 +17,7 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '1.6.0';
-        $values['bolt_name'] = 'RC2';
+        $values['bolt_name'] = 'RC 3';
 
         parent::__construct($values);
 
