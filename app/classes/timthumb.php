25fb552697694e7488b140ac43bdde5738abd650
@doerteDev (Stefan Staudenmeyer) <stefan.staudenmeyer@csiweb.de>
2014-05-15 11:44:57 +0200

    My bad. Sorry.

--- app/classes/timthumb.php
+++ app/classes/timthumb.php
@@ -960,6 +960,10 @@ class timthumb {
 
         // Make sure the paths exists. Try to create it, if possible.
         $pathparts = explode("/", $_GET['requestname']);
+        $path      = BOLT_PROJECT_ROOT_DIR
+                        . DIRECTORY_SEPARATOR
+                        . implode(DIRECTORY_SEPARATOR,
+                                  array_slice($pathparts, 0, (count($pathparts)-1))));
         $fileSystem->mkdir($path);
 
         // Copy the file, and chmod
