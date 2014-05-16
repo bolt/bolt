5c9bbfd295c67223044d22706e375c2be7200601
Ross Riley <riley.ross@gmail.com>
2014-05-16 11:55:13 +0100

    allow support for passing in resources to constructor

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -23,6 +23,10 @@ class Application extends Silex\Application
 
         // Initialize the config. Note that we do this here, on 'construct'.
         // All other initialisation is triggered from bootstrap.php
+        if(isset($this['resources'])) {
+            $this['resources']->setApp($this);
+            $this['resources']->initialize();
+        }
         $this->initConfig();
 
         $this['debug'] = $this['config']->get('general/debug', false);
