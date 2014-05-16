da21368015f422f3fb6bc4bcfc6f43930bcdfcad
Ross Riley <riley.ross@gmail.com>
2014-05-16 12:50:46 +0100

    use a default path if a configuration isnt set

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -24,7 +24,7 @@ class Application extends Silex\Application
         // Initialize the config. Note that we do this here, on 'construct'.
         // All other initialisation is triggered from bootstrap.php
         if(!isset($this['resources'])) {
-            $this['resources'] = new Configuration\ResourceManager();
+            $this['resources'] = new Configuration\ResourceManager(BOLT_PROJECT_ROOT_DIR);
         }
         
         $this['resources']->setApp($this);
