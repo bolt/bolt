18712a5809a8f79eb0515dd82660b2ee32c578c7
Bob den Otter <bobdenotter@gmail.com>
2014-06-07 16:10:25 +0200

    Simplify Gruntfile.js

--- app/view/Gruntfile.js
+++ app/view/Gruntfile.js
@@ -44,9 +44,7 @@ module.exports = function(grunt) {
 
     });
 
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+    require('load-grunt-tasks')(grunt);
 
     grunt.registerTask('default', ['sass', 'watch']);
 
