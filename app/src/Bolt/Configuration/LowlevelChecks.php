819417ace5743fcfc4c3a11067dbfea56c6a6f1c
Ross Riley <riley.ross@gmail.com>
2014-06-18 14:06:49 +0100

    Add css into the class template

--- app/src/Bolt/Configuration/LowlevelChecks.php
+++ app/src/Bolt/Configuration/LowlevelChecks.php
@@ -200,7 +200,7 @@ class LowlevelChecks
     <meta charset="utf-8" />
     <title>Bolt - Error</title>
     <style>
-        body{font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;color:#333;}
+        body{font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;color:#333;font-size:14px;line-height:20px;margin:0px;}
         h1 {font-size: 38.5px;line-height: 40px;margin: 10px 0px;}
         p{margin: 0px 0px 10px;}
         strong{font-weight:bold;}
@@ -208,6 +208,7 @@ class LowlevelChecks
         code {padding: 2px 4px;color: #D14;background-color: #F7F7F9;border: 1px solid #E1E1E8;white-space: nowrap;}
         a {color: #08C;text-decoration: none;}
         ul, ol {padding: 0px;margin: 0px 0px 10px 25px;}
+        hr{margin:20px 0;border:0;border-top:1px solid #eeeeee;border-bottom:1px solid #ffffff;}
     </style>
 </head>
 <body style="padding: 20px;">
