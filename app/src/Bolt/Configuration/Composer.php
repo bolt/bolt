3d93c79095d2e75101965066eb650e56e133611b
Ross Riley <riley.ross@gmail.com>
2014-06-18 15:11:55 +0100

    fix to wroing class name

--- app/src/Bolt/Configuration/Composer.php
+++ app/src/Bolt/Configuration/Composer.php
@@ -4,17 +4,7 @@ namespace Bolt\Configuration;
 
 use Bolt\Application;
 
-
-/**
- * A Class to handle lookups and checks of all paths and resources within a Bolt App.
- *
- * Intended to simplify the ability to override resource location 
- *
- *
- * @author Ross Riley, riley.ross@gmail.com
- *
- */
-class Composer extends Default
+class Composer extends Standard
 {
 
     /**
