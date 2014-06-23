305fa4f1e44b23dc1ea4988dbaaf6a1793500816
Bob den Otter <bobdenotter@gmail.com>
2014-06-23 21:18:59 +0200

    Tweaking the check for PHP 5.3.3. See #1300.

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+// Check if we're at least on PHP 5.3.3. We do this check here, because as soon as
+// we require any other files, we'll get a fatal error, because the parser chokes
+// on the backslashes in Namespaces.
+if (version_compare(PHP_VERSION, '5.3.3', '<')) {
+    require_once dirname(__FILE__) . '/app/legacy.php';
+    exit;
+}
+
 // PHP -S (built-in webserver) doesn't handle static assets without a `return false`
 // For more information, see: http://silex.sensiolabs.org/doc/web_servers.html#php-5-4
 if ('cli-server' === php_sapi_name()) {
