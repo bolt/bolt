821a5c9117349b88a075b51768bacf20ef112fa2
Ross Riley <riley.ross@gmail.com>
2014-08-02 10:37:58 +0100

    remove need for yaml parser

--- app/src/Bolt/Controllers/Upload.php
+++ app/src/Bolt/Controllers/Upload.php
@@ -10,7 +10,6 @@ use Silex\ServiceProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Yaml\Parser;
 
 use Sirius\Upload\Handler as UploadHandler;
 use Sirius\Upload\Container\Local;
