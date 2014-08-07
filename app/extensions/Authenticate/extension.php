ada10f626e8d8d9d97fd1a7ee4c50c1482fad616
Gawain Lynch <gawain.lynch@gmail.com>
2014-08-07 12:07:29 +0100

    Undo one CS-Fixer change

--- app/extensions/Authenticate/extension.php
+++ app/extensions/Authenticate/extension.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class extension extends \Bolt\BaseExtension
+class Extension extends \Bolt\BaseExtension
 {
     public function info()
     {
