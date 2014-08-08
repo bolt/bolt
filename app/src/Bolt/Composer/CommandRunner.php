952cb3d50a68daa974ca1bb4097d227f75f86d76
Ross Riley <riley.ross@gmail.com>
2014-08-08 19:23:31 +0100

    add support for a composer install

--- app/src/Bolt/Composer/CommandRunner.php
+++ app/src/Bolt/Composer/CommandRunner.php
@@ -91,7 +91,7 @@ class CommandRunner
     public function installAll()
     {
         $response = $this->execute("install -d extensions/");
-        return implode($response, "<br>" );
+        return implode(array_slice($response, 2), "<br>" );
     }
     
     public function uninstall($package)
