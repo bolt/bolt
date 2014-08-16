b6b8e3ffb79337f1d713ab334a889921f7a0b5fa
Adrian Guenter <adrianguenter@gmail.com>
2014-08-16 14:47:47 -0400

    Wrap the bootstrapper in a closure which returns $app back out

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,29 +1,36 @@
 <?php
 
-mb_internal_encoding('UTF-8');
-mb_http_output('UTF-8');
+// Do bootstrapping within a new local scope to avoid polluting the global
+$app = call_user_func(
+  function () {
+      mb_internal_encoding('UTF-8');
+      mb_http_output('UTF-8');
 
-// First, do some low level checks, like whether autoload is present, the cache
-// folder is writable, etc.
-require_once __DIR__ . '/classes/lib.php';
-require_once __DIR__ . '/src/Bolt/Configuration/LowlevelChecks.php';
+      // First, do some low level checks, like whether autoload is present, the cache
+      // folder is writable, etc.
+      require_once __DIR__ . '/classes/lib.php';
+      require_once __DIR__ . '/src/Bolt/Configuration/LowlevelChecks.php';
 
-$checker = new Bolt\Configuration\LowlevelChecks;
-require_once $checker->autoloadCheck(getcwd());
-
-
-if (strpos(__DIR__, "/vendor/") !== false) {
-    $config = new Bolt\Configuration\Composer(getcwd());
-} else {
-    $config = new Bolt\Configuration\Standard(__DIR__."/../");
-}
-$config->verify();
-$config->compat();
+      $checker = new Bolt\Configuration\LowlevelChecks;
+      require_once $checker->autoloadCheck(getcwd());
 
 
-// Create the 'Bolt application'
-$app = new Bolt\Application(array('resources' => $config));
+      if (strpos(__DIR__, "/vendor/") !== false) {
+          $config = new Bolt\Configuration\Composer(getcwd());
+      } else {
+          $config = new Bolt\Configuration\Standard(__DIR__ . "/../");
+      }
+      $config->verify();
+      $config->compat();
 
 
-// Initialize the 'Bolt application': Set up all routes, providers, database, templating, etc..
-$app->initialize();
+      // Create the 'Bolt application'
+      $app = new Bolt\Application(array('resources' => $config));
+
+
+      // Initialize the 'Bolt application': Set up all routes, providers, database, templating, etc..
+      $app->initialize();
+
+      return $app;
+  }
+);
