d2f363b0d466076396c32c86430ef87129a108c2
Bob den Otter <bob@twokings.nl>
2014-08-17 10:43:00 +0200

    Channging "bolt_name" to something that better reflects the current state of the app.

--- app/src/Bolt/Application.php
+++ app/src/Bolt/Application.php
@@ -17,7 +17,7 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '2.0';
-        $values['bolt_name'] = 'very pre-alpha';
+        $values['bolt_name'] = 'almost beta';
 
         parent::__construct($values);
 
