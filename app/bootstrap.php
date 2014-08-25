e6c892eb175db0f22e6291ed31b708e083fb33f5
Rixbeck <rix@neologik.hu>
2014-08-25 17:10:49 +0200

    PSRize

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,75 +3,75 @@ namespace Bolt;
 
 use Bolt\Configuration\LowlevelException;
 // Do bootstrapping within a new local scope to avoid polluting the global
-return call_user_func(
-  function () {
+return call_user_func(function ()
+{
 
-      $dirSep = DIRECTORY_SEPARATOR;
+    $dirSep = DIRECTORY_SEPARATOR;
 
-      // First ensure load.php was called right before bootstrap.php
-      $includes     = get_included_files();
-      $loaderPath   = __DIR__ . $dirSep . 'load.php';
-      $includeCount = count($includes);
-      // Should be at least 3 includes at this point:
-      //   <load-invoker>.php (usually entry point), load.php, bootstrap.php
-      // Second to last entry must be load.php
-      $isLoadChainOk = $includeCount >= 3 && $includes[$includeCount - 2] == $loaderPath;
+    // First ensure load.php was called right before bootstrap.php
+    $includes = get_included_files();
+    $loaderPath = __DIR__ . $dirSep . 'load.php';
+    $includeCount = count($includes);
+    // Should be at least 3 includes at this point:
+    // <load-invoker>.php (usually entry point), load.php, bootstrap.php
+    // Second to last entry must be load.php
+    $isLoadChainOk = $includeCount >= 3 && $includes[$includeCount - 2] == $loaderPath;
 
-      require_once __DIR__ . '/src/Bolt/Configuration/LowlevelException.php';
+    require_once __DIR__ . '/src/Bolt/Configuration/LowlevelException.php';
 
-      if (!$isLoadChainOk) {
-          throw new LowlevelException('Include load.php, not bootstrap.php');
-      }
+    if (! $isLoadChainOk) {
+        throw new LowlevelException('Include load.php, not bootstrap.php');
+    }
 
+    // Bootstrap:
 
-      // Bootstrap:
+    // TODO: Phase out lib.php
+    require_once __DIR__ . '/lib.php';
 
-      // TODO: Phase out lib.php
-      require_once __DIR__ . '/lib.php';
+    // Use UTF-8 for all multi-byte functions
+    mb_internal_encoding('UTF-8');
+    mb_http_output('UTF-8');
 
-      // Use UTF-8 for all multi-byte functions
-      mb_internal_encoding('UTF-8');
-      mb_http_output('UTF-8');
+    // Resolve Bolt-root
+    $boltRootPath = realpath(__DIR__ . $dirSep . '..');
 
-      // Resolve Bolt-root
-      $boltRootPath = realpath(__DIR__ . $dirSep . '..');
+    // Look for the autoloader in known positions relative to the Bolt-root,
+    // and autodetect an appropriate configuration class based on this
+    // information. (autoload.php path maps to a configuration class)
+    $autodetectionMappings = array(
+        $boltRootPath . $dirSep . 'vendor' . $dirSep . 'autoload.php' => 'Standard',
+        $boltRootPath . $dirSep . '..' . $dirSep . '..' . $dirSep . 'autoload.php' => 'Composer'
+    );
 
-      // Look for the autoloader in known positions relative to the Bolt-root,
-      //  and autodetect an appropriate configuration class based on this
-      //  information. (autoload.php path maps to a configuration class)
-      $autodetectionMappings = array(
-        $boltRootPath . $dirSep . 'vendor' . $dirSep . 'autoload.php'              => 'Standard',
-        $boltRootPath . $dirSep . '..' . $dirSep . '..' . $dirSep . 'autoload.php' => 'Composer',
-      );
+    foreach ($autodetectionMappings as $autoloadPath => $configType) {
+        if (file_exists($autoloadPath)) {
+            require_once $autoloadPath;
+            $configClass = '\\Bolt\\Configuration\\' . $configType;
+            $config = new $configClass($boltRootPath);
+            break;
+        }
+    }
 
-      foreach ($autodetectionMappings as $autoloadPath => $configType) {
-          if (file_exists($autoloadPath)) {
-              require_once $autoloadPath;
-              $configClass = '\\Bolt\\Configuration\\' . $configType;
-              $config      = new $configClass($boltRootPath);
-              break;
-          }
-      }
+    // None of the mappings matched, error
+    if (! isset($config)) {
+        throw new LowlevelException("Configuration autodetection failed because The file " .
+            "<code>vendor/autoload.php</code> doesn't exist. Make sure " .
+            "you've installed the required components with Composer.");
+    }
 
-      // None of the mappings matched, error
-      if (!isset($config)) {
-            throw new LowlevelException(
-                "Configuration autodetection failed because The file ".
-                "<code>vendor/autoload.php</code> doesn't exist. Make sure " .
-                "you've installed the required components with Composer."
-            );
-      }
+    /**
+     * @var $config Configuration\ResourceManager
+     */
+    $config->verify();
+    $config->compat();
 
-      /** @var $config Configuration\ResourceManager */
-      $config->verify();
-      $config->compat();
+    // Create the 'Bolt application'
+    $app = new Application(array(
+        'resources' => $config
+    ));
 
-      // Create the 'Bolt application'
-      $app = new Application(array('resources' => $config));
+    // Initialize the 'Bolt application': Set up all routes, providers, database, templating, etc..
+    $app->initialize();
 
-      // Initialize the 'Bolt application': Set up all routes, providers, database, templating, etc..
-      $app->initialize();
-
-      return $app;
-  }
-);
+    return $app;
+});
