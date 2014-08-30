ae4129520e90927f42717279128278042142f58a
Ross Riley <riley.ross@gmail.com>
2014-08-30 23:57:12 +0100

    redo list command

--- app/src/Bolt/Nut/Extensions.php
+++ app/src/Bolt/Nut/Extensions.php
@@ -11,46 +11,13 @@ class Extensions extends BaseCommand
     {
         $this
             ->setName('extensions')
-            ->setDescription('Lists all installed extensions, and whether they\'re enabled or disabled.');
-            //->addArgument('name', InputArgument::OPTIONAL, 'Who do you want to greet?')
-            //->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters');
+            ->setDescription('Lists all installed extensions');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $extensions = $this->app['extensions']->getInfo();
-
-        // print_r($extensions);
-
-        $enabled = "";
-        $disabled = "";
-        $lines = array();
-
-        foreach ($extensions as $key => $extension) {
-            $line = sprintf("%-20s v. %-7s", $key, $extension['version']);
-            if ($extension['enabled']) {
-                $line .= "<info>[+]</info>\n";
-                $enabled .= $line;
-            } else {
-                $line .= "[-]\n";
-                $disabled .= $line;
-            }
-        }
-
-        $lines[] = "\n<options=bold>Enabled Extensions</options=bold>";
-        if (!empty($enabled)) {
-            $lines[] = $enabled;
-        } else {
-            $lines[] = "(none)\n";
-        }
-
-        $lines[] = "<options=bold>Disabled Extensions</options=bold>";
-        if (!empty($disabled)) {
-            $lines[] = $disabled;
-        } else {
-            $lines[] = "(none)\n";
-        }
-
+        $result = $this->app['extend.runner']->installed();
+        print_r($result); exit;
         $output->writeln(implode("\n", $lines));
     }
 }
