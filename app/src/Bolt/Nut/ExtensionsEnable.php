e38c40c170f9cfb744faad9ca96f4b784bda2b3c
Ross Riley <riley.ross@gmail.com>
2014-08-31 08:27:14 +0100

    implement extension add command

--- app/src/Bolt/Nut/ExtensionsEnable.php
+++ app/src/Bolt/Nut/ExtensionsEnable.php
@@ -13,51 +13,31 @@ class ExtensionsEnable extends BaseCommand
         $this
             ->setName('extensions:enable')
             ->setDescription('Enables an extension.')
-            ->addArgument('name', InputArgument::REQUIRED, 'Name of the extension to enable');
-            //->addOption('yell', null, InputOption::VALUE_NONE, 'If set, the task will yell in uppercase letters');
+            ->addArgument('name', InputArgument::REQUIRED, 'Name of the extension to enable')
+            ->addArgument('version', InputArgument::REQUIRED, 'Version of the extension to enable');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $name = $input->getArgument('name');
-
-        $extensions = $this->app['extensions']->getInfo();
-
-        $enabled = array();
-        $lines = array();
-        $update = false;
-
-        foreach ($extensions as $key => $extension) {
-            if ($extension['enabled']) {
-                $enabled[] = $key;
-            }
-
-            if (strtolower($key) == strtolower($name)) {
-                if (!in_array($key, $enabled)) {
-                    $lines[] = "<info>Enabling <options=bold>$key</options=bold>.</info>";
-                    $update = true;
-                    $enabled[] = $key;
-                } else {
-                    $lines[] = "<info><options=bold>$key</options=bold> is already enabled.</info>";
-                }
-            }
-
+        $version = $input->getArgument('version');
+        if (!isset($name) || !isset($version)) {
+            $output->writeln(
+                "<error>".
+                $this->app['translator']->trans('You must specify both a name and a version to install!').
+                "</error>"
+            );
+            return;
+        }
+        
+        $result = $this->app['extend.runner']->install($name, $version);
+        if (is_array($result)) {
+            $output->writeln($result);
+            $output->write(" <info>[Done]</info>");
+        } else {
+            $output->writeln("<error>".$result."<error>");
         }
 
-        if ($update) {
-            $key = "enabled_extensions";
-            $file = BOLT_CONFIG_DIR . "/config.yml";
-            $yaml = new \Bolt\YamlUpdater($file);
-            $result = $yaml->change($key, $enabled);
-
-            if ($result) {
-                $lines[] = sprintf("New value for <info>%s</info> was succesful. File updated.", $key);
-            } else {
-                $lines[] = sprintf("<error>%s not found, or file not writable.</error>", $key);
-            }
-
-        }
-
-        $output->writeln(implode("\n", $lines));
+        
     }
 }
