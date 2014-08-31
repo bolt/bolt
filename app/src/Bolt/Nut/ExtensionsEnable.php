0f87c6b4574608a50e0b142c03363c896bf6d439
Ross Riley <riley.ross@gmail.com>
2014-08-31 09:55:44 +0100

    add enable alias

--- app/src/Bolt/Nut/ExtensionsEnable.php
+++ app/src/Bolt/Nut/ExtensionsEnable.php
@@ -12,6 +12,7 @@ class ExtensionsEnable extends BaseCommand
     {
         $this
             ->setName('extensions:enable')
+            ->setAliases(array('extensions:install'))
             ->setDescription('Enables an extension.')
             ->addArgument('name', InputArgument::REQUIRED, 'Name of the extension to enable')
             ->addArgument('version', InputArgument::REQUIRED, 'Version of the extension to enable');
