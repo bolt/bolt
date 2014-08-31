875b8b9214250cf5abcb1b6d85aba31f0c0485e2
Ross Riley <riley.ross@gmail.com>
2014-08-31 09:01:23 +0100

    add an isset check to only allow config/readme access on installed extensions

--- app/src/Bolt/Composer/CommandRunner.php
+++ app/src/Bolt/Composer/CommandRunner.php
@@ -237,16 +237,19 @@ class CommandRunner
         }
 
         // For Bolt, we also need to know if the extension has a 'README' and a 'config.yml' file.
-        $paths = $this->app['resources']->getPaths();
-        if (is_readable($paths['extensionspath'] . '/vendor/' . $pack['name'] . '/README.md' )) {
-            $pack['readme'] = $pack['names'] . '/README.md';
-        } elseif (is_readable($paths['extensionspath'] . '/vendor/' . $pack['name'] . '/readme.md' )) {
-            $pack['readme'] = $pack['names'] . '/readme.md';
-        }
+        // Note we only do this for successfully loaded extensions.
+        if (isset($this->app['extensions']->composer[$name])) {
+            $paths = $this->app['resources']->getPaths();
+            if (is_readable($paths['extensionspath'] . '/vendor/' . $pack['name'] . '/README.md' )) {
+                $pack['readme'] = $pack['names'] . '/README.md';
+            } elseif (is_readable($paths['extensionspath'] . '/vendor/' . $pack['name'] . '/readme.md' )) {
+                $pack['readme'] = $pack['names'] . '/readme.md';
+            }
 
-        $configfile = $paths['extensionsconfig'] . '/' . $this->app['extensions']->composer[$name]['name'] . '.yml';
-        if (is_readable($configfile)) {
-            $pack['config'] = $configfile;
+            $configfile = $paths['extensionsconfig'] . '/' . $this->app['extensions']->composer[$name]['name'] . '.yml';
+            if (is_readable($configfile)) {
+                $pack['config'] = $configfile;
+            }
         }
         return $pack;
     }
