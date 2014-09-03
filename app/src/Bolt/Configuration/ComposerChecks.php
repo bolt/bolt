cb2d4c556d4b59249925330dd1e17798a8352c6d
Ross Riley <riley.ross@gmail.com>
2014-09-03 11:48:19 +0100

    add config check

---
+++ app/src/Bolt/Configuration/ComposerChecks.php
@@ -0,0 +1,47 @@
+<?php
+namespace Bolt\Configuration;
+
+/**
+ * Inherits from default and adds some specific checks for composer installs.
+ *
+ * @author Ross Riley <riley.ross@gmail.com> 
+ **/
+
+class ComposerChecks extends LowlevelChecks
+{
+
+
+    /**
+     * The constructor requires a resource manager object to perform checks against.
+     * This should ideally be typehinted to Bolt\Configuration\ResourceManager
+     *
+     * @return void
+     **/
+    public function __construct($config = null)
+    {
+        parent::__construct($config);
+        $this->addCheck('config');
+    }
+    
+    
+    public function checkConfig()
+    {
+        $message =  "Bolt needs a local config directory to store site-specific configuration. ";             
+                    
+        if (!is_dir($this->config->getPath('config'))) {
+            throw new LowlevelException(
+                $message . "The default folder <code>" . $this->config->getPath('config') . 
+                "</code> doesn't exist. Make sure it's " .
+                "present and writable to the user that the webserver is using.");
+        } elseif (!is_writable($this->config->getPath('cache'))) {
+            throw new LowlevelException(
+                $message . "The default folder <code>" . $this->config->getPath('cache') . 
+                "</code> isn't writable. Make sure it's writable to the user that the webserver is using."
+            );
+        }
+    }
+    
+    
+
+
+}
