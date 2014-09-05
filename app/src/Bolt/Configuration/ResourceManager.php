42c02a432ee061b92f57c0c2d4ffbc0e99f64e35
Ross Riley <riley.ross@gmail.com>
2014-09-05 12:18:05 +0100

    calculate and map a relative path to web root

--- app/src/Bolt/Configuration/ResourceManager.php
+++ app/src/Bolt/Configuration/ResourceManager.php
@@ -309,4 +309,48 @@ class ResourceManager
 
         return static::$_app;
     }
+    
+    /**
+    *
+    * Find the relative file system path between two file system paths
+    *
+    * @param string $frompath Path to start from
+    * @param string $topath Path we want to end up in
+    *
+    * @return string Path leading from $frompath to $topath
+    */
+    public function findRelativePath( $frompath, $topath ) 
+    {
+        $from = explode( DIRECTORY_SEPARATOR, $frompath ); // Folders/File
+        $to = explode( DIRECTORY_SEPARATOR, $topath ); // Folders/File
+        $relpath = '';
+     
+        $i = 0;
+    
+        // Find how far the path is the same
+        while ( isset($from[$i]) && isset($to[$i]) ) {
+            if ( $from[$i] != $to[$i] ) break;
+                $i++;
+            }
+            $j = count($from) - 1;
+            
+            // Add '..' until the path is the same
+            while ( $i <= $j ) {
+                if ( !empty($from[$j]) ) {
+                    $relpath .= '..'.DIRECTORY_SEPARATOR;
+                }
+                $j--;
+            }
+        
+            // Go to folder from where it starts differing
+            while ( isset($to[$i]) ) {
+            if ( !empty($to[$i]) ) {
+                $relpath .= $to[$i].DIRECTORY_SEPARATOR;
+            }
+            $i++;
+        }
+        
+        // Strip last separator
+        return substr($relpath, 0, -1);
+    }
 }
