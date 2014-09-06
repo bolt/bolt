0fa5abd47f5306fc583ebbfc02fab35c364aacc4
Gawain Lynch <gawain.lynch@gmail.com>
2014-09-06 19:06:50 +0100

    Set and use a flag in saveContent() to indicate if a record is being created or updated

--- app/src/Bolt/Storage.php
+++ app/src/Bolt/Storage.php
@@ -601,6 +601,13 @@ class Storage
             return false;
         }
 
+        // Test to see if this is a new record, or an update
+        if (empty($fieldvalues['id'])) {
+            $create = true;
+        } else {
+            $create = false;
+        }
+
         if (! $this->inDispatcher && $this->app['dispatcher']->hasListeners(StorageEvents::PRE_SAVE)) {
             // Block dispatcher loops
             $this->inDispatcher = true;
@@ -702,11 +709,11 @@ class Storage
         }
 
         // We need to verify if the slug is unique. If not, we update it.
-        $get_id = isset($fieldvalues['id']) ? $fieldvalues['id'] : null;
+        $get_id = $create ? null : $fieldvalues['id'];
         $fieldvalues['slug'] = $this->getUri($fieldvalues['slug'], $get_id, $contenttype['slug'], false, false);
 
         // Decide whether to insert a new record, or update an existing one.
-        if (empty($fieldvalues['id'])) {
+        if ($create) {
             $id = $this->insertContent($fieldvalues, $contenttype, '', $comment);
             $fieldvalues['id'] = $id;
             $content->setValue('id', $id);
