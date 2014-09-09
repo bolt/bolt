dfe8a8ba826ce9ee7504e6f3d70df899c4810202
Bob den Otter <bob@twokings.nl>
2014-09-09 20:37:59 +0200

    Only change the height, if it's larger than specified in CSS. Goes with #1588

--- app/view/js/bolt.js
+++ app/view/js/bolt.js
@@ -805,8 +805,12 @@ var Sidebar = Backbone.Model.extend({
 
         }
 
+
         // Make sure the sidebar is as long as the document height. Also: Typecasting! love it or hate it! 
-        $('#navpage-secondary').height( $('#navpage-content').height() + 22 + "px");
+        var documentheight = $('#navpage-content').height() + 22;
+        if (documentheight > $('#navpage-secondary').height()) {
+            $('#navpage-secondary').height( documentheight + "px");
+        }
 
     },
 
