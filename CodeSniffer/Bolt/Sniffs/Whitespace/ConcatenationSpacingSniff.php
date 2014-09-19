12c8da8ae551be8e17fe14f4446375b92b38576e
rarila <magnet@gmx.net>
2014-09-19 14:42:03 +0200

    Added sniff requiring spaces around string concatenating operator

---
+++ CodeSniffer/Bolt/Sniffs/Whitespace/ConcatenationSpacingSniff.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Makes sure there are spaces between the concatenation operator (.) and the strings being concatenated.
+ */
+
+class Bolt_Sniffs_Whitespace_ConcatenationSpacingSniff implements PHP_CodeSniffer_Sniff
+{
+    public function register()
+    {
+        return array(T_STRING_CONCAT);
+    }
+
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+
+        if ($tokens[($stackPtr - 1)]['code'] !== T_WHITESPACE || $tokens[($stackPtr + 1)]['code'] !== T_WHITESPACE) {
+            $phpcsFile->addError('Concatenation operator must be surrounded by whitespace', $stackPtr);
+        }
+    }
+}
