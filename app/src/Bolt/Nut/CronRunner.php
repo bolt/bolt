47a03074bb5a5e74bc186eeb06f12708b14e2b35
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-06 09:44:23 +0100

    Backport cron update from Bolt 2

--- app/src/Bolt/Nut/CronRunner.php
+++ app/src/Bolt/Nut/CronRunner.php
@@ -2,7 +2,6 @@
 
 namespace Bolt\Nut;
 
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -12,33 +11,30 @@ class CronRunner extends BaseCommand
 {
     protected function configure()
     {
+        $interims = array('  - cron.Hourly', '  - cron.Daily', '  - cron.Weekly', '  - cron.Monthly', '  - cron.Yearly');
         $this
             ->setName('cron')
             ->setDescription('Cron virtual daemon')
-            ->addOption('single', null, InputOption::VALUE_NONE, 'If set, tell Bolt cron to run a single task')
-            ->addArgument('name', InputArgument::OPTIONAL, 'Name of task to run'
-            );
+            ->addOption('run', null, InputOption::VALUE_REQUIRED, "Run a particular interim's jobs:\n" . implode("\n", $interims));
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if ($input->getOption('single')) {
-            $param['single'] = true;
+        if ($input->getOption('run')) {
+            $param = array(
+                'run' => true,
+                'event' => $input->getOption('run')
+            );
         } else {
-            $param['single'] = false;
+            $param = array(
+                'run' => false,
+                'event' => ''
+            );
         }
 
-        $name = $input->getArgument('name');
-        if ($name) {
-            $param['name'] = $name;
-        }
-
-        //$result = $this->app['cron']->execute($param);
-        $result = new Cron($this->app, $output);
-
+        $result = new Cron($this->app, $output, $param);
         if ($result) {
             $output->writeln("<info>Cron run!</info>");
         }
     }
 }
-
