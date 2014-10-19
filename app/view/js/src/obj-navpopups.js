d9d40ae7b20f047008e06aa47538bb9e47bd0d2d
Jadwigo <lodewijk@jadwigo.nl>
2014-10-19 14:42:01 +0200

    mobile menu event handlers added

--- app/view/js/src/obj-navpopups.js
+++ app/view/js/src/obj-navpopups.js
@@ -27,27 +27,44 @@ var Navpopups = Backbone.Model.extend({
                 $(this).attr('data-html', true).attr('data-content', menu);
             }
         );
-        // Add hover focus and leave blur event handlers for popovers - on desktop
-        $('#navpage-secondary')
-            .on('mouseover focus', 'a.menu-pop', function () {
-                    var item = this;
-                    window.clearTimeout(menuTimeout);
-                    menuTimeout = window.setTimeout(function () {
-                        $('#navpage-secondary a.menu-pop').not(item).popover('hide');
-                        $(item).popover('show');
-                    }, 400);
-                }
-            )
-            .on('mouseenter focus', '.popover', function () {
-                    window.clearTimeout(menuTimeout);
-                }
-            )
-            .on('mouseleave blur', 'a.menu-pop, .popover', function () {
-                    window.clearTimeout(menuTimeout);
-                    menuTimeout = window.setTimeout(function () {
-                        $('#navpage-secondary a.menu-pop').popover('hide');
-                    }, 800);
+        if ($('.navbar-toggle').is(':visible')) {
+            // we're on mobile view - so do not trigger the popups,
+            // console.log('mobile view');
+            // only trigger the mobile open action
+            $('#navpage-secondary a.menu-pop').on('click', function(e) {
+                    e.preventDefault();
+                    var name = $(this).attr('data-name');
+                    if ($('#navpage-secondary .submenu-' + name).hasClass('show')) {
+                        $('#navpage-secondary .submenu-' + name).removeClass('show');
+                    } else {
+                        $('#navpage-secondary .submenu').removeClass('show');
+                        $('#navpage-secondary .submenu-' + name).addClass('show');
+                    }
                 }
             );
+        } else {
+            // Add hover focus and leave blur event handlers for popovers - on desktop
+            $('#navpage-secondary')
+                .on('mouseover focus', 'a.menu-pop', function () {
+                        var item = this;
+                        window.clearTimeout(menuTimeout);
+                        menuTimeout = window.setTimeout(function () {
+                            $('#navpage-secondary a.menu-pop').not(item).popover('hide');
+                            $(item).popover('show');
+                        }, 400);
+                    }
+                )
+                .on('mouseenter focus', '.popover', function () {
+                        window.clearTimeout(menuTimeout);
+                    }
+                )
+                .on('mouseleave blur', 'a.menu-pop, .popover', function () {
+                        window.clearTimeout(menuTimeout);
+                        menuTimeout = window.setTimeout(function () {
+                            $('#navpage-secondary a.menu-pop').popover('hide');
+                        }, 800);
+                    }
+                );
+        }
     }
 });
