57bcde4428136978814c8ccbc2f0b52fc71589da
Rixbeck <rix@neologik.hu>
2014-10-22 23:06:40 +0200

    Describing index name

--- tests/Bolt/Tests/ApplicationTest.php
+++ tests/Bolt/Tests/ApplicationTest.php
@@ -8,7 +8,7 @@ class ApplicationTest extends \PHPUnit_Framework_TestCase
 
     public function testConstructorSetBoltVersion()
     {
-        $app = new Application(array('classloader' => TEST_ROOT));
+        $app = new Application(array('rootpath' => TEST_ROOT));
 
         $this->arrayHasKey($app, 'bolt_version');
         $this->arrayHasKey($app, 'bolt_name');
