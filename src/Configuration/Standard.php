be5c1a3383eb811db3ed8ac4ae117a34d38d0d47
Rixbeck <rix@neologik.hu>
2014-10-22 20:21:40 +0200

    For backward compatibility must ensure String or DI based construction

--- src/Configuration/Standard.php
+++ src/Configuration/Standard.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Bolt\Configuration;
 
 use Bolt\Application;
@@ -7,9 +6,19 @@ use Bolt\Application;
 /**
  * Left as a blank extension of ResourceManager for now, this semantically represents a default configuration
  * for a Bolt application.
- *
- **/
-
+ */
 class Standard extends ResourceManager
 {
+
+    public function __construct($loader)
+    {
+        if ($loader instanceof \Pimple) {
+            $container = $loader;
+        } else {
+            $container = new \Pimple();
+            $container['rootpath'] = $loader;
+        }
+
+        parent::__construct($container);
+    }
 }
