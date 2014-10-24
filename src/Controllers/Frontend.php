e71a22f8bd8bd78e99a76bc0c91468eb9dbbfa97
Gawain Lynch <gawain.lynch@gmail.com>
2014-10-24 14:00:56 +0100

    makeSlug() to Lib:makeSlug() in Bolt\Controllers\Frontend

--- src/Controllers/Frontend.php
+++ src/Controllers/Frontend.php
@@ -124,7 +124,7 @@ class Frontend
     {
         $contenttype = $app['storage']->getContentType($contenttypeslug);
 
-        $slug = makeSlug($slug, -1);
+        $slug = Lib::makeSlug($slug, -1);
 
         // First, try to get it by slug.
         $content = $app['storage']->getContent($contenttype['slug'], array('slug' => $slug, 'returnsingle' => true));
