ed5b6dd36e985626ce02981acdaf6b0cbf81595e
Bob den Otter <bob@twokings.nl>
2014-10-27 21:56:14 +0100

    "Object parameters should be type hinted"

--- src/Storage.php
+++ src/Storage.php
@@ -582,7 +582,7 @@ class Storage
         }
     }
 
-    public function saveContent($content, $comment = null)
+    public function saveContent(\Bolt\Content $content, $comment = null)
     {
         $contenttype = $content->contenttype;
         $fieldvalues = $content->values;
@@ -2159,11 +2159,11 @@ class Storage
     /**
      * Helper function for sorting Records of content that have a Grouping.
      *
-     * @param  object $a
-     * @param  object $b
+     * @param  \Bolt\Content $a
+     * @param  \Bolt\Content $b
      * @return int
      */
-    private function groupingSort($a, $b)
+    private function groupingSort(\Bolt\Content $a, \Bolt\Content $b)
     {
         // Same group, sort within group..
         if ($a->group['slug'] == $b->group['slug']) {
