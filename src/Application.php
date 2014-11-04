8353445dfdf40ec05debaaefa4b4240968a3748a
bobdenotter <bob@twokings.nl>
2014-11-04 10:47:59 +0100

    Bumping version number to '2.0.0 beta 3'.

--- src/Application.php
+++ src/Application.php
@@ -25,7 +25,7 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '2.0.0';
-        $values['bolt_name'] = 'beta 2-pl1';
+        $values['bolt_name'] = 'beta 3';
 
         parent::__construct($values);
 
