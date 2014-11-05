c5c5851a79db6cc47399c5e62689b788539391f4
Tobias Dammers <tobias@twokings.nl>
2014-11-05 11:50:58 +0100

    Whitespace fix

--- src/BaseExtension.php
+++ src/BaseExtension.php
@@ -23,7 +23,7 @@ abstract class BaseExtension implements ExtensionInterface
     private $composerJsonLoaded;
     private $composerJson;
     private $configLoaded;
-    
+
 
     public function __construct(Application $app)
     {
@@ -336,15 +336,14 @@ abstract class BaseExtension implements ExtensionInterface
         $this->initializeTwig();
         $this->twigExtension->addTwigFilter(new \Twig_SimpleFilter($name, array($this, $callback), $options));
     }
-    
+
     protected function initializeTwig()
     {
         if (!$this->twigExtension) {
             $this->twigExtension = new TwigProxy($this->getName());
         }
     }
-    
-    
+
     public function getTwigExtensions()
     {
         if ($this->twigExtension) {
