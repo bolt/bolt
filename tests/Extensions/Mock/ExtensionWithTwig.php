bc955cd3524e99ca274b4836610376a015d3c28d
Ross Riley <riley.ross@gmail.com>
2014-11-07 06:24:56 +0000

    move test files into root folder and start work on writing tests

---
+++ tests/Extensions/Mock/ExtensionWithTwig.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bolt\Tests\Extensions\Mock;
+
+use Bolt\Extensions\ExtensionInterface;
+use Bolt\Application;
+
+/**
+ * Class to test correct operation and locations of composer configuration.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class ExtensionWithTwig extends Extension
+{
+
+    
+    public function getName()
+    {
+        return "extensionwithtwig";
+    }
+    
+    public function getTwigExtensions()
+    {
+        return array();
+    }
+
+   
+}
