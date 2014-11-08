e467d919ff0999f8fe5e9aa567c8b16d91a4ad08
Ross Riley <riley.ross@gmail.com>
2014-11-08 10:25:16 +0000

    start work on data collector tests

---
+++ tests/DataCollector/BoltDataCollectorTest.php
@@ -0,0 +1,86 @@
+<?php
+namespace Bolt\Tests\DataCollector;
+
+use Bolt\Application;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\DataCollector\BoltDataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * Class to test correct operation and locations of src/DataCollector/BoltDataCollector.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class BoltDataCollectorTest extends BoltUnitTest
+{
+
+    
+    public function testBasicData()
+    {
+        $app = $this->getApp();
+        
+        
+        $request = Request::create(
+            '/',
+            'GET',
+            array(),
+            array(),
+            array(),
+            array()
+        );
+        
+        
+        $this->expectOutputRegex('#Redirecting to /bolt/users/edit/#');
+        $app->run($request);
+        
+        $response = new Response();
+        
+        
+        $data = new BoltDataCollector($app);
+        $data->collect($request, $response);
+        $this->assertNotEmpty($data->getName());
+        $this->assertNotEmpty($data->getVersion());
+        $this->assertNotEmpty($data->getFullVersion());
+        $this->assertNotEmpty($data->getVersionName());
+        $this->assertNotEmpty($data->getPayoff());
+        $this->assertNotEmpty($data->getAboutLink());
+        $this->assertEmpty($data->getEditLink());
+        $this->assertEmpty($data->getEditTitle());
+    }
+    
+    public function testBrandingData()
+    {
+        $app = $this->getApp();
+        $app['config']->set("general/branding/provided_by/0", 'testperson');
+        $app['config']->set("general/branding/provided_by/1", 'testemail');
+        $request = Request::create('/','GET');
+        $this->expectOutputRegex('#Redirecting to /bolt/users/edit/#');
+        $app->run($request);
+        $response = new Response();
+        $data = new BoltDataCollector($app);
+        $data->collect($request, $response);
+        $this->assertRegExp('/testperson/', $data->getBranding());
+        $this->assertRegExp('/testemail/', $data->getBranding());
+    }
+    
+    public function testEditLinks()
+    {
+        $app = $this->getApp();
+        $app['editlink'] = "editlink";
+        $app['edittitle'] = "edittitle";
+        $request = Request::create('/','GET');
+        $this->expectOutputRegex('#Redirecting to /bolt/users/edit/#');
+        $app->run($request);
+        $response = new Response();
+        $data = new BoltDataCollector($app);
+        $data->collect($request, $response);
+        $this->assertEquals("editlink", $data->getEditlink());
+        $this->assertEquals("edittitle", $data->getEdittitle());
+        
+    }
+
+   
+}
---
+++ tests/DataCollector/TwigDataCollectorTest.php
@@ -0,0 +1,63 @@
+<?php
+namespace Bolt\Tests\DataCollector;
+
+use Bolt\Application;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\DataCollector\TwigDataCollector;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use Doctrine\DBAL\Logging\DebugStack;
+
+/**
+ * Class to test src/DataCollector/TwigDataCollector.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class DatabaseDataCollectorTest extends BoltUnitTest
+{
+
+    
+    public function testBasicData()
+    {
+        $debug = new DebugStack();
+        $data = new DatabaseDataCollector($debug);
+        $debug->startQuery("Robert'); DROP TABLE students;");
+        $debug->stopQuery();
+        
+        $app = $this->getApp();
+        $request = Request::create('/','GET');
+        $this->expectOutputRegex('#Redirecting to /bolt/users/edit/#');
+        $app->run($request);
+        $response = new Response();
+        
+        $data->collect($request, $response);
+        $this->assertEquals('db', $data->getName());
+        $this->assertEquals(1, $data->getQueryCount());
+        $this->assertNotEmpty($data->getTime());
+        $this->assertNotEmpty($data->getQueries());
+    }
+    
+    public function testPragmaIgnored()
+    {
+        $debug = new DebugStack();
+        $data = new DatabaseDataCollector($debug);
+        $debug->startQuery("PRAGMA test");
+        $debug->stopQuery();
+        
+        $app = $this->getApp();
+        $request = Request::create('/','GET');
+        $this->expectOutputRegex('#Redirecting to /bolt/users/edit/#');
+        $app->run($request);
+        $response = new Response();
+        
+        $data->collect($request, $response);
+        $this->assertEquals(0, $data->getQueryCount());
+
+    }
+    
+
+   
+}
