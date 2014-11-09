fd2ac490fe8a43704fd55315d00e5f6ab2b11006
Ross Riley <riley.ross@gmail.com>
2014-11-09 07:54:53 +0000

    setup tests for composer

---
+++ tests/Composer/BootstrapGeneratorTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bolt\Tests\Composer;
+
+use Bolt\Application;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Composer\BootstrapGenerator;
+
+/**
+ * Class to test src/Composer/BootstrapGenerator.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class BootstrapGeneratorTest extends BoltUnitTest
+{
+
+    public function testConstruct()
+    {
+        
+    }
+    
+    
+    
+    
+    
+ 
+   
+}
