435f4d16cfd8ba1915d5d55992c0247d4a691ed3
rarila <magnet@gmx.net>
2014-11-09 13:39:51 +0100

    Unlucky try to get 12h/24h working

--- app/view/js/src/init.js
+++ app/view/js/src/init.js
@@ -605,14 +605,52 @@ var init = {
                 setDate = $.datepicker.parseDate('yy-mm-dd', inpData.val()),
                 options = {},
                 fieldOptions = $(this).data('field-options'),
+                is12h = moment.localeData()._longDateFormat.LT.replace(/\[.+?\]/gi, '').match(/A/i) ? true : false,
                 setfnc;
 
             setfnc = function () {
-                var date = $.datepicker.formatDate('yy-mm-dd', inpDate.datepicker('getDate')),
-                    dt = new Date(date + 'T'+(inpTime.length && inpTime.val() !== ''? inpTime.val() : '00:00') + ':00');
+                var date = moment(inpDate.datepicker('getDate')),
+                    time = moment([2001, 11, 24]),
+                    hours = 0,
+                    minutes = 0,
+                    h,
+                    t;
 
-                if (Object.prototype.toString.call(dt) === '[object Date]' && !isNaN(dt.getTime())) {
-                    inpData.val($.formatDateTime('yy-mm-dd hh:ii:00', dt));
+                // Process time field
+                if (inpTime.length) {
+                    res = inpTime.val().match(/^\s*(?:(?:([01]?[0-9]|2[0-3])[:,.]([0-5]?[0-9]))|(1[012]|0?[1-9])[:,.]([0-5]?[0-9])(?:\s*([AP])[. ]?M\.?))\s*$/i);
+                    if (res) {
+                        hours = parseInt(res[1] ? res[1] :res[3]);
+                        minutes = parseInt(res[2] ? res[2] :res[4]);
+                        if ((res[5] === 'p' || res[5] === 'P') && hours !== 12) {
+                            hours += 12;
+                        } else if ((res[5] === 'a' || res[5] === 'A') && hours === 12) {
+                            hours -= 12;
+                        }
+                        time = moment([2001, 11, 24, hours, minutes]);
+                    }
+                }
+                // Set data
+                if (date.isValid()) {
+                    inpData.val(date.format('YYYY-MM-DD') + ' ' + time.format('HH:mm:00'));
+                } else if (inpData.val() === '') {
+                    inpData.val('');
+                }
+                // Write back
+                if (inpData.val() !== '' && date.isValid()) {
+                    inpDate.datepicker('setDate', $.datepicker.parseDate('yy-mm-dd', date.format('YYYY-MM-DD')));
+                    if (inpTime.length) {
+                        if (is12h) {
+                            h = parseInt(inpData.val().slice(11, 13));
+                            t = (inpData.val().slice(11, 13) % 12 || 12) + inpData.val().slice(13, 16) + ' ' + (h < 12 ? 'AM' : 'PM');
+                        } else {
+                            t = inpData.val().slice(11, 16);
+                        }
+                        inpTime.val(t);
+                    }
+                } else {
+                    inpDate.datepicker('setDate', '');
+                    inpTime.val('');
                 }
             };
 
@@ -635,6 +673,17 @@ var init = {
 
             // If a time field exists, bind it
             if (inpTime.length) {
+                if (setDate == '' || inpData.val() === '') {
+                    inpTime.val('');
+                } else {
+                    if (is12h) {
+                        h = parseInt(inpData.val().slice(11, 13));
+                        t = (inpData.val().slice(11, 13) % 12 || 12) + inpData.val().slice(13, 16) + ' ' + (h < 12 ? 'AM' : 'PM');
+                    } else {
+                        t = inpData.val().slice(11, 16);
+                    }
+                    inpTime.val(t);
+                }
                 inpTime.change(setfnc);
             }
         });
