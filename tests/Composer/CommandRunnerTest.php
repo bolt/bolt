1501a5c5d4e7980197d798fb83407c2870c75c15
Ross Riley <riley.ross@gmail.com>
2014-11-09 19:12:29 +0000

    start work on composer module tests

---
+++ tests/Composer/CommandRunnerTest.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bolt\Tests\Composer;
+
+use Bolt\Application;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Composer\CommandRunner;
+
+/**
+ * Class to test src/Composer/CommandRunner.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class CommandRunnerTest extends BoltUnitTest
+{
+
+
+    public function testConstruct()
+    {
+
+    }
+    
+
+    
+    
+ 
+   
+}
