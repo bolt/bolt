c3b7359466c07aaa4e83d5f38b2a6a8eba7b0d32
Gawain Lynch <gawain.lynch@gmail.com>
2014-11-10 15:56:21 +0000

    Move changelog to logger

--- src/Logger/Handler/RecordChangeHandler.php
+++ src/Logger/Handler/RecordChangeHandler.php
@@ -8,6 +8,8 @@ use Monolog\Logger;
 use Monolog\Handler\AbstractProcessingHandler;
 
 use Bolt\Application;
+use Bolt\Content;
+use Bolt\DeepDiff;
 use Bolt\Helpers\String;
 use Bolt\Logger\Formatter\RecordChange;
 
@@ -32,6 +34,11 @@ class RecordChangeHandler extends AbstractProcessingHandler
      * @var string
      */
     private $tablename;
+
+    /**
+     * @var array
+     */
+    private $allowed;
 
     /**
      *
@@ -68,31 +75,113 @@ class RecordChangeHandler extends AbstractProcessingHandler
 
     protected function write(array $record)
     {
+        // Simply exit if we're not enabled
+        if (!$this->app['config']->get('general/changelog/enabled')) {
+            return;
+        }
+
+        // Check for a valid call
+        if (!in_array($record['context']['action'], $this->allowed)) {
+            throw new \Exception("Invalid action '{$record['context']['action']}' specified for changelog (must be one of [ " . implode(', ', $this->allowed) . " ])");
+        }
+        if (empty($record['context']['old']) && empty($record['context']['new'])) {
+            throw new \Exception("Tried to log something that cannot be: both old and new content are empty");
+        }
+        if (empty($record['context']['old']) && in_array($record['context']['action'], array('UPDATE', 'DELETE'))) {
+            throw new \Exception("Cannot log action '{$record['context']['action']}' when old content doesn't exist");
+        }
+        if (empty($record['context']['new']) && in_array($record['context']['action'], array('INSERT', 'UPDATE'))) {
+            throw new \Exception("Cannot log action '{$record['context']['action']}' when new content is empty");
+        }
+
+        // Initialise ourselves if not already
         if (!$this->initialized) {
             $this->initialize();
         }
 
+        $data = array();
+        switch ($record['context']['action']) {
+            case 'UPDATE':
+                $diff = DeepDiff::diff($record['context']['old'], $record['context']['new']);
+                foreach ($diff as $item) {
+                    list($k, $old, $new) = $item;
+                    if (isset($record['context']['new'][$k])) {
+                        $data[$k] = array($old, $new);
+                    }
+                }
+                break;
+            case 'INSERT':
+                foreach ($record['context']['new'] as $k => $val) {
+                    $data[$k] = array(null, $val);
+                }
+                break;
+            case 'DELETE':
+                foreach ($record['context']['old'] as $k => $val) {
+                    $data[$k] = array($val, null);
+                }
+                break;
+        }
+
+        if ($record['context']['new']) {
+            $content = new Content($this->app, $record['context']['contenttype'], $record['context']['new']);
+        } else {
+            $content = new Content($this->app, $record['context']['contenttype'], $record['context']['old']);
+        }
+
+        $title = $content->getTitle();
+        if (empty($title)) {
+            $content = $this->getContent($record['context']['contenttype'] . '/' . $record['context']['id']);
+            $title = $content->getTitle();
+        }
+        $str = json_encode($data);
+
         try {
             $this->app['db']->insert($this->tablename, array(
                 'date'          => $record['datetime']->format('Y-m-d H:i:s'),
-                'ownerid'       => $record[''],
-                'title'         => $record[''],
-                'contenttype'   => $record[''],
-                'contentid'     => $record[''],
-                'mutation_type' => $record[''],
-                'diff'          => $record[''],
-                'comment'       => $record['']
+                'ownerid'       => $this->app['users']->getCurrentUser(),
+                'title'         => $title,
+                'contenttype'   => $record['context']['contenttype'],
+                'contentid'     => $record['context']['id'],
+                'mutation_type' => $record['context']['action'],
+                'diff'          => $str,
+                'comment'       => $record['context']['comment']
             ));
         } catch (\Exception $e) {
             // Nothing..
         }
+    }
 
-//         $this->statement->execute(array(
-//             'channel' => $record['channel'],
-//             'level' => $record['level'],
-//             'message' => $record['formatted'],
-//             'time' => $record['datetime']->format('U'),
-//         ));
+
+
+    /**
+     * Writes a content-changelog entry.
+     *
+     * @param string $action Must be one of 'INSERT', 'UPDATE', or 'DELETE'.
+     * @param string $contenttype The contenttype setting to log.
+     * @param int $contentid ID of the content item to log.
+     * @param array $newContent For 'INSERT' and 'UPDATE', the new content;
+     *                          null for 'DELETE'.
+     * @param array $oldContent For 'UPDATE' and 'DELETE', the current content;
+     *                          null for 'INSTERT'.
+     * For the 'UPDATE' and 'DELETE' actions, this function fetches the
+     * previous data from the database; this means that you must call it
+     * _before_ running the actual update/delete query; for the 'INSERT'
+     * action, this is not necessary, and since you really want to provide
+     * an ID, you can only really call the logging function _after_ the update.
+     * @param string $comment Add a comment to save on change log.
+     * @throws \Exception
+     */
+    private function writeChangelog($action, $contenttype, $id, $newContent = null, $oldContent = null, $comment = null)
+    {
+        array(
+            'action' => '',
+            'contenttype' => '',
+            'id' => '',
+            'new' => '',
+            'old' => '',
+            'comment' => '',
+        );
+
     }
 
     /**
@@ -118,6 +207,7 @@ class RecordChangeHandler extends AbstractProcessingHandler
     private function initialize()
     {
         $this->tablename = sprintf("%s%s", $this->app['config']->get('general/database/prefix', "bolt_"), 'content_changelog');
+        $this->allowed = array('INSERT', 'UPDATE', 'DELETE');
         $this->initialized = true;
     }
 
