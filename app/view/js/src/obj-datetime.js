16336b047bec84f3afd6466b21b5cf9cb6e0f026
rarila <magnet@gmx.net>
2014-11-11 19:34:11 +0100

    Remove debug setting for 24h

--- app/view/js/src/obj-datetime.js
+++ app/view/js/src/obj-datetime.js
@@ -101,7 +101,6 @@ bolt.datetimes = function () {
 
             // Find out if locale uses 24h format
             this.is24h = moment.localeData()._longDateFormat.LT.replace(/\[.+?\]/gi, '').match(/A/) ? false : true;
-            this.is24h = false;
 
             // Initialize each available date/datetime field
             $('.datepicker').each(function () {
