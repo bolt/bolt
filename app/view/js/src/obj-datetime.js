93afbcb8896c4016b4872ee42c55ca97788aa350
rarila <magnet@gmx.net>
2014-11-11 07:49:34 +0100

    Add evaluation

--- app/view/js/src/obj-datetime.js
+++ app/view/js/src/obj-datetime.js
@@ -2,20 +2,18 @@
  * DateTime object
  */
 var datetimes = function () {
-    function hasChanged(field) {
-        console.log('hasChanged()');
-
+    function evaluate(field) {
         var date = moment(field.date.datepicker('getDate')),
             time = moment([2001, 11, 24]),
             hours = 0,
-            minutes = 0,
-            h,
-            t;
+            minutes = 0;
 
         // Process time field
-        if (field.time) {
+        if (field.time.length) {
+            console.log('<'+field.time.val()+'>');
             res = field.time.val().match(/^\s*(?:(?:([01]?[0-9]|2[0-3])[:,.]([0-5]?[0-9]))|(1[012]|0?[1-9])[:,.]([0-5]?[0-9])(?:\s*([AP])[. ]?M\.?))\s*$/i);
             if (res) {
+            console.log(res);
                 hours = parseInt(res[1] ? res[1] :res[3]);
                 minutes = parseInt(res[2] ? res[2] :res[4]);
                 if ((res[5] === 'p' || res[5] === 'P') && hours !== 12) {
@@ -26,27 +24,14 @@ var datetimes = function () {
                 time = moment([2001, 11, 24, hours, minutes]);
             }
         }
-        // Set data
+
+        // Set data field
         if (date.isValid()) {
             field.data.val(date.format('YYYY-MM-DD') + ' ' + time.format('HH:mm:00'));
-        } else if (field.data.val() === '') {
+        } else if (field.date.val() === '') {
             field.data.val('');
-        }
-        // Write back
-        if (field.data.val() !== '' && date.isValid()) {
-            field.date.datepicker('setDate', $.datepicker.parseDate('yy-mm-dd', date.format('YYYY-MM-DD')));
-            if (field.time.length) {
-                if (this.is24h) {
-                    t = field.data.val().slice(11, 16);
-                } else {
-                    h = parseInt(field.data.val().slice(11, 13));
-                    t = (field.data.val().slice(11, 13) % 12 || 12) + field.data.val().slice(13, 16) + ' ' + (h < 12 ? 'AM' : 'PM');
-                }
-                field.time.val(t);
-            }
         } else {
-            field.date.datepicker('setDate', '');
-            field.time.val('');
+            // Error
         }
     }
 
@@ -73,7 +58,7 @@ var datetimes = function () {
                 time = field.data.val().slice(11, 16);
             } else {
                 hour = parseInt(time.slice(0, 2));
-                time = (hour % 12 || 12) + time.slice(2, 5) + ' ' + (hour < 12 ? 'AM' : 'PM');
+                time = (hour % 12 || 12) + time.slice(2, 5) + (hour < 12 ? ' AM' : ' PM');
             }
             field.time.val(time);
         }
@@ -121,10 +106,12 @@ var datetimes = function () {
 
                 // Bind change action to date and time field
                 field.date.change(function () {
-                    hasChanged(field);
+                    evaluate(field);
+                    display(field);
                 });
                 field.time.change(function () {
-                    hasChanged(field);
+                    evaluate(field);
+                    display(field);
                 });
             });
         }
