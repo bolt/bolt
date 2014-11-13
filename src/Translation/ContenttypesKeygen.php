02ecbb537d38a0e0dc44254722d4a5fb3eec51a5
rarila <magnet@gmx.net>
2014-11-13 22:42:24 +0100

    Add skeleton class for contenttypes translation keys

---
+++ src/Translation/ContenttypesKeygen.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bolt\Translation;
+
+use Silex;
+
+/**
+ * Generates translations keys for contenttypes
+ */
+class ContenttypesKeygen
+{
+    /**
+     * Injected Application object
+     *
+     * @var type
+     */
+    private $app;
+
+    /**
+     * Constructor
+     *
+     * @param Silex\Application $app
+     */
+    public function __construct(Silex\Application $app)
+    {
+    }
+
+    /**
+     * Generates translations keys for contenttypes
+     */
+    public function generate()
+    {
+    }
+}
