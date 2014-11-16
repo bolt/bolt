194de12baf288ec8a94a21e42446c303f7da9453
Gawain Lynch <gawain.lynch@gmail.com>
2014-11-16 09:47:52 +0000

    Dump dump from the logger

--- src/Logger/Handler/SystemHandler.php
+++ src/Logger/Handler/SystemHandler.php
@@ -103,8 +103,7 @@ class SystemHandler extends AbstractProcessingHandler
                 'ip'          => $this->app['request']->getClientIp(),
                 'contenttype' => $contenttype,
                 'content_id'  => $content_id,
-                'context'     => isset($record['context']['event']) ? $record['context']['event'] : '',
-                'dump'        => ''
+                'context'     => isset($record['context']['event']) ? $record['context']['event'] : ''
             ));
         } catch (\Exception $e) {
             // Nothing..
