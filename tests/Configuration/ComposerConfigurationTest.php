ae31597ab5879fc871cd6ecc1ecfe940ba1ddbd6
Ross Riley <riley.ross@gmail.com>
2014-11-18 11:50:55 +0000

    rebase merge

--- tests/Configuration/ComposerConfigurationTest.php
+++ tests/Configuration/ComposerConfigurationTest.php
@@ -4,7 +4,6 @@ namespace Bolt\Tests\Configuration;
 use Bolt\Application;
 use Bolt\Configuration\Composer;
 use Bolt\Configuration\ComposerChecks;
-use Bolt\Configuration\LowlevelException;
 
 /**
  * Class to test correct operation and locations of composer configuration.
@@ -39,16 +38,6 @@ class ComposerConfigurationTest extends \PHPUnit_Framework_TestCase
         $config->setVerifier($verifier);
         $config->verify(); 
     }
-    
-    public function testCheckSummary()
-    {
-        $config = new Composer(TEST_ROOT);
-        $verifier = new ComposerChecks($config);
-        $this->setExpectedException(LowlevelException::class);
-        $this->expectOutputRegex("/Bolt - Fatal Error/");
-        $verifier->checkDir('/non/existent/path');
-    }
-    
 
    
 }
