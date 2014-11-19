b49408c79a7f9859025cc6f8a5c9b426f198d329
rarila <magnet@gmx.net>
2014-11-19 13:48:05 +0100

    Refactored contenttype handling

--- src/Translation/Translator.php
+++ src/Translation/Translator.php
@@ -14,10 +14,15 @@ class Translator
     * Encode array values as html special chars
     *
     * @param array $params Parameter to encode
+    * @param string $removeKey If not empty the key is removed from result
     * @return array
     */
-    private static function htmlencodeParams($params)
+    private static function htmlencodeParams($params, $removeKey = '')
     {
+        if ($removeKey) {
+            unset($params[$removeKey]);
+        }
+
         return array_map(
             function ($val) {
                 return htmlspecialchars($val, ENT_QUOTES);
@@ -32,19 +37,20 @@ class Translator
      * @param string $key
      * @param array $parameters
      * @param string $domain
-     * @param string $locale
+     * @param mixed $locale
+     * @param mixed $default
      * @return string
      */
-    public static function trans($key, array $parameters = array(), $domain = 'messages', $locale = null)
+    public static function trans($key, array $parameters = array(), $domain = 'messages', $locale = null, $default = null)
     {
         $app = ResourceManager::getApp();
 
         // Handle default parameter
         if (isset($parameters['DEFAULT'])) {
-            $default = $parameters['DEFAULT'];
+            if ($default === null) {
+                $default = $parameters['DEFAULT'];
+            }
             unset($parameters['DEFAULT']);
-        } else {
-            $default = null;
         }
 
         // Handle number parameter
@@ -78,6 +84,84 @@ class Translator
     }
 
     /**
+     * Returns translated contenttype name with fallback to name/slug from 'contenttypes.yml'
+     *
+     * @param string $contenttype The contentype
+     * @param bool $singular Singular or plural requested?
+     * @param string $locale Translate to this locale
+     * @return string
+     */
+    private static function transContenttypeName($contenttype, $singular, $locale)
+    {
+        $key = 'contenttypes.' . $contenttype . '.name.' . ($singular ? 'singular' : 'plural');
+
+        $name = static::trans($key, array(), 'contenttypes', $locale);
+        if ($name === $key) {
+            $app = ResourceManager::getApp();
+
+            $name = $app['config']->get('contenttypes/' . $contenttype . ($singular ? '/singular_name' : '/name'));
+            if (empty($name)) {
+                $name = ucfirst(
+                    $app['config']->get(
+                        'contenttypes/' . $contenttype . ($singular ? '/singular_slug' : '/slug'),
+                        $contenttype
+                    )
+                );
+            }
+            // Escape names coming from 'contenttypes.yml'
+            $name = htmlspecialchars($name, ENT_QUOTES);
+        }
+
+        return $name;
+    }
+
+    /**
+     * Translates contentype specific messages and falls back to building generic message or fallback locale
+     *
+     * @param string $genericKey
+     * @param array $params
+     * @param string $id
+     * @param boolean $singular
+     * @param mixed $locale
+     * @return boolean
+     */
+    private static function transContenttype($genericKey, $params, $id, $singular, $locale)
+    {
+        $contenttype = $params[$id];
+        $encParams = static::htmlencodeParams($params, $id);
+        $key = 'contenttypes.' . $contenttype . '.text.' . substr($genericKey, 21);
+
+        // Try to get a real translation from contenttypes.xx_XX.yml
+        $trans = static::trans($key, $encParams, 'contenttypes', $locale, false);
+        $transFallback = static::trans($key, $encParams, 'contenttypes', \Bolt\Application::DEFAULT_LOCALE, false);
+
+        // We don't want fallback translation here
+        if ($trans === $transFallback) {
+            $trans = false;
+        }
+
+        // No translation found, build string from generic translation pattern
+        if ($trans === false) {
+            // Get generic translation with name replaced
+            $encParams[$id] = static::transContenttypeName($contenttype, $singular, $locale);
+            $transGeneric = static::trans($genericKey, $encParams, 'messages', $locale, false);
+        } else {
+            $transGeneric = false;
+        }
+
+        // Return: translation => generic translation => fallback translation => key
+        if ($trans) {
+            return $trans;
+        } elseif ($transGeneric) {
+            return $transGeneric;
+        } elseif ($transFallback) {
+            return $transFallback;
+        } else {
+            return $genericKey;
+        }
+    }
+
+    /**
      * i18n made right, second attempt...
      *
      * Instead of calling directly $app['translator']->trans(), we check
@@ -87,12 +171,11 @@ class Translator
      * and try to get a translated string. If there is not, we revert to
      * the generic (%contenttype%) string, which must have a translation.
      */
-    public static function /*@codingStandardsIgnoreStart*/__/*@codingStandardsIgnoreEnd*/($key, $parameters = array(), $domain = 'messages', $locale = null)
+    public static function /*@codingStandardsIgnoreStart*/__/*@codingStandardsIgnoreEnd*/($key, array $parameters = array(), $domain = 'messages', $locale = null)
     {
-        $app = ResourceManager::getApp();
-
         // Set locale
         if ($locale === null) {
+            $app = ResourceManager::getApp();
             $locale = $app['request']->getLocale();
         }
 
@@ -107,42 +190,12 @@ class Translator
             $key = join('.', $key);
         }
 
-        // Check for contenttype(s) placeholder
-        if (count($parameters) > 0) {
+        // Handle generic contenttypes
+        if (substr($key, 0, 21) == 'contenttypes.generic.') {
             if (isset($parameters['%contenttype%'])) {
-                $key_arg = '%contenttype%';
+                return static::transContenttype($key, $parameters, '%contenttype%', true, $locale);
             } elseif (isset($parameters['%contenttypes%'])) {
-                $key_arg = '%contenttypes%';
-            } else {
-                $key_arg = false;
-            }
-            $key_generic = $key;
-            if ($key_arg && substr($key_generic, 0, 21) == 'contenttypes.generic.') {
-
-                $ctype = $parameters[$key_arg];
-                unset($parameters[$key_arg]);
-                $key_ctype = 'contenttypes.' . $ctype . '.text.' . substr($key_generic, 21);
-
-                // Try to get a direct translation, fallback to en
-                $trans = static::trans($key_ctype, static::htmlencodeParams($parameters), 'contenttypes', $locale);
-
-                // No translation found, use generic translation
-                if ($trans === $key_ctype) {
-                    // Get contenttype name
-                    $key_name = 'contenttypes.' . $ctype . '.name.' . (($key_arg == '%contenttype%') ? 'singular' : 'plural');
-                    $key_ctname = ($key_arg == '%contenttype%') ? 'singular_name' : 'name';
-
-                    $ctname = static::trans($key_name, array(), 'contenttypes', $app['request']->getLocale());
-                    if ($ctname === $key_name) {
-                        $ctypes = $app['config']->get('contenttypes');
-                        $ctname = empty($ctypes[$ctype][$key_ctname]) ? ucfirst($ctype) : $ctypes[$ctype][$key_ctname];
-                    }
-                    // Get generic translation with name replaced
-                    $parameters[$key_arg] = $ctname;
-                    $trans = static::trans($key_generic, static::htmlencodeParams($parameters), 'messages', $locale);
-                }
-
-                return $trans;
+                return static::transContenttype($key, $parameters, '%contenttypes%', false, $locale);
             }
         }
 
