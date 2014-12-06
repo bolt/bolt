a81faf07ce829dd91c51301ce14093e560086e84
Ross Riley <riley.ross@gmail.com>
2014-12-06 16:27:37 +0000

    add class alias

--- src/Nut/ConfigSet.php
+++ src/Nut/ConfigSet.php
@@ -3,6 +3,7 @@
 namespace Bolt\Nut;
 
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
