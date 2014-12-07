2cd1a3e369992e3d0b0c6ce97cc6f9de7fa03a0f
www-data <www-data@infractory.org>
2014-12-07 16:43:32 +0100

    Pushing something clean?

--- src/Controllers/Frontend.php
+++ src/Controllers/Frontend.php
@@ -39,7 +39,8 @@ class Frontend
                 $contenttypeslug = (string) $content;
                 $contentid = null;
             }
-            if (!$app['users']->isAllowed('frontend', $contenttypeslug, $contentid)) {
+
+            if (!$app['users']->isAllowed('frontend', $content, $contenttypeslug, $contentid)) {
                 $app->abort(403, 'Not allowed.');
             }
         }
@@ -123,7 +124,7 @@ class Frontend
      * @param string            $slug            The content slug
      * @return mixed
      */
-    public static function record(Silex\Application $app, $contenttypeslug, $slug)
+    public static function record(Silex\Application $app, $contenttypeslug, $slug, $noFrontendPermissionCheck = null)
     {
         $contenttype = $app['storage']->getContentType($contenttypeslug);
 
@@ -142,7 +143,8 @@ class Frontend
             $content = $app['storage']->getContent($contenttype['slug'], array('id' => $slug, 'returnsingle' => true));
         }
 
-        self::checkFrontendPermission($app, $content);
+        if(isset($content) && !isset($noFrontendPermissionCheck))
+          self::checkFrontendPermission($app, $content);
 
         // No content, no page!
         if (!$content) {
@@ -251,6 +253,12 @@ class Frontend
         $amount = (!empty($contenttype['listing_records']) ? $contenttype['listing_records'] : $app['config']->get('general/listing_records'));
         $order = (!empty($contenttype['sort']) ? $contenttype['sort'] : $app['config']->get('general/listing_sort'));
         $content = $app['storage']->getContent($contenttype['slug'], array('limit' => $amount, 'order' => $order, 'page' => $page, 'paging' => true));
+
+        /*
+          TODO reactivate that
+          change how permissions are checked.
+          defined a default frontend_view, use it there.
+        */
         self::checkFrontendPermission($app, $contenttype['slug']);
 
         $template = $app['templatechooser']->listing($contenttype);
