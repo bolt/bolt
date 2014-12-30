dc637d08478ca7cc871dd1bf76262069556f48f7
rarila <magnet@gmx.net>
2014-12-30 22:15:54 +0100

    Add jshint option: devel

--- app/view/Gruntfile.js
+++ app/view/Gruntfile.js
@@ -126,7 +126,8 @@ module.exports = function(grunt) {
 
         jshint: {
             options: {
-                browser: true   // Defines globals exposed by modern browsers
+                browser: true,  // Defines globals exposed by modern browsers
+                devel: true     // Defines globals that are usually used for logging/debugging
             },
             src: ['js/src/*.js']
         }
