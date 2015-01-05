2366017678f9037db0367e17d8658c9e3c16f19d
Gawain Lynch <gawain.lynch@gmail.com>
2015-01-06 06:02:34 +1100

    Add Composer class to dump autoloader

---
+++ src/Composer/Action/DumpAutoload.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Bolt\Composer\Action;
+
+class DumpAutoload
+{
+    /**
+     * @param $io       Composer\IO\BufferIO
+     * @param $composer Composer\Composer
+     * @param $options  array
+     */
+    public function __construct($io, $composer, $options)
+    {
+        $this->options = $options;
+        $this->io = $io;
+        $this->composer = $composer;
+    }
+
+    /**
+     * Dump autoloaders
+     */
+    public function execute()
+    {
+        $installationManager = $this->composer->getInstallationManager();
+        $localRepo = $this->composer->getRepositoryManager()->getLocalRepository();
+        $package = $this->composer->getPackage();
+        $config = $this->composer->getConfig();
+
+        if ($this->options['optimizeautoloader']) {
+            // Generating optimized autoload files
+        } else {
+            // Generating autoload files
+        }
+
+        $generator = $this->composer->getAutoloadGenerator();
+        $generator->setDevMode(!$this->options['nodev']);
+        $generator->dump($config, $localRepo, $package, $installationManager, 'composer', $this->options['optimizeautoloader']);
+    }
+}
