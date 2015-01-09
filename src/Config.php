e09b97a3eed336e75a1432432f3c38068d528807
Gawain Lynch <gawain.lynch@gmail.com>
2015-01-09 17:38:06 +0700

    Also use XMLHttpRequest to detect AJAX requests in getWhichEnd()

--- src/Config.php
+++ src/Config.php
@@ -910,10 +910,12 @@ class Config
 
         // If the request URI is '/bolt' or '/async' (or starts with '/bolt/' etc.), assume backend or async.
         $mountpoint = '/' . ltrim($mountpoint, '/');
-        if ($scripturi === $mountpoint || strpos($scripturi, $mountpoint . '/') === 0) {
-            $end = 'backend';
-        } elseif ($scripturi === '/async' || strpos($scripturi, '/async/') === 0) {
+        if (isset($_SERVER['HTTP_X_REQUESTED_WITH'] ) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest')
+            || elseif ($scripturi === '/async' || strpos($scripturi, '/async/') === 0) {
             $end = 'async';
+        }
+        elseif ($scripturi === $mountpoint || strpos($scripturi, $mountpoint . '/') === 0) {
+            $end = 'backend';
         } else {
             $end = 'frontend';
         }
