577e6c7e5ad4855bc321e3e9ec5cc4a65974f192
rarila <magnet@gmx.net>
2015-01-10 14:32:01 +0100

    Extract jquery-catchpaste from jquery.plugins.js

---
+++ app/view/lib/jquery-catchpaste-1.0.0p/jquery-catchpaste.js
@@ -0,0 +1,105 @@
+/*
+ Copyright (c) 2011 Sean Cusack
+
+ MIT-LICENSE:
+
+ Permission is hereby granted, free of charge, to any person obtaining
+ a copy of this software and associated documentation files (the
+ "Software"), to deal in the Software without restriction, including
+ without limitation the rights to use, copy, modify, merge, publish,
+ distribute, sublicense, and/or sell copies of the Software, and to
+ permit persons to whom the Software is furnished to do so, subject to
+ the following conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+(function($){
+
+    var TEXTAREA_ID = 'jquery-catchpaste-textarea';
+    function getTextArea() {
+        var ta = $("#"+TEXTAREA_ID);
+        if( ta.length <= 0 ) ta = $('<textarea id="'+TEXTAREA_ID+'">').css("left","-9999px").css("position","absolute").css("width","1px").css("height","1px").appendTo("body");
+        return ta;
+    }
+
+    function waitForPasteData( callback, options, returnHereWhenDone ) {
+        // the "this" here is the target of the paste command, so the <input>
+        var textarea = getTextArea();
+        textarea.css("display","block").val("");
+        var target = $(this);
+        function returnWhenDone(event) {
+            // the "this" here is the target of the keyup event, so the <textarea>
+            $(textarea).css("display","none");
+            $(textarea).unbind("keyup");
+            var pasted = $(textarea).val();
+            var called = callback.call( $(target), pasted, options );
+            //console.log(pasted,called);
+            $(target).focus();
+            if( called !== null )
+            {
+                $(target).val( $(target).caret().replace( called ) );
+            }
+        }
+        textarea.focus();
+        textarea.bind( "keyup", returnWhenDone );
+    }
+
+    // why? there's no logical way (that i can find) to trap text that's being dragged in, including newlines
+    // --> by the time it hits the input, the newlines have been stripped, and before that point, there's no
+    //     way to retrieve it from the events leading up to it, for things dragged in that were not explicitly
+    //     set to draggable (and thus can be tracked nicely)A
+    // why do i think this is impossible? because even googledocs spreadsheets don't support it. QED :-P
+    function cancelDragDrop(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        return false;
+    }
+
+    var IS_FIREFOX = null;
+
+    $.fn.catchpaste = function( callback, options ) {
+        //console.log(callback,options);
+        if( ! options  ) options = {};
+        if( IS_FIREFOX === null )
+        {
+            if(navigator.userAgent.match(/Firefox/)) {
+                IS_FIREFOX = true;
+            } else {
+                IS_FIREFOX = false;
+            }
+        }
+        if( IS_FIREFOX )
+        {
+            // works for Firefox:
+            var pasteEvent   = "keydown";
+            var pasteClosure = function(event) {
+                // the "this" here is the target of the paste command, so the <input>
+                if( ( ( event.keyCode == 86 ) || ( event.keyCode == 118 ) ) && ( event.metaKey || event.ctrlKey ) )
+                { // CTRL-V or COMMAND-V
+                    waitForPasteData.call( $(this), callback, options );
+                }
+            }
+        }
+        else
+        {
+            // works for Chrome and Safari:
+            var pasteEvent   = "paste";
+            var pasteClosure = function(event) {
+                // the "this" here is the target of the paste command, so the <input>
+                waitForPasteData.call( $(this), callback, options );
+            }
+        }
+        return $(this).each( function() { $(this).bind( pasteEvent, pasteClosure ); $(this).bind("dragover",cancelDragDrop); } );
+    };
+
+})(jQuery);
