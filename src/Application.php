3f0563ef18060fc4998695d1e6b65a24da940e2e
Carson Full <carson.full@gmomail.org>
2015-01-12 12:31:51 -0600

    Reapply "Refactor Twig setup"

--- src/Application.php
+++ src/Application.php
@@ -186,6 +186,13 @@ class Application extends Silex\Application
                 )
             )
         );
+        // Add the Bolt Twig Extension.
+        $this['twig'] = $this->share($this->extend('twig', function(\Twig_Environment $twig, $app) {
+            $twig->addExtension(new TwigExtension($app));
+            return $twig;
+        }));
+
+        $this->register(new Provider\SafeTwigServiceProvider());
 
         $this->register(new Provider\RenderServiceProvider());
         $this->register(new Provider\RenderServiceProvider(true));
@@ -257,7 +264,6 @@ class Application extends Silex\Application
             ->register(new Provider\OmnisearchServiceProvider())
             ->register(new Provider\TemplateChooserServiceProvider())
             ->register(new Provider\CronServiceProvider())
-            ->register(new Provider\SafeTwigServiceProvider())
             ->register(new Provider\FilePermissionsServiceProvider())
             ->register(new Controllers\Upload())
             ->register(new Controllers\Extend())
@@ -266,17 +272,9 @@ class Application extends Silex\Application
 
         $this['paths'] = $this['resources']->getPaths();
 
-        $this['twig']->addGlobal('paths', $this['paths']);
-
         // For some obscure reason, and under suspicious circumstances $app['locale'] might become 'null'.
         // Re-set it here, just to be sure. See https://github.com/bolt/bolt/issues/1405
         $this['locale'] = $currentlocale;
-
-        // Add the Bolt Twig functions, filters and tags.
-        $this['twig']->addExtension(new TwigExtension($this));
-        $this['safe_twig']->addExtension(new TwigExtension($this, true));
-
-        $this['twig']->addTokenParser(new SetcontentTokenParser());
 
         // Initialize stopwatch even if debug is not enabled.
         $this['stopwatch'] = $this->share(
@@ -335,38 +333,6 @@ class Application extends Silex\Application
         $this->mount('', new Controllers\Routing());
     }
 
-
-    /**
-     * Add all the global twig variables, like 'user' and 'theme'
-     */
-    private function addTwigGlobals()
-    {
-        $this['twig']->addGlobal('bolt_name', $this['bolt_name']);
-        $this['twig']->addGlobal('bolt_version', $this['bolt_version']);
-
-        $this['twig']->addGlobal('frontend', false);
-        $this['twig']->addGlobal('backend', false);
-        $this['twig']->addGlobal('async', false);
-        $this['twig']->addGlobal($this['config']->getWhichEnd(), true);
-
-        $this['twig']->addGlobal('user', $this['users']->getCurrentUser());
-        $this['twig']->addGlobal('users', $this['users']->getUsers());
-        $this['twig']->addGlobal('config', $this['config']);
-        $this['twig']->addGlobal('theme', $this['config']->get('theme'));
-
-        $this['safe_twig']->addGlobal('bolt_name', $this['bolt_name']);
-        $this['safe_twig']->addGlobal('bolt_version', $this['bolt_version']);
-
-        $this['safe_twig']->addGlobal('frontend', false);
-        $this['safe_twig']->addGlobal('backend', false);
-        $this['safe_twig']->addGlobal('async', false);
-        $this['safe_twig']->addGlobal($this['config']->getWhichEnd(), true);
-
-        $this['safe_twig']->addGlobal('user', $this['users']->getCurrentUser());
-        $this['safe_twig']->addGlobal('theme', $this['config']->get('theme'));
-    }
-
-
     /**
      * Initializes the Console Application that is responsible for CLI interactions.
      */
@@ -387,9 +353,6 @@ class Application extends Silex\Application
     {
         // Start the 'stopwatch' for the profiler.
         $this['stopwatch']->start('bolt.app.before');
-
-        // Set the twig Globals, like 'user' and 'theme'.
-        $this->addTwigGlobals();
 
         if ($response = $this['render']->fetchCachedRequest()) {
             // Stop the 'stopwatch' for the profiler.
@@ -557,9 +520,6 @@ class Application extends Silex\Application
         $end = $this['config']->getWhichEnd();
 
         $trace = $exception->getTrace();
-
-        // Set the twig Globals, like 'user' and 'theme'.
-        $this->addTwigGlobals();
 
         foreach ($trace as $key => $value) {
             if (!empty($value['file']) && strpos($value['file'], '/vendor/') > 0) {
