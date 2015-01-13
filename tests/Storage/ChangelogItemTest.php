9127227b054f35d0ea1ce6a9da424ee43892464f
Ross Riley <riley.ross@gmail.com>
2015-01-13 10:13:59 +0000

    add test

---
+++ tests/Storage/ChangelogItemTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Bolt\Tests\Storage;
+
+use Bolt\Application;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\ChangelogItem;
+
+/**
+ * Class to test src/Stack.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class ChangelogItemTest extends BoltUnitTest
+{
+
+
+    public function testSetup()
+    {
+        $app = $this->getApp();
+        
+    }
+    
+    
+
+    
+    
+ 
+   
+}
