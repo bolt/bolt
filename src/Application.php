1b56daae1379114f9e938647388fd7b4d428f530
Bob den Otter <bobdenotter@gmail.com>
2015-01-13 11:24:40 +0100

    Preparing the 2.0.3 release.

--- src/Application.php
+++ src/Application.php
@@ -25,8 +25,8 @@ class Application extends Silex\Application
 
     public function __construct(array $values = array())
     {
-        $values['bolt_version'] = '2.0.2';
-        $values['bolt_name'] = '';
+        $values['bolt_version'] = '2.0.3';
+        $values['bolt_name'] = 'RC';
 
         parent::__construct($values);
 
