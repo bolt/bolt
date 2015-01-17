f5ea57eafafa6035e08485d84d1bd5b82d72f1a7
Christian Silver <christian@decode.org.nz>
2015-01-17 18:14:25 +1300

    Rebuild CKEditor to fix icons
    And probably other stuff

--- app/view/lib/ckeditor/build-config.js
+++ app/view/lib/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/bafffa83388897102bddaa66107c1f1b
+ * (2) http://ckeditor.com/builder/0372e3b673780b6213769577539d095a
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/bafffa83388897102bddaa66107c1f1b
+ * (3) http://ckeditor.com/builder/download/0372e3b673780b6213769577539d095a
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -52,34 +52,47 @@ var CKBUILDER_CONFIG = {
 		'autogrow' : 1,
 		'basicstyles' : 1,
 		'blockquote' : 1,
+		'button' : 1,
 		'clipboard' : 1,
 		'codemirror' : 1,
 		'codesnippet' : 1,
 		'colorbutton' : 1,
 		'contextmenu' : 1,
+		'dialog' : 1,
+		'dialogui' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
+		'fakeobjects' : 1,
 		'filebrowser' : 1,
 		'find' : 1,
 		'floatingspace' : 1,
+		'floatpanel' : 1,
 		'format' : 1,
 		'htmlwriter' : 1,
 		'image' : 1,
+		'indent' : 1,
 		'indentlist' : 1,
 		'justify' : 1,
 		'link' : 1,
 		'list' : 1,
+		'listblock' : 1,
 		'maximize' : 1,
+		'menu' : 1,
 		'oembed' : 1,
+		'panel' : 1,
+		'panelbutton' : 1,
 		'pastefromword' : 1,
 		'pastetext' : 1,
+		'popup' : 1,
 		'removeformat' : 1,
 		'resize' : 1,
+		'richcombo' : 1,
 		'showborders' : 1,
 		'sourcearea' : 1,
 		'specialchar' : 1,
 		'table' : 1,
 		'tableresize' : 1,
+		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
 		'wysiwygarea' : 1
