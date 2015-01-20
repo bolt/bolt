a912354381daf23653d05823a0a8ba6d93f7a1a1
Gawain Lynch <gawain.lynch@gmail.com>
2015-01-20 09:02:05 +0000

    Have nut's extension enable call API correctly

--- src/Nut/ExtensionsEnable.php
+++ src/Nut/ExtensionsEnable.php
@@ -33,7 +33,7 @@ class ExtensionsEnable extends BaseCommand
             return;
         }
 
-        $result = $this->app['extend.manager']->install($name, $version);
+        $result = $this->app['extend.manager']->requirePackage(array('name' => $name, 'version' => $version));
 
         $output->writeln("<info>[Done]</info> ");
         $output->writeln($result, OutputInterface::OUTPUT_PLAIN);
