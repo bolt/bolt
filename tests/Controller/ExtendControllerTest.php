6891e3db80160128942a591f3d1c6338a76f439f
Gawain Lynch <gawain.lynch@gmail.com>
2015-01-20 13:45:49 +0000

    Use new PackageManager over CommandRunner

--- tests/Controller/ExtendControllerTest.php
+++ tests/Controller/ExtendControllerTest.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Bolt\Tests\BoltUnitTest;
 use Bolt\Controllers\Extend;
-use Bolt\Composer\CommandRunner;
+use Bolt\Composer\PackageManager;
 use Bolt\Users;
 
 /**
@@ -18,19 +18,17 @@ use Bolt\Users;
 
 class ExtendControllerTest extends BoltUnitTest
 {
-
-    
     public function testDefaultRegistries()
     {
-        
+
         $app = $this->getApp();
         $this->assertNotEmpty($app['extend.site']);
         $this->assertNotEmpty($app['extend.repo']);
-        $runner = $app['extend.runner'];
-        $this->assertInstanceOf('Bolt\Composer\CommandRunner', $runner);
-        
+        $runner = $app['extend.manager'];
+        $this->assertInstanceOf('Bolt\Composer\PackageManager', $runner);
+
     }
-    
+
     public function testMethodsReturnTemplates()
     {
         $app = $this->getApp();
@@ -42,12 +40,12 @@ class ExtendControllerTest extends BoltUnitTest
         $app['request'] = $request;
         $response = $extend->overview($app, $request);
         $this->assertRegExp('#<title>Extend[^<]*</title>#', $response);
-        
-        
+
+
         $response = $extend->installPackage($app, $request);
         $this->assertNotEmpty($response);
-        
-        
+
+
         // This currently checks for a live extension on the extend site. At some point we should mock this
         // But if it fails then replacing the package name will fix the test.
         $request = Request::create("/", "GET", array('package'=>'bolt/theme-2014'));
@@ -58,30 +56,26 @@ class ExtendControllerTest extends BoltUnitTest
 
         $response = $extend->installInfo($app, $request);
         $this->assertNotEmpty($response);
-        
+
         $request = Request::create("/", "GET", array('package'=>'bolt/theme-2014','version'=>'dev-master'));
         $extend->expects($this->any())
             ->method('packageInfo')
             ->will($this->returnValue(new Response('{"name":"bolt\/theme-2014","version":"unknown","type":"unknown","descrip":""}')));
-        
-        
+
+
         $response = $extend->packageInfo($app, $request);
         $this->assertNotEmpty($response);
         $content = json_decode($response->getContent());
         $this->assertAttributeNotEmpty('name', $content);
-        
+
         $request = Request::create("/");
 
         $extend->expects($this->any())
             ->method('check')
             ->will($this->returnValue(new Response('{"updates":[],"installs":[]}')));
-        
+
         $response = $extend->check($app, $request);
         $this->assertNotEmpty($response);
     }
-    
-
-
-
 
 }
