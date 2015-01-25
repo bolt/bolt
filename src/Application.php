98ac48b2ced8b19b91aee2a322a98e28e46db6c8
Bob den Otter <bobdenotter@gmail.com>
2015-01-25 13:02:04 +0100

    Set version to "2.1.0 alpha1"

--- src/Application.php
+++ src/Application.php
@@ -27,8 +27,8 @@ class Application extends Silex\Application
 
     public function __construct(array $values = array())
     {
-        $values['bolt_version'] = '2.0.5';
-        $values['bolt_name'] = '';
+        $values['bolt_version'] = '2.1.0';
+        $values['bolt_name'] = 'alpha1';
 
         parent::__construct($values);
 
