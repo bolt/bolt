1baae24852c64deed57eb59f1537f265d316d1a3
Ross Riley <riley.ross@gmail.com>
2015-01-30 16:55:56 +0000

    add more method tests

--- tests/Extensions/BaseExtensionTest.php
+++ tests/Extensions/BaseExtensionTest.php
@@ -34,5 +34,37 @@ class BaseExtensionTest extends BoltUnitTest
         $config = $app['extensions.testlocal']->getExtensionConfig();
         $this->assertNotEmpty($config);
     }
+    
+    public function testGetBasePath()
+    {
+        $app = $this->makeApp();
+        $ext = $this->getMock('Bolt\BaseExtension', null, array($app));
+        $this->assertNotEmpty(strpos($ext->getBasePath()->string(), 'MockObject'));
+    }
+    
+    public function testGetBaseUrl()
+    {
+        $app = $this->makeApp();
+        $ext = $this->getMock('Bolt\BaseExtension', null, array($app));
+        $this->assertEquals(0, strpos($ext->getBaseUrl(), '/extensions') );
+    }
+    
+    public function testGetComposerNameDefault()
+    {
+        $app = $this->makeApp();
+        $ext = $this->getMock('Bolt\BaseExtension', null, array($app));
+        $this->assertNull($ext->getComposerName());
+    }
+    
+    public function testGetComposerName()
+    {
+        $app = $this->makeApp();
+        $ext = $this->getMock('Bolt\BaseExtension', array('getComposerJSON'), array($app));
+        $ext->expects($this->any())
+            ->method('getComposerJSON')
+            ->will($this->returnValue(array('name'=>'valuefrommock')));
+            
+        $this->assertEquals('valuefrommock', $ext->getComposerName());
+    }
 
 }
