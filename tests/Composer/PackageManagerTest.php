6af81577b50775dcd9516420db03ba96ad57cdd5
Ross Riley <riley.ross@gmail.com>
2015-02-02 06:53:44 +0000

    test for PackageManager getRootDependencies

--- tests/Composer/PackageManagerTest.php
+++ tests/Composer/PackageManagerTest.php
@@ -2,10 +2,10 @@
 namespace Bolt\Tests\Composer;
 
 use Bolt\Tests\BoltUnitTest;
-use Bolt\Composer\CommandRunner;
+use Bolt\Composer\PackageManager;
 
 /**
- * Class to test src/Composer/CommandRunner.
+ * Class to test src/Composer/PackageManager.
  *
  * @author Ross Riley <riley.ross@gmail.com>
  *
@@ -16,5 +16,14 @@ class PackageManagerTest extends BoltUnitTest
     public function testConstruct()
     {
 
+    }
+    
+    public function testGetRootDependencies()
+    {
+        $app = $this->getApp();
+        $manager = new PackageManager($app);
+        $dependencies = $manager->getRootDependencies($app);
+
+        $this->assertTrue(is_array($dependencies));
     }
 }
