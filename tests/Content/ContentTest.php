dc57eb94285044ba3cc53ef3598e1841e5909a49
Gawain Lynch <gawain.lynch@gmail.com>
2015-02-02 08:37:11 +0000

    Add start of Bolt\Content tests

---
+++ tests/Content/ContentTest.php
@@ -0,0 +1,149 @@
+<?php
+namespace Bolt\tests\Content;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Content;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class to test src/Storage.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ContentTest extends BoltUnitTest
+{
+
+    public function testgetValues()
+    {
+        $app = $this->getApp();
+    }
+
+    public function testsetValues()
+    {
+    }
+
+    public function testsetValue()
+    {
+    }
+
+    public function testsetFromPost()
+    {
+    }
+
+    public function testsetContenttype()
+    {
+    }
+
+    public function testsetTaxonomy()
+    {
+    }
+
+    public function testsortTaxonomy()
+    {
+    }
+
+    public function testsetRelation()
+    {
+    }
+
+    public function testgetTaxonomyType()
+    {
+    }
+
+    public function testsetGroup()
+    {
+    }
+
+    public function testgetDecodedValue()
+    {
+    }
+
+    public function testpreParse()
+    {
+    }
+
+    public function testgetTemplateContext()
+    {
+    }
+
+    public function testget()
+    {
+    }
+
+    public function testgetTitle()
+    {
+    }
+
+    public function testgetTitleColumnName()
+    {
+    }
+
+    public function testgetImage()
+    {
+    }
+
+    public function testgetReference()
+    {
+    }
+
+    public function testeditlink()
+    {
+    }
+
+    public function testlink()
+    {
+    }
+
+    public function testprevious()
+    {
+    }
+
+    public function testnext()
+    {
+    }
+
+    public function testrelated()
+    {
+    }
+
+    public function testfieldinfo()
+    {
+    }
+
+    public function testfieldtype()
+    {
+    }
+
+    public function testexcerpt()
+    {
+    }
+
+    public function testrss_safe()
+    {
+    }
+
+    public function testweighSearchResult()
+    {
+    }
+
+    public function testgetSearchResultWeight()
+    {
+    }
+
+    public function testoffsetExists()
+    {
+    }
+
+    public function testoffsetGet()
+    {
+    }
+
+    public function testoffsetSet()
+    {
+    }
+
+    public function testoffsetUnset()
+    {
+    }
+}
