c7ec06a790f2a696b0850b0522c1124fbf557d41
rarila <magnet@gmx.net>
2015-02-05 12:09:51 +0100

    Remove '4' from comment (dangerous;-)

--- app/view/Gruntfile.js
+++ app/view/Gruntfile.js
@@ -200,7 +200,7 @@ module.exports = function(grunt) {
                     'lib/tmp/jquery-iframe-transport.min.js',                       //   2 kb
                     'lib/tmp/jquery-fileupload.min.js',                             //  15 kb
                     'lib/backbone/backbone-min.js',                                 //  20 kb
-                    'lib/bootstrap-sass.generated/bootstrap.min.js',                //   2 kb4
+                    'lib/bootstrap-sass.generated/bootstrap.min.js',                //   2 kb
                     'lib/select2-3.5.1/select2.min.js',                             //  66 kb
                     'node_modules/moment/min/moment.min.js',                        //  35 kb
                     'lib/modernizr-2.8.3/modernizr.custom.min.js'                   //   5 kb
