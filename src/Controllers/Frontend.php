75e05812dd16c079922274742e1e4f0730732466
Bob den Otter <bobdenotter@gmail.com>
2015-02-07 11:35:11 +0100

    Don't use (string) '-1' as separator. Fixes #2742

--- src/Controllers/Frontend.php
+++ src/Controllers/Frontend.php
@@ -131,7 +131,7 @@ class Frontend
             $app->abort(404, "Page $contenttypeslug/$slug not found.");
         }
 
-        $slug = $app['slugify']->slugify($slug, -1);
+        $slug = $app['slugify']->slugify($slug);
 
         // First, try to get it by slug.
         $content = $app['storage']->getContent($contenttype['slug'], array('slug' => $slug, 'returnsingle' => true));
