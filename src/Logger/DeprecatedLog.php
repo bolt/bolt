5c11ac32495d8f7f6b44de4974f4be50508d6619
Gawain Lynch <gawain.lynch@gmail.com>
2015-02-16 13:45:44 +0000

    Add service wrapper for old log service as still used by extensions

---
+++ src/Logger/DeprecatedLog.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bolt\Logger;
+
+use Silex\Application;
+
+/**
+ * Handler for deprecated log message.
+ *
+ * @deprecated since version 2.1
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class DeprecatedLog
+{
+    /**
+     * @var Silex\Application
+     */
+    private $app;
+
+    /**
+     * @param Application $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @param string $msg
+     */
+    public function add($msg)
+    {
+        $backtrace = debug_backtrace();
+        $backtrace = $backtrace[0];
+        $file = str_replace($this->app['resources']->getPath('root'), "", $backtrace['file']);
+
+        $this->app['logger.system']->addInfo($msg, array('event' => 'extension'));
+        $this->app['logger.system']->addWarning("[DEPRECATED]: Previous message logged using deprecated log service: {$file}::{$backtrace['line']}", array('event' => 'deprecated'));
+    }
+}
