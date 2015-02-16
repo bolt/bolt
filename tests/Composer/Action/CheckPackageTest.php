0977e1ea150ffa7276a685541d4d86357ef4182d
Ross Riley <riley.ross@gmail.com>
2015-02-16 14:47:06 +0000

    work on the CheckPackage test

---
+++ tests/Composer/Action/CheckPackageTest.php
@@ -0,0 +1,48 @@
+<?php
+namespace Bolt\Tests\Composer\Action;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Composer\Action\BoltExtendJson;
+use Bolt\Composer\Action\CheckPackage;
+use Bolt\Composer\PackageManager;
+
+
+/**
+ * Class to test src/Composer/Action/CheckPackage.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ *
+ */
+class CheckPackageTest extends BoltUnitTest
+{
+    
+
+    
+    public function testConstruct()
+    {
+        $app = $this->getApp();
+        $action = new CheckPackage($app);
+        $result = $action->execute();
+        $this->assertTrue(is_array($result['updates']));
+        $this->assertTrue(is_array($result['installs']));
+    }   
+    
+    public function testNewlyAdded()
+    {
+        $app = $this->getApp();
+        $options = $app['extend.manager']->getOptions();
+        $boltJson = new BoltExtendJson($options);
+        $json = $boltJson->updateJson($app);
+        $json['require'][] = array('gawain/clippy'=>'~2.0');
+        $boltJson->execute($options['composerjson'], $json);        
+        
+        $action = new CheckPackage($app);
+        $result = $action->execute();
+        print_r($result);
+    }    
+    
+
+    
+    
+    
+}
