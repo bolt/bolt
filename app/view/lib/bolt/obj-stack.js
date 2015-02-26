b99095cfef570c30117e63d6e271c34ff5369ec4
rarila <magnet@gmx.net>
2015-02-26 16:02:42 +0100

    Remove debug alert

--- app/view/lib/bolt/obj-stack.js
+++ app/view/lib/bolt/obj-stack.js
@@ -17,7 +17,6 @@ var Stack = Backbone.Model.extend({
         // In the modal dialog, to navigate folders..
         $('#selectImageModal-stack').on('click', '.folder', function (e) {
             e.preventDefault();
-            alert('hoi');
             $('#selectImageModal-stack .modal-content').load($(this).attr('href'));
         });
 
