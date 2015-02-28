2046bd42f4501870b241505330136b1d701f2954
Carson Full <carsonfull@gmail.com>
2015-02-27 23:38:13 -0600

    formatting

--- src/Provider/RoutingServiceProvider.php
+++ src/Provider/RoutingServiceProvider.php
@@ -7,8 +7,8 @@ use Bolt\Routing\UrlMatcher;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
-class RoutingServiceProvider implements ServiceProviderInterface {
-
+class RoutingServiceProvider implements ServiceProviderInterface
+{
     public function register(Application $app)
     {
         $app['url_matcher'] = $app->share(
