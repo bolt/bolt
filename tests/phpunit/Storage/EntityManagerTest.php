3c052feb66d61915dd1920d2aa87030686bf4c14
Ross Riley <riley.ross@gmail.com>
2015-03-06 17:45:20 +0000

    setup test for new EntityManager class

---
+++ tests/phpunit/Storage/EntityManagerTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bolt\Tests\Storage;
+
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Storage\EntityManager;
+
+/**
+ * Class to test src/Storage/EntityManager.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+class EntityManagerTest extends BoltUnitTest
+{
+    public function testConnect()
+    {
+        $app = $this->getApp();
+        $em = new EntityManager($app['db'], $app['db.event_manager']);
+        $this->assertSame($app['db'], \PHPUnit_Framework_Assert::readAttribute($em, 'conn'));
+        $this->assertSame($app['db.event_manager'], \PHPUnit_Framework_Assert::readAttribute($em, 'eventManager'));
+
+        
+        
+    }
+    
+    public function testCreateQueryBuilder()
+    {
+        $app = $this->getApp();
+        $em = new EntityManager($app['db'], $app['db.event_manager']);
+        
+        $qb = $em->createQueryBuilder();
+        $this->assertInstanceOf('Doctrine\DBAL\Query\QueryBuilder', $qb);
+    }
+    
+}
