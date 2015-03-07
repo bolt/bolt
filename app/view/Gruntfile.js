c545201a10f535bbf7ef6724da845a218acd35ae
rarila <magnet@gmx.net>
2015-03-07 22:30:11 +0100

    Move cssmin task to own file

--- app/view/Gruntfile.js
+++ app/view/Gruntfile.js
@@ -29,27 +29,6 @@ module.exports = function(grunt) {
         ],
 
         /*
-         * CSSMIN: Compress CSS files
-         */
-        cssmin: {
-            installLibCss: {
-                options: {
-                    compatibility: 'ie8',
-                    relativeTo: 'css/',
-                    target: 'css/'
-                },
-                files: {
-                    'css/lib.css': [
-                        'lib/jquery-ui-1.10.3/jquery-ui.custom.min.css',            // 20 kb
-                        'bower_components/magnific-popup/dist/magnific-popup.css',  //  9 kb
-                        'lib/select2/select2.css',                                  // 19 kb
-                        'lib/jquery-fileupload/jquery-fileupload-ui.css'            //  2 kb
-                    ]
-                }
-            }
-        },
-
-        /*
          * UGLIFY: Minify files with UglifyJS
          */
         uglify: {
