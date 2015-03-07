1b8587c35959661596d9e0cee17b1b58183a769f
rarila <magnet@gmx.net>
2015-03-08 00:03:30 +0100

    Rename watch target

--- app/view/grunt/watch.js
+++ app/view/grunt/watch.js
@@ -8,7 +8,7 @@ module.exports = function(grunt, options) {
             livereload: true
         },
 
-        sass: {
+        boltCss: {
             files: [
                 'sass/**/*.scss'
             ],
