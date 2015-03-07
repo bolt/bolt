052f06e53b43a58225a6ea23125750114138dee1
Ross Riley <riley.ross@gmail.com>
2015-03-07 11:04:44 +0000

    simplify the save and delete interface for entity manager

--- src/Storage/EntityManager.php
+++ src/Storage/EntityManager.php
@@ -56,18 +56,15 @@ class EntityManager
     }
 
     /**
-     * Tells the ObjectManager to make an instance managed and persistent.
      *
-     * The object will be entered into the database as a result of the flush operation.
+     * The object will be entered into the database as a result of this operation.
      *
-     * NOTE: The persist operation always considers objects that are not yet known to
-     * this ObjectManager as NEW. Do not pass detached objects to the persist operation.
      *
-     * @param object $object The instance to make managed and persistent.
+     * @param object $object The instance to persist to storage.
      *
-     * @return void
+     * @return boolean
      */
-    public function persist($object)
+    public function save($object)
     {
         
     }
@@ -75,29 +72,17 @@ class EntityManager
     /**
      * Removes an object instance.
      *
-     * A removed object will be removed from the database as a result of the flush operation.
+     * Passed in object will be removed from the database as a result of this operation.
      *
      * @param object $object The object instance to remove.
      *
-     * @return void
+     * @return boolean
      */
-    public function remove($object)
+    public function delete($object)
     {
         
     }
 
-
-    /**
-     * Flushes all changes to objects that have been queued up to now to the database.
-     * This effectively synchronizes the in-memory state of managed objects with the
-     * database.
-     *
-     * @return void
-     */
-    public function flush()
-    {
-        
-    }
 
     /**
      * Gets the repository for a class.
