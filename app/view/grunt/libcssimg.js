647a2f64516658506dc9ade63966a701e2c5754f
rarila <magnet@gmx.net>
2015-03-07 20:28:25 +0100

    Put custom tasks into own folder/file

---
+++ app/view/grunt/libcssimg.js
@@ -0,0 +1,44 @@
+/*
+ * Copy lib images & rebase urls
+ */
+module.exports = function (grunt) {
+    grunt.registerTask('libcssimg', 'Copy lib images & rebase urls', function () {
+        var css = grunt.file.read('css/lib.css'),
+            out = css,
+            urls = /url\((['"]?)(.+?)\1\)/g,
+            repl = {
+                'jquery-ui':    /^\.\.\/lib\/jquery-ui-.+?\/images\/ui-/,
+                'select2':      /^\.\.\/lib\/select2\//,
+                'jquery-upl':   /^\.\.\/lib\/jquery-fileupload\/img\//
+            },
+            done = {},
+            url,
+            dest,
+            to;
+
+        grunt.file.mkdir('img/lib');
+
+        while ((url = urls.exec(css)) !== null) {
+            for (to in repl) {
+                dest = url[2].replace(repl[to], 'img/lib/' + to + '-');
+                if (dest !== url[2]) {
+                    if (!done[dest]) {
+                        grunt.file.copy(url[2].replace(/^\.\.\/lib\//, 'lib/'), dest);
+                        out = out.replace(
+                            new RegExp(url[0].replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), 'g'),
+                            'url(' + url[1] + '../' + dest + url[1] + ')'
+                        );
+                        done[dest] = 1;
+                    }
+                    break;
+                } else {
+                    dest = false;
+                }
+            }
+            if (!dest && !url[2].match(/^data:/)) {
+                grunt.fail.warn('URL not handled: ' + url[2]);
+            }
+        }
+        grunt.file.write('css/lib.css', out);
+    });
+};
