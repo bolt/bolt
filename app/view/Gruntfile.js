1448b69e79a7b88bcf5f0468bc4fef1628ded647
rarila <magnet@gmx.net>
2015-03-07 22:19:46 +0100

    Move sass task to own file

--- app/view/Gruntfile.js
+++ app/view/Gruntfile.js
@@ -64,33 +64,6 @@ module.exports = function(grunt) {
         },
 
         /*
-         * SASS: Compile Sass to CSS
-         */
-        sass: {
-            boltCss: {
-                options: {
-                    outputStyle: 'compressed',
-                    includePaths: [
-                        'node_modules/bootstrap-sass/assets/stylesheets/',
-                        'node_modules/font-awesome/scss/'
-                    ],
-                    lineNumbers: false,
-                    unixNewlines: true,
-                    banner: "/**\n" +
-                            " * These are Bolt's COMPILED CSS files!\n" +
-                            " * Do not edit these files, because all changes will be lost.\n" +
-                            " * You can edit ../scss/app.scss & ../scss/app-old-ie.scss, and run 'grunt' to generate this file.\n" +
-                            " */\n",
-                    precision: 5
-                },
-                files: {
-                    'css/bolt-old-ie.css': 'sass/app-old-ie.scss',
-                    'css/bolt.css': 'sass/app.scss'
-                }
-            }
-        },
-
-        /*
          * EOL: Convert line endings
          */
         eol: {
