b9a32c97252c05c815c5429832b0e5e09c6a2252
Ross Riley <riley.ross@gmail.com>
2015-03-08 10:09:43 +0000

    test listeners are called correctly on db queries

--- tests/phpunit/Storage/RespositoryTest.php
+++ tests/phpunit/Storage/RespositoryTest.php
@@ -15,6 +15,8 @@ use Bolt\Storage\Entity\AuthToken;
 class RepositoryTest extends BoltUnitTest
 {
     
+    public $eventCount = array();
+    
     public function testConstruct()
     {
         $app = $this->getApp();
@@ -68,7 +70,7 @@ class RepositoryTest extends BoltUnitTest
         $em = new EntityManager($app['db'], $app['dispatcher']);
         $repo = new Repository($em, $entityName);
         $result = $repo->findAll();
-        
+                
         $this->assertTrue(is_array($result));
         foreach ($result as $obj) {
             $this->assertInstanceOf($entityName, $obj); 
@@ -83,7 +85,7 @@ class RepositoryTest extends BoltUnitTest
         $em = new EntityManager($app['db'], $app['dispatcher']);
         $repo = new Repository($em, $entityName);
         $result = $repo->findBy(array('id'=>1));
-        
+                
         $this->assertTrue(is_array($result));
         $this->assertInstanceOf($entityName, $result[0]); 
 
@@ -93,19 +95,22 @@ class RepositoryTest extends BoltUnitTest
     {
         $app = $this->getApp();
         $entityName = 'Bolt\Entity\Users';
+        $this->runListenCount($app, 'preHydrate');
         
         $em = new EntityManager($app['db'], $app['dispatcher']);
         $repo = new Repository($em, $entityName);
         $result = $repo->findOneBy(array('id'=>1));
-                
-        $this->assertInstanceOf($entityName, $result); 
 
+        $this->assertInstanceOf($entityName, $result); 
+        $this->assertEquals(1, $this->eventCount['preHydrate']);
     }
     
     public function testInsert()
     {
         $app = $this->getApp();
         $entityName = 'Bolt\Entity\Users';
+        $this->runListenCount($app, 'preSave');
+        $this->runListenCount($app, 'postSave');
         
         $em = new EntityManager($app['db'], $app['dispatcher']);
         $repo = new Repository($em, $entityName);
@@ -122,25 +127,66 @@ class RepositoryTest extends BoltUnitTest
         $this->assertEquals(1, $repo->save($entity));
         
         $result = $repo->findOneBy(array('displayname'=>'Test User'));
+        
         $this->assertInstanceOf($entityName, $result);
         $this->assertEquals('test', $result->getUsername());
+        $this->assertEquals(1, $this->eventCount['preSave']);
+        $this->assertEquals(1, $this->eventCount['postSave']);
                         
     }
     
     public function testUpdate()
     {
         $app = $this->getApp();
-        $entityName = 'Bolt\Entity\Users';
+        $entityName = 'Bolt\Entity\Users';        
+        $this->runListenCount($app, 'preSave');
+        $this->runListenCount($app, 'postSave');
         
         $em = new EntityManager($app['db'], $app['dispatcher']);
         $repo = new Repository($em, $entityName);
         $existing = $repo->findOneBy(array('displayname'=>'Test User'));
+
         $existing->setUsername('testupdated');
         $em->save($existing);
         
         $existing2 = $repo->findOneBy(array('displayname'=>'Test User'));
         $this->assertEquals('testupdated', $existing2->getUsername());
+        $this->assertEquals(1, $this->eventCount['preSave']);
+        $this->assertEquals(1, $this->eventCount['postSave']);
     }
+    
+    public function testDelete()
+    {
+        $app = $this->getApp();
+        $entityName = 'Bolt\Entity\Users';
+        $this->runListenCount($app, 'preDelete');
+        $this->runListenCount($app, 'postDelete');
+        
+        $em = new EntityManager($app['db'], $app['dispatcher']);
+        $repo = new Repository($em, $entityName);
+        $existing = $repo->findOneBy(array('displayname'=>'Test User'));
+        
+        $result = $repo->delete($existing);
+        $confirm = $repo->findOneBy(array('displayname'=>'Test User'));
+        $this->assertFalse($confirm);
+        
+        $this->assertEquals(1, $this->eventCount['preDelete']);
+        $this->assertEquals(1, $this->eventCount['postDelete']);
+    }
+    
+    
+    protected function runListenCount($app, $event)
+    {
+        $this->eventCount[$event] = 0;
+        $phpunit = $this;
+        $app['dispatcher']->addListener($event, function() use($count, $phpunit, $event){
+           $count ++;
+           $phpunit->eventCount[$event] = $count;
+        });
+        
+
+    }
+    
     
 
     
