1f42d0451479e20a6fc1f4119536c9075d321283
rarila <magnet@gmx.net>
2015-03-09 22:28:54 +0100

    Make usage of grunt templates in cssmin task

--- app/src/grunt/cssmin.js
+++ app/src/grunt/cssmin.js
@@ -9,11 +9,11 @@ module.exports = {
             target: 'css/'
         },
         files: {
-            'css/lib.css': [
-                'lib/jquery-ui-1.10.3/jquery-ui.custom.min.css',            // 20 kb
-                'bower_components/magnific-popup/dist/magnific-popup.css',  //  9 kb
-                'lib/select2/select2.css',                                  // 19 kb
-                'lib/jquery-fileupload/jquery-fileupload-ui.css'            //  2 kb
+            '<%= path.dest.css %>/lib.css': [
+                '<%= path.src.lib %>/jquery-ui-1.10.3/jquery-ui.custom.min.css',            // 20 kb
+                '<%= path.src.bower %>/magnific-popup/dist/magnific-popup.css',  //  9 kb
+                '<%= path.src.lib %>/select2/select2.css',                                  // 19 kb
+                '<%= path.src.lib %>/jquery-fileupload/jquery-fileupload-ui.css'            //  2 kb
             ]
         }
     }
