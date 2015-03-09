900c8d1233568fdd0f3f1f2f32a2f1141dc099e0
rarila <magnet@gmx.net>
2015-03-10 00:50:40 +0100

    Convert sass task to object style

--- app/src/grunt/sass.js
+++ app/src/grunt/sass.js
@@ -1,24 +1,22 @@
 /*
  * SASS: Compile Sass to CSS
  */
-module.exports = function(grunt, options) {
-    return {
-        boltCss: {
-            options: {
-                outputStyle: 'compressed',
-                includePaths: [
-                    '<%= path.src.node %>/bootstrap-sass/assets/stylesheets/',
-                    '<%= path.src.node %>/font-awesome/scss/'
-                ],
-                lineNumbers: false,
-                unixNewlines: true,
-                banner: options.banner.boltCss,
-                precision: 5
-            },
-            files: {
-                '<%= path.dest.css %>/bolt-old-ie.css': '<%= path.src.sass %>/app-old-ie.scss',
-                '<%= path.dest.css %>/bolt.css': '<%= path.src.sass %>/app.scss'
-            }
+module.exports = {
+    boltCss: {
+        options: {
+            outputStyle: 'compressed',
+            includePaths: [
+                '<%= path.src.node %>/bootstrap-sass/assets/stylesheets/',
+                '<%= path.src.node %>/font-awesome/scss/'
+            ],
+            lineNumbers: false,
+            unixNewlines: true,
+            banner: '<%= banner.boltCss %>',
+            precision: 5
+        },
+        files: {
+            '<%= path.dest.css %>/bolt-old-ie.css': '<%= path.src.sass %>/app-old-ie.scss',
+            '<%= path.dest.css %>/bolt.css': '<%= path.src.sass %>/app.scss'
         }
-    };
+    }
 };
