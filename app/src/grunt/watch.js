10fa355d8c0064ecef3d2fea2cef1b8de4d0aa0b
rarila <magnet@gmx.net>
2015-03-10 00:56:04 +0100

    Convert watch task to object style

--- app/src/grunt/watch.js
+++ app/src/grunt/watch.js
@@ -1,37 +1,35 @@
 /*
  * WATCH: Run predefined tasks whenever watched file patterns are added, changed or deleted
  */
-module.exports = function(grunt, options) {
-    return {
+module.exports = {
+    options: {
+        spawn: false,
+        livereload: true
+    },
+
+    boltCss: {
+        files: [
+            '<%= path.src.sass %>/**/*.scss'
+        ],
+        tasks: [
+            'sass:boltCss'
+        ]
+    },
+
+    boltJs: {
+        files: '<%= files.boltJs %>',
+        tasks: [
+            'jshint:boltJs',
+            'uglify:boltJs'
+        ]
+    },
+
+    gruntfile: {
+        files: [
+            'Gruntfile.js'
+        ],
         options: {
-            spawn: false,
-            livereload: true
-        },
-
-        boltCss: {
-            files: [
-                '<%= path.src.sass %>/**/*.scss'
-            ],
-            tasks: [
-                'sass:boltCss'
-            ]
-        },
-
-        boltJs: {
-            files: options.files.boltJs,
-            tasks: [
-                'jshint:boltJs',
-                'uglify:boltJs'
-            ]
-        },
-
-        gruntfile: {
-            files: [
-                'Gruntfile.js'
-            ],
-            options: {
-                reload: true
-            }
+            reload: true
         }
-    };
+    }
 };
