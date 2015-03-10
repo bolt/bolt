829aaa30b5ab764c90d9be38d3c6a5c05ec68af8
rarila <magnet@gmx.net>
2015-03-10 01:59:36 +0100

    Fix libcssimg task and add verbose output

--- app/src/grunt/libcssimg.js
+++ app/src/grunt/libcssimg.js
@@ -3,31 +3,47 @@
  */
 module.exports = function (grunt, option) {
     grunt.registerTask('libcssimg', 'Copy lib images & rebase urls', function () {
-        var css = grunt.file.read(option.path.dest.css + '/lib.css'),
-            out = css,
+        var pathLibCss = option.path.dest.css + '/lib.css',
+            pathDestImg = option.path.dest.img + '/lib',
             urls = /url\((['"]?)(.+?)\1\)/g,
             repl = {
-                'jquery-ui':    /^\.\.\/lib\/jquery-ui-.+?\/images\/ui-/,
-                'select2':      /^\.\.\/lib\/select2\//,
-                'jquery-upl':   /^\.\.\/lib\/jquery-fileupload\/img\//
+                'jquery-ui':    /^lib\/jquery-ui-.+?\/images\/ui-/,
+                'select2':      /^lib\/select2\//,
+                'jquery-upl':   /^lib\/jquery-fileupload\/img\//
             },
             done = {},
+            css,
+            out,
             url,
             dest,
-            to;
+            to,
+            newurl;
 
-        grunt.file.mkdir(option.path.dest.img + '/lib');
+        grunt.verbose.writeln('Read css: ' + pathLibCss);
+        css = grunt.file.read(pathLibCss);
+        out = css;
+
+        grunt.verbose.writeln('Create dir: ' + pathDestImg);
+        grunt.file.mkdir(pathDestImg);
 
         while ((url = urls.exec(css)) !== null) {
             for (to in repl) {
-                dest = url[2].replace(repl[to], option.path.dest.img + '/lib/' + to + '-');
+                dest = url[2].replace(repl[to], pathDestImg + '/' + to + '-');
                 if (dest !== url[2]) {
                     if (!done[dest]) {
-                        grunt.file.copy(url[2].replace(/^\.\.\/lib\//, 'lib/'), dest);
-                        out = out.replace(
-                            new RegExp(url[0].replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), 'g'),
-                            'url(' + url[1] + '../' + dest + url[1] + ')'
+                        grunt.verbose.writeln(
+                            '- Copy: ' + url[2] + '\n' +
+                            '    To: ' + dest
                         );
+                        grunt.file.copy(url[2].replace(/^\.\.\/lib\//, 'lib/'), dest);
+
+                        newurl = 'url(' + url[1] + dest.slice(option.path.dest.base.length + 1) + url[1] + ')';
+                        grunt.verbose.writeln(
+                            '  Repl: ' + url[0] + '\n' +
+                            '  With: ' + newurl
+                        );
+                        out = out.replace(new RegExp(url[0].replace(/[.*+?^${}()|[\]\\]/g, "\\$&"), 'g'), newurl);
+
                         done[dest] = 1;
                     }
                     break;
@@ -39,6 +55,8 @@ module.exports = function (grunt, option) {
                 grunt.fail.warn('URL not handled: ' + url[2]);
             }
         }
-        grunt.file.write(option.path.dest.css + '/lib.css', out);
+
+        grunt.verbose.writeln('Write: ' + pathLibCss);
+        grunt.file.write(pathLibCss, out);
     });
 };
