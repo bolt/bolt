597da3e55d401a05d544c6b184d7b40c1931562e
Adam Cooper <adam@networkpie.co.uk>
2015-03-12 09:38:28 +0000

    Refactoring into some slightly more manageable methods.

--- src/Menu/Builder.php
+++ src/Menu/Builder.php
@@ -75,60 +75,95 @@ class Builder
      */
     private function menuHelper($item)
     {
-        if (isset($item['submenu']) && is_array($item['submenu'])) {
+        // recurse into submenu's
+        if (isset($item['submenu']) && is_array($item['submenu']))
             $item['submenu'] = $this->menuHelper($item['submenu']);
-        }
 
-        if (isset($item['path']) && $item['path'] == "homepage") {
-            $item['link'] = $this->app['paths']['root'];
-        } elseif (isset($item['route'])) {
-            $param = empty($item['param']) ? array() : $item['param'];
-            $add = empty($item['add']) ? '' : $item['add'];
+        if (isset($item['route'])) {
+
+            $param = !empty($item['param']) ?: array();
+            $add = !empty($item['add']) ?: '';
 
             $item['link'] = Lib::path($item['route'], $param, $add);
-        } elseif (isset($item['path']) && !isset($item['link'])) {
-            if (preg_match('#^(https?://|//)#i', $item['path'])) {
-                // We have a mistakenly placed URL, allow it but log it.
-                $item['link'] = $item['path'];
-                $this->app['logger.system']->error(Trans::__('Invalid menu path (%PATH%) set in menu.yml. Probably should be a link: instead!', array('%PATH%' => $item['path'])), array('event' => 'config'));
-            } else {
-                // Get a copy of the path minus trainling/leading slash
-                $path = ltrim(rtrim($item['path'], '/'), '/');
 
-                // Pre-set our link in case the match() throws an exception
-                $item['link'] = '/' . $path;
+        } elseif (isset($item['path'])) {
 
-                try {
-                    // See if we have a 'content/id' or 'content/slug' path
-                    if (preg_match('#^([a-z0-9_-]+)/([a-z0-9_-]+)$#i', $path)) {
+            $item = $this->resolvePathToContent($item);
+        }
 
-                        // Determine if the provided path first matches any routes
-                        // that we have, this will catch any valid configured
-                        // contenttype slug and record combination, or throw a
-                        // ResourceNotFoundException exception otherwise
-                        $this->app['url_matcher']->match('/' . $path);
+        return $item;
+    }
 
-                        // If we found a valid routing match then we're still here,
-                        // attempt to retrive the actual record.
-                        $content = $this->app['storage']->getContent($path);
-                        if ($content instanceof \Bolt\Content) {
-                            if (empty($item['label'])) {
-                                $item['label'] = !empty($content->values['title']) ? $content->values['title'] : "";
-                            }
+    private function resolvePathToContent($item)
+    {
+        if ($item['path'] === 'homepage') {
+            $item['link'] = $this->app['resources']->getUrl('root');
 
-                            if (empty($item['title'])) {
-                                $item['title'] = !empty($content->values['subtitle']) ? $content->values['subtitle'] : "";
-                            }
+            return $item;
+        }
 
-                            $item['link'] = $content->link();
-                        }
-                    } else {
-                        $item['link'] = '/' . $path;
-                    }
-                } catch (ResourceNotFoundException $e) {
-                    $this->app['logger.system']->error(Trans::__('Invalid menu path (%PATH%) set in menu.yml. Does not match any configured contenttypes or routes.', array('%PATH%' => $item['path'])), array('event' => 'config'));
-                }
+        // We have a mistakenly placed URL, allow it but log it.
+        if (preg_match('#^(https?://|//)#i', $item['path'])) {
+            $item['link'] = $item['path'];
+            $this->app['logger.system']->error(
+                Trans::__(
+                    'Invalid menu path (%PATH%) set in menu.yml. Probably should be a link: instead!',
+                    array('%PATH%' => $item['path'])
+                ),
+                array('event' => 'config')
+            );
+
+            return $item;
+        }
+
+        // Get a copy of the path minus trailing/leading slash
+        $path = ltrim(rtrim($item['path'], '/'), '/');
+
+        // Pre-set our link in case the match() throws an exception
+        $item['link'] = '/' . $path;
+
+        try {
+            // See if we have a 'content/id' or 'content/slug' path
+            if (preg_match('#^([a-z0-9_-]+)/([a-z0-9_-]+)$#i', $path)) {
+
+                // Determine if the provided path first matches any routes
+                // that we have, this will catch any valid configured
+                // contenttype slug and record combination, or throw a
+                // ResourceNotFoundException exception otherwise
+                $this->app['url_matcher']->match('/' . $path);
+
+                // If we found a valid routing match then we're still here,
+                // attempt to retrive the actual record and use its values.
+                $this->populateItemFromRecord($item, $path);
             }
+        } catch (ResourceNotFoundException $e) {
+            $this->app['logger.system']->error(
+                Trans::__(
+                    'Invalid menu path (%PATH%) set in menu.yml. Does not match any configured contenttypes or routes.',
+                    array('%PATH%' => $item['path'])
+                ),
+                array('event' => 'config')
+            );
+        }
+
+        return $item;
+    }
+
+    private function populateItemFromRecord($item, $path)
+    {
+        /** @var \Bolt\Content $content */
+        $content = $this->app['storage']->getContent($path);
+
+        if ($content !== false) {
+            if (empty($item['label'])) {
+                $item['label'] = !empty($content->values['title']) ?: '';
+            }
+
+            if (empty($item['title'])) {
+                $item['title'] = !empty($content->values['subtitle']) ?: '';
+            }
+
+            $item['link'] = $content->link();
         }
 
         return $item;
