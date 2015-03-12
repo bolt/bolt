3af67197ca691faa6c308b0571dde2fbadab9654
rarila <magnet@gmx.net>
2015-03-12 02:26:36 +0100

    Refinement of the Files mixin

--- app/src/js/modules/files.js
+++ app/src/js/modules/files.js
@@ -23,7 +23,7 @@
      * @param {string} oldName - Old name of the file to be renamed.
      * @param {Object} element - The object that calls this function, usually of type HTMLAnchorElement
      */
-    files.rename = function(namespace, parentPath, oldName, element)
+    files.rename = function (namespace, parentPath, oldName, element)
     {
         var newName = window.prompt(bolt.data('files.rename_msg'), oldName);
 
@@ -54,7 +54,7 @@
      * @param {string} filename - The filename.
      * @param {Object} element - The object that calls this function, usually of type HTMLAnchorElement
      */
-    files.delete = function(namespace, filename, element)
+    files.delete = function (namespace, filename, element)
     {
         if (confirm(bolt.data('files.delete_msg', {'%FILENAME%': filename}))) {
             $.ajax({
@@ -85,7 +85,7 @@
      * @param {string} namespace - The namespace.
      * @param {string} filename - The filename.
      */
-    files.duplicate = function(namespace, filename) {
+    files.duplicate = function (namespace, filename) {
         $.ajax({
             url: bolt.conf('paths.async') + 'duplicatefile',
             type: 'POST',
