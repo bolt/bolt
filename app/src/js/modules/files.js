cb041a5ef8174e1fb3f1b696bf044af51294361b
rarila <magnet@gmx.net>
2015-03-12 01:06:41 +0100

    Refinement of the Files mixin

--- app/src/js/modules/files.js
+++ app/src/js/modules/files.js
@@ -6,11 +6,14 @@
  * @type {function}
  * @mixin
  */
-var BoltFiles = (function (bolt, $) {
+(function (bolt, $) {
     /*
      * BoltFiles mixin
+     *
+     * @private
+     * @type {Object}
      */
-    bolt.files = {};
+    var files = {};
 
     /**
      * Rename a file.
@@ -20,7 +23,7 @@ var BoltFiles = (function (bolt, $) {
      * @param {string} oldName - Old name of the file to be renamed.
      * @param {Object} element - The object that calls this function, usually of type HTMLAnchorElement
      */
-    bolt.files.rename = function(namespace, parentPath, oldName, element)
+    files.rename = function(namespace, parentPath, oldName, element)
     {
         var newName = window.prompt(bolt.data('files.rename_msg'), oldName);
 
@@ -47,11 +50,11 @@ var BoltFiles = (function (bolt, $) {
     /**
      * Delete a file from the server.
      *
-     * @param {string} - The namespace.
-     * @param {string} - The filename.
-     * @param {Object} - The object that calls this function, usually of type HTMLAnchorElement
+     * @param {string} namespace - The namespace.
+     * @param {string} filename - The filename.
+     * @param {Object} element - The object that calls this function, usually of type HTMLAnchorElement
      */
-    bolt.files.delete = function(namespace, filename, element)
+    files.delete = function(namespace, filename, element)
     {
         if (confirm(bolt.data('files.delete_msg', {'%FILENAME%': filename}))) {
             $.ajax({
@@ -79,10 +82,10 @@ var BoltFiles = (function (bolt, $) {
     /**
      * Delete a file from the server.
      *
-     * @param {string} - The namespace.
-     * @param {string} - The filename.
+     * @param {string} namespace - The namespace.
+     * @param {string} filename - The filename.
      */
-    bolt.files.duplicate = function(namespace, filename) {
+    files.duplicate = function(namespace, filename) {
         $.ajax({
             url: bolt.conf('paths.async') + 'duplicatefile',
             type: 'POST',
@@ -98,6 +101,8 @@ var BoltFiles = (function (bolt, $) {
             }
         });
     };
+
+    bolt.files = files;
 
     return bolt;
 })(Bolt || {}, jQuery);
