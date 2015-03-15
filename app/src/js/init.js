79b7bed950649e68712684ea6a4b3255ac3c2cd1
rarila <magnet@gmx.net>
2015-03-15 04:30:45 +0100

    Remove now unused shortcut code

--- app/src/js/init.js
+++ app/src/js/init.js
@@ -324,42 +324,6 @@ var init = {
         });
     },
 
-    /**
-     * Initialize keyboard shortcuts:
-     * - Click 'save' in Edit content screen.
-     * - Click 'save' in "edit file" screen.
-     *
-     * @returns {undefined}
-     */
-    keyboardShortcuts: function () {
-        function confirmExit() {
-            if ($('form').hasChanged()) {
-                return "You have unfinished changes on this page. " +
-                    "If you continue without saving, you will lose these changes.";
-            }
-        }
-
-        // We're on a regular 'edit content' page, if we have a sidebarsavecontinuebutton.
-        // If we're on an 'edit file' screen,  we have a #saveeditfile
-        if ($('#sidebarsavecontinuebutton').is('*') || $('#saveeditfile').is('*')) {
-
-            // Bind ctrl-s and meta-s for saving..
-            $('body, input').bind('keydown.ctrl_s keydown.meta_s', function (event) {
-                event.preventDefault();
-                $('form').watchChanges();
-                $('#sidebarsavecontinuebutton, #saveeditfile').trigger('click');
-            });
-
-            // Initialize watching for changes on "the form".
-            window.setTimeout(function () {
-                $('form').watchChanges();
-            }, 1000);
-
-            // Initialize handler for 'closing window'
-            window.onbeforeunload = confirmExit;
-        }
-    },
-
     /*
      * Initialize the Magnific popup shizzle. Fancybox is still here as a trigger, for backwards compatibility.
      */
