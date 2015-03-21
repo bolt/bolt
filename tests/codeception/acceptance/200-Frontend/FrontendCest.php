09dacefb9d9b593ec3712110f420de6bd9ed6824
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-21 18:17:22 +0000

    Check the about page and pagebind route

--- tests/codeception/acceptance/200-Frontend/FrontendCest.php
+++ tests/codeception/acceptance/200-Frontend/FrontendCest.php
@@ -17,7 +17,6 @@ class FrontendCest
      */
     public function _before(\AcceptanceTester $I)
     {
-        $this->user = Fixtures::get('users');
     }
 
     /**
@@ -28,13 +27,13 @@ class FrontendCest
     }
 
     /**
-     * Create the first site user
+     * Check the homepage
      *
      * @param \AcceptanceTester $I
      */
-    public function createFirstUserTest(\AcceptanceTester $I)
+    public function checkFrontPageTest(\AcceptanceTester $I)
     {
-        $I->wantTo('see that the frontpage works');
+        $I->wantTo('see that the homepage works');
 
         $I->amOnPage('');
 
@@ -42,5 +41,22 @@ class FrontendCest
         $I->see('Recent Pages');
         $I->see('Recent Entries');
         $I->see('Recent Showcases');
+        $I->dontSee('Recent Typewriters');
+    }
+
+    /**
+     * Check the about page and pagebind route
+     *
+     * @param \AcceptanceTester $I
+     */
+    public function checkAboutPageTest(\AcceptanceTester $I)
+    {
+        $I->wantTo('see that the about page and pagebind route works');
+
+        $I->amOnPage('about');
+
+        $I->see("Easy for editors, and a developer's dream cms", 'h1');
+        $I->see('Bolt is an open source Content Management Tool', 'h2');
+        $I->see('The fully responsive dashboard works on desktop computers, laptops, tablets and mobile phones alike, so you can control anything from wherever you are.', 'p');
     }
 }
