a305e58bd8d21536250cdc3538eb0a8be07a6160
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-22 15:19:27 +0000

    PHPDoc touch up

--- src/Helpers/Menu.php
+++ src/Helpers/Menu.php
@@ -17,14 +17,14 @@ class Menu
     private $resolved;
 
     /**
-     * @param string $name The name of the menu
-     * @param array $menu The items that the menu contains
-     * @param bool $resolved
+     * @param string $name     The name of the menu
+     * @param array  $menu     The items that the menu contains
+     * @param bool   $resolved
      */
     public function __construct($name, array $menu, $resolved = false)
     {
-        $this->name = $name;
-        $this->menu = $menu;
+        $this->name     = $name;
+        $this->menu     = $menu;
         $this->resolved = $resolved;
     }
 
