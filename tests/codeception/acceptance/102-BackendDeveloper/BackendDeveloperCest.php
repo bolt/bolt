a2e0d7fb27650036b0bac4cced6b06446756463a
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-22 08:37:21 +0000

    Better naming

--- tests/codeception/acceptance/102-BackendDeveloper/BackendDeveloperCest.php
+++ tests/codeception/acceptance/102-BackendDeveloper/BackendDeveloperCest.php
@@ -1,6 +1,7 @@
 <?php
 
 use Codeception\Util\Fixtures;
+use Codeception\Util\Locator;
 
 /**
  * Backend 'developer' tests
@@ -28,7 +29,7 @@ class BackendDeveloperCest
     }
 
     /**
-     * Login as the editor user
+     * Login as the developer user.
      *
      * @param \AcceptanceTester $I
      */
@@ -37,5 +38,32 @@ class BackendDeveloperCest
         $I->wantTo("Login as 'developer' user");
         $I->loginAs($this->user['developer']);
         $I->see('Dashboard');
+    }
+
+    /**
+     * Test the file management interface
+     *
+     * @param \AcceptanceTester $I
+     */
+    public function fileManagementUploadedFilesTest(\AcceptanceTester $I)
+    {
+        $I->wantTo("Use the file management 'uploaded files' interface as the 'developer' user");
+        $I->loginAs($this->user['developer']);
+
+        $I->amOnPage('bolt/files');
+
+        // $soft has the string with soft hyphens U+00AD
+        $soft = 'a­g­r­i­c­u­l­t­u­r­e­-­c­e­r­e­a­l­s­-­f­i­e­l­d­-­6­2­1­.j­p­g';
+        $file = 'agriculture-cereals-field-621.jpg';
+
+        $I->see('Create folder', Locator::find('a', ['href' => '#']));
+        $I->see($soft, Locator::href("/thumbs/1000x1000r/$file"));
+
+        $I->see('66.23 KiB', 'td');
+        $I->see('800 × 533 px', 'td ');
+        $I->see('Place on stack',  Locator::find('a', ['href' => '#']));
+        $I->see("Rename $file",    Locator::find('a', ['href' => '#']));
+        $I->see("Delete $file",    Locator::find('a', ['href' => '#']));
+        $I->see("Duplicate $file", Locator::find('a', ['href' => '#']));
     }
 }
