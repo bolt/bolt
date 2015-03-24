dbcbfbeeff6f445d1717965e7e69bd0579492c21
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-24 18:36:57 +0000

    Check that export file can be created

--- src/Nut/DatabaseExport.php
+++ src/Nut/DatabaseExport.php
@@ -7,6 +7,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Exception\IOException;
 
 /**
  * Nut database exporter command
@@ -36,10 +38,43 @@ class DatabaseExport extends BaseCommand
             }
         }
 
+        // Check if export file can be created
         $file = $input->getOption('file');
+        if (!$this->isFileWriteable($file, $output)) {
+            return;
+        }
+
+
         $contenttype = $input->getOption('contenttype');
 
         $contenttype = join(' ', $contenttype);
         $output->writeln("<info>Database exported to $file: $contenttype</info>");
+    }
+
+    /**
+     * Check/create target export file
+     *
+     * @param string          $file
+     * @param OutputInterface $output
+     *
+     * @return boolean
+     */
+    private function isFileWriteable($file, OutputInterface $output)
+    {
+        $fs = new Filesystem();
+
+        if ($fs->exists($file)) {
+            $output->writeln("<error>Specified export file '$file' already exists! Aborting export.</error>");
+            return false;
+        }
+
+        try {
+            $fs->touch($file);
+        } catch (IOException $e) {
+            $output->writeln("<error>Specified export file '$file' can not be created! Aborting export.</error>");
+            return false;
+        }
+
+        return true;
     }
 }
