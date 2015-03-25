3eff8d3aad34a139b1dd72e3d1688e4333fe64a0
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-25 09:25:55 +0000

    Add audit logging to Bolt\Nut\LogTrim

--- src/Nut/LogTrim.php
+++ src/Nut/LogTrim.php
@@ -5,20 +5,30 @@ namespace Bolt\Nut;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+/**
+ * Nut command to clear the system & change logs
+ */
 class LogTrim extends BaseCommand
 {
+    /**
+     * @see \Symfony\Component\Console\Command\Command::configure()
+     */
     protected function configure()
     {
         $this
             ->setName('log:trim')
-            ->setDescription('Trim the activitylog to recent/important items only.');
+            ->setDescription('Trim the system & change logs.');
     }
 
+    /**
+     * @see \Symfony\Component\Console\Command\Command::execute()
+     */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->app['logger.manager']->trim('system');
         $this->app['logger.manager']->trim('change');
 
-        $output->writeln("<info>Activity logs trimmed!</info>");
+        $this->auditLog(__CLASS__, 'System system & change logs trimmed');
+        $output->writeln("<info>System & change logs trimmed!</info>");
     }
 }
