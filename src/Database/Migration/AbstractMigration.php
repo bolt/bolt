80691875481b6d50dd5ac1b5944dc4a55696c346
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-26 12:02:43 +0000

    Add isMigrationFileValid() to test if a file extension is valid, and conditionally if the file exists

--- src/Database/Migration/AbstractMigration.php
+++ src/Database/Migration/AbstractMigration.php
@@ -2,6 +2,9 @@
 
 namespace Bolt\Database\Migration;
 
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use Symfony\Component\HttpFoundation\File\File;
+
 /**
  * Abstract base class for database import/export
  *
@@ -14,6 +17,9 @@ abstract class AbstractMigration
 
     /** @var array */
     protected $errorMessages = array();
+
+    /** @var array */
+    protected $validExtensions = array('json', 'yaml', 'yml');
 
     /**
      * Get the error state.
@@ -59,6 +65,44 @@ abstract class AbstractMigration
     public function setErrorMessage($errorMessage)
     {
         $this->errorMessages[] = $errorMessage;
+
+        return $this;
+    }
+
+    /**
+     * Determine if file(s) specified exist and have a valid extension
+     *
+     * @param string|array $files  File(s) to check
+     * @param boolean      $exists If true, then test that the file exists
+     *
+     * @return boolean
+     */
+    public function isMigrationFileValid($files, $exists = false)
+    {
+        if (is_array($files)) {
+            foreach ($files as $file) {
+                return $this->isMigrationFileValid($file);
+            }
+        }
+
+        // Get the file extension and check existace if required
+        if ($exists) {
+            // Check the file exists
+            try {
+                $fileObj = new File($files);
+                $ext = $fileObj->getExtension();
+            } catch (FileNotFoundException $e) {
+                $this->setError(true)->setErrorMessage("File '$file' not found!");
+            }
+        } else {
+            $fileObj = new \SplFileInfo($files);
+            $ext = $fileObj->getExtension();
+        }
+
+        // Check the file extension
+        if (!in_array($ext, $this->validExtensions)) {
+            $this->setError(true)->setErrorMessage("File '$files' has an invalid extension! Must be either '.json', '.yml' or '.yaml'.");
+        }
 
         return $this;
     }
