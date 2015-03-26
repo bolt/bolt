97d1485c8476a39a83f2a6da5413b05950a46ec4
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-26 12:43:51 +0000

    Add migration file read functionality

--- src/Database/Migration/Import.php
+++ src/Database/Migration/Import.php
@@ -3,6 +3,7 @@
 namespace Bolt\Database\Migration;
 
 use Bolt\Application;
+use Symfony\Component\Yaml\Parser;
 
 /**
  * Database records iport class
@@ -22,5 +23,69 @@ class Import extends AbstractMigration
     public function __construct(Application $app)
     {
         $this->app = $app;
+    }
+
+    /**
+     * Read a migration file.
+     *
+     * This function will determine what type based on extension.
+     *
+     * @param string $file
+     *
+     * @return array
+     */
+    protected function readMigrationFile($file)
+    {
+        $fileObj = new \SplFileInfo($file);
+        $ext = $fileObj->getExtension();
+
+        if (is_readable($file)) {
+            if ($ext === 'yml' || $ext === 'yaml') {
+                return $this->readYamlFile($file);
+            } else {
+                return $this->readJsonFile($file);
+            }
+        } else {
+            $this->setError(true)->setErrorMessage("File '$file' not readable!");
+
+            return false;
+        }
+    }
+
+    /**
+     * Read a YAML migration file.
+     *
+     * @param string $file
+     *
+     * @return array
+     */
+    private function readYamlFile($file, $output)
+    {
+        $parser = new Parser();
+
+        try {
+            return $parser->parse(file_get_contents($file) . "\n");
+        } catch (ParseException $e) {
+            $this->setError(true)->setErrorMessage("File '$file' has invalid YAML!");
+
+            return false;
+        }
+    }
+
+    /**
+     * Read a JSON migration file.
+     *
+     * @param string $file
+     *
+     * @return array
+     */
+    private function readJsonFile($file)
+    {
+        $json = json_decode(file_get_contents($file), true);
+        if ($json === false) {
+            $this->setError(true)->setErrorMessage("File '$file' has invalid JSON!");
+        }
+
+        return $json;
     }
 }
