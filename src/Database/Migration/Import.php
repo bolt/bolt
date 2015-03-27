f7febf8fb55bc5682852ec2bddfa26fb6a561fb5
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-27 12:17:24 +0000

    Implement setMigrationFiles() for Import

--- src/Database/Migration/Import.php
+++ src/Database/Migration/Import.php
@@ -11,5 +11,29 @@ use Symfony\Component\Yaml\Parser;
  */
 class Import extends AbstractMigration
 {
+    /**
+     * Set the migration files.
+     *
+     * Also creates an input file objects.
+     *
+     * @see \Bolt\Database\Migration\AbstractMigration::setMigrationFiles()
+     */
+    public function setMigrationFiles($files)
+    {
+        parent::setMigrationFiles($files);
 
+        if ($this->getError()) {
+            return $this;
+        }
+
+        foreach ($this->files as &$file) {
+            if ($file['type'] === 'yaml') {
+                $file['handler'] = new Input\YamlFile($this, $file['file']);
+            } elseif ($file['type'] === 'json') {
+                $file['handler'] = new Input\JsonFile($this, $file['file']);
+            }
+        }
+
+        return $this;
+    }
 }
