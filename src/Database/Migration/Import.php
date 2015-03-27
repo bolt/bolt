6d66dba0423a6d695b7b084e2deeff8742871d20
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-27 12:19:24 +0000

    Add function to do imports on given files

--- src/Database/Migration/Import.php
+++ src/Database/Migration/Import.php
@@ -2,6 +2,7 @@
 
 namespace Bolt\Database\Migration;
 
+use Bolt\Helpers\Arr;
 use Symfony\Component\Yaml\Parser;
 
 /**
@@ -32,6 +33,33 @@ class Import extends AbstractMigration
             } elseif ($file['type'] === 'json') {
                 $file['handler'] = new Input\JsonFile($this, $file['file']);
             }
+        }
+
+        return $this;
+    }
+
+    /**
+     *
+     * @return \Bolt\Database\Migration\Import
+     */
+    public function importMigrationFiles()
+    {
+        if ($this->getError()) {
+            return $this;
+        }
+
+        foreach ($this->files as $file) {
+            $file['output']->readFile();
+            $filename = (string) $file['file'];
+
+            // Our import arrays should be indexed, if not we have a problem
+            if (!Arr::isIndexedArray($this->data)) {
+                $this->setError(true)->setErrorMessage("File '$filename' has an invalid import format!");
+                continue;
+            }
+
+            // Import the records from the given file
+            $this->importRecords($filename);
         }
 
         return $this;
