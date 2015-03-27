ab3d92821e97ab70b78fe99313c1133c8ae1c216
Gawain Lynch <gawain.lynch@gmail.com>
2015-03-27 07:15:49 +0000

    Add interface for migration export files

---
+++ src/Database/Migration/Output/OutputFileInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bolt\Database\Migration\Output;
+
+use Bolt\Database\Migration\Export;
+use Symfony\Component\HttpFoundation\File\File;
+
+/**
+ * Interface for migration export files
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+interface OutputFileInterface
+{
+    /**
+     * Constructor.
+     *
+     * @param Export       $export
+     * @param \SplFileInfo $file
+     */
+    public function __construct(Export $export, \SplFileInfo $file);
+
+    public function __destruct();
+
+    /**
+     * Add a record
+     *
+     * @param array   $data An array of values to write out
+     * @param boolean $last Flag that indicates last record
+     */
+    public function addRecord(array $data, $last);
+}
