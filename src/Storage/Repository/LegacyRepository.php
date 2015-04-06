17f99640c71cbeba70542f13849d6ecc76638977
Ross Riley <riley.ross@gmail.com>
2015-04-06 18:49:32 +0100

    implement all the storage methods on the legacy repository

--- src/Storage/Repository/LegacyRepository.php
+++ src/Storage/Repository/LegacyRepository.php
@@ -42,118 +42,118 @@ class LegacyRepository extends Repository
     
     public function getContentObject($contenttype, $values = array())
     {
-        
+        return $this->getStorage()->getContentObject($contenttype, $values);
     }
     
     public function preFill($contenttypes = array())
     {
-        return $this->getStorage()->prefill($contenttypes)
+        return $this->getStorage()->prefill($contenttypes);
     }
     
     public function saveContent(Content $content, $comment = null)
     {
-        
+        return $this->getStorage()->saveContent($content, $comment);
     }
     
     public function deleteContent($contenttype, $id)
     {
-        
+        return $this->getStorage()->deleteContent($contenttype, $id);
     }
     
     public function updateSingleValue($contenttype, $id, $field, $value)
     {
-        
+        return $this->getStorage()->updateSingleValue($contenttype, $id, $field, $value);
     }
     
     public function getEmptyContent($contenttypeslug)
     {
-        
+        return $this->getStorage()->getEmptyContent($contenttypeslug);
     }
 
     public function searchContent($q, array $contenttypes = null, array $filters = null, $limit = 9999, $offset = 0)
     {
-        
+        return $this->getStorage()->searchContent($q, $contenttypes, $filters, $limit, $offset);
     }
     
     public function searchAllContentTypes(array $parameters = array(), &$pager = array())
     {
-        
+        return $this->getStorage()->searchAllContentTypes($parameters, $pager);
     }
 
     public function searchContentType($contenttypename, array $parameters = array(), &$pager = array())
     {
-        
+        return $this->getStorage()->searchContentType($contenttypename, $parameters, $pager);
     }
     
     public function getContentByTaxonomy($taxonomyslug, $name, $parameters = "")
     {
-        
+        return $this->getStorage()->getContentByTaxonomy($taxonomyslug, $name, $parameters);
     }
 
     public function publishTimedRecords($contenttype)
     {
-        
+        return $this->getStorage()->publishTimedRecords($contenttype);
     }
     
     public function depublishExpiredRecords($contenttype)
     {
-        
+        return $this->getStorage()->depublishExpiredRecords($contenttype);
     }
 
     public function getContent($textquery, $parameters = '', &$pager = array(), $whereparameters = array())
     {
-        
+        return $this->getStorage()->getContent($textquery, $parameters, $pager, $whereparameters);
     }
     
     public function getSortOrder($name = '-datepublish')
     {
-        
+        return $this->getStorage()->getSortOrder($name);
     }
     
     public function getContentType($contenttypeslug)
     {
-        
+        return $this->getStorage()->getContentType($contenttypeslug);
     }
     
     public function getTaxonomyType($taxonomyslug)
     {
-        
+        return $this->getStorage()->getTaxonomyType($taxonomyslug);
     }
     
     public function getContentTypes()
     {
-        
+        return $this->getStorage()->getContentTypes();
     }
     
     public function getContentTypeFields($contenttypeslug)
     {
-        
+        return $this->getStorage()->getContentTypeFields($contenttypeslug);
     }
     
     public function getContentTypeFieldType($contenttypeslug, $fieldname)
     {
-        
+        return $this->getStorage()->getContentTypeFieldType($contenttypeslug, $fieldname);
     }
     
     public function getContentTypeGrouping($contenttypeslug)
     {
-        
+        return $this->getStorage()->getContentTypeGrouping($contenttypeslug);
     }
     
     public function getContentTypeTaxonomy($contenttypeslug)
     {
-        
+        return $this->getStorage()->getContentTypeTaxonomy($contenttypeslug);
     }
     
     public function getLatestId($contenttypeslug)
     {
-        
+        return $this->getStorage()->getLatestId($contenttypeslug);
     }
     
     
     public function getUri($title, $id = 0, $contenttypeslug = "", $fulluri = true, $allowempty = true, $slugfield = 'slug')
     {
-        
+        return $this->getStorage()->getUri($title, $id, $contenttypeslug, $fulluri, $allowempty, $slugfield);
     }
 
 
