8f9b41d03661b8219d51816c9ca5df33e962ba5a
SahAssar <svante.richter@gmail.com>
2015-04-07 22:47:45 +0200

    move gps2num and getGPS and add "documentation"

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -382,29 +382,6 @@ class TwigExtension extends \Twig_Extension
 
         return $str;
     }
-    
-    private function getGps($exifCoord, $hemi) {
-
-        $degrees = count($exifCoord) > 0 ? $this->gps2Num($exifCoord[0]) : 0;
-        $minutes = count($exifCoord) > 1 ? $this->gps2Num($exifCoord[1]) : 0;
-        $seconds = count($exifCoord) > 2 ? $this->gps2Num($exifCoord[2]) : 0;
-
-        return ($degrees + $minutes / 60 + $seconds / 3600);
-
-    }
-
-    private function gps2Num($coordPart) {
-
-        $parts = explode('/', $coordPart);
-
-        if (count($parts) <= 0)
-            return 0;
-
-        if (count($parts) == 1)
-            return $parts[0];
-
-        return floatval($parts[0]) / floatval($parts[1]);
-    }
 
     /**
      * Get an array with the dimensions of an image, together with its
@@ -490,6 +467,45 @@ class TwigExtension extends \Twig_Extension
         $info['square'] = !$info['landscape'] && !$info['portrait'];
 
         return $info;
+    }
+    
+    /**
+     * Return a decimal value from the GPS coordinates from EXIF.
+     *
+     * @param array $exifCoord
+     *
+     * @return float A decimal of the GPS coordinates
+     */
+    
+    private function getGps($exifCoord) {
+
+        $degrees = count($exifCoord) > 0 ? $this->gps2Num($exifCoord[0]) : 0;
+        $minutes = count($exifCoord) > 1 ? $this->gps2Num($exifCoord[1]) : 0;
+        $seconds = count($exifCoord) > 2 ? $this->gps2Num($exifCoord[2]) : 0;
+
+        return ($degrees + $minutes / 60 + $seconds / 3600);
+
+    }
+    
+    /**
+     * Return The specific value of a GPS part (degrees, minutes, seconds)
+     *
+     * @param string The part a degree based GPS coordinate
+     *
+     * @return float 
+     */
+    
+    private function gps2Num($coordPart) {
+
+        $parts = explode('/', $coordPart);
+
+        if (count($parts) <= 0)
+            return 0;
+
+        if (count($parts) == 1)
+            return $parts[0];
+
+        return floatval($parts[0]) / floatval($parts[1]);
     }
 
     /**
