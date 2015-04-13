4c66a68f3e1def95f272e1df7b45731eadff5a8b
Ross Riley <riley.ross@gmail.com>
2015-04-13 09:23:56 +0100

    make the hydrator take an entity name as part of the constructor

--- src/Storage/Hydrator.php
+++ src/Storage/Hydrator.php
@@ -10,14 +10,33 @@ use Doctrine\Common\EventSubscriber;
 class Hydrator
 {
     
+    protected $handler;
+    
+    /**
+     * Hydrator class, converts database values into PHP OO Structure
+     * Handlers can be registered to replace default strategy.
+     *
+     * @param $classHandler A PHP Class that will be used to store Hydrated data.
+     * 
+     * @return void
+     **/
+    public function __construct($classHandler)
+    {
+        if (!class_exists($classHandler)) {
+            throw new \InvalidArgumentException("Value passed must be a valid class name", 1);
+        }
+        $this->handler = $classHandler;   
+    }
+    
     /**
      *  @param array source data
      * 
      *  @return Object Entity
      */
-    public function hydrate(array $source, $entity)
+    public function hydrate(array $source)
     {
-        return new $entity($source);
+        $classname = $this->handler;
+        return new $classname($source);
     }
 
     
