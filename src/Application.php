fdbd80be0d429831056664c6017f0d69d61a4d92
Bob den Otter <bobdenotter@gmail.com>
2015-04-13 17:01:48 +0200

    Bumping version to '2.1.6'.

--- src/Application.php
+++ src/Application.php
@@ -30,8 +30,8 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '2.1.6';
-        $values['bolt_name'] = 'RC1';
-        $values['bolt_released'] = false; // `true` for stable releases, `false` for alpha, beta and RC.
+        $values['bolt_name'] = '';
+        $values['bolt_released'] = true; // `true` for stable releases, `false` for alpha, beta and RC.
 
         parent::__construct($values);
 
