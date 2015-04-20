44d71d3107935a9c158637479aabac52478d50d7
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-20 06:38:24 +0100

    Move the ExtensionsInfoService to use the Guzzle provider

--- src/Extensions/ExtensionsInfoService.php
+++ src/Extensions/ExtensionsInfoService.php
@@ -1,7 +1,7 @@
 <?php
 namespace Bolt\Extensions;
 
-use Bolt\Exception\PackageManagerException;
+use GuzzleHttp\Client;
 
 /**
  * Class to provide querying of the Bolt Extensions repo as a
@@ -9,21 +9,40 @@ use Bolt\Exception\PackageManagerException;
  *
  * @author Ross Riley <riley.ross@gmail.com>
  **/
-
 class ExtensionsInfoService
 {
     public $site;
     public $urls;
     public $format = 'json';
 
+    /** @var \GuzzleHttp\Client|\Guzzle\Service\Client */
+    private $client;
+    /** @var boolean */
+    private $deprecated;
+
     /**
-     * @param $site string
-     * @param $urls array
-     **/
-    public function __construct($site, $urls = array())
+     * Constructor function.
+     *
+     * @param \GuzzleHttp\Client|\Guzzle\Service\Client $client
+     * @param string                                    $site
+     * @param array                                     $urls
+     * @param boolean                                   $deprecated
+     */
+    public function __construct($client, $site, $urls = array(), $deprecated = false)
     {
-        $this->site = $site;
-        $this->urls = $urls;
+        /** @deprecated remove when PHP 5.3 support is dropped */
+        $this->deprecated = $deprecated;
+        if (!($client instanceof \GuzzleHttp\Client || $client instanceof \Guzzle\Service\Client)) {
+            throw new \InvalidArgumentException(sprintf(
+                'First argument passed to %s must be an instance of GuzzleHttp\Client or Guzzle\Service\Client, instance of %s given.',
+                __CLASS__,
+                get_class($client)
+            ));
+        }
+
+        $this->client = $client;
+        $this->site   = $site;
+        $this->urls   = $urls;
     }
 
     public function all()
@@ -33,6 +52,14 @@ class ExtensionsInfoService
         return $this->execute($url);
     }
 
+    /**
+     * Make an extension package info request.
+     *
+     * @param string $package Composer package name 'author/extension'
+     * @param string $bolt    Bolt version number
+     *
+     * @return string|boolean
+     */
     public function info($package, $bolt)
     {
         $url = $this->urls['info'];
@@ -46,22 +73,29 @@ class ExtensionsInfoService
         $this->format = $format;
     }
 
+    /**
+     * Execute the query.
+     *
+     * @param string $url
+     * @param array  $params
+     *
+     * @return string|boolean
+     */
     public function execute($url, $params = array())
     {
-        if (!ini_get('allow_url_fopen')) {
-            throw new PackageManagerException('Please enable "allow_url_fopen" in your php.ini file to use extensions');
-        }
-        $endpoint = rtrim($this->site, '/') . '/' . ltrim($url, '/') . '?' . http_build_query($params);
-        $endpoint = rtrim($endpoint, '?');
-        try {
-            $result = file_get_contents($endpoint);
-        } catch (\Exception $e) {
-            $result = false;
-        }
-        if ($this->format === 'json') {
-            return json_decode($result);
-        }
+        $uri = rtrim(rtrim($this->site, '/') . '/' . ltrim($url, '/') . '?' . http_build_query($params), '?');
 
-        return $result;
+        try {
+            if ($this->deprecated) {
+                /** @deprecated remove when PHP 5.3 support is dropped */
+                $result = $this->client->get($uri, array('timeout' => 10))->send()->getBody(true);
+            } else {
+                $result = $this->client->get($uri, array('timeout' => 10))->getBody(true);
+            }
+
+            return ($this->format === 'json') ? json_decode($result) : (string) $result;
+        } catch (\Exception $e) {
+            return false;
+        }
     }
 }
