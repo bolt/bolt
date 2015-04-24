22f42ad9acafb284591c6a1a1ce8d05c4b91f441
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-24 14:59:18 +0100

    Move before() function into BackendBase until refactoring if further along

--- src/Controllers/Backend/BackendBase.php
+++ src/Controllers/Backend/BackendBase.php
@@ -2,7 +2,10 @@
 namespace Bolt\Controllers\Backend;
 
 use Bolt\Controllers\Base;
+use Bolt\Library as Lib;
+use Bolt\Translation\Translator as Trans;
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
 abstract class BackendBase extends Base
 {
@@ -10,6 +13,10 @@ abstract class BackendBase extends Base
     {
         $c = parent::connect($app);
         $c->value('end', 'backend'); // For now
+
+        $c->before(array($this, 'before'));
+        $c->method('GET|POST');
+
         return $c;
     }
 
@@ -19,5 +26,85 @@ abstract class BackendBase extends Base
             $variables['context'] = $variables;
         }
         return parent::render($template, $variables, $globals);
+    }
+
+    /**
+     * Middleware function to check whether a user is logged on.
+     *
+     * @param Request     $request The Symfony Request
+     * @param Application $app     The application/container
+     *
+     * @return null|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public static function before(Request $request, Application $app)
+    {
+        // Start the 'stopwatch' for the profiler.
+        $app['stopwatch']->start('bolt.backend.before');
+
+        $route = $request->get('_route');
+
+        $app['debugbar'] = true;
+
+        // Sanity checks for doubles in in contenttypes.
+        // unfortunately this has to be done here, because the 'translator' classes need to be initialised.
+        $app['config']->checkConfig();
+
+        // If we had to reload the config earlier on because we detected a version change, display a notice.
+        if ($app['config']->notify_update) {
+            $notice = Trans::__("Detected Bolt version change to <b>%VERSION%</b>, and the cache has been cleared. Please <a href=\"%URI%\">check the database</a>, if you haven't done so already.",
+                array('%VERSION%' => $app->getVersion(), '%URI%' => $app['resources']->getUrl('bolt') . 'dbcheck'));
+            $app['logger.system']->notice(strip_tags($notice), array('event' => 'config'));
+            $app['session']->getFlashBag()->add('info', $notice);
+        }
+
+        // Check the database users table exists
+        $tableExists = $app['integritychecker']->checkUserTableIntegrity();
+
+        // Test if we have a valid users in our table
+        $hasUsers = false;
+        if ($tableExists) {
+            $hasUsers = $app['users']->hasUsers();
+        }
+
+        // If the users table is present, but there are no users, and we're on /bolt/userfirst,
+        // we let the user stay, because they need to set up the first user.
+        if ($tableExists && !$hasUsers && $route == 'userfirst') {
+            return null;
+        }
+
+        // If there are no users in the users table, or the table doesn't exist. Repair
+        // the DB, and let's add a new user.
+        if (!$tableExists || !$hasUsers) {
+            $app['integritychecker']->repairTables();
+            $app['session']->getFlashBag()->add('info', Trans::__('There are no users in the database. Please create the first user.'));
+
+            return Lib::redirect('userfirst');
+        }
+
+        // Confirm the user is enabled or bounce them
+        if ($app['users']->getCurrentUser() && !$app['users']->isEnabled() && $route !== 'userfirst' && $route !== 'login' && $route !== 'postLogin' && $route !== 'logout') {
+            $app['session']->getFlashBag()->add('error', Trans::__('Your account is disabled. Sorry about that.'));
+
+            return Lib::redirect('logout');
+        }
+
+        // Check if there's at least one 'root' user, and otherwise promote the current user.
+        $app['users']->checkForRoot();
+
+        // Most of the 'check if user is allowed' happens here: match the current route to the 'allowed' settings.
+        if (!$app['users']->isValidSession() && !$app['users']->isAllowed($route)) {
+            $app['session']->getFlashBag()->add('info', Trans::__('Please log on.'));
+
+            return Lib::redirect('login');
+        } elseif (!$app['users']->isAllowed($route)) {
+            $app['session']->getFlashBag()->add('error', Trans::__('You do not have the right privileges to view that page.'));
+
+            return Lib::redirect('dashboard');
+        }
+
+        // Stop the 'stopwatch' for the profiler.
+        $app['stopwatch']->stop('bolt.backend.before');
+
+        return null;
     }
 }
