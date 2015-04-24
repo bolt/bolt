7deb52d802fab58d71409b102882b030c23402bb
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-24 16:16:08 +0100

    [Tests] Skeleton for BackendAuthenticationTests

---
+++ tests/phpunit/unit/Controller/Backend/AuthenticationTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Bolt\Tests\Controller\Backend;
+
+use Bolt\Configuration\ResourceManager;
+use Bolt\Controllers\Backend\Authentication;
+use Bolt\Tests\BoltUnitTest;
+use Silex\ControllerCollection;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class to test correct operation of src/Controllers/Backend/Authentication.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ **/
+class AuthenticationTest extends BoltUnitTest
+{
+}
