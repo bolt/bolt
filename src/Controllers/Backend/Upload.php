11b01fdd0a29fd55f5127b196ac76a97155473fd
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-26 06:37:18 +0100

    Refactor and comment upload handler

--- src/Controllers/Backend/Upload.php
+++ src/Controllers/Backend/Upload.php
@@ -2,23 +2,18 @@
 
 namespace Bolt\Controllers\Backend;
 
-use Bolt\Filesystem\FlysystemContainer;
-use Bolt\Library as Lib;
 use Bolt\Translation\Translator as Trans;
 use Silex;
 use Silex\Application;
 use Silex\ControllerCollection;
-use Silex\ServiceProviderInterface;
-use Sirius\Upload\Handler as UploadHandler;
 use Sirius\Upload\Result\Collection;
 use Sirius\Upload\Result\File;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Class to handle uploads
+ * Class to handle file uploads.
  *
  * @author Ross Riley <riley.ross@gmail.com>
  **/
@@ -32,57 +27,6 @@ class Upload extends BackendBase
             ->bind('upload');
 
         return $c;
-    }
-
-    public function register(Silex\Application $app)
-    {
-        // This exposes the main upload object as a service
-        $app['upload'] = function () use ($app) {
-            $allowedExensions = $app['config']->get('general/accept_file_types');
-            $uploadHandler = new UploadHandler($app['upload.container']);
-            $uploadHandler->setPrefix($app['upload.prefix']);
-            $uploadHandler->setOverwrite($app['upload.overwrite']);
-            $uploadHandler->addRule('extension', array('allowed' => $allowedExensions));
-
-            $pattern = $app['config']->get('general/upload/pattern', '[^A-Za-z0-9\.]+');
-            $replacement = $app['config']->get('general/upload/replacement', '-');
-            $lowercase = $app['config']->get('general/upload/lowercase', true);
-
-            $uploadHandler->setSanitizerCallback(
-                function ($filename) use ($pattern, $replacement, $lowercase) {
-                    if ($lowercase) {
-                        return preg_replace("/$pattern/", $replacement, strtolower($filename));
-                    }
-
-                    return preg_replace("/$pattern/", $replacement, $filename);
-                }
-            );
-
-            return $uploadHandler;
-        };
-
-        // This exposes the file container as a configurable object please refer to:
-        // Sirius\Upload\Container\ContainerInterface
-        // Any compatible file handler can be used.
-        $app['upload.container'] = function () use ($app) {
-            $base = $app['resources']->getPath($app['upload.namespace']);
-            if (!is_writable($base)) {
-                throw new \RuntimeException("Unable to write to upload destination. Check permissions on $base", 1);
-            }
-            $container = new FlysystemContainer($app['filesystem']->getFilesystem($app['upload.namespace']));
-
-            return $container;
-        };
-
-        // This allows multiple upload locations, all prefixed with a namespace. The default is /files
-        // Note, if you want to provide an alternative namespace, you must set a path on the $app['resources']
-        // service
-        $app['upload.namespace'] = 'files';
-
-        // This gets prepended to all file saves, can be reset to "" or add your own closure for more complex ones.
-        $app['upload.prefix'] = date('Y-m') . '/';
-
-        $app['upload.overwrite'] = false;
     }
 
     /**
@@ -103,8 +47,8 @@ class Upload extends BackendBase
             $app->abort(Response::HTTP_NOT_FOUND, 'You must be logged in to use this.');
         }
 
-        if (!$app['users']->isAllowed("files:uploads")) {
-            $app['session']->getFlashBag()->add('error', Trans::__('You do not have the right privileges to upload.'));
+        if (!$app['users']->isAllowed('files:uploads')) {
+            $this->addFlash('error', Trans::__('You do not have the right privileges to upload.'));
 
             return $this->redirectToRoute('dashboard');
         }
@@ -115,64 +59,45 @@ class Upload extends BackendBase
         return null;
     }
 
+    /**
+     * Route to handle file uploads.
+     *
+     * @param Request $request
+     * @param string  $namespace
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function actionUploadNamspace(Request $request, $namespace)
     {
         if ($handler = $request->get('handler')) {
-            // This block handles the more advanced functionality where multiple upload
-            // handlers are provided. Only the first one is returned as a result, the result
-            // of this first upload is then attempted to copy to the remaining handlers.
+            // Use custom handlers
             if (is_array($handler)) {
-                list($namespace, $prefix) = $this->parser($handler[0]);
+                return $this->processCutomUploadHandler($request, $handler);
+            } else {
+                list($namespace, $prefix) = $this->parser($handler);
                 $this->app['upload.namespace'] = $namespace;
                 $this->app['upload.prefix'] = $prefix;
-
-                // Do the upload
-                $result = $this->uploadFile($request, $namespace);
-
-                array_shift($handler);
-                $original = $namespace;
-
-                if (count($result)) {
-                    $result = $result[0];
-                    foreach ($handler as $copy) {
-                        list($namespace, $prefix) = $this->parser($copy);
-
-                        $manager = $this->app['filesystem'];
-                        $manager->put(
-                            $namespace . '://' . $prefix . basename($result['name']),
-                            $manager->read($original . '://' . $result['name'])
-                        );
-                    }
-                }
-
-                return $this->json($result, Response::HTTP_OK, array('Content-Type' => 'text/plain'));
-            } else {
-                list($namespace, $prefix) = $parser($handler);
             }
-
-            $this->app['upload.namespace'] = $namespace;
-            $this->app['upload.prefix'] = $prefix;
         } else {
             $namespace = $this->app['upload.namespace'];
         }
 
-        return $this->json(
-            $this->uploadFile($request, $namespace),
-            Response::HTTP_OK,
-            array('Content-Type' => 'text/plain')
-        );
+        // Perform the file upload actions and collect the results
+        $fileUpload = $this->handleUploadFiles($request, $namespace);
+
+        return $this->json($fileUpload, Response::HTTP_OK, array('Content-Type' => 'text/plain'));
     }
 
     /**
+     * Parse a string and determine the upload prefix and namespace.
      *
-     *
-     * @param string $setting
+     * @param string $handler
      *
      * @return array
      */
-    private function parser($setting)
+    private function parser($handler)
     {
-        $parts = explode('://', $setting);
+        $parts = explode('://', $handler);
         if (count($parts) === 2) {
             $namespace = $parts[0];
             array_shift($parts);
@@ -185,6 +110,7 @@ class Upload extends BackendBase
     }
 
     /**
+     * Perform a file upload.
      *
      * @param Request $request
      * @param string  $namespace
@@ -192,7 +118,63 @@ class Upload extends BackendBase
      *
      * @return array
      */
-    private function uploadFile(Request $request, $namespace, $files = null)
+    private function handleUploadFiles(Request $request, $namespace, $files = null)
+    {
+        $filesToProcess = $this->getFilesToProcess($request, $namespace, $files);
+
+        /** @var \Sirius\Upload\Result\Collection|\Sirius\Upload\Result\File $result */
+        $result = $this->app['upload']->process($filesToProcess);
+
+        if ($result->isValid()) {
+            // Remove the .lock file attached to the file
+            $result->confirm();
+
+            if ($result instanceof File) {
+                $successfulFiles = array($result->name);
+            } elseif ($result instanceof Collection) {
+                $successfulFiles = array();
+                foreach ($result as $resultFile) {
+                    $successfulFiles[] = array(
+                        'url'  => $namespace . '/' . $resultFile->name,
+                        'name' => $resultFile->name
+                    );
+                }
+            }
+
+            return $successfulFiles;
+        } else {
+            // The file that was saved during process() has a .lock file attached
+            // and can now be cleared, in the case where form processing fails
+            try {
+                $result->clear();
+            } catch (\Exception $e) {
+                // It's an error state anyway
+            }
+
+            $errorFiles = array();
+            foreach ($result as $resultFile) {
+                $errors = $resultFile->getMessages();
+                $errorFiles[] = array(
+                    'url'   => $namespace . '/' . $resultFile->original_name,
+                    'name'  => $resultFile->original_name,
+                    'error' => (string) $errors[0]
+                );
+            }
+
+            return $errorFiles;
+        }
+    }
+
+    /**
+     * Determine the list of files to upload.
+     *
+     * @param Request $request
+     * @param string  $namespace
+     * @param string  $files
+     *
+     * @return array
+     */
+    private function getFilesToProcess(Request $request, $namespace, $files = null)
     {
         $this->app['upload.namespace'] = $namespace;
 
@@ -215,40 +197,44 @@ class Upload extends BackendBase
             }
         }
 
-        /** @var Collection|File $result */
-        $result = $this->app['upload']->process($filesToProcess);
+        return $filesToProcess;
+    }
 
-        if ($result->isValid()) {
-            $result->confirm();
-            if ($result instanceof File) {
-                $successfulFiles = array($result->name);
-            } elseif ($result instanceof Collection) {
-                $successfulFiles = array();
-                foreach ($result as $resultFile) {
-                    $successfulFiles[] = array(
-                        'url'  => $namespace . '/' . $resultFile->name,
-                        'name' => $resultFile->name
-                    );
-                }
-            }
+    /**
+     * This handles the more advanced functionality where multiple upload handlers
+     * are provided. Only the first one is returned as a result, the result
+     * of this first upload is then attempted to copy to the remaining handlers.
+     *
+     * @param Request $request
+     * @param array   $handler
+     *
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    private function processCutomUploadHandler(Request $request, array $handler)
+    {
+        list($namespace, $prefix) = $this->parser($handler[0]);
+        $this->app['upload.namespace'] = $namespace;
+        $this->app['upload.prefix'] = $prefix;
 
-            return $successfulFiles;
-        } else {
-            try {
-                $result->clear();
-            } catch (\Exception $e) {
-            }
-            $errorFiles = array();
-            foreach ($result as $resultFile) {
-                $errors = $resultFile->getMessages();
-                $errorFiles[] = array(
-                    'url'   => $namespace . '/' . $resultFile->original_name,
-                    'name'  => $resultFile->original_name,
-                    'error' => (string) $errors[0]
+        // Do the upload
+        $result = $this->handleUploadFiles($request, $namespace);
+
+        array_shift($handler);
+        $original = $namespace;
+
+        if (count($result)) {
+            $result = $result[0];
+            foreach ($handler as $copy) {
+                list($namespace, $prefix) = $this->parser($copy);
+
+                $manager = $this->app['filesystem'];
+                $manager->put(
+                    $namespace . '://' . $prefix . basename($result['name']),
+                    $manager->read($original . '://' . $result['name'])
                 );
             }
-
-            return $errorFiles;
         }
+
+        return $this->json($result, Response::HTTP_OK, array('Content-Type' => 'text/plain'));
     }
 }
