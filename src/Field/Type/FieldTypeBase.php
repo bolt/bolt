131d3cf4108aa5fab10d568c5012c5a2d4c6fdac
Ross Riley <riley.ross@gmail.com>
2015-04-27 06:40:57 +0100

    standardised field behaviour in a base class

---
+++ src/Field/Type/FieldTypeBase.php
@@ -0,0 +1,77 @@
+<?php
+namespace Bolt\Field\Type;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use Bolt\Storage\EntityManager;
+use Bolt\Mapping\ClassMetadata;
+
+/**
+ * This is an abstract class for a field type that handles
+ * the lifecycle of a field from pre-query to persist.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+abstract class FieldTypeBase implements FieldTypeInterface
+{
+    
+    public $mapping;
+    
+    public function __construct(array $mapping = array())
+    {
+        $this->mapping = $mapping;
+    }
+    
+    /**
+     * Handle or ignore the load event.
+     * 
+     * @param QueryBuilder $query
+     *
+     * @return void
+     */
+    public function load(QueryBuilder $query, ClassMetadata $metadata)
+    {
+        return $query;
+    }
+    
+    /**
+     * Handle or ignore the persist event.
+     *
+     * @return void
+     */
+    public function persist(QueryBuilder $query, EntityManager $em)
+    {
+        
+    }
+    
+    /**
+     * Handle or ignore the hydrate event.
+     *
+     * @return void
+     */
+    public function hydrate($data, $entity)
+    {
+        
+    }
+    
+    /**
+     * Handle or ignore the present event.
+     *
+     * @return void
+     */
+    public function present($entity)
+    {
+        
+    }
+    
+    /**
+     * Returns the name of the hydrator.
+     *
+     * @return string The field name
+     */
+    public function getName()
+    {
+        return 'text';
+    }
+
+    
+}
