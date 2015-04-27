cf2e0883332f721ce5e1feac7c0f7682c9c7c059
rarila <magnet@gmx.net>
2015-04-28 00:06:42 +0200

    Make output directory

--- app/src/grunt-tasks/pages.js
+++ app/src/grunt-tasks/pages.js
@@ -3,5 +3,15 @@
  */
 module.exports = function (grunt) {
     grunt.registerTask('pages', 'Downloads rendered pages from a Bolt server', function () {
+        var outpath;
+
+        // Require config variables.
+        grunt.config.requires(
+            'path.tmp'
+        );
+
+        // Create output directory inside tmp folder.
+        outpath = grunt.config('path.tmp') + '/pages';
+        grunt.file.mkdir(outpath);
     });
 };
