9fa42320e0f88caccb308229222606990e79f4ef
Gawain Lynch <gawain.lynch@gmail.com>
2015-04-27 05:34:36 +0100

    Application now always exists

--- index.php
+++ index.php
@@ -17,8 +17,9 @@
 /**
  * Version must be greater than 5.4.16.
  *
- * Note, we use `dirname(__FILE__)` instead of `__DIR__`. The latter was introduced "only" in
- * PHP 5.3, and we need to be able to show the notice to the poor souls who are still on PHP 5.2.
+ * Note, we use `dirname(__FILE__)` instead of `__DIR__`. The latter was
+ * introduced "only" in PHP 5.3, and we need to be able to show the notice to
+ * the poor souls who are still on PHP 5.2.
  *
  * @see: https://github.com/bolt/bolt/issues/1531
  * @see: https://github.com/bolt/bolt/issues/3371
@@ -32,7 +33,7 @@ if (version_compare(PHP_VERSION, '5.4.16', '<')) {
  * Return false if the requested file is available on the filesystem.
  * @see: http://silex.sensiolabs.org/doc/web_servers.html#php-5-4
  */
-if (php_sapi_name() == 'cli-server') {
+if (php_sapi_name() === 'cli-server') {
     $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 
     if (is_file($filename)) {
@@ -44,9 +45,4 @@ if (php_sapi_name() == 'cli-server') {
  * @var \Bolt\Application $app
  */
 $app = require_once __DIR__ . '/app/bootstrap.php';
-
-if ($app) {
-    $app->run();
-} else {
-    return false;
-}
+$app->run();
