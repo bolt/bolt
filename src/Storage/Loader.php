5a5bb61b3bb50dbf9366ed68cc1206d2690b18ea
Ross Riley <riley.ross@gmail.com>
2015-04-27 06:42:19 +0100

    add new loader class to manage query transformation

---
+++ src/Storage/Loader.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bolt\Storage;
+
+use Bolt\Mapping\ClassMetadata;
+use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Types\Type;
+
+/**
+ * This class works on an instance of QueryBuilder transforming the query to the  
+ * needs of the field handlers. 
+ */
+class Loader
+{
+    
+    
+    public $handlers = array();
+    
+    /**
+     *  @param array source data
+     * 
+     *  @return Object Entity
+     */
+    public function load(QueryBuilder $qb, ClassMetadata $metadata)
+    {
+        foreach ($metadata->getFieldMappings() as $field) {
+            $fieldtype = new $field['fieldtype']($field);
+            $qb = $fieldtype->load($qb, $metadata);
+        }
+
+        return $qb;
+    }
+    
+    
+
+    
+}
