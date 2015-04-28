1bdf60baf0e11f7f3cfeec2b0fd979ada024e85c
rarila <magnet@gmx.net>
2015-04-28 02:36:14 +0200

    Ignore requests with a destination that starts with "#"

--- app/src/grunt-tasks/pages.js
+++ app/src/grunt-tasks/pages.js
@@ -26,26 +26,29 @@ module.exports = function (grunt) {
         // Request all required pages.
         pages = grunt.config('pages.requests');
         for (var dest in pages) {
-            // Set request options.
-            if (typeof pages[dest] === 'object') {
-                options = pages[dest];
-            } else {
-                options = {
-                    url: pages[dest] !== '' ? pages[dest] : dest
-                };
+            // Ignore requests with a destination that starts with "#".
+            if (dest.substr(0, 1) !== '#') {
+                // Set request options.
+                if (typeof pages[dest] === 'object') {
+                    options = pages[dest];
+                } else {
+                    options = {
+                        url: pages[dest] !== '' ? pages[dest] : dest
+                    };
+                }
+                options.baseUrl = options.baseUrl || grunt.config('pages.baseurl');
+                options.followAllRedirects = true; // "followRedirect" doesn't seem to work with 302.
+                options.jar = true;
+
+                // Path, where to put the file. Make it always end with ".html"
+                outfile = outpath + '/' + dest.replace(/^(.+)\.html$/, '$1') + '.html';
+
+                // Build a request queue.
+                queue.push({
+                    opt: options,
+                    out: outfile
+                });
             }
-            options.baseUrl = options.baseUrl || grunt.config('pages.baseurl');
-            options.followAllRedirects = true; // "followRedirect" doesn't seem to work with 302.
-            options.jar = true;
-
-            // Path, where to put the file. Make it always end with ".html"
-            outfile = outpath + '/' + dest.replace(/^(.+)\.html$/, '$1') + '.html';
-
-            // Build a request queue.
-            queue.push({
-                opt: options,
-                out: outfile
-            });
         }
 
         getNextPage();
