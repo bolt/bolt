8b3edf1128ae6c337fb3ee22813d772f7397b103
rarila <magnet@gmx.net>
2015-04-30 10:37:37 +0200

    Allow local overriding of htmllint configuration

--- app/src/grunt/htmllint.js
+++ app/src/grunt/htmllint.js
@@ -1,13 +1,18 @@
 /*
  * HTMLLINT: html validation using the vnu.jar markup checker.
  */
-module.exports = {
-    /*
-     * TARGET:  Check html files in "path.pages"
-     */
-    pages: {
-        options: {
-        },
-        src: '<%= path.pages %>/*.html'
-    }
+module.exports = function (grunt, options) {
+    var conf = {};
+    // Override settings
+    require('deep-extend')(conf, options.htmllint);
+
+    return {
+        /*
+         * TARGET:  Check html files in "path.pages"
+         */
+        pages: {
+            options: conf,
+            src: '<%= path.pages %>/*.html'
+        }
+    };
 };
