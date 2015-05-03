0c3e40136b63e4bc8da6980db5af60272830da3e
Carson Full <carson.full@gmomail.org>
2015-05-03 07:45:36 -0500

    Added BC Routing class

---
+++ src/Controllers/Routing.php
@@ -0,0 +1,14 @@
+<?php
+namespace Bolt\Controllers;
+
+use Bolt\Controller\ConfigurableBase;
+
+/**
+ * @deprecated Use {@see Bolt\Controller\Frontend} instead. Or something ¯\_(ツ)_/¯
+ */
+class Routing extends ConfigurableBase
+{
+    protected function getConfigurationRoutes()
+    {
+    }
+}
