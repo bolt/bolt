8002f76a465e2f5e8644149365d60e42f80d9173
Gawain Lynch <gawain.lynch@gmail.com>
2015-05-05 16:16:29 +1000

    [Tests] Adjust for cron move

--- tests/phpunit/unit/Provider/CronServiceProviderTest.php
+++ tests/phpunit/unit/Provider/CronServiceProviderTest.php
@@ -16,7 +16,7 @@ class CronServiceProviderTest extends BoltUnitTest
         $app = $this->getApp();
         $provider = new CronServiceProvider($app);
         $app->register($provider);
-        $this->assertInstanceOf('Bolt\Controller\Cron', $app['cron']);
+        $this->assertInstanceOf('Bolt\Cron', $app['cron']);
         $app->boot();
     }
 }
