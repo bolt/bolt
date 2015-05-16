f25896c5d1071e456293db57641e8a538a2285f6
Ross Riley <riley.ross@gmail.com>
2015-05-16 18:57:05 +0100

    implement forwarding methods to proxy to entity

--- src/Storage/EntityProxy.php
+++ src/Storage/EntityProxy.php
@@ -13,17 +13,43 @@ class EntityProxy
     
     public $entity;
     public $reference;
+    private $em;
+    protected $loaded = false;
+    protected $proxy;
     
     
-    public function __construct($entity, $reference)
+    public function __construct($entity, $reference, EntityManager $em = null)
     {
         $this->entity = $entity;
         $this->reference = $reference;
+        $this->em = $em;
     }
     
-    public function load(EntityManager $em)
+    public function load()
     {
-        return $em->find($this->entity, $this->reference);
+        if ($this->loaded) {
+            return true;
+        }
+        $this->proxy = $this->em->find($this->entity, $this->reference);
+        $this->loaded = true;
+    }
+    
+    public function __call($method, $args)
+    {
+        $this->load();
+        return call_user_func_array(array($this->proxy, $method), $args);
+    }
+    
+    public function __get($attribute)
+    {
+        $this->load();
+        return $this->proxy->$attribute;
+    }
+    
+    public function __set($attribute, $value)
+    {
+        $this->load();
+        return $this->proxy->$attribute = $value;
     }
     
 
