4cf7be69fd56a0dcaa9b8057cb7aa4de81d263b5
Ross Riley <riley.ross@gmail.com>
2015-05-17 08:40:16 +0100

    start work on load and hydrate for taxonomies

--- src/Field/Type/Taxonomy.php
+++ src/Field/Type/Taxonomy.php
@@ -2,6 +2,7 @@
 namespace Bolt\Field\Type;
 
 use Doctrine\DBAL\Query\QueryBuilder;
+use Bolt\Mapping\ClassMetadata;
 use Bolt\Storage\EntityManager;
 
 
@@ -13,6 +14,33 @@ use Bolt\Storage\EntityManager;
  */
 class Taxonomy extends FieldTypeBase
 {
+    
+     /**
+     * Handle the load event.
+     * 
+     * @param QueryBuilder $query
+     *
+     * @return void
+     */
+    public function load(QueryBuilder $query, ClassMetadata $metadata)
+    {
+        print_r($this->mapping); exit;
+        $field = $this->mapping['fieldname'];
+        $boltname = $metadata->getBoltName();
+        $query->addSelect($this->getPlatformGroupConcat("$field.to_id", $field, $query))
+            ->leftJoin('content', 'bolt_relations', $field, "content.id = $field.from_id AND $field.from_contenttype='$boltname' AND $field.to_contenttype='$field'")
+            ->addGroupBy("content.id");    
+    }
+    
+    /**
+     * Handle the hydrate event.
+     *
+     */
+    public function hydrate($data, $entity, EntityManager $em = null)
+    {
+        $field = $this->mapping['fieldname'];
+        
+    }
     
     /**
      * Returns the name of the field type.
