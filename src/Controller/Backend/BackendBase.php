f5b9966f54a73981658c75960692408310256637
Gawain Lynch <gawain.lynch@gmail.com>
2015-05-22 02:41:15 +0100

    Use shortcuts for providers in BackendBase::before()

--- src/Controller/Backend/BackendBase.php
+++ src/Controller/Backend/BackendBase.php
@@ -48,16 +48,17 @@ abstract class BackendBase extends Base
 
         $route = $request->get('_route');
 
-        // Handle the case where the route doesn't equal the role
+        // Handle the case where the route doesn't equal the role.
         if ($roleRoute === null) {
             $roleRoute = $route;
         }
 
-        // Sanity checks for doubles in in contenttypes.
-        // unfortunately this has to be done here, because the 'translator' classes need to be initialised.
+        // Sanity checks for doubles in in contenttypes. This has to be done
+        // here, because the 'translator' classes need to be initialised.
         $app['config']->checkConfig();
 
-        // If we had to reload the config earlier on because we detected a version change, display a notice.
+        // If we had to reload the config earlier on because we detected a
+        // version change, display a notice.
         if ($app['config']->notify_update) {
             $notice = Trans::__("Detected Bolt version change to <b>%VERSION%</b>, and the cache has been cleared. Please <a href=\"%URI%\">check the database</a>, if you haven't done so already.",
                 array('%VERSION%' => $app->getVersion(), '%URI%' => $app['resources']->getUrl('bolt') . 'dbcheck'));
@@ -71,17 +72,18 @@ abstract class BackendBase extends Base
         // Test if we have a valid users in our table
         $hasUsers = false;
         if ($tableExists) {
-            $hasUsers = $app['users']->hasUsers();
+            $hasUsers = $this->getUsers()->hasUsers();
         }
 
-        // If the users table is present, but there are no users, and we're on /bolt/userfirst,
-        // we let the user stay, because they need to set up the first user.
+        // If the users table is present, but there are no users, and we're on
+        // /bolt/userfirst, we let the user stay, because they need to set up
+        // the first user.
         if ($tableExists && !$hasUsers && $route === 'userfirst') {
             return null;
         }
 
-        // If there are no users in the users table, or the table doesn't exist. Repair
-        // the DB, and let's add a new user.
+        // If there are no users in the users table, or the table doesn't exist.
+        // Repair the DB, and let's add a new user.
         if (!$tableExists || !$hasUsers) {
             $app['integritychecker']->repairTables();
             $app['logger.flash']->info(Trans::__('There are no users in the database. Please create the first user.'));
@@ -90,21 +92,21 @@ abstract class BackendBase extends Base
         }
 
         // Confirm the user is enabled or bounce them
-        if ($app['users']->getCurrentUser() && !$app['users']->isEnabled() && $route !== 'userfirst' && $route !== 'login' && $route !== 'postLogin' && $route !== 'logout') {
+        if ($this->getUsers()->getCurrentUser() && !$this->getUsers()->isEnabled() && $route !== 'userfirst' && $route !== 'login' && $route !== 'postLogin' && $route !== 'logout') {
             $app['logger.flash']->error(Trans::__('Your account is disabled. Sorry about that.'));
 
             return $this->redirectToRoute('logout');
         }
 
         // Check if there's at least one 'root' user, and otherwise promote the current user.
-        $app['users']->checkForRoot();
+        $this->getUsers()->checkForRoot();
 
         // Most of the 'check if user is allowed' happens here: match the current route to the 'allowed' settings.
-        if (!$app['authentication']->isValidSession() && !$app['users']->isAllowed($route)) {
+        if (!$this->getAuthentication()->isValidSession() && !$this->getUsers()->isAllowed($route)) {
             $app['logger.flash']->info(Trans::__('Please log on.'));
 
             return $this->redirectToRoute('login');
-        } elseif (!$app['users']->isAllowed($roleRoute)) {
+        } elseif (!$this->getUsers()->isAllowed($roleRoute)) {
             $app['logger.flash']->error(Trans::__('You do not have the right privileges to view that page.'));
 
             return $this->redirectToRoute('dashboard');
