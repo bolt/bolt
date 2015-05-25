f2fa774a95e126d9f39b5707cb8c19a4c4af977d
Ross Riley <riley.ross@gmail.com>
2015-05-25 14:27:57 +0100

    map value to default when null is passed in

--- src/Storage/Persister.php
+++ src/Storage/Persister.php
@@ -3,6 +3,7 @@
 namespace Bolt\Storage;
 
 use Bolt\Mapping\ClassMetadata;
+use Bolt\Storage\EntityManager;
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\Types\Type;
 
@@ -26,22 +27,25 @@ class Persister
      * 
      *  @return Object Entity
      */
-    public function persist($qb, $entity)
+    public function persist(QuerySet $queries, $entity, EntityManager $em)
     {
-                
+    
         foreach ($this->metadata->getFieldMappings() as $key=>$mapping) {
             
             // First step is to allow each Bolt field to transform the data.
             $field = new $mapping['fieldtype']($mapping);
-            $field->persist($qb, $entity);
+            $field->persist($queries, $entity, $em);
             
             if ($mapping['type'] !== 'null') {
+                $qb = &$queries[0];
                 $valueMethod = 'serialize'.ucfirst($key);
                 $value = $entity->$valueMethod();
                 $meta = $this->metadata->getFieldMapping($key);
                 $type = Type::getType($meta['type']);
                 if (null !== $value) {
                     $value = $type->convertToDatabaseValue($value, $qb->getConnection()->getDatabasePlatform());          
+                } else {
+                    $value = $mapping['default'];
                 }
                 $qb->setValue($key, ":".$key);
                 $qb->set($key, ":".$key);
