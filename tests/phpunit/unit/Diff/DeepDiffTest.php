8f16b8233ef833a0cebc8327d27e2ae6b99fc022
Gawain Lynch <gawain.lynch@gmail.com>
2015-05-25 14:43:28 +0100

    [Tests] Use short array syntax in Bolt\Tests\Diff

--- tests/phpunit/unit/Diff/DeepDiffTest.php
+++ tests/phpunit/unit/Diff/DeepDiffTest.php
@@ -17,58 +17,58 @@ class DeepDiffTest extends \PHPUnit_Framework_TestCase
 
     public function deepDiffProvider()
     {
-        return array(
-            array(
+        return [
+            [
                 //No features at all
-                array(),
-                array(),
-                array()
-            ),
-            array(
+                [],
+                [],
+                []
+            ],
+            [
                 //Feature one stays the same
-                array('feature one' => 'old feature'),
-                array('feature one' => 'old feature'),
-                array()
-            ),
-            array(
+                ['feature one' => 'old feature'],
+                ['feature one' => 'old feature'],
+                []
+            ],
+            [
                 //Feature one gets removed
-                array('feature one' => 'old feature'),
-                array(),
-                array(
-                    array('feature one', 'old feature', null)
-                )
-            ),
-            array(
+                ['feature one' => 'old feature'],
+                [],
+                [
+                    ['feature one', 'old feature', null]
+                ]
+            ],
+            [
                 //feature one gets added
-                array(),
-                array('feature one' => 'new feature'),
-                array(
-                    array('feature one', null, 'new feature')
-                )
-            ),
-            array(
+                [],
+                ['feature one' => 'new feature'],
+                [
+                    ['feature one', null, 'new feature']
+                ]
+            ],
+            [
                 //Feature one gets updated
-                array('feature one' => 'old feature one'),
-                array('feature one' => 'new feature one'),
-                array(
-                    array('feature one', 'old feature one', 'new feature one')
-                )
-            ),
-            array(
+                ['feature one' => 'old feature one'],
+                ['feature one' => 'new feature one'],
+                [
+                    ['feature one', 'old feature one', 'new feature one']
+                ]
+            ],
+            [
                 //multi feature one&two get updated
-                array(
+                [
                     'feature one' => 'old feature one',
                     'feature two' => 'old feature two'
-                ),
-                array(
+                ],
+                [
                     'feature one' => 'new feature one',
                     'feature two' => 'new feature two'
-                ),
-                array(
-                    array('feature one', 'old feature one', 'new feature one'),
-                    array('feature two', 'old feature two', 'new feature two')
-                )
-            ),
-        );
+                ],
+                [
+                    ['feature one', 'old feature one', 'new feature one'],
+                    ['feature two', 'old feature two', 'new feature two']
+                ]
+            ],
+        ];
     }
 }
