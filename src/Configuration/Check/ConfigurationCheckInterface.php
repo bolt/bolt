186653dbac47fa3db9f4a0a7064d8a322d219044
Gawain Lynch <gawain.lynch@gmail.com>
2015-05-28 22:26:51 +0100

    Add a configuration check interface class

---
+++ src/Configuration/Check/ConfigurationCheckInterface.php
@@ -0,0 +1,28 @@
+<?php
+namespace Bolt\Configuration\Check;
+
+use Silex\Application;
+
+/**
+ * System configuration check interface.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+interface ConfigurationCheckInterface
+{
+    /**
+     * Set the options for the check.
+     *
+     * @param array $options
+     *
+     * @return \Bolt\Configuration\Check\ConfigurationCheckInterface
+     */
+    public function setOptions(array $options);
+
+    /**
+     * Execute the test.
+     *
+     * @return \Bolt\Configuration\Check\Result
+     */
+    public function runCheck();
+}
