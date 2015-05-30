f314f0bcb29968f9bc461d3ad59ffa9460735551
Gawain Lynch <gawain.lynch@gmail.com>
2015-05-30 10:12:08 +0100

    Update app['resources']->getPath() use to be consistent with subpaths

--- src/Cache.php
+++ src/Cache.php
@@ -114,7 +114,7 @@ class Cache extends FilesystemCache
         $this->clearCacheHelper($this->getDirectory(), '', $result);
 
         // Clear the thumbs folder.
-        $this->clearCacheHelper($this->app['resources']->getPath('web') . '/thumbs', '', $result);
+        $this->clearCacheHelper($this->app['resources']->getPath('web/thumbs'), '', $result);
 
         return $result;
     }
