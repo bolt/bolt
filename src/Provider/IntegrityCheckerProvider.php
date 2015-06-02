8946877cebf47bae1e1c3c01db8c119b1419d91b
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-02 10:13:59 +0100

    Add the core table objects to the IntegrityChecker provider

--- src/Provider/IntegrityCheckerProvider.php
+++ src/Provider/IntegrityCheckerProvider.php
@@ -3,6 +3,8 @@
 namespace Bolt\Provider;
 
 use Bolt\Database\IntegrityChecker;
+use Bolt\Database\Table;
+use Doctrine\DBAL\Schema\Schema;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -15,6 +17,20 @@ class IntegrityCheckerProvider implements ServiceProviderInterface
                 return new IntegrityChecker($app);
             }
         );
+
+        $app['integritychecker.tables'] = $app->share(function (Application $app) {
+            return new \Pimple([
+                // @codingStandardsIgnoreStart
+                'authtoken'  => $app->share(function () use ($app) { return new Table\AuthToken(); }),
+                'cron'       => $app->share(function () use ($app) { return new Table\Cron(); }),
+                'log_change' => $app->share(function () use ($app) { return new Table\LogChange(); }),
+                'log_system' => $app->share(function () use ($app) { return new Table\LogSystem(); }),
+                'relations'  => $app->share(function () use ($app) { return new Table\Relations(); }),
+                'taxonomy'   => $app->share(function () use ($app) { return new Table\Taxonomy(); }),
+                'users'      => $app->share(function () use ($app) { return new Table\Users(); }),
+                // @codingStandardsIgnoreEnd
+            ]);
+        });
     }
 
     public function boot(Application $app)
