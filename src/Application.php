ca66fccbe033b97e359aaca8c997a18b26a70bf2
Bob den Otter <bobdenotter@gmail.com>
2015-06-03 11:46:15 +0200

    Bumping version to "2.2.0" for release.

--- src/Application.php
+++ src/Application.php
@@ -33,7 +33,7 @@ class Application extends Silex\Application
     public function __construct(array $values = array())
     {
         $values['bolt_version'] = '2.2.0';
-        $values['bolt_name'] = 'rc2';
+        $values['bolt_name'] = '';
         $values['bolt_released'] = true; // `true` for stable releases, `false` for alpha, beta and RC.
 
         /** @internal Parameter to track a deprecated PHP version */
