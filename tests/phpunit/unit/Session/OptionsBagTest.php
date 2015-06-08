0836e2a3830336e011cb80f866b1d81ace3491a1
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-08 10:38:59 +0100

    [Tests] Cover Bolt\Session\OptionsBag

--- tests/phpunit/unit/Session/OptionsBagTest.php
+++ tests/phpunit/unit/Session/OptionsBagTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace Bolt\Tests\Session;
 
+use Bolt\Session\OptionsBag;
 use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * Class to test src/Session/OptionsBag.
@@ -10,7 +12,41 @@ use Bolt\Tests\BoltUnitTest;
  */
 class OptionsBagTest extends BoltUnitTest
 {
-    public function test()
+    public function testInstanceOf() {
+        $bag = new OptionsBag;
+        $this->assertInstanceOf('Symfony\Component\HttpFoundation\ParameterBag', $bag);
+    }
+
+    public function testGet()
     {
+        $bag = new OptionsBag(['antistatic' => 'polyethylene terephthalate']);
+
+        $this->assertObjectHasAttribute('parameters', $bag);
+        $this->assertTrue($bag->has('antistatic'));
+        $this->assertSame('polyethylene terephthalate', $bag->get('antistatic'));
+    }
+
+    public function testOffsetExists()
+    {
+        $bag = new OptionsBag(['antistatic' => 'polyethylene terephthalate']);
+
+        $this->assertTrue($bag->offsetExists('antistatic'));
+    }
+
+    public function testOffsetSet()
+    {
+        $bag = new OptionsBag();
+        $bag->offsetSet('antistatic', 'polyethylene terephthalate');
+
+        $this->assertTrue($bag->offsetExists('antistatic'));
+    }
+
+    public function testOffsetUnset()
+    {
+        $bag = new OptionsBag(['antistatic' => 'polyethylene terephthalate']);
+        $this->assertTrue($bag->has('antistatic'));
+
+        $bag->offsetUnset('antistatic');
+        $this->assertFalse($bag->has('antistatic'));
     }
 }
