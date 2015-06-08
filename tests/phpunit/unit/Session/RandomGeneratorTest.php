dfc44685d6e4c2851a6b3a8d8d6a95b9e88650a1
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-08 09:33:08 +0100

    [Tests] Cover Bolt\Session\Generator\RandomGenerator

--- tests/phpunit/unit/Session/RandomGeneratorTest.php
+++ tests/phpunit/unit/Session/RandomGeneratorTest.php
@@ -1,6 +1,7 @@
 <?php
 namespace Bolt\Tests\Session;
 
+use Bolt\Session\Generator\RandomGenerator;
 use Bolt\Tests\BoltUnitTest;
 
 /**
@@ -10,7 +11,23 @@ use Bolt\Tests\BoltUnitTest;
  */
 class RandomGeneratorTest extends BoltUnitTest
 {
-    public function test()
+    public function testConstructor()
     {
+        $app = $this->getApp();
+        $fooFighters = new RandomGenerator($app['randomgenerator']);
+
+        $this->assertObjectHasAttribute('generator',  $fooFighters);
+        $this->assertObjectHasAttribute('length',     $fooFighters);
+        $this->assertObjectHasAttribute('characters', $fooFighters);
+    }
+
+    public function testGenerateId()
+    {
+        $app = $this->getApp();
+        $fooFighters = new RandomGenerator($app['randomgenerator'], 42);
+        $daveGrohl = $fooFighters->generateId();
+
+        $this->assertNotSame('Nirvana', $daveGrohl);
+        $this->assertSame(42, strlen($daveGrohl));
     }
 }
