c6d4ee87ba4f8419c5c93056c8e275357e788742
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-09 11:09:18 +0100

    Move the target location constants class to the new namespace and deprecate old one

--- src/Extensions/Snippets/Location.php
+++ src/Extensions/Snippets/Location.php
@@ -1,54 +1,11 @@
 <?php
-
 namespace Bolt\Extensions\Snippets;
 
+use Bolt\Assets\Target;
+
 /**
- * Bolt Snippet Locations
- * This class categorizes all possible snippet locations in constants.
+ * @deprecated since 2.3 will be removed in 3.0
  */
-class Location
+class Location extends Target
 {
-    // unpredictable
-    const BEFORE_CSS = "beforecss";
-    const AFTER_CSS = "aftercss";
-    const BEFORE_JS = "beforejs";
-    const AFTER_JS = "afterjs";
-    const AFTER_META = "aftermeta";
-
-    // main structure
-    const START_OF_HEAD = "startofhead";
-    const END_OF_HEAD = "endofhead";
-    const START_OF_BODY = "startofbody";
-    const END_OF_BODY = "endofbody";
-    const END_OF_HTML = "endofhtml";
-    const AFTER_HTML = "afterhtml";
-
-    // substructure
-    const BEFORE_HEAD_META = "beforeheadmeta";
-    const AFTER_HEAD_META = "afterheadmeta";
-
-    const BEFORE_HEAD_CSS = "beforeheadcss";
-    const AFTER_HEAD_CSS = "afterheadcss";
-
-    const BEFORE_HEAD_JS = "beforeheadjs";
-    const AFTER_HEAD_JS = "afterheadjs";
-
-    const BEFORE_BODY_CSS = "beforebodycss";
-    const AFTER_BODY_CSS = "afterbodycss";
-
-    const BEFORE_BODY_JS = "beforebodyjs";
-    const AFTER_BODY_JS = "afterbodyjs";
-
-    /**
-     * Returns all possible locations (which are constants).
-     *
-     * @return array
-     */
-    public function listAll()
-    {
-        // use reflection for this
-        $reflection = new \ReflectionClass($this);
-
-        return $reflection->getConstants();
-    }
 }
