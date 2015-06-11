abe0456f1ca177162d94f0ea8ce9a65e52cdda4c
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-11 06:32:12 +0100

    Add a class for CSS asset injection

---
+++ src/Assets/File/Stylesheet.php
@@ -0,0 +1,20 @@
+<?php
+namespace Bolt\Assets\File;
+
+/**
+ * Cascading stylesheet file object class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmaill.com>
+ */
+class Stylesheet extends AssetBase
+{
+    /**
+     * Return a string representation of the class.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return sprintf('<link rel="stylesheet" href="%s" media="screen">', $this->fileName);
+    }
+}
