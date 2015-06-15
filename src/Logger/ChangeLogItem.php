2ac1b871e3c157d5cc0061a0045d6288f86a6f2a
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-15 18:57:08 +0100

    Refactor ChangeLogItem::getChangedFields()

--- src/Logger/ChangeLogItem.php
+++ src/Logger/ChangeLogItem.php
@@ -239,8 +239,18 @@ class ChangeLogItem implements \ArrayAccess
         // Get the contenttype that we're dealing with
         $contenttype = $this->app['storage']->getContentType($this->contenttype);
         $fields = $contenttype['fields'];
+        $hash = [
+            'html'        => 'fieldText',
+            'markdown'    => 'fieldText',
+            'textarea'    => 'fieldText',
+            'filelist'    => 'fieldList',
+            'imagelist'   => 'fieldList',
+            'geolocation' => 'fieldGeolocation',
+            'image'       => 'fieldImage',
+            'select'      => 'fieldSelect',
+            'video'       => 'fieldVideo',
+        ];
 
-        //
         foreach ($this->diff as $key => $value) {
             $changedfields[$key] = [
                 'type'   => 'normal',
@@ -255,111 +265,176 @@ class ChangeLogItem implements \ArrayAccess
                 ]
             ];
 
-            switch ($fields[$key]['type']) {
-                case 'html':
-                case 'markdown':
-                case 'textarea':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-
-                    break;
-
-                case 'filelist':
-                case 'imagelist':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-                    $before = json_decode($value[0], true);
-                    $after  = json_decode($value[1], true);
-
-                    $changedfields[$key]['before']['render'] = $before;
-                    $changedfields[$key]['after']['render'] = $after;
-
-                    break;
-
-                case 'geolocation':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-                    $before = json_decode($value[0], true);
-                    $after  = json_decode($value[1], true);
-
-                    $changedfields[$key]['before']['render'] = [
-                        'address'           => $before['address'],
-                        'latitude'          => $before['latitude'],
-                        'longitude'         => $before['longitude'],
-                        'formatted_address' => $before['formatted_address']
-                    ];
-
-                    $changedfields[$key]['after']['render'] = [
-                        'address'           => $after['address'],
-                        'latitude'          => $after['latitude'],
-                        'longitude'         => $after['longitude'],
-                        'formatted_address' => $after['formatted_address']
-                    ];
-
-                    break;
-
-                case 'image':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-
-                    $before = json_decode($value[0], true);
-                    $after  = json_decode($value[1], true);
-
-                    $changedfields[$key]['before']['render'] = [
-                        'file'  => $before['file'],
-                        'title' => $before['title']
-                    ];
-                    $changedfields[$key]['after']['render'] = [
-                        'file'  => $after['file'],
-                        'title' => $after['title']
-                    ];
-
-                    break;
-
-                case 'select':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-
-                    if (isset($fields[$key]['multiple']) && $fields[$key]['multiple']) {
-                        $before = json_decode($value[0], true);
-                        $after  = json_decode($value[1], true);
-                    } else {
-                        $before = $value[0];
-                        $after  = $value[1];
-                    }
-
-                    $changedfields[$key]['before']['render'] = $before;
-                    $changedfields[$key]['after']['render'] = $after;
-
-                    break;
-
-                case 'video':
-                    $changedfields[$key]['type'] = $fields[$key]['type'];
-                    $before = json_decode($value[0], true);
-                    $after  = json_decode($value[1], true);
-
-                    $changedfields[$key]['before']['render'] = [
-                        'url'       => $before['url'],
-                        'title'     => $before['title'],
-                        'width'     => $before['width'],
-                        'height'    => $before['height'],
-                        'html'      => $before['html'],
-                        'thumbnail' => $before['thumbnail']
-                    ];
-
-                    $changedfields[$key]['after']['render'] = [
-                        'url'       => $after['url'],
-                        'title'     => $after['title'],
-                        'width'     => $after['width'],
-                        'height'    => $after['height'],
-                        'html'      => $after['html'],
-                        'thumbnail' => $after['thumbnail']
-                    ];
-
-                    break;
-                case 'text':
-                case 'slug':
-                default:
-                    break;
-
+            if (isset($hash[$fields[$key]['type']])) {
+                $func = $hash[$fields[$key]['type']];
+                $changedfields[$key] = array_merge($changedfields[$key], $this->{$func}($key, $value, $fields));
             }
         }
 
         return $changedfields;
+    }
+
+    /**
+     * Compile changes for text field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldText($key, $value, array $fields)
+    {
+        return ['type' => $fields[$key]['type']];
+    }
+
+    /**
+     * Compile changes for list field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldList($key, $value, array $fields)
+    {
+        return [
+            'type' => $fields[$key]['type'],
+            'before' => ['render' => json_decode($value[0], true)],
+            'after'  => ['render' => json_decode($value[1], true)],
+        ];
+    }
+
+    /**
+     * Compile changes for geolocation field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldGeolocation($key, $value, array $fields)
+    {
+        $before = json_decode($value[0], true);
+        $after  = json_decode($value[1], true);
+
+        return [
+            'type'   => $fields[$key]['type'],
+            'before' => [
+                'render' => [
+                    'address'           => $before['address'],
+                    'latitude'          => $before['latitude'],
+                    'longitude'         => $before['longitude'],
+                    'formatted_address' => $before['formatted_address'],
+                ]
+            ],
+            'after'  => [
+                'render' => [
+                    'address'           => $after['address'],
+                    'latitude'          => $after['latitude'],
+                    'longitude'         => $after['longitude'],
+                    'formatted_address' => $after['formatted_address'],
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Compile changes for image field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldImage($key, $value, array $fields)
+    {
+        $before = json_decode($value[0], true);
+        $after  = json_decode($value[1], true);
+
+        return [
+            'type'   => $fields[$key]['type'],
+            'before' => [
+                'render' => [
+                    'file'  => $before['file'],
+                    'title' => $before['title'],
+                ],
+            ],
+            'after'  => [
+                'render' => [
+                    'file'  => $after['file'],
+                    'title' => $after['title'],
+                ]
+            ]
+        ];
+    }
+
+    /**
+     * Compile changes for select field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldSelect($key, $value, array $fields)
+    {
+        if (isset($fields[$key]['multiple']) && $fields[$key]['multiple']) {
+            $before = json_decode($value[0], true);
+            $after  = json_decode($value[1], true);
+        } else {
+            $before = $value[0];
+            $after  = $value[1];
+        }
+
+        return [
+            'type'   => $fields[$key]['type'],
+            'before' => ['render' => $before],
+            'after'  => ['render' => $after],
+        ];
+
+    }
+
+    /**
+     * Compile changes for video field types.
+     *
+     * @param string $key
+     * @param string $value
+     * @param array  $fields
+     *
+     * @return array
+     */
+    private function fieldVideo($key, $value, array $fields)
+    {
+        $before = json_decode($value[0], true);
+        $after  = json_decode($value[1], true);
+
+        return [
+            'type'   => $fields[$key]['type'],
+            'before' => [
+                'render' => [
+                    'url'       => $before['url'],
+                    'title'     => $before['title'],
+                    'width'     => $before['width'],
+                    'height'    => $before['height'],
+                    'html'      => $before['html'],
+                    'thumbnail' => $before['thumbnail'],
+                ]
+            ],
+            'after'  => [
+                'render' => [
+                    'url'       => $after['url'],
+                    'title'     => $after['title'],
+                    'width'     => $after['width'],
+                    'height'    => $after['height'],
+                    'html'      => $after['html'],
+                    'thumbnail' => $after['thumbnail'],
+                ]
+            ],
+        ];
     }
 }
