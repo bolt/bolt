b3db32ba4981b61b24d2c8c1bff63481a86b5bab
Ross Riley <riley.ross@gmail.com>
2015-06-21 14:58:19 +0100

    correctly inject mock in Authentication test

--- tests/phpunit/unit/Users/AuthenticationTest.php
+++ tests/phpunit/unit/Users/AuthenticationTest.php
@@ -34,7 +34,15 @@ class AuthenticationTest extends BoltUnitTest
     public function testLoginWithUsername()
     {
         // Setup test
-        $users = $this->getMock('Bolt\AccessControl\Authentication', ['login'], [$this->getApp()]);
+        $app = $this->getApp();
+        $users = $this->getMock(
+            'Bolt\AccessControl\Authentication', 
+            ['login'], 
+            [
+                $app,
+                $app['storage']->getRepository('Bolt\Storage\Entity\Authtoken')
+            ]);
+        
         $users->expects($this->once())->method('login')->willReturn(true);
 
         // Run test
