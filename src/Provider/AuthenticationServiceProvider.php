26526decf88aeb1f21b54db1294a7495dce90ccc
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-24 14:55:30 +0100

    Split out providers in AuthenticationServiceProvider

--- src/Provider/AuthenticationServiceProvider.php
+++ src/Provider/AuthenticationServiceProvider.php
@@ -10,12 +10,43 @@ class AuthenticationServiceProvider implements ServiceProviderInterface
 {
     public function register(Application $app)
     {
+        $app['authentication'] = $app->share(
+            function ($app) {
+                $repoAuth = $app['storage']->getRepository('Bolt\Storage\Entity\Authtoken');
+                $repoUser = $app['storage']->getRepository('Bolt\Storage\Entity\Users');
+                $cookieOptions = [
+                    'remoteaddr'   => $app['config']>get('general/cookies_use_remoteaddr', true),
+                    'browseragent' => $app['config']->get('general/cookies_use_browseragent', false),
+                    'httphost'     => $app['config']->get('general/cookies_use_httphost', true),
+                ];
+
+                $tracker= new AccessControl\AccessChecker(
+                    $repoAuth,
+                    $repoUser,
+                    $cookieOptions,
+                    $app['session'],
+                    $app['logger.flash'],
+                    $app['logger.system']
+                );
+
+                return $tracker;
+            }
+        );
+
         $app['authentication.login'] = $app->share(
             function ($app) {
                 $repoAuth = $app['storage']->getRepository('Bolt\Storage\Entity\Authtoken');
                 $repoUser = $app['storage']->getRepository('Bolt\Storage\Entity\Users');
 
-                $login = new AccessControl\Login($app, $repoAuth, $repoUser);
+                $login = new AccessControl\Login(
+                    $repoAuth,
+                    $repoUser,
+                    $app['session'],
+                    $app['logger.flash'],
+                    $app['logger.system'],
+                    $app['randomgenerator'],
+                    $app['config']->get('general/cookies_lifetime', 1209600)
+                );
 
                 return $login;
             }
@@ -24,7 +55,11 @@ class AuthenticationServiceProvider implements ServiceProviderInterface
         $app['authentication.logout'] = $app->share(
             function ($app) {
                 $repoAuth = $app['storage']->getRepository('Bolt\Storage\Entity\Authtoken');
-                $logout = new AccessControl\Logout($repoAuth, $app['session'], $app['logger.flash']);
+                $logout = new AccessControl\Logout(
+                    $repoAuth,
+                    $app['session'],
+                    $app['logger.flash']
+                );
 
                 return $logout;
             }
