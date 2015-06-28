3cd070a0dbb02f6207cb36b8c28423b0465219a1
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-28 17:02:22 +0100

    Add setLimitOrder() protected function

--- src/Storage/Repository/LogChange.php
+++ src/Storage/Repository/LogChange.php
@@ -1,6 +1,7 @@
 <?php
 namespace Bolt\Storage\Repository;
 
+use Bolt\Logger\ChangeLogItem;
 use Bolt\Storage\Repository;
 use Doctrine\DBAL\Query\QueryBuilder;
 
@@ -9,4 +10,31 @@ use Doctrine\DBAL\Query\QueryBuilder;
  */
 class LogChange extends BaseLog
 {
+    /**
+     * Conditionally add LIMIT and ORDER BY to a QueryBuilder query.
+     *
+     * @param QueryBuilder $query
+     * @param array        $options The following options are supported:
+     *                              - 'limit' (int)
+     *                              - 'offset' (int)
+     *                              - 'order' (string)
+     *                              - 'direction' (string)
+     *
+     * @return QueryBuilder
+     */
+    protected function setLimitOrder(QueryBuilder $query, array $options)
+    {
+        if (isset($options['order'])) {
+            $query->orderBy($options['order'], $options['direction']);
+        }
+        if (isset($options['limit'])) {
+            $query->setMaxResults(intval($options['limit']));
+
+            if (isset($options['offset'])) {
+                $query->setFirstResult(intval($options['offset']));
+            }
+        }
+
+        return $query;
+    }
 }
