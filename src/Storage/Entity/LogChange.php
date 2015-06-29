13b847fc184f5c4aa3c5b576bee46abaec084968
Gawain Lynch <gawain.lynch@gmail.com>
2015-06-29 14:35:46 +0100

    Add @method annotations

--- src/Storage/Entity/LogChange.php
+++ src/Storage/Entity/LogChange.php
@@ -2,7 +2,26 @@
 namespace Bolt\Storage\Entity;
 
 /**
- * Entity for Auth Tokens.
+ * Entity for change logs.
+ *
+ * @method integer   getId()
+ * @method \DateTime getDate()
+ * @method integer   getOwnerid()
+ * @method string    getTitle()
+ * @method string    getContenttype()
+ * @method integer   getContentid()
+ * @method string    getMutationType()
+ * @method array     getDiff()
+ * @method string    getComment()
+ * @method setId($id)
+ * @method setDate(\DateTime $date)
+ * @method setOwnerid($ownerid)
+ * @method setTitle($title)
+ * @method setContenttype($contenttype)
+ * @method setContentid($contentid)
+ * @method setMutationType($mutationType)
+ * @method setDiff($diff)
+ * @method setComment($comment)
  */
 class LogChange extends Entity
 {
--- src/Storage/Entity/LogSystem.php
+++ src/Storage/Entity/LogSystem.php
@@ -2,7 +2,26 @@
 namespace Bolt\Storage\Entity;
 
 /**
- * Entity for Auth Tokens.
+ * Entity for system logs.
+ *
+ * @method integer   getId()
+ * @method integer   getLevel()
+ * @method \DateTime getDate()
+ * @method string    getMessage()
+ * @method integer   getOwnerid()
+ * @method string    getRoute()
+ * @method string    getIp()
+ * @method string    getContext()
+ * @method array     getSource()
+ * @method setId($id)
+ * @method setLevel($level)
+ * @method setDate(\DateTime $date)
+ * @method setMessage($message)
+ * @method setOwnerid($ownerid)
+ * @method setRoute($route)
+ * @method setIp($ip)
+ * @method setContext($context)
+ * @method setSource($source)
  */
 class LogSystem extends Entity
 {
