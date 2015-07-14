52a30e8ffe17140722d7265cee9b29d04cec730d
Fabien Schurter <fabien.schurter@gmail.com>
2015-07-14 06:36:56 +0200

    Add some test cases for TranslationServiceProvider

--- tests/phpunit/unit/Provider/TranslationServiceProviderTest.php
+++ tests/phpunit/unit/Provider/TranslationServiceProviderTest.php
@@ -1,8 +1,11 @@
 <?php
 namespace Bolt\Tests\Provider;
 
+use Bolt\Application;
+use Bolt\Configuration\ResourceManager;
 use Bolt\Provider\TranslationServiceProvider;
 use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class to test src/Provider/TranslationServiceProvider.
@@ -28,5 +31,70 @@ class TranslationServiceProviderTest extends BoltUnitTest
         $app->register($provider);
         $app->boot();
         $this->assertEquals('de_XX', $app['translator']->getLocale());
+    }
+
+    public function testDefaultTranslationLoading()
+    {
+        $app = $this->makeApp();
+        $app->initialize();
+        $this->registerTranslationServiceWithCachingDisabled($app);
+        $app->boot();
+        $this->assertEquals('About', $app['translator']->trans('general.about'));
+    }
+
+    public function testShortLocaleFallback()
+    {
+        $app = $this->makeApp();
+        $this->initializeFakeTranslationFiles('xx', 'general.about: "So very about"', $app['resources']);
+        $app->initialize();
+        $app['locale'] = 'xx_XX';
+        $this->registerTranslationServiceWithCachingDisabled($app);
+        $app->boot();
+        $this->assertEquals('So very about', $app['translator']->trans('general.about'));
+    }
+
+    public function testTranslationLoadingOverride()
+    {
+        $app = $this->makeApp();
+        $this->initializeFakeTranslationFiles('en_GB', 'general.about: "Not so about"', $app['resources']);
+        $app->initialize();
+        $this->registerTranslationServiceWithCachingDisabled($app);
+        $app->boot();
+        $this->assertEquals('Not so about', $app['translator']->trans('general.about'));
+    }
+
+    /**
+     * It is mandatory to disable translation caching, otherwise it creates
+     * difficulties with regard to testing in isolation.
+     *
+     * For example, without disabling translation caching, `testDefaultTranslationLoading()`
+     * passes, but the following tests fail, as the cached messages are loaded
+     * instead of re-building the whole message catalogue.
+     *
+     * @param Application $app
+     */
+    protected function registerTranslationServiceWithCachingDisabled(Application $app)
+    {
+        $app->register(
+            new \Silex\Provider\TranslationServiceProvider(),
+            [
+                'translator.cache_dir' => null,
+                'locale_fallbacks'     => [Application::DEFAULT_LOCALE],
+            ]
+        );
+    }
+
+    /**
+     * @param string          $locale
+     * @param string          $fileContent
+     * @param ResourceManager $resources
+     */
+    protected function initializeFakeTranslationFiles($locale, $fileContent, ResourceManager $resources)
+    {
+        $fakeAppRoot        = $resources->getPath('cache');
+        $fakeTranslationDir = "{$fakeAppRoot}/app/resources/translations/{$locale}";
+        (new Filesystem())->mkdir($fakeTranslationDir);
+        file_put_contents("{$fakeTranslationDir}/messages.{$locale}.yml", $fileContent);
+        $resources->setPath('root', $fakeAppRoot);
     }
 }
