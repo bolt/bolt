3d1bef1f817490613159353a139ac302f5224ea2
rarila <magnet@gmx.net>
2015-07-14 16:15:28 +0200

    Use shortcut $contenttypeslug instead of $contenttype['slug']

--- src/Storage/RecordModifier.php
+++ src/Storage/RecordModifier.php
@@ -299,9 +299,9 @@ class RecordModifier
         // Build context for Twig
         $contextCan = [
             'upload'             => $this->app['users']->isAllowed('files:uploads'),
-            'publish'            => $this->app['users']->isAllowed('contenttype:' . $contenttype['slug'] . ':publish:' . $content['id']),
-            'depublish'          => $this->app['users']->isAllowed('contenttype:' . $contenttype['slug'] . ':depublish:' . $content['id']),
-            'change_ownership'   => $this->app['users']->isAllowed('contenttype:' . $contenttype['slug'] . ':change-ownership:' . $content['id']),
+            'publish'            => $this->app['users']->isAllowed('contenttype:' . $contenttypeslug . ':publish:' . $content['id']),
+            'depublish'          => $this->app['users']->isAllowed('contenttype:' . $contenttypeslug . ':depublish:' . $content['id']),
+            'change_ownership'   => $this->app['users']->isAllowed('contenttype:' . $contenttypeslug . ':change-ownership:' . $content['id']),
         ];
         $contextHas = [
             'incoming_relations' => is_array($content->relation),
