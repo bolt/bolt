afff1f398dc43654682a2a6690066a9a59003089
Ross Riley <riley.ross@gmail.com>
2015-07-18 21:35:34 +0100

    new filter class that stores a composite expression and associated parameters

---
+++ src/Storage/Query/Filter.php
@@ -0,0 +1,46 @@
+<?php 
+
+namespace Bolt\Storage\Query;
+
+use Doctrine\DBAL\Query\Expression\CompositeExpression;
+
+/**
+*  This class holds a set of filters that are made up of expressions and
+*  values. It delegates parsing to the QueryParameterParser class
+* 
+* 
+*  @author Ross Riley <riley.ross@gmail.com>
+*/
+class Filter
+{
+    
+    protected $filters;
+    protected $parser;
+    
+    protected $expression;
+    protected $parameters = [];
+    
+    
+    public function getExpression()
+    {
+        return $this->expression->__toString();
+    }
+    
+    public function setExpression(CompositeExpression $expression)
+    {
+        $this->expression = $expression;
+    }
+    
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+    
+    public function setParameters(array $parameters)
+    {
+        $this->parameters = $parameters;
+    }
+    
+    
+    
+}
