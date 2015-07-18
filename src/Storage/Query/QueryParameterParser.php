95915c736c4c9eb0c5032900bf85c93b0c1555f2
Ross Riley <riley.ross@gmail.com>
2015-07-18 21:37:10 +0100

    add support for composite expression parsing

--- src/Storage/Query/QueryParameterParser.php
+++ src/Storage/Query/QueryParameterParser.php
@@ -1,8 +1,11 @@
 <?php 
 namespace Bolt\Storage\Query;
 
+use Bolt\Exception\QueryParseException;
 use Bolt\Storage\EntityManager;
+use Bolt\Storage\Query\Filter;
 use Doctrine\Common\Collections\Expr\Comparison;
+use Doctrine\DBAL\Query\Expression\ExpressionBuilder;
 
 
 /**
@@ -16,59 +19,121 @@ class QueryParameterParser
     
     protected $key;
     protected $value;
+    protected $expr;
     
     protected $valueMatchers = [];
     protected $keyMatchers = [];
     
     
-    public function __construct($key, $value = null)
+    public function __construct($key, $value = null, ExpressionBuilder $expression = null)
     {
         $this->key = $key;
         $this->value = $value;
+        $this->expr = $expression;
         $this->setupDefaults();
     }
     
     public function setupDefaults()
     {
-        $this->addValueMatcher('<(\w+)' , ['value'=>"$1", 'operator' => Comparison::LT]);
-        $this->addValueMatcher('<=(\w+)', ['value'=>"$1", 'operator' => Comparison::LTE]);
-        $this->addValueMatcher('>=(\w+)', ['value'=>"$1", 'operator' => Comparison::GTE]);
-        $this->addValueMatcher('>(\w+)' , ['value'=>"$1", 'operator' => Comparison::GT]);
-        $this->addValueMatcher('!(\w+)',  ['value'=>"$1", 'operator' => Comparison::NEQ]);
-        $this->addValueMatcher('(%\w+|\w+%|%\w+%)',  ['value'=>"$1", 'operator' => 'LIKE']);
-        $this->addValueMatcher(
-            '(\w+) ?\|\| ?(\w+)',  
-            [
-                'value'=>'$1,$2', 
-                'operator' => 'orX'
-            ]);
-        $this->addValueMatcher('(\w+)',   ['value'=>"$1", 'operator' => Comparison::EQ]);
+        $this->addValueMatcher('<(\w+)' , ['value'=>"$1", 'operator' => 'lt']);
+        $this->addValueMatcher('<=(\w+)', ['value'=>"$1", 'operator' => 'lte']);
+        $this->addValueMatcher('>=(\w+)', ['value'=>"$1", 'operator' => 'gte']);
+        $this->addValueMatcher('>(\w+)' , ['value'=>"$1", 'operator' => 'gt']);
+        $this->addValueMatcher('!$',      ['value'=>"",   'operator' => 'isNotNull']);
+        $this->addValueMatcher('!(\w+)',  ['value'=>"$1", 'operator' => 'neq']);
+        $this->addValueMatcher('!\[([\w ,]+)\]',  ['value'=>"$1", 'operator' => 'notIn']);
+        $this->addValueMatcher('\[([\w ,]+)\]',  ['value'=>"$1", 'operator' => 'in']);
+        $this->addValueMatcher('(%\w+|\w+%|%\w+%)',  ['value'=>"$1", 'operator' => 'like']);
+        $this->addValueMatcher('(\w+)',   ['value'=>"$1", 'operator' => 'eq']);
     }
     
     /**
      * Runs the keys/values through the relevant parsers
      * 
-     * @return array matched values
+     * @return Expression matched values
+     * @throws Bolt\Exception\QueryParseException
      */
-    public function parse()
+    public function getFilter()
     {
+        $filter = new Filter();
+        $filterParams = [];
+        if (!$this->expr instanceof ExpressionBuilder) {
+            throw new QueryParseException("Cannot call method without an Expression Builder parameter set", 1);
+        }
+        
+        if (strpos($this->value, '&&') && strpos($this->value, '||')) {
+            throw new QueryParseException("Mixed && and || operators are not supported", 1);
+        }
+        
+        if (strpos($this->key, '|||')){
+            $keys = preg_split('/ *(\|\|\|) */', $this->key);
+            $values = preg_split('/ *(\|\|\|) */', $this->value);
+            $values = array_pad($values, count($keys), end($values));
+            $parts = [];
+            $count = 1;
+            while (($key = array_shift($keys)) && ($val = array_shift($values))) {
+                $val = $this->parseValue($val);
+                $placeholder = $key."_".$count;
+                $filterParams[$placeholder] = $val['value'];
+                $exprMethod = $val['operator'];
+                $parts[] = $this->expr->$exprMethod($key, ":$placeholder");
+                $count++;
+            }
+            
+            $filter->setExpression(call_user_func_array([$this->expr, 'orX'], $parts));
+            $filter->setParameters($filterParams);
+            return $filter;
+        }
+        
+        
+        if (strpos($this->value, '&&') || strpos($this->value, '||')) {
+            $values = preg_split('/ *(&&|\|\|) */', $this->value, -1, PREG_SPLIT_DELIM_CAPTURE);
+            $op = $values[1];
+            $key = $this->key;
+            
+            if ($op === '&&') {
+                $comparison = 'andX';
+            } elseif($op === '||') {
+                $comparison = 'orX';
+            }
+            
+
+            $values = array_diff($values, ['&&', '||']);
+            $parts = [];
+            $count = 1;
+            while ($val = array_shift($values)) {
+                $val = $this->parseValue($val);
+                $placeholder = $key."_".$count;
+                $filterParams[$placeholder] = $val['value'];
+                $exprMethod = $val['operator'];
+                $parts[] = $this->expr->$exprMethod($key, ":$placeholder");
+                $count++;
+            }
+            
+            $filter->setExpression(call_user_func_array([$this->expr, $comparison], $parts));
+            $filter->setParameters($filterParams);
+            return $filter;          
+        }        
+        
+        return $filter;
+    }
+        
+    public function parseValue($value = null)
+    {
+        if (!$value) {
+            $value = $this->value;
+        }
         foreach ($this->valueMatchers as $matcher) {
             $regex = sprintf('/%s/', $matcher['token']);
             $values = $matcher['params'];
-            $values['key'] = $this->key;
-            if (preg_match($regex, $this->value)) {
-                $values['value'] = preg_replace($regex, $values['value'], $this->value);
+            if (preg_match($regex, $value)) {
+                $values['value'] = preg_replace($regex, $values['value'], $value);
                 $values['matched'] = $matcher['token'];
                 return $values;
             }
         }
-        
     }
     
-    public function getExpression()
-    {
-        
-    }
     
     /**
      * Adds an additional token to parse for value parameters
