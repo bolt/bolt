99daf7c3f20be783bdf8b5ad91788dbbdaa29223
Ross Riley <riley.ross@gmail.com>
2015-07-19 22:26:07 +0100

    kill this, now implemented in the Query class

--- src/Storage/Repository/ContentRepository.php
+++ src/Storage/Repository/ContentRepository.php
@@ -1,7 +1,7 @@
 <?php
+
 namespace Bolt\Storage\Repository;
 
-use Bolt\Storage\Query\ContentQueryParser;
 use Bolt\Storage\Repository;
 
 /**
@@ -9,26 +9,4 @@ use Bolt\Storage\Repository;
  */
 class ContentRepository extends Repository
 {
-    
-    /**
-     * getContent based on a 'human readable query'.
-     *
-     * Used directly by {% setcontent %} but also in other parts.
-     * For reference refer to @link https://docs.bolt.cm/content-fetching
-     *
-     * @param string $textquery
-     * @param string $parameters
-     *
-     * @return array
-     */
-    public function getContent($query, $parameters = null)
-    {
-        $parser = new ContentQueryParser($this->getEntityManager(), $query, $parameters);
-        $query = $parser->getQuery();
-        foreach($query as $q) {
-            var_dump($q->getSQL());
-        }
-        
-    }
-
 }
