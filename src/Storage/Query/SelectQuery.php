d49c41c410b71ef700932d064ab5241f4e5a91de
Ross Riley <riley.ross@gmail.com>
2015-07-19 09:10:58 +0100

    code quality

--- src/Storage/Query/SelectQuery.php
+++ src/Storage/Query/SelectQuery.php
@@ -1,38 +1,38 @@
-<?php 
+<?php
+
 namespace Bolt\Storage\Query;
 
-use Bolt\Storage\EntityManager;
 use Doctrine\DBAL\Query\QueryBuilder;
 
 /**
-*  This query class coordinates a select query build from Bolt's
-*  custom query DSL as documented here:
-*  @link https://docs.bolt.cm/content-fetching
-* 
-*  The resulting QueryBuilder object is then passed through to the individual
-*  field handlers where they can perform value transformations.
-* 
-*  @author Ross Riley <riley.ross@gmail.com>
-*/
+ *  This query class coordinates a select query build from Bolt's
+ *  custom query DSL as documented here:.
+ *
+ *  @link https://docs.bolt.cm/content-fetching
+ *
+ *  The resulting QueryBuilder object is then passed through to the individual
+ *  field handlers where they can perform value transformations.
+ *
+ *  @author Ross Riley <riley.ross@gmail.com>
+ */
 class SelectQuery
 {
-    
     protected $qb;
     protected $contenttypes;
     protected $params;
     protected $filters = [];
-    
+
     /**
-     * 
      * @param QueryBuilder $qb
      */
-    public function __construct(QueryBuilder $qb, array $contenttypes, array $params = []) {
+    public function __construct(QueryBuilder $qb, array $contenttypes, array $params = [])
+    {
         $this->qb = $qb;
         $this->contenttypes = $contenttypes;
         $this->params = $params;
         $this->processFilters();
     }
-    
+
     public function processFilters()
     {
         foreach ($this->params as $key => $value) {
@@ -40,38 +40,32 @@ class SelectQuery
             $this->addFilter($parser->getFilter());
         }
     }
-    
+
     public function getWhereExpression()
     {
         $expr = $this->qb->expr()->andX();
         foreach ($this->filters as $filter) {
-            $expr = $expr->add($filter->getExpression()); 
+            $expr = $expr->add($filter->getExpression());
         }
-        
+
         return $expr;
     }
-    
+
     public function getWhereParameters()
     {
         $params = [];
         foreach ($this->filters as $filter) {
             $params = array_merge($params, $filter->getParameters());
         }
-        
+
         return $params;
     }
-    
+
     /**
-     * 
      * @param Filter $filter
      */
     public function addFilter(Filter $filter)
     {
-        $this->filters[] = $filter;    
+        $this->filters[] = $filter;
     }
-    
-    
-    
-    
-    
 }
