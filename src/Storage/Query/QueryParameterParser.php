5adcce6d70e76e8b9abc110d1bdf204370d1b6e9
Ross Riley <riley.ross@gmail.com>
2015-07-19 08:59:00 +0100

    separated all the filter handlers into stackable callable methods

--- src/Storage/Query/QueryParameterParser.php
+++ src/Storage/Query/QueryParameterParser.php
@@ -1,138 +1,216 @@
-<?php 
+<?php
+
 namespace Bolt\Storage\Query;
 
 use Bolt\Exception\QueryParseException;
-use Bolt\Storage\EntityManager;
-use Bolt\Storage\Query\Filter;
-use Doctrine\Common\Collections\Expr\Comparison;
+
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\DBAL\Query\Expression\ExpressionBuilder;
 
-
 /**
-*  Handler class to convert the DSL for content query parameters
-*  into equivalent DBAL expressions.
-* 
-*  @author Ross Riley <riley.ross@gmail.com>
-*/
+ *  Handler class to convert the DSL for content query parameters
+ *  into equivalent DBAL expressions.
+ *
+ *  @author Ross Riley <riley.ross@gmail.com>
+ */
 class QueryParameterParser
 {
-    
     protected $key;
     protected $value;
     protected $expr;
-    
+
     protected $valueMatchers = [];
-    protected $keyMatchers = [];
-    
-    
+    protected $filterHandlers = [];
+
     public function __construct($key, $value = null, QueryBuilder $qb = null)
     {
         $this->key = $key;
         $this->value = $value;
-        
+
         if ($qb) {
-            $this->expr = $qb->expr();   
+            $this->expr = $qb->expr();
         }
         $this->setupDefaults();
     }
-    
+
     public function setupDefaults()
     {
-        $this->addValueMatcher('<(\w+)' , ['value'=>"$1", 'operator' => 'lt']);
-        $this->addValueMatcher('<=(\w+)', ['value'=>"$1", 'operator' => 'lte']);
-        $this->addValueMatcher('>=(\w+)', ['value'=>"$1", 'operator' => 'gte']);
-        $this->addValueMatcher('>(\w+)' , ['value'=>"$1", 'operator' => 'gt']);
-        $this->addValueMatcher('!$',      ['value'=>"",   'operator' => 'isNotNull']);
-        $this->addValueMatcher('!(\w+)',  ['value'=>"$1", 'operator' => 'neq']);
-        $this->addValueMatcher('!\[([\w ,]+)\]',  ['value'=>"$1", 'operator' => 'notIn']);
-        $this->addValueMatcher('\[([\w ,]+)\]',  ['value'=>"$1", 'operator' => 'in']);
-        $this->addValueMatcher('(%\w+|\w+%|%\w+%)',  ['value'=>"$1", 'operator' => 'like']);
-        $this->addValueMatcher('(\w+)',   ['value'=>"$1", 'operator' => 'eq']);
+        $this->addValueMatcher('<(\w+)', ['value' => '$1', 'operator' => 'lt']);
+        $this->addValueMatcher('<=(\w+)', ['value' => '$1', 'operator' => 'lte']);
+        $this->addValueMatcher('>=(\w+)', ['value' => '$1', 'operator' => 'gte']);
+        $this->addValueMatcher('>(\w+)', ['value' => '$1', 'operator' => 'gt']);
+        $this->addValueMatcher('!$',      ['value' => '',   'operator' => 'isNotNull']);
+        $this->addValueMatcher('!(\w+)',  ['value' => '$1', 'operator' => 'neq']);
+        $this->addValueMatcher('!\[([\w ,]+)\]',  ['value' => '$1', 'operator' => 'notIn']);
+        $this->addValueMatcher('\[([\w ,]+)\]',  ['value' => '$1', 'operator' => 'in']);
+        $this->addValueMatcher('(%\w+|\w+%|%\w+%)',  ['value' => '$1', 'operator' => 'like']);
+        $this->addValueMatcher('(\w+)',   ['value' => '$1', 'operator' => 'eq']);
+
+        $this->addFilterHandler([$this, 'defaultFilterHandler']);
+        $this->addFilterHandler([$this, 'multipleValueHandler']);
+        $this->addFilterHandler([$this, 'multipleKeyAndValueHandler']);
+        $this->addFilterHandler([$this, 'incorrectQueryHandler']);
     }
-    
+
     /**
-     * Runs the keys/values through the relevant parsers
-     * 
+     * Runs the keys/values through the relevant parsers.
+     *
      * @return Expression matched values
+     *
      * @throws Bolt\Exception\QueryParseException
      */
     public function getFilter()
     {
-        $filter = new Filter();
-        $filterParams = [];
-        
-        // This first block makes sure that invalid queries are caught
         if (!$this->expr instanceof ExpressionBuilder) {
-            throw new QueryParseException("Cannot call method without an Expression Builder parameter set", 1);
+            throw new QueryParseException('Cannot call method without an Expression Builder parameter set', 1);
         }
-        
-        if (strpos($this->value, '&&') && strpos($this->value, '||')) {
-            throw new QueryParseException("Mixed && and || operators are not supported", 1);
+
+        foreach ($this->filterHandlers as $callback) {
+            $result = $callback($this->key, $this->value, $this->expr);
+            if ($result instanceof Filter) {
+                return $result;
+            }
         }
-        
-        // This block handles triple pipe queries
-        if (strpos($this->key, '|||')){
-            $keys = preg_split('/ *(\|\|\|) */', $this->key);
-            $values = preg_split('/ *(\|\|\|) */', $this->value);
+    }
+
+    /**
+     * Handles some errors in key/value string formatting.
+     *
+     * @param string             $key
+     * @param string             $value
+     * @param ExpressionBuilder  $expr
+     */
+    public function incorrectQueryHandler($key, $value, $expr)
+    {
+        if (strpos($value, '&&') && strpos($value, '||')) {
+            throw new QueryParseException('Mixed && and || operators are not supported', 1);
+        }
+    }
+
+    /**
+     * This handler processes 'triple pipe' queries as implemented in Bolt
+     * It looks for three pipes in the key and value and creates an OR composite
+     * expression for example: 'username|||email':'fred|||pete'.
+     *
+     *
+     * @param string            $key
+     * @param string            $value
+     * @param ExpressionBuilder $expr
+     *
+     * @return Filter|void
+     */
+    public function multipleKeyAndValueHandler($key, $value, $expr)
+    {
+        if (strpos($key, '|||')) {
+            $keys = preg_split('/ *(\|\|\|) */', $key);
+            $values = preg_split('/ *(\|\|\|) */', $value);
             $values = array_pad($values, count($keys), end($values));
+
+            $filterParams = [];
             $parts = [];
             $count = 1;
+
             while (($key = array_shift($keys)) && ($val = array_shift($values))) {
                 $val = $this->parseValue($val);
-                $placeholder = $key."_".$count;
+                $placeholder = $key.'_'.$count;
                 $filterParams[$placeholder] = $val['value'];
                 $exprMethod = $val['operator'];
                 $parts[] = $this->expr->$exprMethod($key, ":$placeholder");
                 $count++;
             }
-            
-            $filter->setExpression(call_user_func_array([$this->expr, 'orX'], $parts));
+
+            $filter = new Filter();
+            $filter->setExpression(call_user_func_array([$expr, 'orX'], $parts));
             $filter->setParameters($filterParams);
+
             return $filter;
         }
-        
-        // This block handles the parse if the query is a composite and / or filter
-        if (strpos($this->value, '&&') || strpos($this->value, '||')) {
-            $values = preg_split('/ *(&&|\|\|) */', $this->value, -1, PREG_SPLIT_DELIM_CAPTURE);
+    }
+
+    /**
+     * This handler processes multiple value queries as defined in the Bolt 'Fetching Content'
+     * documentation. It allows a value to be parsed to and AND/OR expression.
+     *
+     * For example, this handler will correctly parse values like:
+     *     'username': 'fred||bob'
+     *     'id': '<5 && !1'
+     *
+     * @param string            $key
+     * @param string            $value
+     * @param ExpressionBuilder $expr
+     *
+     * @return Filter|void
+     */
+    public function multipleValueHandler($key, $value, $expr)
+    {
+        if (strpos($value, '&&') || strpos($value, '||')) {
+            $values = preg_split('/ *(&&|\|\|) */', $value, -1, PREG_SPLIT_DELIM_CAPTURE);
             $op = $values[1];
-            $key = $this->key;
-            
+
             if ($op === '&&') {
                 $comparison = 'andX';
-            } elseif($op === '||') {
+            } elseif ($op === '||') {
                 $comparison = 'orX';
             }
-            
 
             $values = array_diff($values, ['&&', '||']);
+
+            $filterParams = [];
             $parts = [];
             $count = 1;
+
             while ($val = array_shift($values)) {
                 $val = $this->parseValue($val);
-                $placeholder = $key."_".$count;
+                $placeholder = $key.'_'.$count;
                 $filterParams[$placeholder] = $val['value'];
                 $exprMethod = $val['operator'];
                 $parts[] = $this->expr->$exprMethod($key, ":$placeholder");
                 $count++;
             }
-            
-            $filter->setExpression(call_user_func_array([$this->expr, $comparison], $parts));
+
+            $filter = new Filter();
+            $filter->setExpression(call_user_func_array([$expr, $comparison], $parts));
             $filter->setParameters($filterParams);
-            return $filter;          
+
+            return $filter;
         }
-        
-        // Finally this block handle the simple key to value queries     
-        $val = $this->parseValue($this->value);
-        $key = $this->key;
-        $placeholder = $key."_1";
+    }
+
+    /**
+     * The default handler is the last to be run and handler simple value parsing.
+     *
+     * @param string            $key
+     * @param string            $value
+     * @param ExpressionBuilder $expr
+     *
+     * @return Filter
+     */
+    public function defaultFilterHandler($key, $value, $expr)
+    {
+        $val = $this->parseValue($value);
+        $placeholder = $key.'_1';
         $exprMethod = $val['operator'];
-        $filter->setExpression($this->expr->andX($this->expr->$exprMethod($key, ":$placeholder")));
+
+        $filter = new Filter();
+        $filter->setExpression($expr->andX($expr->$exprMethod($key, ":$placeholder")));
         $filter->setParameters([$val['value']]);
-        
+
         return $filter;
     }
-        
+
+    /**
+     * This method uses the defined value matchers to parse a passed in value to the following
+     * component parts:
+     * [
+     *     'value' => <the value remaining after the parse>
+     *     'operator'=> <the operator that should be used>
+     *     'matched' => <the pattern that the value matched>
+     * ].
+     *
+     * @param string $value Value to process
+     *
+     * @return array parsed values
+     */
     public function parseValue($value = null)
     {
         if (!$value) {
@@ -144,24 +222,41 @@ class QueryParameterParser
             if (preg_match($regex, $value)) {
                 $values['value'] = preg_replace($regex, $values['value'], $value);
                 $values['matched'] = $matcher['token'];
+
                 return $values;
             }
         }
     }
-    
-    
+
     /**
-     * Adds an additional token to parse for value parameters
-     * 
-     * @param string $token  regex pattern to match against
-     * @param array  $params array of options to provide to the matched param
+     * The goal of this class is to turn any key:value into a Filter class.
+     * Adding a handler here will push the new filter callback onto the top
+     * of the Queue along with the built in defaults.
+     *
+     * Note: the callback should either return nothing or an instance of Bolt\Storage\Query\Filter
+     *
+     * @param Callable $handler [description]
      */
-    public function addValueMatcher($token, $params = [])
+    public function addFilterHandler(callable $handler)
     {
-        $this->valueMatchers[] = ['token'=>$token, 'params'=>$params];
+        array_unshift($this->filterHandlers, $handler);
     }
-    
-    
-    
-    
+
+    /**
+     * Adds an additional token to parse for value parameters.
+     *
+     * This gives the ability to define additional value -> operator matches
+     *
+     * @param string $token    regex pattern to match against
+     * @param array  $params   array of options to provide to the matched param
+     * @param bool   $priority if set item will be prepended to start of list
+     */
+    public function addValueMatcher($token, $params = [], $priority = null)
+    {
+        if ($priority) {
+            array_unshift($this->valueMatchers, ['token' => $token, 'params' => $params]);
+        } else {
+            $this->valueMatchers[] = ['token' => $token, 'params' => $params];
+        }
+    }
 }
