c44d47639054c5509ea29d96e372262b1cfc8322
Ross Riley <riley.ross@gmail.com>
2015-07-19 07:59:16 +0100

    first tests on select query

--- tests/phpunit/unit/Storage/Query/SelectQueryTest.php
+++ tests/phpunit/unit/Storage/Query/SelectQueryTest.php
@@ -10,14 +10,21 @@ use Bolt\Storage\Query\SelectQuery;
  *
  * @author Ross Riley <riley.ross@gmail.com>
  */
-class ContentQueryParserTest extends BoltUnitTest
+class selectQueryTest extends BoltUnitTest
 {
     public function testQuery()
     {
         $app = $this->getApp();
         
         $qb = $app['storage']->createQueryBuilder();
-        //$query = new SelectQuery($qb);
+        
+        $contenttypes = ['pages'];
+        $filters = ['username'=>'%fred%', 'email'=>'%fred', 'status'=>'published'];
+        
+        $query = new SelectQuery($qb, $contenttypes, $filters);
+        $expr = $query->getWhereExpression();
+        $this->assertEquals('(username LIKE :username_1) AND (email LIKE :email_1) AND (status = :status_1)', $expr->__toString());
+        $this->assertEquals(['%fred%','%fred','published'], $query->getWhereParameters());
     }
     
 }
