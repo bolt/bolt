d4bf2c30bcf075cf950941668e4cbbbc2453b09d
Ross Riley <riley.ross@gmail.com>
2015-07-19 22:37:08 +0100

    psr fixes

--- tests/phpunit/unit/Storage/Query/QueryTest.php
+++ tests/phpunit/unit/Storage/Query/QueryTest.php
@@ -5,21 +5,19 @@ namespace Bolt\Tests\Storage\Query;
 use Bolt\Tests\BoltUnitTest;
 
 /**
- * Class to test src/Storage/Query/Content.
+ * Class to test src/Storage/Query/QueryTest.
  *
  * @author Ross Riley <riley.ross@gmail.com>
  */
 class QueryTest extends BoltUnitTest
 {
-    public function testOperation()
+    public function testgetContent()
     {
         $app = $this->getApp();
         $this->addSomeContent();
-        
-        $results = $app['query']->getContent('pages', ['id'=>'<10']);
-        
-        $this->assertInstanceOf('Bolt\Storage\Query\QueryResultset', $results);
 
+        $results = $app['query']->getContent('pages', ['id' => '<10']);
+
+        $this->assertInstanceOf('Bolt\Storage\Query\QueryResultset', $results);
     }
-    
 }
