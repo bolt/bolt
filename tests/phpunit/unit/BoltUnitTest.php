c0ae46201dd4f9972ec4824426186f59188bf29c
Gawain Lynch <gawain.lynch@gmail.com>
2015-07-21 17:15:12 +0100

    [Tests] Set config path early

--- tests/phpunit/unit/BoltUnitTest.php
+++ tests/phpunit/unit/BoltUnitTest.php
@@ -50,6 +50,7 @@ abstract class BoltUnitTest extends \PHPUnit_Framework_TestCase
         ->getMock();
 
         $config = new Standard(TEST_ROOT);
+        $config->setPath('config', PHPUNIT_WEBROOT . '/app/config');
         $config->verify();
 
         $bolt = new Application(array('resources' => $config));
