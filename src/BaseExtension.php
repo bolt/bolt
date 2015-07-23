3b316f800bd12868177bd9b17ba1e381604f29db
rarila <magnet@gmx.net>
2015-07-23 09:18:29 +0200

    Revert c3c6481b2d bevause of BC

--- src/BaseExtension.php
+++ src/BaseExtension.php
@@ -444,13 +444,13 @@ abstract class BaseExtension implements ExtensionInterface
      * @param string $callback
      * @param array  $extraparameters
      */
-    public function addSnippet($location, $callback, array $extraparameters = [])
+    public function addSnippet($location, $callback, $extraparameters = [])
     {
         if ($callback instanceof BoltResponse) {
             $callback = (string) $callback;
         }
 
-        $this->app['asset.queue.snippet']->add($location, $callback, $this->getName(), $extraparameters);
+        $this->app['asset.queue.snippet']->add($location, $callback, $this->getName(), (array) $extraparameters);
     }
 
     /**
