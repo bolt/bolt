513599079a5e76c63056871ba93bfeab090ae8dc
rarila <magnet@gmx.net>
2015-07-25 07:46:41 +0200

    Use buic.select for relationships

--- app/src/js/modules/fields/relationship.js
+++ app/src/js/modules/fields/relationship.js
@@ -28,8 +28,7 @@
      * @param {FieldConf} fconf
      */
     relationship.init = function (fieldset, fconf) {
-        var select = $(fieldset).find('select'),
-            selectNone = $(fieldset).find('.select-none');
+        var select = $(fieldset).find('select');
 
         select.select2({
             width: '100%',
@@ -40,13 +39,6 @@
 
                 return (label ? label + ': ' : '') + item.text;
             }
-        });
-
-        // Initialize the select-none button.
-        selectNone.prop('title', selectNone.text().trim());
-        selectNone.on('click', function () {
-            select.val(null).trigger('change');
-            this.blur();
         });
     };
 
