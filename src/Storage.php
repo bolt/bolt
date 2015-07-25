3bb606a585c7c097fadad35743482bed62af3177
Gawain Lynch <gawain.lynch@gmail.com>
2015-07-25 11:39:15 +0100

    PHPDoc fix

--- src/Storage.php
+++ src/Storage.php
@@ -1557,7 +1557,7 @@ class Storage
     /**
      * Run existence and perform publish/depublishes.
      *
-     * @param array<string> contenttypeslugs to check
+     * @param array ContentType slugs to check
      *
      * @return mixed false, if any table doesn't exist
      *               true, if all is fine
