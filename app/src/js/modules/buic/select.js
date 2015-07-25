0694b75256965a71d7bc443583d91149a447468b
rarila <magnet@gmx.net>
2015-07-25 21:09:40 +0200

    Handle slect all and clear buttuns on buic side

--- app/src/js/modules/buic/select.js
+++ app/src/js/modules/buic/select.js
@@ -27,6 +27,23 @@
      * @param {Object} buic
      */
     select.init = function (buic) {
+        var select = $(buic).find('select'),
+            buttonAll = $(buic).find('.select-all'),
+            buttonNone = $(buic).find('.select-none');
+
+        // Initialize the select-all button.
+        buttonAll.prop('title', buttonAll.text().trim());
+        buttonAll.on('click', function () {
+            select.find('option').prop('selected', true).trigger('change');
+            this.blur();
+        });
+
+        // Initialize the select-none button.
+        buttonNone.prop('title', buttonNone.text().trim());
+        buttonNone.on('click', function () {
+            select.val(null).trigger('change');
+            this.blur();
+        });
     };
 
     // Apply mixin container
