dfef3d3ea691b1c4a7e522e67d79fd13c41dd1ad
Ross Riley <riley.ross@gmail.com>
2015-07-26 12:10:10 +0100

    define an interface that Bolt Queries will conform to

---
+++ src/Storage/Query/QueryInterface.php
@@ -0,0 +1,27 @@
+<?php 
+
+namespace Bolt\Storage\Query;
+
+/**
+* Interface that defines minimum functionality of a Bolt Query class
+* 
+* The goal of a query is to store select and filter parameters that can be 
+* used to create a relevant SQL expression.
+*/
+interface QueryInterface
+{
+    
+    /**
+     * Builds the query and returns an instance of QueryBuilder
+     * @return QueryBuilder
+     */
+    public function build();
+    
+    /**
+     * Returns the current instance of QueryBuilder
+     * @return QueryBuilder
+     */
+    public function getQueryBuilder();
+    
+    
+}
