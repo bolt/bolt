f1ea72ceb5af2883a5470f03a8d58eaac2c501d3
Gawain Lynch <gawain.lynch@gmail.com>
2015-07-29 00:06:08 +0100

    Move class aliases for Field classes

--- app/deprecated.php
+++ app/deprecated.php
@@ -3,3 +3,8 @@
 /**
  * Autoloaded file to handle deprecation such as class aliases.
  */
+
+// Class aliases for BC
+class_alias('\Bolt\Storage\Field\Base', '\Bolt\Field\Base');
+class_alias('\Bolt\Storage\Field\FieldInterface', '\Bolt\Field\FieldInterface');
+class_alias('\Bolt\Storage\Field\Manager', '\Bolt\Field\Manager');
