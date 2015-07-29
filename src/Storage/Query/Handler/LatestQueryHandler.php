b33688174b15e9f027eb06635f149f9bfe4550d1
Ross Riley <riley.ross@gmail.com>
2015-07-29 09:45:40 +0100

    fix for incorrect handling

--- src/Storage/Query/Handler/LatestQueryHandler.php
+++ src/Storage/Query/Handler/LatestQueryHandler.php
@@ -19,9 +19,7 @@ class LatestQueryHandler
     public function __invoke(ContentQueryParser $contentQuery)
     {
         $contentQuery->setDirective('order', '-id');
-        $contentQuery->setDirective('limit', 1);
-        $contentQuery->setDirective('returnsingle', true);
-
+        
         return call_user_func_array($contentQuery->getHandler('select'), [$contentQuery]);
     }
 }
