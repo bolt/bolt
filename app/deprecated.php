ed73d238bf9f8602399650bc320987a3afccf915
Gawain Lynch <gawain.lynch@gmail.com>
2015-07-29 12:43:47 +0100

    Alias \Bolt\Extensions\Snippets\Location to \Bolt\Asset\Target and remove old class

--- app/deprecated.php
+++ app/deprecated.php
@@ -5,6 +5,7 @@
  */
 
 // Class aliases for BC
+class_alias('\Bolt\Asset\Target', '\Bolt\Extensions\Snippets\Location');
 class_alias('\Bolt\Storage\Field\Base', '\Bolt\Field\Base');
 class_alias('\Bolt\Storage\Field\FieldInterface', '\Bolt\Field\FieldInterface');
 class_alias('\Bolt\Storage\Field\Manager', '\Bolt\Field\Manager');
