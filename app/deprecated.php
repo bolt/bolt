6b3a96978f4c223ae5c147f2322cd991fa69ba3d
Gawain Lynch <gawain.lynch@gmail.com>
2015-07-29 12:47:48 +0100

    Add note about array syntax

--- app/deprecated.php
+++ app/deprecated.php
@@ -2,6 +2,10 @@
 
 /**
  * Autoloaded file to handle deprecation such as class aliases.
+ *
+ * NOTE:
+ * If for any reason arrays are required in this file, only ever use array()
+ * syntax to prevent breakage on PHP < 5.4 and allow the legacy warnings.
  */
 
 // Class aliases for BC
