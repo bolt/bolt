f3e9c4475d6f68c9fbbd387a93b2f901ace344bb
Ross Riley <riley.ross@gmail.com>
2015-08-01 11:06:01 +0100

    add test to make sure that the query builds the correct expression and parameters

--- tests/phpunit/unit/Storage/Query/SearchQueryTest.php
+++ tests/phpunit/unit/Storage/Query/SearchQueryTest.php
@@ -23,6 +23,17 @@ class SearchQueryTest extends BoltUnitTest
         $query->setContentType('pages');
         $query->setSearch($filter);
         $expr = $query->getWhereExpression();
-        echo $expr;
+        $this->assertEquals('((pages.title LIKE :title_1) OR (pages.title LIKE :title_2)) AND ((pages.teaser LIKE :teaser_1) OR (pages.teaser LIKE :teaser_2)) AND ((pages.body LIKE :body_1) OR (pages.body LIKE :body_2)) AND ((pages.chapters LIKE :chapters_1) OR (pages.chapters LIKE :chapters_2))', $expr);
+        $params = $query->getWhereParameters();
+        $this->assertArrayHasKey('title_1', $params);
+        $this->assertArrayHasKey('title_2', $params);
+        $this->assertArrayHasKey('teaser_1', $params);
+        $this->assertArrayHasKey('teaser_2', $params);
+        $this->assertArrayHasKey('body_1', $params);
+        $this->assertArrayHasKey('body_2', $params);
+        $this->assertArrayHasKey('chapters_1', $params);
+        $this->assertArrayHasKey('chapters_2', $params);
+        $this->assertEquals('%lorem%', $params['title_1']);
+        $this->assertEquals('%ipsum%', $params['title_2']);
     }
 }
