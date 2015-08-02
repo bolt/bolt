78e92fd9d56a8b430cfb0e40770a284292972d84
Ross Riley <riley.ross@gmail.com>
2015-08-02 11:11:27 +0100

    new tests to ensure that search queries produce correct LIKE expressions and values

--- tests/phpunit/unit/Storage/Query/SearchQueryTest.php
+++ tests/phpunit/unit/Storage/Query/SearchQueryTest.php
@@ -6,7 +6,7 @@ use Bolt\Storage\Query\SelectQuery;
 use Bolt\Tests\BoltUnitTest;
 
 /**
- * Class to test src/Storage/Query/SelectQuery.
+ * Class to test src/Storage/Query/SearchQuery.
  *
  * @author Ross Riley <riley.ross@gmail.com>
  */
@@ -23,7 +23,29 @@ class SearchQueryTest extends BoltUnitTest
         $query->setContentType('pages');
         $query->setSearch($filter);
         $expr = $query->getWhereExpression();
-        $this->assertEquals('((pages.title LIKE :title_1) OR (pages.title LIKE :title_2)) AND ((pages.teaser LIKE :teaser_1) OR (pages.teaser LIKE :teaser_2)) AND ((pages.body LIKE :body_1) OR (pages.body LIKE :body_2)) AND ((pages.chapters LIKE :chapters_1) OR (pages.chapters LIKE :chapters_2))', $expr);
+        $this->assertEquals('((pages.title LIKE :title_1) OR (pages.title LIKE :title_2)) OR ((pages.teaser LIKE :teaser_1) OR (pages.teaser LIKE :teaser_2)) OR ((pages.body LIKE :body_1) OR (pages.body LIKE :body_2)) OR ((pages.chapters LIKE :chapters_1) OR (pages.chapters LIKE :chapters_2))', $expr);
+        $params = $query->getWhereParameters();
+        $this->assertArrayHasKey('title_1', $params);
+        $this->assertArrayHasKey('title_2', $params);
+        $this->assertArrayHasKey('teaser_1', $params);
+        $this->assertArrayHasKey('teaser_2', $params);
+        $this->assertArrayHasKey('body_1', $params);
+        $this->assertArrayHasKey('body_2', $params);
+        $this->assertArrayHasKey('chapters_1', $params);
+        $this->assertArrayHasKey('chapters_2', $params);
+        $this->assertEquals('%lorem%', $params['title_1']);
+        $this->assertEquals('%ipsum%', $params['title_2']);        
+    }
+    
+    public function testAndParameterQuery()
+    {
+        $app = $this->getApp();
+        $filter = 'lorem + ipsum';
+        $query = $app['query.search'];
+        $query->setContentType('pages');
+        $query->setSearch($filter);
+        $expr = $query->getWhereExpression();
+        $this->assertEquals('((pages.title LIKE :title_1) AND (pages.title LIKE :title_2)) OR ((pages.teaser LIKE :teaser_1) AND (pages.teaser LIKE :teaser_2)) OR ((pages.body LIKE :body_1) AND (pages.body LIKE :body_2)) OR ((pages.chapters LIKE :chapters_1) AND (pages.chapters LIKE :chapters_2))', $expr);
         $params = $query->getWhereParameters();
         $this->assertArrayHasKey('title_1', $params);
         $this->assertArrayHasKey('title_2', $params);
@@ -35,5 +57,12 @@ class SearchQueryTest extends BoltUnitTest
         $this->assertArrayHasKey('chapters_2', $params);
         $this->assertEquals('%lorem%', $params['title_1']);
         $this->assertEquals('%ipsum%', $params['title_2']);
+        $this->assertEquals('%lorem%', $params['teaser_1']);
+        $this->assertEquals('%ipsum%', $params['teaser_2']);
+        $this->assertEquals('%lorem%', $params['body_1']);
+        $this->assertEquals('%ipsum%', $params['body_2']);
+        $this->assertEquals('%lorem%', $params['chapters_1']);
+        $this->assertEquals('%ipsum%', $params['chapters_2']);
+        
     }
 }
