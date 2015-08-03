3dfba8fbbf159ebd618e7f823d9e337c66e68f88
Carson Full <carsonfull@gmail.com>
2015-08-02 20:39:05 -0500

    Remove handler check from SessionSPTest since that logic changed

--- tests/phpunit/unit/Provider/SessionServiceProviderTest.php
+++ tests/phpunit/unit/Provider/SessionServiceProviderTest.php
@@ -2,19 +2,9 @@
 namespace Bolt\Tests\Provider;
 
 use Bolt\Provider\SessionServiceProvider;
-use Bolt\Session\FileSessionHandler;
-use Bolt\Session\Generator\NativeGenerator;
-use Bolt\Session\OptionsBag;
-use Bolt\Session\Serializer\NativeSerializer;
-use Bolt\Session\SessionStorage;
-use Bolt\Session\SessionListener;
 use Bolt\Tests\BoltUnitTest;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
-use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBag;
-use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
 use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\HttpFoundation\Session\Storage\MetadataBag;
 
 /**
  * Class to test src/Provider/SessionServiceProvider.
@@ -39,7 +29,6 @@ class SessionServiceProviderTest extends BoltUnitTest
 
         $this->assertArrayHasKey('main',    $app['sessions.options']);
         $this->assertArrayHasKey('csrf',    $app['sessions.options']);
-        $this->assertArrayHasKey('handler', $app['session.default_options']);
 
         $this->assertSame('main', $app['sessions.default']);
 
