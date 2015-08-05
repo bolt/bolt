e6f92ca891d85ddd6f6c9d102a664a90be2bf8df
Bob den Otter <bobdenotter@gmail.com>
2015-08-05 21:19:51 +0200

    Allow the 'stack' column to be `null`, for backwards compatibility. Fixes #3925

--- src/Storage/Database/Schema/Table/Users.php
+++ src/Storage/Database/Schema/Table/Users.php
@@ -21,7 +21,7 @@ class Users extends BaseTable
         $this->table->addColumn('lastseen',       'datetime',   ['notnull' => false, 'default' => null]);
         $this->table->addColumn('lastip',         'string',     ['length' => 32, 'default' => '']);
         $this->table->addColumn('displayname',    'string',     ['length' => 32]);
-        $this->table->addColumn('stack',          'json_array', ['length' => 1024]);
+        $this->table->addColumn('stack',          'json_array', ['length' => 1024, 'notnull' => false]);
         $this->table->addColumn('enabled',        'boolean',    []);
         $this->table->addColumn('shadowpassword', 'string',     ['length' => 128, 'default' => '']);
         $this->table->addColumn('shadowtoken',    'string',     ['length' => 128, 'default' => '']);
