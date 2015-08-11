916b0edeffa34ec7117ddf18b701ac87e54fe7f0
pivwan <pivwan@gmail.com>
2015-08-11 10:57:38 -0400

    Adding theme override for search_results_records

--- src/Controller/Frontend.php
+++ src/Controller/Frontend.php
@@ -361,8 +361,13 @@ class Frontend extends ConfigurableBase
         $param = Pager::makeParameterId($context);
         $page = $request->query->get($param, $request->query->get('page', 1));
 
-        $pageSize = $this->getOption('general/search_results_records') ?: ($this->getOption('general/listing_records') ?: 10);
-
+        // Theme value takes precedence over default config (https://github.com/bolt/bolt/issues/3951)
+        if(!($pageSize = $this->getOption("theme/search_results_records", false))) {
+            if(!($pageSize = $this->getOption("general/search_results_records", false))) {
+                $pageSize = ($this->getOption('general/listing_records') ?: 10);
+            }
+        }
+        
         $offset = ($page - 1) * $pageSize;
         $limit = $pageSize;
 
