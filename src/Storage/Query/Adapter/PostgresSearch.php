4b2d0d018cc9a827a88a9ec731726d229d739d0c
Ross Riley <riley.ross@gmail.com>
2015-08-15 12:30:03 +0100

    build a native postgres search query

--- src/Storage/Query/Adapter/PostgresSearch.php
+++ src/Storage/Query/Adapter/PostgresSearch.php
@@ -14,6 +14,7 @@ class PostgresSearch
     protected $qb;
     protected $config;
     protected $searchWords;
+    protected $contenttype;
     
     public function __construct(QueryBuilder $qb, SearchConfig $config, array $searchWords)
     {
@@ -21,11 +22,59 @@ class PostgresSearch
         $this->config = $config;
         $this->searchWords = $searchWords;
     }
+    
+    public function setContentType($type) {
+        $this->contenttype = $type;
+    }
    
     public function getQuery()
     {
         $words = implode("&", $this->searchWords);
-        $this->qb->addSelect("ts_rank(bsearch.document, to_tsquery('".$words."'))", "score");
+        $sub = clone $this->qb;
+        $this->qb->addSelect("ts_rank(bsearch.document, to_tsquery('".$words."')) as score");
+        $sub->select("*");
+        $select = [];
 
+        $fieldsToSearch = $this->config->getConfig($this->contenttype);
+        $joins = $this->config->getJoins($this->contenttype);
+        $fieldsToSearch = array_diff_key($fieldsToSearch, array_flip($joins));
+        
+        $from = $this->qb->getQueryPart('from');
+        if (isset($from[0]['alias'])) {
+            $alias = $from[0]['alias'];
+        } else {
+            $alias = $from[0]['table'];
+        }
+        foreach ($fieldsToSearch as $fieldName=>$config) {
+            $weight = $this->getWeight($config['weight']);
+            $select[] = "setweight(to_tsvector($alias.$fieldName), '$weight')";
+        }
+        $sub->select("*, ".implode(' || ', $select). ' AS document');
+        $sub->groupBy("$alias.id");
+
+
+        $this->qb->from("(".$sub->getSQL().")", 'bsearch');
+        
+        $this->qb->where("bsearch.document @@ to_tsquery('".$words."')");
+        $this->qb->orderBy('score', 'DESC');
+        return $this->qb;
+    }
+    
+    public function getWeight($score) {
+        switch (true) {
+            case ($score >= 75):
+                return 'A';
+                break;
+            case ($score >= 50):
+                return 'B';
+                break;
+            case ($score >= 25):
+                return 'C';
+                break;
+            case ($score < 25):
+                return 'D';
+                break;           
+        }
+        return 'A';
     }
 }
