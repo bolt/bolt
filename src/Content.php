8a225f1c0cf29811543d6c80d14569ca826c5729
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-19 15:59:18 +0100

    Content::preParse() don't return an error, log it and return a generic message

--- src/Content.php
+++ src/Content.php
@@ -434,7 +434,10 @@ class Content implements \ArrayAccess
             try {
                 return $this->app['safe_render']->render($snippet, $this->getTemplateContext());
             } catch (\Exception $e) {
-                return $e->getMessage();
+                $message = 'Rendering a record Twig snippet failed.';
+                $this->app['logger.system']->critical($message, ['event' => 'exception', 'exception' => $e]);
+
+                return $message;
             }
         }
 
