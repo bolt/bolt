c3c7fb9133f139dc8dab8b3d2a5a16aa66c825b3
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-20 12:46:58 +0100

    Add deprecation class alias for Bolt\Storage

--- app/deprecated.php
+++ app/deprecated.php
@@ -11,6 +11,7 @@
 // Class aliases for BC
 class_alias('\Bolt\Asset\Target', '\Bolt\Extensions\Snippets\Location');
 class_alias('\Bolt\Legacy\Content', '\Bolt\Content');
+class_alias('\Bolt\Legacy\Storage', '\Bolt\Storage');
 class_alias('\Bolt\Storage\Field\Base', '\Bolt\Field\Base');
 class_alias('\Bolt\Storage\Field\FieldInterface', '\Bolt\Field\FieldInterface');
 class_alias('\Bolt\Storage\Field\Manager', '\Bolt\Field\Manager');
