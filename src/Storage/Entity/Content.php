d47a34d4923e7a1d8aa6dda6a2135b5cdb16b1bd
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-22 09:33:36 +0100

    Add generic content field getter

--- src/Storage/Entity/Content.php
+++ src/Storage/Entity/Content.php
@@ -31,6 +31,18 @@ class Content extends Entity
     protected $status;
     protected $templatefields;
 
+    /**
+     * Getter for templates using {{ content.get(title) }} functions.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        return $this->$key;
+    }
+
     public function getDatecreated()
     {
         if (!$this->datecreated) {
