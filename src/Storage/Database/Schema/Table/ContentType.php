174c6464b118cfb30ed173a927b8a7b8b531c78a
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-27 18:32:06 +0100

    Add templatefields back into the schema manager

--- src/Storage/Database/Schema/Table/ContentType.php
+++ src/Storage/Database/Schema/Table/ContentType.php
@@ -58,6 +58,7 @@ class ContentType extends BaseTable
         $this->table->addColumn('username',       'string',     ['length' => 32, 'default' => '', 'notnull' => false]); // We need to keep this around for backward compatibility. For now.
         $this->table->addColumn('ownerid',        'integer',    ['notnull' => false]);
         $this->table->addColumn('status',         'string',     ['length' => 32]);
+        $this->table->addColumn('templatefields', 'json_array', ['notnull' => false]);
         // @codingStandardsIgnoreEnd
     }
 
