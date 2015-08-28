da9bf28ad3c1f0e6ed146063ed7c7c9909974031
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-28 15:55:45 +0100

    Use a legacy ArrayAccess bridge for ContentType array/string handling

--- src/Storage/ContentLegacyService.php
+++ src/Storage/ContentLegacyService.php
@@ -48,10 +48,17 @@ class ContentLegacyService
      */
     public function setupContenttype(Entity\Entity $entity)
     {
+        // Ross version
         if (is_string($entity->getContenttype())) {
             $raw = $this->app['storage']->getContenttype($entity->getContenttype());
             $ct = new ContentType($raw);
             $entity->contenttype = $ct;
+        }
+
+        // Gawain version
+        $contentType = $entity->getContenttype();
+        if (is_string($contentType)) {
+            $entity->contenttype = new ContentType($contentType, $this->app['storage']->getContenttype($contentType));
         }
     }
 }
