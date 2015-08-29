cd991bf595c6d080073badfa479f3d06909e7544
Ross Riley <riley.ross@gmail.com>
2015-08-29 11:49:53 +0100

    make the field types take responsibility for storage type

--- src/Storage/Field/Type/HtmlType.php
+++ src/Storage/Field/Type/HtmlType.php
@@ -1,6 +1,9 @@
 <?php
 namespace Bolt\Storage\Field\Type;
 
+use Doctrine\DBAL\Types\Type;
+
+
 /**
  * This is one of a suite of basic Bolt field transformers that handles
  * the lifecycle of a field from pre-query to persist.
@@ -15,5 +18,15 @@ class HtmlType extends FieldTypeBase
     public function getName()
     {
         return 'html';
+    }
+    
+    /**
+     * Returns the name of the Doctrine storage type to use for a field.
+     *
+     * @return Type
+     */
+    public function getStorageType()
+    {
+        return Type::getType('text');
     }
 }
