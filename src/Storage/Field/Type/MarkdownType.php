e3ce98351ab1168db25913739ff100065614a973
Ross Riley <riley.ross@gmail.com>
2015-08-29 11:56:24 +0100

    add type namespace

--- src/Storage/Field/Type/MarkdownType.php
+++ src/Storage/Field/Type/MarkdownType.php
@@ -1,6 +1,8 @@
 <?php
 namespace Bolt\Storage\Field\Type;
 
+use Doctrine\DBAL\Types\Type;
+
 /**
  * This is one of a suite of basic Bolt field transformers that handles
  * the lifecycle of a field from pre-query to persist.
