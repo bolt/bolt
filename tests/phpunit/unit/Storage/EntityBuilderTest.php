7478b0f8a15d4a5444cac2bb10bd60ca05189a34
Ross Riley <riley.ross@gmail.com>
2015-08-30 17:14:51 +0100

    start work on an EntityBuilder test class

---
+++ tests/phpunit/unit/Storage/EntityBuilderTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Bolt\Tests\Storage;
+
+use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class to test src/Storage/Repository and field transforms for load and hydrate
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+class EntityBuilderTest extends BoltUnitTest
+{
+    public function testBuilderCreate()
+    {
+        $app = $this->getApp();
+        $builder = $app['storage.entity_builder'];
+        
+        
+        
+    }
+
+    
+}
