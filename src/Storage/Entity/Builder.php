3c86159b6d69881ff34a4ea2f9cdab21c28ae964
Gawain Lynch <gawain.lynch@gmail.com>
2015-08-31 09:11:54 +0100

    Add missing use statements

--- src/Storage/Entity/Builder.php
+++ src/Storage/Entity/Builder.php
@@ -11,35 +11,32 @@ use Bolt\Storage\Mapping\MetadataDriver;
 class Builder
 {
     /**
-     *
      * The class to use for new instances.
      *
      * @var string
-     *
      */
     protected $class = 'Bolt\Storage\Entity\Content';
-    
+
     protected $metadata;
     protected $fieldFactory;
     protected $transformers = [];
-    
+
     public function __construct(MetadataDriver $metadata, FieldFactory $fieldFactory)
     {
         $this->metadata = $metadata;
         $this->fieldFactory = $fieldFactory;
     }
-    
+
     public function setClass($class)
     {
         $this->class = $class;
     }
-    
+
     public function setTransformer($fieldTypeClass, callable $handler)
     {
         $this->transformers[$fieldTypeClass] = $handler;
     }
-    
-    
+
     /**
      *
      * Creates a new entity object.
@@ -53,28 +50,28 @@ class Builder
     {
         $class = $this->class;
         $classMetadata = $this->metadata->loadMetadataForClass($class);
-        
+
         $entity = new $class;
-        
+
         // set fields
         foreach ($this->metadata->getFieldMappings() as $key => $mapping) {
             if (array_key_exists($key, $data)) {
                 $fieldType = $this->fieldFactory->get($mapping['fieldtype'], $mapping);
-                
+
                 // If we have a transformer setup then this takes precedence
                 $handler = $this->handlers[$mapping['fieldtype']];
-                
+
                 if ($handler) {
                     $value = call_user_func_array($handler, [$entity, $data]);
                 } else {
                     $value = call_user_func_array([$fieldType, 'set'], [$entity, $data]);
                 }
-                
+
             }
         }
-        
+
         return $entity;
     }
-    
-    
+
+
 }
