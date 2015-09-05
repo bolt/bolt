4a44134f531be6f0fcd1ad9fab7a8e29929c40d9
Ross Riley <riley.ross@gmail.com>
2015-09-05 19:39:19 +0100

    defer to base type

--- src/Storage/Field/Type/TextType.php
+++ src/Storage/Field/Type/TextType.php
@@ -11,17 +11,6 @@ use Bolt\Storage\EntityProxy;
  */
 class TextType extends FieldTypeBase
 {
-
-    public function set($entity, $value)
-    {
-        $key = $this->mapping['fieldname'];
-        
-        if (is_array($value)) {
-            $value = implode(",", $value);
-        }
-        
-        $entity->$key = $value;
-    }
     
     /**
      * {@inheritdoc}
