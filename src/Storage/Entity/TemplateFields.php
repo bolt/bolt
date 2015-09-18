ad099b794414ad59fdc76b0071e3f97cded21704
Ross Riley <riley.ross@gmail.com>
2015-09-18 19:14:02 +0100

    add support for value gets on templatefields

--- src/Storage/Entity/TemplateFields.php
+++ src/Storage/Entity/TemplateFields.php
@@ -7,4 +7,18 @@ namespace Bolt\Storage\Entity;
 class TemplateFields extends Entity
 {
     
+    
+    /**
+     * Getter for templates using {{ content.get(title) }} functions.
+     *
+     * @param string $key
+     *
+     * @return mixed
+     */
+    public function get($key)
+    {
+        $accessor = 'get'.ucfirst($key);
+        return $this->$accessor();
+    }
+    
 }
