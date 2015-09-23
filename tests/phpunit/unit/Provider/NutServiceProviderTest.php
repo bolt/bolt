350d12c0eea3dc7ea06bbe49577e8a68d4dabc23
Carson Full <carsonfull@gmail.com>
2015-09-23 14:07:14 -0500

    Updated nut.commands.add shortcut to accept callable. Also added example usage.

--- tests/phpunit/unit/Provider/NutServiceProviderTest.php
+++ tests/phpunit/unit/Provider/NutServiceProviderTest.php
@@ -3,33 +3,76 @@ namespace Bolt\Tests\Provider;
 
 use Bolt\Provider\NutServiceProvider;
 use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\Console\Command\Command;
 
 /**
  * Class to test src/Provider/NutServiceProvider.
  *
  * @author Ross Riley <riley.ross@gmail.com>
+ * @author Carson Full <carsonfull@gmail.com>
  */
 class NutServiceProviderTest extends BoltUnitTest
 {
     public function testProvider()
     {
         $app = $this->getApp();
-        $provider = new NutServiceProvider($app);
-        $app->register($provider);
+        $app->register(new NutServiceProvider());
+        $app->boot();
+
         $this->assertInstanceOf('Symfony\Component\Console\Application', $app['nut']);
         $this->assertInstanceOf('Symfony\Component\Console\Application', $app['console']);
         $this->assertTrue(is_array($app['nut.commands']));
-        $app->boot();
     }
 
-    public function testAddCommand()
+    public function testAddCommandCallableSingle()
     {
-        $app = $this->makeApp();
-        $provider = new NutServiceProvider($app);
-        $app->register($provider);
-        $app->boot();
-        $command = $this->getMock('Symfony\Component\Console\Command\Command', null, ['mockCommand']);
-        $app['nut.commands.add']($command);
-        $this->assertTrue(in_array($command, $app['nut.commands']));
+        $app = $this->getApp();
+        $app->register(new NutServiceProvider());
+
+        $app['nut.commands.add'](function ($app) {
+            return new Command('command1');
+        });
+
+        $this->assertTrue($app['nut']->has('command1'));
+    }
+
+    public function testAddCommandCallableMultiple()
+    {
+        $app = $this->getApp();
+        $app->register(new NutServiceProvider());
+
+        $app['nut.commands.add'](function ($app) {
+            return [
+                new Command('command1'),
+                new Command('command2'),
+            ];
+        });
+
+        $this->assertTrue($app['nut']->has('command1'));
+        $this->assertTrue($app['nut']->has('command2'));
+    }
+
+    public function testAddCommandSingle()
+    {
+        $app = $this->getApp();
+        $app->register(new NutServiceProvider());
+
+        $command1 = new Command('command1');
+        $app['nut.commands.add']($command1);
+
+        $this->assertTrue($app['nut']->has('command1'));
+    }
+
+    public function testAddCommandMultiple()
+    {
+        $app = $this->getApp();
+        $app->register(new NutServiceProvider());
+
+        $command1 = new Command('command1');
+        $command2 = new Command('command2');
+        $app['nut.commands.add']([$command1, $command2]);
+
+        $this->assertTrue($app['nut']->has('command1'));
+        $this->assertTrue($app['nut']->has('command2'));
     }
 }
