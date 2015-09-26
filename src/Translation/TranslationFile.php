d4785bfa914228afd35e93994d2b17952cab5d35
Gawain Lynch <gawain.lynch@gmail.com>
2015-09-26 13:24:07 +0100

    Don't call reset() on an ArrayAccess
    Prevents the PHP error "Indirect modification of overloaded element of
    Bolt\Application has no effect"

--- src/Translation/TranslationFile.php
+++ src/Translation/TranslationFile.php
@@ -427,7 +427,8 @@ class TranslationFile
         // if the file doesn't exist yet, point to the fallback one
         if (!file_exists($path) || filesize($path) < 10) {
             // fallback
-            list($path) = $this->buildPath('infos', reset($this->app['locale_fallbacks']));
+            $localeFallbacks = $app['locale_fallbacks'];
+            list($path) = $this->buildPath('infos', reset($localeFallbacks));
 
             if (!file_exists($path)) {
                 $this->app['logger.flash']->error('Locale infos yml file not found. Fallback also not found.');
