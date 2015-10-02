2597cc4efce96f822524e0318285a13efb090a02
Ross Riley <riley.ross@gmail.com>
2015-10-02 17:21:30 +0100

    new tables to store fields and field values

---
+++ src/Storage/Database/Schema/Table/FieldValue.php
@@ -0,0 +1,45 @@
+<?php
+namespace Bolt\Storage\Database\Schema\Table;
+
+/**
+ * Table for field values with separate columns for each data type.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+class FieldValue extends BaseTable
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function addColumns()
+    {
+        // @codingStandardsIgnoreStart
+        $this->table->addColumn('id',               'integer',      ['autoincrement' => true]);
+        $this->table->addColumn('field_id',         'integer',      []);
+        $this->table->addColumn('value_varchar',    'string',       ['length' => 255]);
+        $this->table->addColumn('value_text',       'text',         ['default' => $this->getTextDefault()]);
+        $this->table->addColumn('value_integer',    'integer',      ['default' => 0]);
+        $this->table->addColumn('value_float',      'float',        ['default' => 0]);
+        $this->table->addColumn('value_decimal',    'decimal',      ['precision' => '18', 'scale' => '9', 'default' => 0]);
+        $this->table->addColumn('value_date',       'date',         ['notnull' => false]);
+        $this->table->addColumn('value_datetime',   'datetime',     ['notnull' => false]);
+        $this->table->addColumn('value_json',       'json_array',   ['notnull' => false]);
+        // @codingStandardsIgnoreEnd
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function addIndexes()
+    {
+        $this->table->addIndex(['field_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setPrimaryKey()
+    {
+        $this->table->setPrimaryKey(['id']);
+    }
+}
