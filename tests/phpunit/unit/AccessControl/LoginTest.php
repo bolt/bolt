cba2038734154a9e8ee3520b7daf084cfedd7114
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-06 14:16:11 +0100

    [Tests] Tests Login::login() disabled user name (temporarily skipped)

--- tests/phpunit/unit/AccessControl/LoginTest.php
+++ tests/phpunit/unit/AccessControl/LoginTest.php
@@ -49,4 +49,35 @@ class LoginTest extends BoltUnitTest
 
         $login->login($request, 'koala', 'sneaky');
     }
+
+    public function testLoginDisabledUsername()
+    {
+        $this->markTestSkipped('See issue #4237 Unable to disable users');
+
+        $app = $this->getApp();
+        $this->addDefaultUser($app);
+
+        $logger = $this->getMock('\Monolog\Logger', ['error'], ['testlogger']);
+        $logger->expects($this->atLeastOnce())
+            ->method('error')
+            ->with($this->equalTo("Attempt to login with disabled account by 'admin'"));
+        $app['logger.system'] = $logger;
+
+        $logger = $this->getMock('\Bolt\Logger\FlashLogger', ['error']);
+        $logger->expects($this->atLeastOnce())
+            ->method('error')
+            ->with($this->equalTo('Your account is disabled. Sorry about that.'));
+        $app['logger.flash'] = $logger;
+
+        $entityName = 'Bolt\Storage\Entity\Users';
+        $repo = $app['storage']->getRepository($entityName);
+        $userEntity = $repo->getUser('admin');
+        $userEntity->setEnabled(false);
+        $repo->save($userEntity);
+
+        $login = new Login($app);
+        $request = new Request();
+
+        $login->login($request, 'admin', 'sneaky');
+    }
 }
