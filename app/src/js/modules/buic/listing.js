882964b6e97bacf26b935e75900a546857ef1064
rarila <magnet@gmx.net>
2015-10-06 23:51:07 +0200

    Add select/unselect all functionality

--- app/src/js/modules/buic/listing.js
+++ app/src/js/modules/buic/listing.js
@@ -28,6 +28,22 @@
      * @param {Object} buic
      */
     listing.init = function (buic) {
+        // Select/unselect all rows in a listing section.
+        $(buic).find('tr.header input:checkbox[name="checkRow"]').on('click', function () {
+            var setStatus = this.checked;
+
+            $(this).closest('tbody').find('td input:checkbox[name="checkRow"]').each(function () {
+                var row = $(this).closest('tr');
+
+                this.checked = setStatus;
+
+                if (setStatus) {
+                    row.addClass('row-checked');
+                } else {
+                    row.removeClass('row-checked');
+                }
+            });
+        });
     };
 
     // Apply mixin container
