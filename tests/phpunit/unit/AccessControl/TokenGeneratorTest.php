049ded647781d4659d5ed3befabdb3260fb384c8
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-07 07:54:22 +0100

    [Tests] Token\Generator no remote address

--- tests/phpunit/unit/AccessControl/TokenGeneratorTest.php
+++ tests/phpunit/unit/AccessControl/TokenGeneratorTest.php
@@ -27,4 +27,22 @@ class TokenGeneratorTest extends BoltUnitTest
 
         $this->assertSame('a186bd81a42ade8d9db20d67f3e3dedb', (string) $generator);
     }
+
+    public function testGenerateNoRemoteAddress()
+    {
+        $username = 'koala';
+        $salt = 'vinagre';
+        $remoteIP = '8.8.8.8';
+        $hostName = 'tests.bolt.cm';
+        $userAgent = 'Smith';
+        $cookieOptions = [
+            'remoteaddr'   => false,
+            'httphost'     => true,
+            'browseragent' => true,
+        ];
+
+        $generator = new Generator($username, $salt, $remoteIP, $hostName, $userAgent, $cookieOptions);
+
+        $this->assertSame('fab8d7c5234c667135c6272593801cde', (string) $generator);
+    }
 }
