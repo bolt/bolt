62f885aab4f68358a180806a3a3f2b731a5c1466
Ross Riley <riley.ross@gmail.com>
2015-10-08 19:27:50 +0100

    implementation for the repeating field type

--- src/Storage/Field/Type/RepeaterType.php
+++ src/Storage/Field/Type/RepeaterType.php
@@ -1,8 +1,9 @@
 <?php
 namespace Bolt\Storage\Field\Type;
 
+use Bolt\Exception\FieldConfigurationException;
+use Bolt\Storage\Field\Collection\RepeatingFieldCollection;
 use Bolt\Storage\Mapping\ClassMetadata;
-use Bolt\Storage\Query\QueryInterface;
 use Bolt\Storage\QuerySet;
 use Bolt\Storage\ValuesCollection;
 use Doctrine\DBAL\Query\QueryBuilder;
@@ -20,7 +21,7 @@ class RepeaterType extends FieldTypeBase
      * For repeating fields, the load method adds extra joins and selects to the query that
      * fetches the related records from the field and field value tables in the same query as the content fetch.
      *
-     * @param QueryBuilder  $query
+     * @param QueryBuilder $query
      * @param ClassMetadata $metadata
      */
     public function load(QueryBuilder $query, ClassMetadata $metadata)
@@ -37,17 +38,24 @@ class RepeaterType extends FieldTypeBase
         }
 
         $query->addSelect($this->getPlatformGroupConcat('fields', $query))
-            ->leftJoin($alias, $this->mapping['tables']['field'], 'f', "f.content_id = $alias.id AND f.contenttype='$boltname'")
-            ->leftJoin($alias, $this->mapping['tables']['field_value'], 'fv', "fv.field_id=f.id");
+            ->leftJoin(
+                $alias,
+                $this->mapping['tables']['field_value'],
+                'f',
+                "f.content_id = $alias.id AND f.contenttype='$boltname' AND f.name='$field'"
+            );
     }
 
     public function persist(QuerySet $queries, $entity)
     {
-        $field = $this->mapping['fieldname'];
-        $existingFields = $this->getFieldsQuery();
+        $this->normalize($entity);
+        $key = $this->mapping['fieldname'];
+        $accessor = 'get'.ucfirst($key);
 
-        $this->addDeleteFieldValuesQuery($queries, $existingFields);
-        $this->addDeleteFieldsQuery($queries, $existingFields);
+        $collection = $entity->$accessor();
+
+        $this->addToInsertQuery($queries, $collection->getNew(), $entity);
+        $this->addToUpdateQuery($queries, $collection->getExisting(), $entity);
     }
 
     public function hydrate($data, $entity)
@@ -59,13 +67,44 @@ class RepeaterType extends FieldTypeBase
             $split = explode('_', $fieldKey);
             $values[$split[0]][$split[1]][] = $split[2];
         }
+        $collection = new RepeatingFieldCollection($this->em);
+        $collection->setName($key);
 
-        $entityVals = [];
-        foreach ((array)$values[$key] as $field) {
-            $entityVals[] = new ValuesCollection($field, $this->em);
+        if (count($values[$key])) {
+            foreach ($values[$key] as $group => $refs) {
+                $collection->addFromReferences($refs, $group);
+            }
         }
 
-        $this->set($entity, $entityVals);
+        $this->set($entity, $collection);
+    }
+
+    /**
+     * Normalize step ensures that we have correctly hydrated objects at the collection
+     * and entity level.
+     *
+     * @param $entity
+     */
+    public function normalize($entity)
+    {
+        $key = $this->mapping['fieldname'];
+        $accessor = 'get'.ucfirst($key);
+
+        $outerCollection = $entity->$accessor();
+        $newVal = [];
+        if (!$outerCollection instanceof RepeatingFieldCollection) {
+            $collection = new RepeatingFieldCollection($this->em);
+            $collection->setName($key);
+            foreach ($outerCollection as $group => $fields) {
+                if (is_array($fields)) {
+                    $collection->addFromArray($fields, $group);
+                }
+            }
+
+            $setter = 'set'.ucfirst($key);
+            $entity->$setter($collection);
+        }
+
     }
 
 
@@ -80,7 +119,6 @@ class RepeaterType extends FieldTypeBase
     /**
      * Get platform specific group_concat token for provided column
      *
-     * @param string       $column
      * @param string       $alias
      * @param QueryBuilder $query
      *
@@ -92,11 +130,11 @@ class RepeaterType extends FieldTypeBase
 
         switch ($platform) {
             case 'mysql':
-                return "GROUP_CONCAT(CONCAT_WS('_', f.name, f.id, fv.id)) as $alias";
+                return "GROUP_CONCAT(CONCAT_WS('_', f.name, f.grouping, f.id)) as $alias";
             case 'sqlite':
-                return "GROUP_CONCAT(f.name||'_'||f.id||'_'||fv.id) as $alias";
+                return "GROUP_CONCAT(f.name||'_'||f.grouping||'_'||f.id) as $alias";
             case 'postgresql':
-                return "string_agg(f.name||'_'||f.id||'_'||fv.id) as $alias";
+                return "string_agg(f.name||'_'||f.grouping||'_'||f.id) as $alias";
         }
     }
 
@@ -107,11 +145,11 @@ class RepeaterType extends FieldTypeBase
      *
      * @return array
      */
-    protected function getFieldsQuery($entity)
+    protected function getExistingFields($entity)
     {
         $query = $this->em->createQueryBuilder()
-            ->select('id')
-            ->from($this->mapping['tables']['field'])
+            ->select('grouping, id', 'name')
+            ->from($this->mapping['tables']['field_value'])
             ->where('content_id = :id')
             ->andWhere('contenttype = :contenttype')
             ->andWhere('name = :name')
@@ -121,27 +159,147 @@ class RepeaterType extends FieldTypeBase
                 'name'        => $this->mapping['fieldname'],
             ]);
 
-        $result = $query->execute()->fetchAll();
+        $results = $query->execute()->fetchAll();
 
-        return $result ?: [];
+        $fields = [];
+
+        if (!$results) {
+            return $fields;
+        }
+
+        foreach ($results as $result) {
+            $fields[$result['grouping']][$result['name']] = $result['id'];
+        }
+
+        return $fields;
+    }
+
+    protected function getQueryStatuses($entity)
+    {
+        $persisted = $this->getExistingFields($entity);
+
+        $key = $this->mapping['fieldname'];
+        $accessor = 'get'.ucfirst($key);
+
+        $index = [];
+
+        if (!$proposed instanceof RepeatingFields) {
+            $this->set($entity, $proposed);
+            $proposed = $entity->$accessor();
+        }
+
+        var_dump($proposed); exit;
+
+        foreach ($proposed as $group => $fields) {
+            foreach ($fields as $key=> $fieldValue) {
+                if(isset($persisted[$group][$key])) {
+                    $index['update'][$group][] = $key;
+                } else {
+                    $index['insert'][$group][] = $key;
+                }
+
+            }
+        }
+
+        foreach ($persisted as $group => $fields) {
+            foreach ($fields as $key=>$fieldValue) {
+                if (!isset($proposed[$group][$key])) {
+                    $index['delete'][$group][] = $key;
+                }
+            }
+
+        }
+
+        return $index;
+    }
+
+
+    /**
+     * Query to insert new field values.
+     *
+     * @param QuerySet $queries
+     * @param array $changes
+     * @param $entity
+     */
+    protected function addToInsertQuery(QuerySet $queries, $changes, $entity)
+    {
+        foreach ($changes as $fieldValue) {
+            $repo = $this->em->getRepository(get_class($fieldValue));
+            $field = $this->getFieldType($fieldValue->getFieldname());
+            $type = $field->getStorageType();
+            $typeCol = 'value_'.$type->getName();
+
+            $fieldValue->$typeCol = $fieldValue->getValue();
+            $fieldValue->setFieldtype($type->getName());
+            $fieldValue->setContenttype((string)$entity->getContenttype());
+
+            // This takes care of instances where an entity might be inserted, and thus not
+            // have an id. This registers a callback to set the id parameter when available.
+            $queries->onResult(function ($query, $result, $id) use ($repo, $fieldValue) {
+                if ($result === 1 && $id) {
+                    $fieldValue->setContent_id($id);
+                    $repo->save($fieldValue);
+                }
+            });
+
+        }
+
     }
 
     /**
      * Query to delete existing field values.
      *
-     * @param mixed $entity
-     *
-     * @return array
+     * @param QuerySet $queries
+     * @param $changes
      */
-    protected function addDeleteFieldValuesQuery(QuerySet $queries, $ids)
+    protected function addToDeleteQuery(QuerySet $queries, $changes)
     {
-        $query = $this->em->createQueryBuilder()
-            ->delete($this->mapping['tables']['field_value'], 'fv')
-            ->where('fv.field_id IN(:ids)')
-            ->setParameter('ids', $ids, Connection::PARAM_INT_ARRAY);
 
-        echo $query->getSQL(); exit;
 
-        $queries->append($query);
+    }
+
+    /**
+     * Query to insert new field values.
+     *
+     * @param QuerySet $queries
+     * @param array $changes
+     * @param $entity
+     */
+    protected function addToUpdateQuery(QuerySet $queries, $changes, $entity)
+    {
+        foreach ($changes as $fieldValue) {
+            $repo = $this->em->getRepository(get_class($fieldValue));
+            $field = $this->getFieldType($fieldValue->getName());
+            $type = $field->getStorageType();
+            $typeCol = 'value_'.$type->getName();
+            $fieldValue->$typeCol = $fieldValue->getValue();
+
+            var_dump($fieldValue); exit;
+            // This takes care of instances where an entity might be inserted, and thus not
+            // have an id. This registers a callback to set the id parameter when available.
+            $queries->onResult(function ($query, $result, $id) use ($repo, $fieldValue) {
+                if ($result === 1) {
+                    $repo->save($fieldValue);
+                }
+            });
+
+        }
+
+    }
+
+    /**
+     * @param $field
+     * @return mixed
+     * @throws FieldConfigurationException
+     */
+    protected function getFieldType($field)
+    {
+        if (!isset($this->mapping['data']['fields'][$field]['fieldtype'])) {
+            throw new FieldConfigurationException('Invalid repeating field configuration for '.$field);
+        }
+        $mapping = $this->mapping['data']['fields'][$field];
+        $setting = $mapping['fieldtype'];
+
+        return $this->em->getFieldManager()->get($setting, $mapping);
     }
 }
