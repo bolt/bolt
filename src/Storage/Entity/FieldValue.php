5c3123491b28214810cfef4935864e6a44eb2a06
Ross Riley <riley.ross@gmail.com>
2015-10-08 19:26:05 +0100

    add a custom entity for field values

--- src/Storage/Entity/FieldValue.php
+++ src/Storage/Entity/FieldValue.php
@@ -7,4 +7,46 @@ namespace Bolt\Storage\Entity;
 class FieldValue extends Entity
 {
 
+    protected $value;
+    protected $name;
+    protected $grouping;
+    protected $type;
+    protected $contenttype;
+    protected $content_id;
+
+    /**
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    public function __toString()
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+
 }
