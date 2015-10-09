5efb3ffd33bdd9507835e1a73598a62a52a012e7
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-09 13:34:36 +0100

    Remove old delete() function

--- src/Controller/Async/Records.php
+++ src/Controller/Async/Records.php
@@ -21,38 +21,4 @@ class Records extends AsyncBase
         $c->post('/content/{action}/{contenttypeslug}/{id}', 'modify')
             ->bind('contentaction');
     }
-
-    /**
-     * Delete a record.
-     *
-     * @param Request $request         The Symfony Request
-     * @param string  $contenttypeslug
-     * @param integer $id
-     *
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse
-     */
-    public function delete(Request $request, $contenttypeslug, $id)
-    {
-        $ids = explode(',', $id);
-        $contenttype = $this->getContentType($contenttypeslug);
-
-        foreach ($ids as $id) {
-            $content = $this->getContent($contenttype['slug'], ['id' => $id, 'status' => '!undefined']);
-            $title = $content->getTitle();
-
-            if (!$this->isAllowed("contenttype:$contenttypeslug:delete:$id")) {
-                $this->flashes()->error(Trans::__('Permission denied', []));
-            } elseif ($this->checkAntiCSRFToken() && $this->storage()->deleteContent($contenttypeslug, $id)) {
-                $this->flashes()->info(Trans::__("Content '%title%' has been deleted.", ['%title%' => $title]));
-            } else {
-                $this->flashes()->info(Trans::__("Content '%title%' could not be deleted.", ['%title%' => $title]));
-            }
-        }
-
-        // Get the referer's query parameters
-        $queryParams = $this->getRefererQueryParameters($request);
-        $queryParams['contenttypeslug'] = $contenttypeslug;
-
-        return $this->redirectToRoute('overview', $queryParams);
-    }
 }
