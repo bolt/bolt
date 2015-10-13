02dd11af7d2fad35c7329f0cf1802e84a175f7ca
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-13 16:03:31 +0100

    [Tests] Class for AccessControl\AccessChecker

---
+++ tests/phpunit/unit/AccessControl/AccessCheckerTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace Bolt\Tests;
+
+use Bolt\AccessControl\AccessChecker;
+use Bolt\AccessControl\Token\Token;
+use Bolt\Storage\Entity;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Test for AccessControl\AccessChecker
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class AccessCheckerTest extends BoltUnitTest
+{
+    public function tearDown()
+    {
+        $this->resetDb();
+    }
+
+    /**
+     * @return \Bolt\AccessControl\AccessChecker
+     */
+    protected function getAccessControl()
+    {
+        $request = Request::createFromGlobals();
+        $request->server->set('HTTP_USER_AGENT', 'Bolt PHPUnit tests');
+
+        $app = $this->getApp();
+        $app['access_control']->setRequest($request);
+
+        return $app['access_control'];
+    }
+}
