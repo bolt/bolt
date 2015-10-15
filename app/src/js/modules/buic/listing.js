0c2a7e0a151e68bd7cca7cf83e2f5135eaa7814e
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-15 08:58:02 +0100

    return $undo--;

--- app/src/js/modules/buic/listing.js
+++ app/src/js/modules/buic/listing.js
@@ -136,7 +136,7 @@
             } else {
                 msg = Bolt.data('recordlisting.confirm.multi', {'%NUMBER%': '<b>' + selectedIds.length + '</b>'});
             }
-            msg = msg + '<br>' + Bolt.data('recordlisting.confirm.no-undo');
+            msg = msg + '<br><br><h5>' + Bolt.data('recordlisting.confirm.no-undo') + '</h5>';
 
             // Remove when done:
             msg = msg + '<hr><b style="color:red;">Anti CSRF token functionality still disabled ' +
