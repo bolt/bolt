fcdb0026ee6a1e0d04b793a75090ea8675651599
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-16 07:34:25 +0100

    Class to manage record listing options

---
+++ src/Storage/ContentRequest/ListingOptions.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Bolt\Storage\ContentRequest;
+
+/**
+ * Class to manage record listing options.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ListingOptions
+{
+    /** @var string */
+    protected $order;
+    /** @var integer */
+    protected $page;
+    /** @var array */
+    protected $taxonomies;
+    /** @var string */
+    protected $filter;
+
+    /**
+     * Set the order.
+     *
+     * @param string $order
+     */
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    /**
+     * Get the order… Would you like fries with that?
+     *
+     * @return string
+     */
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    /**
+     * Set the page.
+     *
+     * @param integer $page
+     */
+    public function setPage($page)
+    {
+        $this->page = $page;
+        return $this;
+    }
+
+    /**
+     * Get the page.
+     *
+     * @return integer
+     */
+    public function getPage()
+    {
+        return $this->page;
+    }
+
+    /**
+     * Get the previous page number.
+     *
+     * @return integer
+     */
+    public function getPreviousPage()
+    {
+        return $this->page && $this->page > 1
+            ? $this->page - 1
+            : $this->page;
+    }
+
+    /**
+     * Set the taxonomies.
+     *
+     * @param array $taxonomies
+     */
+    public function setTaxonomies($taxonomies)
+    {
+        $this->taxonomies = $taxonomies;
+        return $this;
+    }
+
+    /**
+     * Get the taxonomies.
+     *
+     * @return array
+     */
+    public function getTaxonomies()
+    {
+        return $this->taxonomies;
+    }
+
+    /**
+     * Set the filter.
+     *
+     * @param string $filter
+     */
+    public function setFilter($filter)
+    {
+        $this->filter = $filter;
+        return $this;
+    }
+
+    /**
+     * Get the filter.
+     *
+     * @return string
+     */
+    public function getFilter()
+    {
+        return $this->filter;
+    }
+}
