6338f8a88863a7d6cb77d56babaccd5877001d4c
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-19 18:27:06 +0100

    [Tests] Bolt\Twig\Handler\ImageHandler::imageInfo()

--- tests/phpunit/unit/Twig/Handler/ImageHandlerTest.php
+++ tests/phpunit/unit/Twig/Handler/ImageHandlerTest.php
@@ -4,6 +4,7 @@ namespace Bolt\Tests\Twig;
 
 use Bolt\Tests\BoltUnitTest;
 use Bolt\Twig\Handler\ImageHandler;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Class to test Bolt\Twig\Handler\ImageHandler
@@ -12,6 +13,14 @@ use Bolt\Twig\Handler\ImageHandler;
  */
 class ImageHandlerTest extends BoltUnitTest
 {
+    protected function setUp()
+    {
+        $app = $this->getApp();
+        $files = $app['resources']->getPath('filespath');
+        $fs = new Filesystem();
+        $fs->copy(PHPUNIT_ROOT . '/resources/generic-logo.png', $files . '/generic-logo.png', true);
+    }
+
     public function testImageFileNameLegacy()
     {
         $app = $this->getApp();
@@ -81,5 +90,47 @@ class ImageHandlerTest extends BoltUnitTest
         $this->assertSame('/thumbs/20x20c/generic-logo.png', $result);
         $result = $handler->image(['filename' => 'generic-logo.png'], 20, 20, 'crop');
         $this->assertSame('/thumbs/20x20c/generic-logo.png', $result);
+    }
+
+    public function testImageInfoSafe()
+    {
+        $app = $this->getApp();
+        $handler = new ImageHandler($app);
+
+        $result = $handler->imageInfo('generic-logo.png', true);
+        $this->assertNull($result);
+    }
+
+    public function testImageInfoNotReadable()
+    {
+        $app = $this->getApp();
+        $handler = new ImageHandler($app);
+
+        $result = $handler->imageInfo('koala.jpg', false);
+        $this->assertFalse($result);
+    }
+
+    public function testImageInfo()
+    {
+        $app = $this->getApp();
+        $handler = new ImageHandler($app);
+
+        $result = $handler->imageInfo('generic-logo.png', false);
+        $this->assertSame(624, $result['width']);
+        $this->assertSame(351, $result['height']);
+        $this->assertSame('jpeg', $result['type']);
+        $this->assertSame('image/jpeg', $result['mime']);
+        $this->assertRegExp('#1.7777#', (string) $result['aspectratio']);
+        $this->assertSame('generic-logo.png', $result['filename']);
+        $this->assertRegExp('#tests/phpunit/web-root/files/generic-logo.png#', $result['fullpath']);
+        $this->assertSame('/files/generic-logo.png', $result['url']);
+        $this->assertSame('', $result['exif']['latitude']);
+        $this->assertFalse($result['exif']['longitude']);
+        $this->assertFalse($result['exif']['datetime']);
+        $this->assertFalse($result['exif']['orientation']);
+        $this->assertRegExp('#1.7777#', (string) $result['exif']['aspectratio']);
+        $this->asserttrue($result['landscape']);
+        $this->assertFalse($result['portrait']);
+        $this->assertFalse($result['square']);
     }
 }
