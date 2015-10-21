fb139be64acdf2168ef2859e32f0cdc0f3e4c2f1
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-21 09:54:05 +0100

    [Tests] Bolt\Twig\Handler\UtilsHandler::printBacktrace

--- tests/phpunit/unit/Twig/Handler/UtilsHandlerTest.php
+++ tests/phpunit/unit/Twig/Handler/UtilsHandlerTest.php
@@ -2,8 +2,9 @@
 
 namespace Bolt\Tests\Twig;
 
-use Bolt\Tests\BoltUnitTest;
 use Bolt\Twig\Handler\UtilsHandler;
+use Bolt\Tests\BoltUnitTest;
+use Symfony\Component\VarDumper\VarDumper;
 
 /**
  * Class to test Bolt\Twig\Handler\UtilsHandler
@@ -12,6 +13,24 @@ use Bolt\Twig\Handler\UtilsHandler;
  */
 class UtilsHandlerTest extends BoltUnitTest
 {
+    protected function tearDown()
+    {
+        parent::tearDown();
+        VarDumper::setHandler(null);
+    }
+
+    /**
+     * Override Symfony's default handler to get the output
+     */
+    protected function stubVarDumper()
+    {
+        VarDumper::setHandler(
+            function ($var) {
+                return $var;
+            }
+        );
+    }
+
     public function testFileExists()
     {
         $app = $this->getApp();
@@ -28,5 +47,41 @@ class UtilsHandlerTest extends BoltUnitTest
 
         $result = $handler->fileExists(__FILE__, true);
         $this->assertFalse($result);
+    }
+
+    public function testPrintBacktraceSafeDebugOn()
+    {
+        $app = $this->getApp();
+        $app['debug'] = true;
+        $handler = new UtilsHandler($app);
+
+        $result = $handler->printBacktrace(5, true);
+        $this->assertNull($result);
+    }
+
+    public function testPrintBacktraceNoSafeDebugOff()
+    {
+        $app = $this->getApp();
+        $app['debug'] = false;
+        $handler = new UtilsHandler($app);
+
+        $result = $handler->printBacktrace(5, false);
+        $this->assertNull($result);
+    }
+
+    public function testPrintBacktraceNoSafeDebugOn()
+    {
+        $this->stubVarDumper();
+
+        $app = $this->getApp();
+        $app['debug'] = true;
+        $handler = new UtilsHandler($app);
+
+        $result = $handler->printBacktrace(5, false);
+        $this->assertCount(5, $result);
+        $this->assertArrayHasKey('file', $result[0]);
+        $this->assertArrayHasKey('line', $result[0]);
+        $this->assertArrayHasKey('function', $result[0]);
+        $this->assertArrayHasKey('class', $result[0]);
     }
 }
