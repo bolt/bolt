15f0ad46126a20b1350494d605783cbe05384675
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-22 11:15:23 +0100

    [Tests] Bolt\Twig\Handler\HtmlHandler::editable

--- tests/phpunit/unit/Twig/Handler/HtmlHandlerTest.php
+++ tests/phpunit/unit/Twig/Handler/HtmlHandlerTest.php
@@ -4,6 +4,7 @@ namespace Bolt\Tests\Twig;
 
 use Bolt\Tests\BoltUnitTest;
 use Bolt\Twig\Handler\HtmlHandler;
+use Bolt\Legacy\Content;
 
 /**
  * Class to test Bolt\Twig\Handler\HtmlHandler
@@ -59,5 +60,28 @@ class HtmlHandlerTest extends BoltUnitTest
 
         $result = $handler->decorateTT('Lorem `ipsum` dolor.');
         $this->assertSame('Lorem <tt>ipsum</tt> dolor.', $result);
+    }
+
+    public function testEditableSafe()
+    {
+        $app = $this->getApp();
+        $handler = new HtmlHandler($app);
+
+        $result = $handler->editable('<blink>Drop Bear Warning!</blink>', new Content($app), 'paddock', true);
+        $this->assertNull($result);
+    }
+
+    public function testEditable()
+    {
+        $app = $this->getApp();
+        $handler = new HtmlHandler($app);
+        $content = new Content($app);
+        $content->setValues([
+            'id'          => 42,
+            'contenttype' => ['slug' => 'snail'],
+        ]);
+
+        $result = $handler->editable('<blink>Drop Bear Warning!</blink>', $content, 'paddock', false);
+        $this->assertSame('<div class="Bolt-editable" data-id="42" data-contenttype="" data-field="paddock"><blink>Drop Bear Warning!</blink></div>', $result);
     }
 }
