fa5047d8fb23c18888af673866ab7967666824dc
Gawain Lynch <gawain.lynch@gmail.com>
2015-10-27 15:55:56 +0000

    Sparate relation inbound/outbound classes

---
+++ src/Storage/Field/Type/RelationInboundType.php
@@ -0,0 +1,15 @@
+<?php
+namespace Bolt\Storage\Field\Type;
+
+use Bolt\Storage\EntityProxy;
+use Bolt\Storage\Mapping\ClassMetadata;
+use Bolt\Storage\Query\QueryInterface;
+use Bolt\Storage\QuerySet;
+use Doctrine\DBAL\Query\QueryBuilder;
+
+/**
+ *
+ */
+class RelationInboundType extends RelationTypeBase
+{
+}
