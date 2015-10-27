b175bf5ad30fb6aecb92f4e91be0b529081a3489
rarila <magnet@gmx.net>
2015-10-27 03:36:56 +0100

    Update library file sizes

--- app/src/grunt/concat.js
+++ app/src/grunt/concat.js
@@ -82,22 +82,22 @@ module.exports = function (grunt, option) {
             },
             nonull: true,
             src: [
-                '<%= path.tmp %>/jquery.min.js',                                //  95 kb
+                '<%= path.tmp %>/jquery.min.js',                                //  83 kb
                 '<%= path.tmp %>/jquery.cookie.min.js',                         //   2 kb
                 '<%= path.tmp %>/jquery.formatDateTime.min.js',                 //   3 kb
                 '<%= path.tmp %>/jquery.tagcloud.min.js',                       //   2 kb
                 '<%= path.tmp %>/underscore.min.js',                            //  16 kb
-                '<%= path.tmp %>/backbone.min.js',                              //  19 kb
-                '<%= path.tmp %>/bootbox.min.js',                               //   9 kb
+                '<%= path.tmp %>/backbone.min.js',                              //  22 kb
+                '<%= path.tmp %>/bootbox.min.js',                               //  10 kb
                 '<%= path.tmp %>/jquery.magnific-popup.min.js',                 //  21 kb
                 '<%= path.src.lib %>/jquery-ui-1.11.4.custom/jquery-ui.min.js', //  96 kb
-                '<%= path.tmp %>/bootstrap-file-input.min.js',                  //   1 kb
+                '<%= path.tmp %>/bootstrap-file-input.min.js',                  //   2 kb
                 '<%= path.tmp %>/jquery-hotkeys.min.js',                        //   2 kb
-                '<%= path.tmp %>/jquery.iframe-transport.min.js',               //   2 kb
-                '<%= path.tmp %>/jquery.fileupload.min.js',                     //  15 kb
+                '<%= path.tmp %>/jquery.iframe-transport.min.js',               //   3 kb
+                '<%= path.tmp %>/jquery.fileupload.min.js',                     //  18 kb
                 '<%= path.tmp %>/jquery.fileupload-process.min.js',             //   2 kb
                 '<%= path.tmp %>/jquery.fileupload-validate.min.js',            //   2 kb
-                '<%= path.tmp %>/bootstrap.min.js',                             //   2 kb
+                '<%= path.tmp %>/bootstrap.min.js',                             //  24 kb
                 '<%= path.src.bower %>/select2/dist/js/select2.min.js',         //  62 kb
                 '<%= path.tmp %>/moment.min.js',                                //  35 kb
                 '<%= path.tmp %>/modernizr-custom.min.js'                       //   5 kb
