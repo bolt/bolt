3962f5a9164588b864da95cce1d824e5a7aabaed
rarila <magnet@gmx.net>
2015-10-28 14:47:18 +0100

    Add dist file for bootlint config

---
+++ app/src/grunt-local/dist/bootlint.js
@@ -0,0 +1,10 @@
+/**
+ *  This is just a sample file!
+ *  Copy it to /app/src/grunt-local/ to have an effect!
+ */
+module.exports = {
+    relaxerror: ["W012"],
+    showallerrors: false,
+    stoponerror: true,
+    stoponwarning: false
+};
