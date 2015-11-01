4c61bb1034ac3c21b91620799cc86acde72b3119
rarila <magnet@gmx.net>
2015-11-01 02:12:55 +0100

    Chain list bindings

--- app/src/js/upload-files.js
+++ app/src/js/upload-files.js
@@ -76,87 +76,85 @@ var FilelistHolder = Backbone.View.extend({
 
     bindEvents: function () {
         var thislist = this,
-            fieldset = this.fieldset;
+            fieldset = this.fieldset,
+            lastClick = null;
 
-        this.list.sortable({
-            helper: function (e, item) {
-                if (!item.hasClass('selected')) {
-                    item.toggleClass('selected');
-                }
-
-                return $('<div></div>');
-            },
-            start: function (e, ui) {
-                var elements = fieldset.find('.selected').not('.ui-sortable-placeholder'),
-                    len = elements.length,
-                    currentOuterHeight = ui.placeholder.outerHeight(true),
-                    currentInnerHeight = ui.placeholder.height(),
-                    margin = parseInt(ui.placeholder.css('margin-top')) + parseInt(ui.placeholder.css('margin-bottom'));
-
-                elements.css('display', 'none');
-                ui.placeholder.height(currentInnerHeight + len * currentOuterHeight - currentOuterHeight - margin);
-                ui.item.data('items', elements);
-            },
-            beforeStop: function (e, ui) {
-                ui.item.before(ui.item.data('items'));
-            },
-            stop: function () {
-                fieldset.find('.ui-state-active').css('display', '');
-                thislist.serialize();
-            },
-            delay: 100,
-            distance: 5
-        });
-
-        var lastClick = null;
-        this.list.on('click', '.list-item', function (e) {
-            if ($(e.target).hasClass('list-item')) {
-                if (e.shiftKey) {
-                    if (lastClick) {
-                        var currentIndex = $(this).index(),
-                            lastIndex = lastClick.index();
-
-                        if (lastIndex > currentIndex) {
-                            $(this).nextUntil(lastClick).add(this).add(lastClick).addClass('selected');
-                        } else if (lastIndex < currentIndex) {
-                            $(this).prevUntil(lastClick).add(this).add(lastClick).addClass('selected');
-                        } else {
-                            $(this).toggleClass('selected');
-                        }
+        this.list
+            .sortable({
+                helper: function (e, item) {
+                    if (!item.hasClass('selected')) {
+                        item.toggleClass('selected');
                     }
-                } else if (e.ctrlKey || e.metaKey) {
-                    $(this).toggleClass('selected');
-                } else {
-                    fieldset.find('.list-item').not($(this)).removeClass('selected');
-                    $(this).toggleClass('selected');
-                }
 
-                if (!e.shiftKey && !e.ctrlKey && !e.metaKey && !$(this).hasClass('selected')) {
-                    lastClick = null;
-                } else {
-                    lastClick = $(this);
+                    return $('<div></div>');
+                },
+                start: function (e, ui) {
+                    var elements = fieldset.find('.selected').not('.ui-sortable-placeholder'),
+                        len = elements.length,
+                        currentOuterHeight = ui.placeholder.outerHeight(true),
+                        currentInnerHeight = ui.placeholder.height(),
+                        margin = parseInt(ui.placeholder.css('margin-top')) + parseInt(ui.placeholder.css('margin-bottom'));
+
+                    elements.css('display', 'none');
+                    ui.placeholder.height(currentInnerHeight + len * currentOuterHeight - currentOuterHeight - margin);
+                    ui.item.data('items', elements);
+                },
+                beforeStop: function (e, ui) {
+                    ui.item.before(ui.item.data('items'));
+                },
+                stop: function () {
+                    fieldset.find('.ui-state-active').css('display', '');
+                    thislist.serialize();
+                },
+                delay: 100,
+                distance: 5
+            })
+            .on('click', '.list-item', function (e) {
+                if ($(e.target).hasClass('list-item')) {
+                    if (e.shiftKey) {
+                        if (lastClick) {
+                            var currentIndex = $(this).index(),
+                                lastIndex = lastClick.index();
+
+                            if (lastIndex > currentIndex) {
+                                $(this).nextUntil(lastClick).add(this).add(lastClick).addClass('selected');
+                            } else if (lastIndex < currentIndex) {
+                                $(this).prevUntil(lastClick).add(this).add(lastClick).addClass('selected');
+                            } else {
+                                $(this).toggleClass('selected');
+                            }
+                        }
+                    } else if (e.ctrlKey || e.metaKey) {
+                        $(this).toggleClass('selected');
+                    } else {
+                        fieldset.find('.list-item').not($(this)).removeClass('selected');
+                        $(this).toggleClass('selected');
+                    }
+
+                    if (!e.shiftKey && !e.ctrlKey && !e.metaKey && !$(this).hasClass('selected')) {
+                        lastClick = null;
+                    } else {
+                        lastClick = $(this);
+                    }
                 }
-            }
-        });
+            })
+            .on('click', '.remove-button', function (evt) {
+                evt.preventDefault();
+
+                if (confirm(Bolt.data(thislist.datRemove))) {
+                    $(this).closest('.list-item').remove();
+                    thislist.serialize();
+                }
+            })
+            .on('change', 'input', function () {
+                thislist.serialize();
+            });
 
         fieldset.find('.remove-selected-button').on('click', function () {
             if (confirm(Bolt.data(thislist.datRemoveMulti))) {
                 fieldset.find('.selected').closest('.list-item').remove();
                 thislist.serialize();
             }
-        });
-
-        this.list.on('click', '.remove-button', function (evt) {
-            evt.preventDefault();
-
-            if (confirm(Bolt.data(thislist.datRemove))) {
-                $(this).closest('.list-item').remove();
-                thislist.serialize();
-            }
-        });
-
-        this.list.on('change', 'input', function () {
-            thislist.serialize();
         });
     },
 
