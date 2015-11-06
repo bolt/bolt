6fe710f04747fae6c86f57d312a9fe804d010a22
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-06 15:40:18 +0000

    Table modification handler class

---
+++ src/Storage/Database/Schema/TableModifier.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bolt\Storage\Database\Schema;
+
+use Bolt\Logger\FlashLoggerInterface;
+use Bolt\Translation\Translator as Trans;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Table modification handler class.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class TableModifier
+{
+    /** @var \Doctrine\DBAL\Connection */
+    protected $connection;
+    /** @var \Psr\Log\LoggerInterface */
+    protected $loggerSystem;
+    /** @var \Bolt\Logger\FlashLoggerInterface */
+    protected $loggerFlash;
+
+    /**
+     * Constructor.
+     *
+     * @param Connection           $connection
+     * @param LoggerInterface      $loggerSystem
+     * @param FlashLoggerInterface $loggerFlash
+     */
+    public function __construct(Connection $connection, LoggerInterface $loggerSystem, FlashLoggerInterface $loggerFlash)
+    {
+        $this->connection = $connection;
+        $this->loggerSystem = $loggerSystem;
+        $this->loggerFlash = $loggerFlash;
+    }
+}
