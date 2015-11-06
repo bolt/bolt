634570a078c661e3be901bf233934e8c0918194b
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-06 15:25:35 +0000

    Builder for Bolt content tables.

---
+++ src/Storage/Database/Schema/Builder/ContentTables.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace Bolt\Storage\Database\Schema\Builder;
+
+use Bolt\Config;
+use Bolt\Storage\Database\Schema\Table\ContentType;
+use Bolt\Storage\Field\Manager as FieldManager;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+
+/**
+ * Builder for Bolt content tables.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ContentTables extends BaseBuilder
+{
+    /**
+     * Build the schema for Bolt ContentType tables.
+     *
+     * @param Schema $schema
+     * @param Config $config
+     *
+     * @return \Doctrine\DBAL\Schema\Table[]
+     */
+    public function getSchemaTables(Schema $schema, Config $config)
+    {
+        /** @var $fieldManager FieldManager */
+        $fieldManager = $config->getFields();
+        $contentTypes = $config->get('contenttypes');
+        $tables = [];
+        foreach ($this->tables->keys() as $name) {
+            $contentType = $contentTypes[$name];
+            $tables[$name] = $this->tables[$name]->buildTable($schema, $this->prefix. $name, $name);
+            if (isset($contentType['fields']) && is_array($contentType['fields'])) {
+                $this->addContentTypeTableColumns($this->tables[$name], $tables[$name], $contentType['fields'], $fieldManager);
+            }
+        }
+
+        return $tables;
+    }
+
+    /**
+     * Add the custom columns for the ContentType.
+     *
+     * @param \Bolt\Storage\Database\Schema\Table\ContentType $tableObj
+     * @param \Doctrine\DBAL\Schema\Table                     $table
+     * @param array                                           $fields
+     * @param FieldManager                                    $fieldManager
+     */
+    private function addContentTypeTableColumns(ContentType $tableObj, Table $table, array $fields, FieldManager $fieldManager)
+    {
+        // Check if all the fields are present in the DB.
+        foreach ($fields as $fieldName => $values) {
+            /** @var \Doctrine\DBAL\Platforms\Keywords\KeywordList $reservedList */
+            $reservedList = $this->connection->getDatabasePlatform()->getReservedKeywordsList();
+            if ($reservedList->isKeyword($fieldName)) {
+                $error = sprintf(
+                    "You're using '%s' as a field name, but that is a reserved word in %s. Please fix it, and refresh this page.",
+                    $fieldName,
+                    $this->connection->getDatabasePlatform()->getName()
+                );
+                $this->flashLogger->error($error);
+                continue;
+            }
+
+            $this->addContentTypeTableColumn($tableObj, $table, $fieldName, $values, $fieldManager);
+        }
+    }
+
+    /**
+     * Add a single column to the ContentType table.
+     *
+     * @param \Bolt\Storage\Database\Schema\Table\ContentType $tableObj
+     * @param \Doctrine\DBAL\Schema\Table                     $table
+     * @param string                                          $fieldName
+     * @param array                                           $values
+     * @param FieldManager                                    $fieldManager
+     */
+    private function addContentTypeTableColumn(ContentType $tableObj, Table $table, $fieldName, array $values, FieldManager $fieldManager)
+    {
+        if ($tableObj->isKnownType($values['type'])) {
+            // Use loose comparison on true as 'true' in YAML is a string
+            $addIndex = isset($values['index']) && (boolean) $values['index'] === true;
+            // Add the contenttype's specific fields
+            $tableObj->addCustomFields($fieldName, $this->getContentTypeTableColumnType($values), $addIndex);
+        } elseif ($handler = $fieldManager->getDatabaseField($values['type'])) {
+            // Add template fields
+            /** @var $handler \Bolt\Storage\Field\FieldInterface */
+            $table->addColumn($fieldName, $handler->getStorageType(), $handler->getStorageOptions());
+        }
+    }
+
+    /**
+     * Certain field types can have single or JSON array types, figure it out.
+     *
+     * @param array $values
+     *
+     * @return string
+     */
+    private function getContentTypeTableColumnType(array $values)
+    {
+        // Multi-value selects are stored as JSON arrays
+        if (isset($values['type']) && $values['type'] === 'select' && isset($values['multiple']) && $values['multiple'] === 'true') {
+            return 'selectmultiple';
+        }
+
+        return $values['type'];
+    }
+}
