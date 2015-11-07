b48697e9350abb8ec285c05089a890bda49e5511
rarila <magnet@gmx.net>
2015-11-07 01:23:10 +0100

    Implement select widget functionality

--- app/src/js/widgets/buic/select.js
+++ app/src/js/widgets/buic/select.js
@@ -40,6 +40,38 @@
          * @private
          */
         _create: function () {
+            var select = this.element.find('select'),
+                buttonAll = this.element.find('.select-all'),
+                buttonNone = this.element.find('.select-none'),
+                setButtonState;
+
+            // Initialize the select-all button.
+            buttonAll.prop('title', buttonAll.text().trim());
+            buttonAll.on('click', function () {
+                select.find('option').prop('selected', true).trigger('change');
+                this.blur();
+            });
+
+            // Initialize the select-none button.
+            buttonNone.prop('title', buttonNone.text().trim());
+            buttonNone.on('click', function () {
+                select.val(null).trigger('change');
+                this.blur();
+            });
+
+            // Enable/disable buttons.
+            setButtonState = function () {
+                var options = select.find('option'),
+                    count = options.length,
+                    selected = options.filter(':selected').length,
+                    empty = select.prop('multiple') ? selected === 0 : select.val() === '';
+
+                buttonAll.prop('disabled', selected === count);
+                buttonNone.prop('disabled', empty);
+            };
+
+            setButtonState();
+            select.on('change', setButtonState);
         }
     });
 })(jQuery);
