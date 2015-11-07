ef77785c4c4a4217df73851f11ca0e3f182f5615
rarila <magnet@gmx.net>
2015-11-07 14:12:38 +0100

    Add listingpart widget

---
+++ app/src/js/widgets/buic/listingpart.js
@@ -0,0 +1,45 @@
+/**
+ * See (http://jquery.com/).
+ * @name jQuery
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name widget
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ * @memberOf jQuery
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name bolt
+ * @class
+ * @memberOf jQuery.widget
+ * @param {object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * Bolt listingpart - tbody inside a listing.
+     *
+     * @class listingpart
+     * @memberOf jQuery.widget.bolt
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     */
+    $.widget('bolt.listingpart', /** @lends jQuery.widget.bolt.listingpart */ {
+        /**
+         * The constructor of the listingpart widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
