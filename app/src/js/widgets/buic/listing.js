b0e3eef0233179142456041c239728a5279f3fca
rarila <magnet@gmx.net>
2015-11-07 02:20:50 +0100

    Create listing widget stub

---
+++ app/src/js/widgets/buic/listing.js
@@ -0,0 +1,45 @@
+/**
+ * See (http://jquery.com/).
+ * @name jQuery
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name widget
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ * @memberOf jQuery
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name bolt
+ * @class
+ * @memberOf jQuery.widget
+ * @param {object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * Bolt listing.
+     *
+     * @class listing
+     * @memberOf jQuery.widget.bolt
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     */
+    $.widget('bolt.listing', /** @lends jQuery.widget.bolt.listing */ {
+        /**
+         * The constructor of the listing widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
