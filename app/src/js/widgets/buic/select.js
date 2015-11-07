bd9580d7c0eb9c3c234c30aceff6411c4aa15f53
rarila <magnet@gmx.net>
2015-11-07 01:07:46 +0100

    Create select widget

---
+++ app/src/js/widgets/buic/select.js
@@ -0,0 +1,45 @@
+/**
+ * See (http://jquery.com/).
+ * @name jQuery
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name widget
+ * @class
+ * See the jQuery Library  (http://jquery.com/) for full details. This just
+ * documents the function and classes that are added to jQuery by this plug-in.
+ * @memberOf jQuery
+ */
+
+/**
+ * See (http://jquery.com/)
+ * @name bolt
+ * @class
+ * @memberOf jQuery.widget
+ * @param {object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * Bolt select.
+     *
+     * @class select
+     * @memberOf jQuery.widget.bolt
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     */
+    $.widget('bolt.select', /** @lends jQuery.widget.bolt.select */ {
+        /**
+         * The constructor of the select widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
