01d48bb10b90cd96a1ea68e00018f4fa07e12b92
rarila <magnet@gmx.net>
2015-11-09 15:01:56 +0100

    Fix js documentation: don't include widget class multiple times

--- app/src/js/widgets/buic/checkbox.js
+++ app/src/js/widgets/buic/checkbox.js
@@ -1,14 +1,5 @@
 /**
  * See (http://jquery.com/)
- * @name widget
- * @class
- * See the jQuery Library  (http://jquery.com/) for full details. This just
- * documents the function and classes that are added to jQuery by this plug-in.
- * @memberOf jQuery
- */
-
-/**
- * See (http://jquery.com/)
  * @name bolt
  * @class
  * @memberOf jQuery.widget
