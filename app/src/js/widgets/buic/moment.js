4b10c3e097967f1428991fe514ff2064a26c3e71
rarila <magnet@gmx.net>
2015-11-09 14:58:26 +0100

    Fix js documentation: don't include jQuery class multiple tmes

--- app/src/js/widgets/buic/moment.js
+++ app/src/js/widgets/buic/moment.js
@@ -1,10 +1,3 @@
-/**
- * See (http://jquery.com/).
- * @name jQuery
- * @class
- * See the jQuery Library  (http://jquery.com/) for full details. This just
- * documents the function and classes that are added to jQuery by this plug-in.
- */
 
 /**
  * See (http://jquery.com/)
--- app/src/js/widgets/buic/select.js
+++ app/src/js/widgets/buic/select.js
@@ -1,12 +1,4 @@
 /**
- * See (http://jquery.com/).
- * @name jQuery
- * @class
- * See the jQuery Library  (http://jquery.com/) for full details. This just
- * documents the function and classes that are added to jQuery by this plug-in.
- */
-
-/**
  * See (http://jquery.com/)
  * @name widget
  * @class
