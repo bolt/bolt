0e4af20e103b116f66a8b422d437a3fe585a4715
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-10 16:31:12 +0000

    [Tests] Bolt\Nut\UserResetPassword with hash verification

---
+++ tests/phpunit/unit/Nut/UserResetPasswordTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Bolt\Tests\Nut;
+
+use Bolt\Nut\UserResetPassword;
+use Bolt\Tests\BoltUnitTest;
+use Bolt\Storage\Entity;
+use PasswordLib\PasswordLib;
+use Symfony\Component\Console\Helper\HelperSet;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * Class to test src/Nut/UserResetPassword.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class UserResetPasswordTest extends BoltUnitTest
+{
+    public function testRun()
+    {
+        $this->resetDb();
+        $app = $this->getApp();
+        $repo = $app['storage']->getRepository('Bolt\Storage\Entity\Users');
+        $user = new Entity\Users([
+            'username'    => 'koala',
+            'password'    => 'GumL3@ve$',
+            'email'       => 'koala@drop.bear.com.au',
+            'displayname' => 'Drop Bear',
+            'roles'       => ['root'],
+        ]);
+        $repo->save($user);
+
+        $command = new UserResetPassword($app);
+        $tester = new CommandTester($command);
+
+        $helper = $this->getMock('\Symfony\Component\Console\Helper\QuestionHelper', ['ask']);
+        $helper->expects($this->once())
+            ->method('ask')
+            ->will($this->returnValue(true));
+        $set = new HelperSet(['question' => $helper]);
+        $command->setHelperSet($set);
+
+        $tester->execute(['username' => 'koala'], ['interactive' => false]);
+        $result = $tester->getDisplay();
+        $this->assertRegExp('#New password for koala is #', trim($result));
+        $this->assertSame(38, strlen(trim($result)));
+
+        // Test that the saved value matches the hash
+        $repo = $app['storage']->getRepository('Bolt\Storage\Entity\Users');
+        $userEntity = $repo->getUser('koala');
+        $crypt = new PasswordLib();
+
+        // Check the old password isn't valid
+        $auth = $crypt->verifyPasswordHash('GumL3@ve$', $userEntity->getPassword());
+        $this->assertFalse($auth);
+
+        // Check the new password is valid
+        $bits = explode(' ', trim($result));
+        $auth = $crypt->verifyPasswordHash($bits[5], $userEntity->getPassword());
+        $this->assertTrue($auth);
+    }
+}
