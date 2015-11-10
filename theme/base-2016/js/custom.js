1f5dcc56712c96774fd2b90eeba18e647634a2d7
Svante Richter <svante.richter@gmail.com>
2015-11-10 23:40:59 +0100

    Working on the base, perhaps I'm all about that base...
    all about that base, all about that base, all about that base

--- theme/base-2016/js/custom.js
+++ theme/base-2016/js/custom.js
@@ -15,10 +15,8 @@ $(document).ready(function () {
             easing: 'ease-in-out'
         }
     });
-
-    // Menu JS
-    $("#menu-icon").on("click", function () {
-        $("ul.menu").slideToggle();
-        $(this).toggleClass("active");
-    });
+});
+document.querySelector("#menu-icon").addEventListener("click", function() {
+    document.querySelector("ul.menu").classList.toggle("show");
+    document.querySelector("#menu-icon").classList.toggle("active");
 });
