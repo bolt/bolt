812708eb8452be6663943e6c4b2ce30e3cade99c
rarila <magnet@gmx.net>
2015-11-12 15:29:18 +0100

    Add imagelist field widget

---
+++ app/src/js/widgets/fields/fieldImagelist.js
@@ -0,0 +1,35 @@
+/**
+ * Imagelist field widget.
+ *
+ * @param {Object} $ - Global jQuery object
+ * @param {Object} bolt - The Bolt module.
+ */
+(function ($, bolt) {
+    'use strict';
+
+    /**
+     * Imagelist field widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class fieldImagelist
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.fieldImagelist', /** @lends jQuery.widget.bolt.fieldImagelist */ {
+        /**
+         * The constructor of the imagelist field widget.
+         *
+         * @private
+         */
+        _create: function () {
+            bolt.uploads.bindList(
+                this.element,
+                {
+                    removeSingle: bolt.data('field.imagelist.message.remove'),
+                    removeMulti: bolt.data('field.imagelist.message.removeMulti')
+                }
+            );
+        }
+    });
+})(jQuery, Bolt);
