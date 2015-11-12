dcb883c41b42c7fd0420adfe2fd472b54e1fd9a9
rarila <magnet@gmx.net>
2015-11-12 15:28:34 +0100

    Add filelist field widget

---
+++ app/src/js/widgets/fields/fieldFilelist.js
@@ -0,0 +1,35 @@
+/**
+ * Filelist field widget.
+ *
+ * @param {Object} $ - Global jQuery object
+ * @param {Object} bolt - The Bolt module.
+ */
+(function ($, bolt) {
+    'use strict';
+
+    /**
+     * Filelist field widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class fieldFilelist
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.fieldFilelist', /** @lends jQuery.widget.bolt.fieldFilelist */ {
+        /**
+         * The constructor of the filelist field widget.
+         *
+         * @private
+         */
+        _create: function () {
+            bolt.uploads.bindList(
+                this.element,
+                {
+                    removeSingle: bolt.data('field.filelist.message.remove'),
+                    removeMulti: bolt.data('field.filelist.message.removeMulti')
+                }
+            );
+        }
+    });
+})(jQuery, Bolt);
