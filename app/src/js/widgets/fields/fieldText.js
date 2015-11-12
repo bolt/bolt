a858646ce4544da9bb2ee91f0308a92804925889
rarila <magnet@gmx.net>
2015-11-12 03:35:55 +0100

    Create text field widget

---
+++ app/src/js/widgets/fields/fieldText.js
@@ -0,0 +1,27 @@
+/**
+ * Text field widget.
+ *
+ * @param {object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * Text field widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class fieldText
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.fieldText', /** @lends jQuery.widget.bolt.fieldText */ {
+        /**
+         * The constructor of the text field widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
