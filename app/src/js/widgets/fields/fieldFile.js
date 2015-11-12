9fe1d5665e4f93d2f4814c1201a9938c47b59afb
rarila <magnet@gmx.net>
2015-11-12 12:55:58 +0100

    Add textarea widget field

---
+++ app/src/js/widgets/fields/fieldFile.js
@@ -0,0 +1,29 @@
+/**
+ * File field widget.
+ *
+ * @param {Object} $ - Global jQuery object
+ * @param {Object} bolt - The Bolt module.
+ */
+(function ($, bolt) {
+    'use strict';
+
+    /**
+     * File field widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class fieldFile
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.fieldFile', /** @lends jQuery.widget.bolt.fieldFile */ {
+        /**
+         * The constructor of the file field widget.
+         *
+         * @private
+         */
+        _create: function () {
+            bolt.uploads.bindField(this.element);
+        }
+    });
+})(jQuery, Bolt);
---
+++ app/src/js/widgets/fields/fieldTextarea.js
@@ -0,0 +1,27 @@
+/**
+ * Textarea field widget.
+ *
+ * @param {object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * Textarea field widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class fieldTextarea
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.fieldTextarea', /** @lends jQuery.widget.bolt.fieldTextarea */ {
+        /**
+         * The constructor of the textarea field widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
