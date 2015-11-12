1e5c80ccda9f506289765b4e4b2146beacca1c78
rarila <magnet@gmx.net>
2015-11-12 16:24:46 +0100

    Convert releationship field to widget

--- app/src/Gruntfile.js
+++ app/src/Gruntfile.js
@@ -60,7 +60,6 @@ module.exports = function(grunt) {
                 '<%= path.src.js %>/modules/fields/categories.js',
                 '<%= path.src.js %>/modules/fields/geolocation.js',
                 '<%= path.src.js %>/modules/fields/repeater.js',
-                '<%= path.src.js %>/modules/fields/relationship.js',
                 '<%= path.src.js %>/modules/fields/select.js',
                 '<%= path.src.js %>/modules/fields/slug.js',
                 '<%= path.src.js %>/modules/fields/tags.js',
