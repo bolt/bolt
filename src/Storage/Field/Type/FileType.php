3b18291e33097f7640b0b7c2a17c3c935d12322d
Ross Riley <riley.ross@gmail.com>
2015-11-15 18:40:18 +0000

    add explicit storage types for fields

--- src/Storage/Field/Type/FileType.php
+++ src/Storage/Field/Type/FileType.php
@@ -1,6 +1,8 @@
 <?php
 namespace Bolt\Storage\Field\Type;
 
+use Doctrine\DBAL\Types\Type;
+
 /**
  * This is one of a suite of basic Bolt field transformers that handles
  * the lifecycle of a field from pre-query to persist.
@@ -15,5 +17,13 @@ class FileType extends FieldTypeBase
     public function getName()
     {
         return 'file';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getStorageType()
+    {
+        return Type::getType('json_array');
     }
 }
