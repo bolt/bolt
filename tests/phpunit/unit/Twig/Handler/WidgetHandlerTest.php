3c4f1d97a8f2fba908739df9589ea8c141206a33
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-16 04:50:44 +1100

    [Tests] Twig getWidgets() function

--- tests/phpunit/unit/Twig/Handler/WidgetHandlerTest.php
+++ tests/phpunit/unit/Twig/Handler/WidgetHandlerTest.php
@@ -28,6 +28,23 @@ class WidgetHandlerTest extends BoltUnitTest
         $this->assertSame(1, $count);
     }
 
+    public function testGetWidgets()
+    {
+        $app = $this->getApp();
+        $handler = new WidgetHandler($app);
+        $widget = (new Widget())
+            ->setType('frontend')
+            ->setLocation('gum-tree')
+            ->setContent('<blink>Drop Bear Warning!</blink>')
+        ;
+
+        $app['asset.queue.widget']->add($widget);
+        $result = $handler->getWidgets('gum-tree', 'frontend');
+        $this->assertCount(1, $result);
+
+        $this->assertInstanceOf('Bolt\Asset\Widget\Widget', reset($result));
+    }
+
     public function testWidget()
     {
         $app = $this->getApp();
