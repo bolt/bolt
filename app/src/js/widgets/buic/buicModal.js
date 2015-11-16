a343b548ceaffb475e49d2a9b6f3a4eaebd5d170
rarila <magnet@gmx.net>
2015-11-16 01:04:46 +0100

    Add stub for buic modal

---
+++ app/src/js/widgets/buic/buicModal.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * BUIC modal widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class buicModal
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.buicModal', /** @lends jQuery.widget.bolt.buicModal.prototype */ {
+        /**
+         * The constructor of the modal widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
