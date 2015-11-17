7f96e7fb76e65b5738275519c2c68af4b9134ab6
rarila <magnet@gmx.net>
2015-11-17 05:10:02 +0100

    Refactor to allow creation on demand

--- app/src/js/widgets/buic/buicModal.js
+++ app/src/js/widgets/buic/buicModal.js
@@ -15,87 +15,50 @@
      */
     $.widget('bolt.buicModal', /** @lends jQuery.widget.bolt.buicModal.prototype */ {
         /**
+         * Default options.
+         *
+         * @property {boolean}       [closer=false] - Add a close button
+         * @property {Object|string} [headline]     - Add a headline
+         * @property {Object|string} [body]         - Add a body
+         * @property {Object|string} [footer]       - Add a footer
+         */
+        options: {
+            closer:   false,
+            headline: '',
+            body:     '',
+            footer:   ''
+        },
+
+        /**
          * The constructor of the modal widget.
          *
          * @private
          */
         _create: function () {
-            this.element
-                .attr('tabindex', -1)
-                .attr('role', 'dialog')
-                .addClass('buic-modal modal fade');
-        },
+            var self = this;
 
-        /**
-         * Clears the modal.
-         *
-         * @private
-         */
-        _clear: function () {
-            this.element
-                .removeData('bs.modal')
-                .empty();
-        },
+            this._content = $('<div/>')
+                .addClass('modal-content');
 
-        /**
-         * Builds and shows the modal.
-         *
-         * @param {Object}        def                - Definition of the modal
-         * @param {boolean}       [def.closer=false] - Add a close button
-         * @param {Object|string} [def.headline]     - Add a headline
-         * @param {Object|string} [def.body]         - Add a body
-         * @param {Object|string} [def.footer]       - Add a footer
-         */
-        go: function (def) {
-            var self = this,
-                dialog,
-                dialogHeader = '',
-                dialogBody = '',
-                dialogFooter = '',
-                closer = '',
-                headline = '';
-
-            // Header
-            if (def.closer || def.headline) {
-                if (def.closer) {
-                    closer = '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
-                                '<span aria-hidden="true">&times;</span>' +
-                             '</button>';
-                }
-
-                if (def.headline) {
-                    headline = $('<h4>').addClass('modal-title').append(def.headline);
-                }
-
-                dialogHeader = $('<div>').addClass('modal-header').append(closer).append(headline);
-            }
-
-            // Body
-            dialogBody = $('<div>').addClass('modal-body').append(def.body || '');
-
-            // Footer
-            if (def.footer) {
-                dialogFooter = $('<div>').addClass('modal-footer').append(def.footer);
-            }
-
-            // Dialog
-            dialog =
-                $('<div>')
-                    .addClass('modal-dialog')
-                    .attr('role', 'document')
+            this._modal =
+                $('<div/>')
+                    .attr('tabindex', -1)
+                    .attr('role', 'dialog')
+                    .addClass('buic-modal modal fade')
                     .append(
-                        $('<div>')
-                            .addClass('modal-content')
-                            .append(dialogHeader)
-                            .append(dialogBody)
-                            .append(dialogFooter)
+                        $('<div/>')
+                            .addClass('modal-dialog')
+                            .attr('role', 'document')
+                            .append(this._content)
                     );
 
-            // Clear the modal.
-            this._clear();
+            this._addHeader();
+            this._addBody();
+            this._addFooter();
 
-            this.element
-                .append(dialog)
+            this.element.prepend(this._modal);
+
+            this._modal
                 .on('show.bs.modal', function () {
                     self._trigger('show');
                 })
@@ -107,9 +70,72 @@
                 })
                 .on('hidden.bs.modal', function () {
                     self._trigger('hidden');
-                    self._clear();
+                    self.destroy();
                 })
                 .modal('show');
+        },
+
+        /**
+         * Cleaning up.
+         *
+         * @private
+         */
+        _destroy: function () {
+            this._modal
+                .data('modal', null)
+                .remove();
+        },
+
+        /**
+         * Adds a header to the modal.
+         */
+        _addHeader: function () {
+            var closer = '',
+                headline = '';
+
+            // Header
+            if (this.options.closer || this.options.headline) {
+                if (this.options.closer) {
+                    closer = '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+                                '<span aria-hidden="true">&times;</span>' +
+                             '</button>';
+                }
+
+                if (this.options.headline) {
+                    headline = $('<h4/>').addClass('modal-title').append(this.options.headline);
+                }
+
+                this._content.append(
+                    $('<div/>')
+                        .addClass('modal-header')
+                        .append(closer)
+                        .append(headline)
+                );
+            }
+        },
+
+        /**
+         * Adds a body to the modal.
+         */
+        _addBody: function () {
+            this._content.append(
+                $('<div/>')
+                    .addClass('modal-body')
+                    .append(this.options.body || '')
+            );
+        },
+
+        /**
+         * Adds a Footer to the modal.
+         */
+        _addFooter: function () {
+            if (this.options.footer) {
+                this._content.append(
+                    $('<div/>')
+                        .addClass('modal-footer')
+                        .append(this.options.footer)
+                );
+            }
         }
     });
 })(jQuery);
