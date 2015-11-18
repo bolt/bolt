cb5454abea6402c7edacda8522ec5d38c23721af
rarila <magnet@gmx.net>
2015-11-18 16:56:24 +0100

    Load filebrowser dynamically

--- app/src/js/widgets/buic/buicFilebrowser.js
+++ app/src/js/widgets/buic/buicFilebrowser.js
@@ -15,6 +15,15 @@
      */
     $.widget('bolt.buicFilebrowser', /** @lends jQuery.widget.bolt.buicFilebrowser.prototype */ {
         /**
+         * Default options.
+         *
+         * @property {string} [url] - URL to browse
+         */
+        options: {
+            url: ''
+        },
+
+        /**
          * The constructor of the filebrowser widget.
          *
          * @private
@@ -22,6 +31,33 @@
         _create: function () {
             this._on({
                 'click': function() {
+                    this._browse();
+                }
+            });
+        },
+
+        /**
+         *
+         *
+         * @private
+         */
+        _browse: function () {
+            var self = this,
+                params = {};
+
+            $('body').buicModal({
+                large: true,
+                remote: {
+                    url:    self.options.url,
+                    params: params
+                },
+                loaded: function (evt, data) {
+                    data.body
+                        .off('click.bolt')
+                        .on('click.bolt', '[data-fbrowser-chdir]', function () {
+                        self.options.url = $(this).data('fbrowser-chdir');
+                        self._browse();
+                    });
                 }
             });
         }
