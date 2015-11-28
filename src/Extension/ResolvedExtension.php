d9f7941bbc9a1ec2dd8a91396f2d2d9588a3a6fc
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-29 07:41:48 +1100

    PHPDoc functions

--- src/Extension/ResolvedExtension.php
+++ src/Extension/ResolvedExtension.php
@@ -25,38 +25,80 @@ class ResolvedExtension
         $this->innerExtension = $innerExtension;
     }
 
+    /**
+     * Returns the wrapped extension.
+     *
+     * @return \Bolt\Extension\ExtensionInterface
+     */
     public function getInnerExtension()
     {
         return $this->innerExtension;
     }
 
+    /**
+     * Returns the extension name (the class short name).
+     *
+     * @return string
+     */
     public function getName()
     {
         return $this->innerExtension->getName();
     }
 
+    /**
+     * Returns the extension namespace.
+     *
+     * @return string
+     */
     public function getNamespace()
     {
         return $this->innerExtension->getNamespace();
     }
 
+    /**
+     * Returns the extensions directory path.
+     *
+     * The path should always be returned as a Unix path (with /).
+     *
+     * @return string
+     */
     public function getPath()
     {
         return $this->innerExtension->getPath();
     }
 
+    /**
+     * Return the extension's base URL.
+     *
+     * @return string
+     */
     public function getUrl()
     {
     }
 
+    /**
+     * Return the extension's install type, either 'composer' or 'local'.
+     *
+     * @return string
+     */
     public function getInstallType()
     {
     }
 
+    /**
+     * Check if the extension is enabled.
+     *
+     * @return bool
+     */
     public function isEnabled()
     {
     }
 
+    /**
+     * Enable or disable an extension at runtime.
+     *
+     * @param bool|int $enabled
+     */
     public function setEnabled($enabled)
     {
     }
