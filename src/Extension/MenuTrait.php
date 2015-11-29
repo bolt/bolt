5d719310c21253c4c173a89167ab4a3f8babe817
Gawain Lynch <gawain.lynch@gmail.com>
2015-11-29 17:31:27 +1100

    Trait for extensions to add backend menu items

---
+++ src/Extension/MenuTrait.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Bolt\Extension;
+
+use Bolt\Helpers\MenuEntry;
+
+/**
+ * Admin menu handling trait for an extension.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+trait MenuTrait
+{
+    /** @return \Silex\Application */
+    abstract protected function getApp();
+
+    /**
+     * Add a menu option to backend menu.
+     *
+     * @param string $label
+     * @param string $path
+     * @param string $icon
+     * @param string $permission
+     */
+    protected function addMenu($label, $path, $icon = null, $permission = null)
+    {
+        /** @var MenuEntry $menus */
+        $menus = $this->getApp()['menu.admin'];
+        $child = (new MenuEntry($label, $path))
+            ->setLabel($label)
+            ->setIcon($icon)
+            ->setPermission($permission)
+        ;
+
+        $menus->getChild('extend')->addChild($child);
+    }
+}
