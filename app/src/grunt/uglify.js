befac9cd081283565a2150dccdec5563e9b5e275
rarila <magnet@gmx.net>
2015-12-01 21:45:01 +0100

    Let napa get jquery-ui

--- app/src/grunt/uglify.js
+++ app/src/grunt/uglify.js
@@ -75,7 +75,7 @@ module.exports = {
         files: [{
             expand: true,
             ext: '.min.js',
-            cwd: '<%= path.src.bower %>/jquery-ui/ui/i18n',
+            cwd: '<%= path.src.npm %>/jquery-ui/ui/i18n',
             src: '*.js',
             dest: '<%= path.dest.js %>/locale/datepicker',
             rename: function (destBase, destPath) {
