2140b303fa2c26e5fcd0fe98d9d0f313fe52f366
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-06 16:11:58 +1100

    Deprecate Bolt\Library

--- src/Library.php
+++ src/Library.php
@@ -10,6 +10,8 @@ use Symfony\Component\HttpFoundation\Response;
 /**
  * Class for Bolt's generic library functions.
  *
+ * @deprecated Deprecated since 3.0, to be removed in 4.0.
+ *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
 class Library
