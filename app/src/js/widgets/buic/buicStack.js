bcb92a3482f68aeddcb6a034cb1727abfe26aafd
rarila <magnet@gmx.net>
2015-12-09 03:39:09 +0100

    Add stub for stack widget

---
+++ app/src/js/widgets/buic/buicStack.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object} $ - Global jQuery object
+ */
+(function ($) {
+    'use strict';
+
+    /**
+     * BUIC stack widget.
+     *
+     * @license http://opensource.org/licenses/mit-license.php MIT License
+     * @author rarila
+     *
+     * @class buicStack
+     * @memberOf jQuery.widget.bolt
+     */
+    $.widget('bolt.buicStack', /** @lends jQuery.widget.bolt.buicStack.prototype */ {
+        /**
+         * The constructor of the stack widget.
+         *
+         * @private
+         */
+        _create: function () {
+        }
+    });
+})(jQuery);
