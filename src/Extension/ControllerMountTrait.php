9cca39d7c53529b3e666ef7dae1058e6b6e9f6de
Carson Full <carsonfull@gmail.com>
2015-12-09 19:09:40 -0600

    Split registerControllers to registerFrontendControllers and registerBackendControllers

--- src/Extension/ControllerMountTrait.php
+++ src/Extension/ControllerMountTrait.php
@@ -3,6 +3,7 @@
 namespace Bolt\Extension;
 
 use Bolt\Events\MountEvent;
+use Pimple as Container;
 use Silex\ControllerCollection;
 use Silex\ControllerProviderInterface;
 
@@ -14,7 +15,7 @@ use Silex\ControllerProviderInterface;
 trait ControllerMountTrait
 {
     /**
-     * Returns a list of controllers to mount.
+     * Returns a list of frontend controllers to mount.
      *
      * <pre>
      *  return [
@@ -24,7 +25,25 @@ trait ControllerMountTrait
      *
      * @return ControllerCollection[]|ControllerProviderInterface[]
      */
-    protected function registerControllers()
+    protected function registerFrontendControllers()
+    {
+        return [];
+    }
+
+    /**
+     * Returns a list of backend controllers to mount.
+     *
+     * Note: The backend prefix will be automatically prepended to prefixes defined here.
+     *
+     * <pre>
+     *  return [
+     *      '/foobar' => new FooController(),
+     *  ];
+     * </pre>
+     *
+     * @return ControllerCollection[]|ControllerProviderInterface[]
+     */
+    protected function registerBackendControllers()
     {
         return [];
     }
@@ -36,8 +55,17 @@ trait ControllerMountTrait
      */
     final public function onMount(MountEvent $event)
     {
-        foreach ($this->registerControllers() as $prefix => $collection) {
+        foreach ($this->registerFrontendControllers() as $prefix => $collection) {
             $event->mount($prefix, $collection);
         }
+
+        $app = $this->getContainer();
+        $backendPrefix = $app['controller.backend.mount_prefix'];
+        foreach ($this->registerBackendControllers() as $prefix => $collection) {
+            $event->mount($backendPrefix . '/' . ltrim($prefix, '/'), $collection);
+        }
     }
+
+    /** @return Container */
+    abstract protected function getContainer();
 }
