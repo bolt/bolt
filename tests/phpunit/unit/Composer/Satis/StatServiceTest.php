1a175850f44f15947ae4d9fc8ecafc799c262f8e
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-11 05:18:40 +0000

    [Tests] Update constructor parameters

--- tests/phpunit/unit/Composer/Satis/StatServiceTest.php
+++ tests/phpunit/unit/Composer/Satis/StatServiceTest.php
@@ -13,7 +13,7 @@ class StatServiceTest extends BoltUnitTest
     public function testSetup()
     {
         $app = $this->getApp();
-        $stat = $this->getMock('Bolt\Composer\Satis\StatService', ['recordInstall'], [$app]);
+        $stat = $this->getMock('Bolt\Composer\Satis\StatService', ['recordInstall'], [$app['guzzle.client'], $app['logger.system'], $app['extend.site']]);
         $stat->expects($this->once())
             ->method('recordInstall')
             ->with('mytest', '1.0.0');
