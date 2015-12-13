f53a7dee131c9b62c957dc1c8086af2843fb1fca
Ross Riley <riley.ross@gmail.com>
2015-12-13 10:13:59 +0000

    if set is an array, filter the values first

--- src/Storage/Field/Type/FieldTypeBase.php
+++ src/Storage/Field/Type/FieldTypeBase.php
@@ -115,6 +115,9 @@ abstract class FieldTypeBase implements FieldTypeInterface
     public function set($entity, $value)
     {
         $key = $this->mapping['fieldname'];
+        if (is_array($value)) {
+            $value = array_filter($value);
+        }
         if (!$value && isset($this->mapping['data']['default'])) {
             $value = $this->mapping['data']['default'];
         }
