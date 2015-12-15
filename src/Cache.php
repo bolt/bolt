aec85fb2ecfb0a4bc120f59b289704e8fc1d3b21
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-15 04:48:24 +0100

    Remove override of getFilename()
    This has been fixed upstream and included since 1.5.0
    - https://github.com/doctrine/cache/pull/94

--- src/Cache.php
+++ src/Cache.php
@@ -23,13 +23,6 @@ class Cache extends FilesystemCache
     private $resourceManager;
     /** @var string */
     private $boltVersion;
-    /** @var string[] replacements for disallowed file characters */
-    private $replacementCharacters = ['__', '-'];
-    /** @var string[] regular expressions for replacing disallowed characters in file name */
-    private $disallowedCharacterPatterns = [
-        '/\-/', // replaced to disambiguate original `-` and `-` derived from replacements
-        '/[^a-zA-Z0-9\-_\[\]]/', // also excludes non-ascii chars (not supported, depending on FS),
-    ];
 
     /**
      * Cache constructor.
@@ -45,33 +38,6 @@ class Cache extends FilesystemCache
         parent::__construct($directory, $extension, $umask);
         $this->resourceManager = $resourceManager;
         $this->boltVersion = $boltVersion;
-    }
-
-    /**
-     * Generate a filename for the cached items in our filebased cache.
-     *
-     * The original Doctrine/cache function stored files in folders that
-     * were nested 32 layers deep. In practice this led to cache folders
-     * containing up to 600,000 folders, while containing only about 15,000
-     * cached items. This is a huge overkill. Here, we use only two levels,
-     * which still means each folder will in practice contain only a very
-     * limited amount of files. i.e.: for 15,000 files, there are 256*256
-     * folders, which statstically means one or two files per folder.
-     *
-     * @param string $id
-     *
-     * @return string
-     */
-    protected function getFilename($id)
-    {
-        $foldername = implode(str_split(substr(hash('sha256', $id), 0, 4), 2), DIRECTORY_SEPARATOR);
-
-        return $this->directory
-            . DIRECTORY_SEPARATOR
-            . $foldername
-            . DIRECTORY_SEPARATOR
-            . preg_replace($this->disallowedCharacterPatterns, $this->replacementCharacters, $id)
-            . self::EXTENSION;
     }
 
     /**
