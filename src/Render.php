2f34ee7e58c47759cb6a5d3bea8940f0e5b2ed50
cdowdy <cdowdy46@gmail.com>
2015-12-16 11:53:46 -0500

    add in cache-control max age to go along with s-max-age directive

--- src/Render.php
+++ src/Render.php
@@ -106,7 +106,12 @@ class Render
                 // maximum duration, otherwise a proxy/cache might keep the
                 // cache twice as long in the worst case scenario, and now it's
                 // only 50% max, but likely less
-                $response->setSharedMaxAge($this->cacheDuration() / 2);
+                // 's_maxage' sets the cache for shared caches.
+                // max_age sets it for regular browser caches
+                $response->setCache(array(
+                    'max_age'       => $this->cacheDuration() / 2,
+                    's_maxage'      => $this->cacheDuration() / 2
+                ));
             }
         }
 
