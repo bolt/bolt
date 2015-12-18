97250528bcde526b7cbb4557e164a14ebdfdbc46
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-18 14:18:38 +0100

    Migrate Composer\Action\RemovePackage to use bolt/filesystem

--- src/Composer/Action/RemovePackage.php
+++ src/Composer/Action/RemovePackage.php
@@ -3,9 +3,7 @@
 namespace Bolt\Composer\Action;
 
 use Bolt\Exception\PackageManagerException;
-use Composer\Config\JsonConfigSource;
 use Composer\Installer;
-use Composer\Json\JsonFile;
 
 /**
  * Composer remove package class.
@@ -31,46 +29,42 @@ final class RemovePackage extends BaseAction
 
         $io = $this->getIO();
 
-        $jsonFile = new JsonFile($this->getOption('composerjson'));
-        $composerDefinition = $jsonFile->read();
-        $composerBackup = file_get_contents($jsonFile->getPath());
-
-        $json = new JsonConfigSource($jsonFile);
-
+        /** @var \Bolt\Filesystem\Handler\JsonFile $jsonFile */
+        $jsonFile = $this->app['filesystem']->get($this->getOption('composerjson'));
+        $composerJson = $composerBackup = $jsonFile->parse();
         $type = $this->getOption('dev') ? 'require-dev' : 'require';
 
         // Remove packages from JSON
-        foreach ($packages as $package) {
-            if (isset($composerDefinition[$type][$package])) {
-                $json->removeLink($type, $package);
-            }
+        foreach ($composerJson[$type] as $package) {
+            unset($composerJson[$type][$package]);
         }
 
         // Reload Composer config
         $composer = $this->resetComposer();
-
-        $install = Installer::create($io, $composer);
+        // Create the installer
+        $install = Installer::create($io, $composer)
+            ->setVerbose($this->getOption('verbose'))
+            ->setDevMode(!$this->getOption('updatenodev'))
+            ->setUpdate(true)
+            ->setUpdateWhitelist($packages)
+            ->setWhitelistDependencies($this->getOption('updatewithdependencies'))
+            ->setIgnorePlatformRequirements($this->getOption('ignoreplatformreqs'))
+        ;
 
         try {
-            $install
-                ->setVerbose($this->getOption('verbose'))
-                ->setDevMode(!$this->getOption('updatenodev'))
-                ->setUpdate(true)
-                ->setUpdateWhitelist($packages)
-                ->setWhitelistDependencies($this->getOption('updatewithdependencies'))
-                ->setIgnorePlatformRequirements($this->getOption('ignoreplatformreqs'));
-
             $status = $install->run();
-
-            if ($status !== 0) {
-                // Write out old JSON file
-                file_put_contents($jsonFile->getPath(), $composerBackup);
-            }
         } catch (\Exception $e) {
             $msg = __CLASS__ . '::' . __FUNCTION__ . ' recieved an error from Composer: ' . $e->getMessage() . ' in ' . $e->getFile() . '::' . $e->getLine();
             $this->app['logger.system']->critical($msg, ['event' => 'exception', 'exception' => $e]);
 
             throw new PackageManagerException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        if ($status !== 0) {
+            // Write out old JSON file
+            $jsonFile->dump($composerBackup);
+        } else {
+            $jsonFile->dump($composerJson);
         }
 
         return $status;
