1d7bfafa1c7a1f202ac8f98b06f05e3c76edc097
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-20 14:57:58 +0100

    Getter for an extension's composer.json

--- src/Extension/Manager.php
+++ src/Extension/Manager.php
@@ -2,6 +2,8 @@
 
 namespace Bolt\Extension;
 
+use Bolt\Filesystem\FilesystemInterface;
+use Bolt\Filesystem\Handler\JsonFile;
 use Silex\Application;
 
 /**
@@ -14,6 +16,8 @@ class Manager
     /** @var \Bolt\Composer\ExtensionLoader */
     protected $loader;
 
+    /** @var FilesystemInterface */
+    private $filesystem;
     /** @var Application */
     private $app;
 
@@ -26,5 +30,26 @@ class Manager
     {
         $this->app = $app;
         $this->loader = $app['extensions.loader'];
+        $this->filesystem = $app['filesystem']->getFilesystem('extensions');
+    }
+
+    /**
+     * Get an extension's composer.json data.
+     *
+     * @param $name
+     *
+     * @return array
+     */
+    public function getComposerJson($name)
+    {
+        if ($extension = $this->loader->get($name)) {
+            $jsonFile = file_get_contents($extension->getPath() . '/composer.json');
+            return json_decode($jsonFile, true);
+
+            /** @var JsonFile $jsonFile */
+            //$jsonFile = $this->filesystem->get($extension->getPath() . '/composer.json');
+            //
+            //return $jsonFile->parse();
+        }
     }
 }
