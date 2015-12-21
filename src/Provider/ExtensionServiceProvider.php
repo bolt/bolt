3885603d7bd7e5c4a4e81347b97e07eceeebbd85
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-21 14:08:52 +0100

    Composer action options class

--- src/Provider/ExtensionServiceProvider.php
+++ src/Provider/ExtensionServiceProvider.php
@@ -9,6 +9,7 @@ use Bolt\Composer\JsonManager;
 use Bolt\Composer\PackageManager;
 use Bolt\Composer\Satis;
 use Bolt\Extension\Manager;
+use Bolt\Filesystem\Handler\JsonFile;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
 
@@ -106,36 +107,7 @@ class ExtensionServiceProvider implements ServiceProviderInterface
 
         $app['extend.action.options'] = $app->share(
             function ($app) {
-                return [
-                    'basedir'                => $app['resources']->getPath('extensions'),
-                    'composerjson'           => $app['resources']->getPath('extensions/composer.json'),
-
-                    'dryrun'                 => null,  // dry-run              - Outputs the operations but will not execute anything (implicitly enables --verbose)
-                    'verbose'                => true,  // verbose              - Shows more details including new commits pulled in when updating packages
-                    'nodev'                  => null,  // no-dev               - Disables installation of require-dev packages
-                    'noautoloader'           => null,  // no-autoloader        - Skips autoloader generation
-                    'noscripts'              => null,  // no-scripts           - Skips the execution of all scripts defined in composer.json file
-                    'withdependencies'       => true,  // with-dependencies    - Add also all dependencies of whitelisted packages to the whitelist
-                    'ignoreplatformreqs'     => null,  // ignore-platform-reqs - Ignore platform requirements (php & ext- packages)
-                    'preferstable'           => null,  // prefer-stable        - Prefer stable versions of dependencies
-                    'preferlowest'           => null,  // prefer-lowest        - Prefer lowest versions of dependencies
-
-                    'sortpackages'           => true,  // sort-packages        - Sorts packages when adding/updating a new dependency
-                    'prefersource'           => false, // prefer-source        - Forces installation from package sources when possible, including VCS information
-                    'preferdist'             => true,  // prefer-dist          - Forces installation from package dist (archive) even for dev versions
-                    'update'                 => true,  // [Custom]             - Do package update as well
-                    'noupdate'               => null,  // no-update            - Disables the automatic update of the dependencies
-                    'updatenodev'            => true,  // update-no-dev        - Run the dependency update with the --no-dev option
-                    'updatewithdependencies' => true,  // update-with-dependencies - Allows inherited dependencies to be updated with explicit dependencies
-
-                    'dev'                    => null,  // dev - Add requirement to require-dev
-                                                       //       Removes a package from the require-dev section
-                                                       //       Disables autoload-dev rules
-
-                    'onlyname'               => true,  // only-name - Search only in name
-
-                    'optimizeautoloader'     => true,  // optimize-autoloader - Optimizes PSR0 and PSR4 packages to be loaded with classmaps too, good for production.
-                ];
+                return new Action\Options($app['filesystem']->get('extensions://composer.json', new JsonFile()));
             }
         );
     }
