6e262b4b35134201256041714b6cb061f2c559aa
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-22 12:14:09 +0100

    Pas the path after all

--- src/Composer/Action/BaseAction.php
+++ src/Composer/Action/BaseAction.php
@@ -68,7 +68,7 @@ abstract class BaseAction
 
             try {
                 // Use the factory to get a new Composer object
-                $this->composer = Factory::create($this->getIO(), $this->getOptions()->composerJson()->parse(), false);
+                $this->composer = Factory::create($this->getIO(), $this->getOptions()->composerJson()->getPath(), false);
             } catch (\Exception $e) {
                 $this->app['logger.system']->critical($e->getMessage(), ['event' => 'exception', 'exception' => $e]);
                 throw $e;
