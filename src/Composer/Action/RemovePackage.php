e66b8a205657addf8778c65f2e75e927e1ece8cf
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-23 10:21:41 +0100

    Unset require/require-dev if empty

--- src/Composer/Action/RemovePackage.php
+++ src/Composer/Action/RemovePackage.php
@@ -36,6 +36,9 @@ final class RemovePackage extends BaseAction
         foreach ($packages as $package) {
             unset($composerJson[$type][$package]);
         }
+        if (empty($composerJson[$type])) {
+            unset($composerJson[$type]);
+        }
         $jsonFile->dump($composerJson);
 
         $io = $this->getIO();
@@ -52,8 +55,8 @@ final class RemovePackage extends BaseAction
             ->setRunScripts(!$this->getOptions()->noScripts())
         ;
 
-        $status = $install->run();
         try {
+            $status = $install->run();
         } catch (\Exception $e) {
             $msg = sprintf('%s recieved an error from Composer: %s in %s::%s', __METHOD__, $e->getMessage(), $e->getFile(), $e->getLine());
             $this->app['logger.system']->critical($msg, ['event' => 'exception', 'exception' => $e]);
