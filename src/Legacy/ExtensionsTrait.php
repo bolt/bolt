537784ce627f501fc0b7a6bac1f1a9d24b3b4c8d
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-24 14:22:36 +0100

    Add in placeholder functions

--- src/Legacy/ExtensionsTrait.php
+++ src/Legacy/ExtensionsTrait.php
@@ -5,15 +5,18 @@ namespace Bolt\Legacy;
 use Bolt;
 use Bolt\Asset\Snippet\Snippet;
 use Bolt\Asset\Target;
+use Silex\Application;
 
 /**
- * Deprecated function dump for Bolt\Extensions class.
+ * Deprecated Bolt\Extensions class.
  *
  * Farewell little functions, and rest in peace…
  *
+ * @deprecated Deprecated since 3.0, to be removed in 4.0.
+ *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-trait DeprecatedFunctionsTrait
+trait ExtensionsTrait
 {
     /** @return \Silex\Application */
     abstract protected function getApp();
@@ -22,7 +25,8 @@ trait DeprecatedFunctionsTrait
      * Legacy function that returns a list of all css and js assets that are
      * added via extensions.
      *
-     * @deprecated Deprecated since 3.0, to be removed in 4.0. Use $app['asset.queue.file']->getQueue() and/or $app['asset.queue.snippet']->getQueue()
+     * @deprecated Deprecated since 3.0, to be removed in 4.0. Use $app['asset.queue.file']->getQueue() and/or
+     *             $app['asset.queue.snippet']->getQueue()
      *
      * @return array
      */
@@ -31,12 +35,14 @@ trait DeprecatedFunctionsTrait
         $files = $this->getApp()['asset.queue.file']->getQueue();
         $assets = [
             'css' => [],
-            'js'  => []
+            'js'  => [],
         ];
 
+        /** @var \Bolt\Asset\File\JavaScript $file */
         foreach ($files['javascript'] as $file) {
             $assets['js'][] = $file->getFileName();
         }
+        /** @var \Bolt\Asset\File\Stylesheet $file */
         foreach ($files['stylesheet'] as $file) {
             $assets['css'][] = $file->getFileName();
         }
@@ -179,8 +185,145 @@ trait DeprecatedFunctionsTrait
     /**
      * @deprecated Deprecated since 3.0, to be removed in 4.0.
      */
-    public function insertAfterJs($tag, $html, $insidehead = true)
+    public function insertAfterJs($tag, $html)
     {
-        return $this->getApp()['asset.injector']->inject($tag, Target::AFTER_JS, $html, $insidehead);
+        return $this->getApp()['asset.injector']->inject($tag, Target::AFTER_JS, $html);
+    }
+
+    /**
+     * Placeholder functions
+     */
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function addCss()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function addJavascript()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function addJquery()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function addMenuOption()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function addTwigExtension()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function autoload()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function checkLocalAutoloader()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function clearAssets()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function disableJquery()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function getComposerConfig()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function getEnabled()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function getMenuOptions()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function getSnippets()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function hasMailSenders()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function hasMenuOptions()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function initialize()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function isEnabled($name)
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function localload()
+    {
+    }
+
+    /**
+     * @deprecated Deprecated since 3.0, to be removed in 4.0.
+     */
+    public function register()
+    {
     }
 }
