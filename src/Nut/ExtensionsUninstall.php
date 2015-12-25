87f17082e8505b8a18c8919f067d9ba98095dc75
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-25 09:11:44 +0100

    Update ExtensionsUninstall

--- src/Nut/ExtensionsUninstall.php
+++ src/Nut/ExtensionsUninstall.php
@@ -10,33 +10,40 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Nut command to uninstall an extension
  *
  * @author Ross Riley <riley.ross@gmail.com>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
 class ExtensionsUninstall extends BaseCommand
 {
     /**
-     * @see \Symfony\Component\Console\Command\Command::configure()
+     * {@inheritdoc}
      */
     protected function configure()
     {
         $this
-            ->setName('extensions:disable')
-            ->setAliases(['extensions:uninstall'])
+            ->setName('extensions:uninstall')
+            ->setAliases(['extensions:disable'])
             ->setDescription('Uninstalls an extension.')
             ->addArgument('name', InputArgument::REQUIRED, 'Name of the extension to uninstall')
         ;
     }
 
     /**
-     * @see \Symfony\Component\Console\Command\Command::execute()
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $name = $input->getArgument('name');
-        $result = $this->app['extend.manager']->removePackage([$name]);
 
+        $output->write("\n<info>Starting uninstall of {$name}… </info>");
+
+        $result = $this->app['extend.manager']->removePackage([$name]);
         if ($result === 0) {
+            $output->write("<info>[DONE]</info>\n");
             $this->auditLog(__CLASS__, "Removed extension $name");
+        } else {
+            $output->write("<error>[FAILED]</error>\n");
         }
-        $output->writeln('<info>' . $result . '</info>', OutputInterface::OUTPUT_PLAIN);
+
+        $output->writeln(sprintf('<comment>%s</comment>', $this->app['extend.action.io']->getOutput()), OutputInterface::OUTPUT_PLAIN);
     }
 }
