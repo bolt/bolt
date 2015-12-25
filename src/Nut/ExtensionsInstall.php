cfe1d9fa775eebf6ad1b9decb3f09498a0d978c6
Gawain Lynch <gawain.lynch@gmail.com>
2015-12-25 09:11:30 +0100

    Update ExtensionsInstall

--- src/Nut/ExtensionsInstall.php
+++ src/Nut/ExtensionsInstall.php
@@ -10,17 +10,18 @@ use Symfony\Component\Console\Output\OutputInterface;
  * Nut command to install an extension
  *
  * @author Ross Riley <riley.ross@gmail.com>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
 class ExtensionsInstall extends BaseCommand
 {
     /**
-     * @see \Symfony\Component\Console\Command\Command::configure()
+     * {@inheritdoc}
      */
     protected function configure()
     {
         $this
-            ->setName('extensions:enable')
-            ->setAliases(['extensions:install'])
+            ->setName('extensions:install')
+            ->setAliases(['extensions:enable'])
             ->setDescription('Installs an extension by name and version.')
             ->addArgument('name', InputArgument::REQUIRED, 'Name of the extension to enable')
             ->addArgument('version', InputArgument::REQUIRED, 'Version of the extension to enable')
@@ -28,20 +29,23 @@ class ExtensionsInstall extends BaseCommand
     }
 
     /**
-     * @see \Symfony\Component\Console\Command\Command::execute()
+     * {@inheritdoc}
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $name = $input->getArgument('name');
         $version = $input->getArgument('version');
 
-        $result = $this->app['extend.manager']->requirePackage(['name' => $name, 'version' => $version]);
+        $output->write("\n<info>Starting install of {$name}:{$version}… </info>");
 
+        $result = $this->app['extend.manager']->requirePackage(['name' => $name, 'version' => $version]);
         if ($result === 0) {
+            $output->write("<info>[DONE]</info>\n");
             $this->auditLog(__CLASS__, "Installed extension $name");
+        } else {
+            $output->write("<error>[FAILED]</error>\n");
         }
 
-        $output->writeln('<info>[Done]</info> ');
-        $output->writeln($result, OutputInterface::OUTPUT_PLAIN);
+        $output->writeln(sprintf('<comment>%s</comment>', $this->app['extend.action.io']->getOutput()), OutputInterface::OUTPUT_PLAIN);
     }
 }
