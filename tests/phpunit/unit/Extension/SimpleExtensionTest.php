da3f9b59aee8c53e606eec2b43b06b28d0f11287
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-03 13:55:56 +0100

    [Tests] SimpleExtension coverage

--- tests/phpunit/unit/Extension/SimpleExtensionTest.php
+++ tests/phpunit/unit/Extension/SimpleExtensionTest.php
@@ -2,7 +2,9 @@
 
 namespace Bolt\Tests\Extension;
 
+use Bolt\Events\ControllerEvents;
 use Bolt\Tests\BoltUnitTest;
+use Bolt\Tests\Extension\Mock\NormalExtension;
 
 /**
  * Class to test Bolt\Extension\SimpleExtension
@@ -11,8 +13,50 @@ use Bolt\Tests\BoltUnitTest;
  */
 class SimpleExtensionTest extends BoltUnitTest
 {
-    public function test()
+    public function testRegister()
     {
+        $app = $this->getApp();
+        $mock = $this->getMock('Bolt\Tests\Extension\Mock\NormalExtension', ['getContainer']);
+        $mock->expects($this->atLeast(4))->method('getContainer')->willReturn($app);
 
+        $mock->setContainer($app);
+        $mock->register($app);
+    }
+
+    public function testSubscribe()
+    {
+        $app = $this->getApp();
+        $ext = new NormalExtension();
+        $ext->setContainer($app);
+        $ext->boot($app);
+
+        $listeners = $app['dispatcher']->getListeners('dropbear.sighting');
+        $this->assertInstanceOf('Bolt\Tests\Extension\Mock\NormalExtension', $listeners[0][0]);
+
+        $this->setExpectedException('RuntimeException', 'Drop Bear Alert!');
+        $app['dispatcher']->dispatch('dropbear.sighting');
+    }
+
+    public function testGetServiceProvider()
+    {
+        $ext = new NormalExtension();
+
+        $this->assertInstanceOf('Bolt\Extension\AbstractExtension', $ext->getServiceProvider());
+        $this->assertInstanceOf('Silex\ServiceProviderInterface', $ext->getServiceProvider());
+        $this->assertInstanceOf('Symfony\Component\EventDispatcher\EventSubscriberInterface', $ext->getServiceProvider());
+    }
+
+    public function testGetSubscribedEvents()
+    {
+        $ext = new NormalExtension();
+        $events = $ext->getSubscribedEvents();
+        $expected = [
+            ControllerEvents::MOUNT => [
+                ['onMountRoutes', 0],
+                ['onMountControllers', 0],
+            ],
+        ];
+
+        $this->assertSame($expected, $events);
     }
 }
