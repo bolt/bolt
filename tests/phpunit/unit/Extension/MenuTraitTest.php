6900df934b9a68e71d8cbc671ddd76861545fe37
Gawain Lynch <gawain.lynch@gmail.com>
2016-01-06 05:02:35 +0100

    [Tests] MenuTrait coverage

--- tests/phpunit/unit/Extension/MenuTraitTest.php
+++ tests/phpunit/unit/Extension/MenuTraitTest.php
@@ -2,7 +2,10 @@
 
 namespace Bolt\Tests\Extension;
 
+use Bolt\Menu\MenuEntry;
 use Bolt\Tests\BoltUnitTest;
+use Bolt\Tests\Extension\Mock\MenuExtension;
+use Bolt\Tests\Extension\Mock\NormalExtension;
 
 /**
  * Class to test Bolt\Extension\MenuTrait
@@ -11,8 +14,42 @@ use Bolt\Tests\BoltUnitTest;
  */
 class MenuTraitTest extends BoltUnitTest
 {
-    public function test()
+    public function testEmptyMenus()
     {
+        $app = $this->getApp();
 
+        $ext = new NormalExtension();
+        $ext->setContainer($app);
+        $ext->register($app);
+
+        /** @var MenuEntry $extendMenu */
+        $extendMenu = $app['menu.admin']->get('extend');
+        $this->assertSame('extend', $extendMenu->getName());
+        $this->assertSame('Extensions', $extendMenu->getLabel());
+        $this->assertSame([], $extendMenu->children());
+    }
+
+    public function testMenuAdds()
+    {
+        $app = $this->getApp();
+
+        $ext = new MenuExtension();
+        $ext->setContainer($app);
+        $ext->register($app);
+        /** @var MenuEntry $extendMenu */
+        $extendMenu = $app['menu.admin']->get('extend');
+        $children = $extendMenu->children();
+
+        $this->assertSame('koala', $children['koala']->getName());
+        $this->assertSame('Koalas', $children['koala']->getLabel());
+        $this->assertSame('/bolt/extend/koalas-are-us', $children['koala']->getUri());
+        $this->assertSame('fa-thumbs-o-up', $children['koala']->getIcon());
+        $this->assertSame('config', $children['koala']->getPermission());
+
+        $this->assertSame('Drop Bear', $children['Drop Bear']->getName());
+        $this->assertSame('Drop Bear', $children['Drop Bear']->getLabel());
+        $this->assertSame('/bolt/extend/look-up-live', $children['Drop Bear']->getUri());
+        $this->assertSame('fa-thumbs-o-down', $children['Drop Bear']->getIcon());
+        $this->assertSame('dangerous', $children['Drop Bear']->getPermission());
     }
 }
