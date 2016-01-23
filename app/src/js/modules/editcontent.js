da96969ff45c0031f3786ff7d8ddf9de8fcf2ff9
Svante Richter <svante.richter@gmail.com>
2016-01-23 09:48:20 +0100

    Update event names to match #4721

--- app/src/js/modules/editcontent.js
+++ app/src/js/modules/editcontent.js
@@ -231,10 +231,10 @@
      * @function initSaveContinue
      * @memberof Bolt.editcontent
      * 
-     * @fires start.bolt.editcontent.save
-     * @fires done.bolt.editcontent.save
-     * @fires fail.bolt.editcontent.save
-     * @fires always.bolt.editcontent.save
+     * @fires start.bolt.content.save
+     * @fires done.bolt.content.save
+     * @fires fail.bolt.content.save
+     * @fires always.bolt.content.save
      *
      * @param {BindData} data - Editcontent configuration data
      */
@@ -267,14 +267,14 @@
                 watchChanges();
 
                 // Trigger save started event
-                $(Bolt).trigger('start.bolt.editcontent.save');
+                $(Bolt).trigger('start.bolt.content.save');
 
                 // Existing record. Do an 'ajaxy' post to update the record.
                 // Let the controller know we're calling AJAX and expecting to be returned JSON.
                 $.post('?returnto=ajax', $('#editcontent').serialize())
                     .done(function (data) {
                         // Trigger save done event
-                        $(Bolt).trigger('done.bolt.editcontent.save', data);
+                        $(Bolt).trigger('done.bolt.content.save', data);
 
                         $('p.lastsaved').html(savedon);
                         $('p.lastsaved').find('strong').text(moment(data.datechanged).format('MMM D, HH:mm'));
@@ -321,13 +321,13 @@
                     })
                     .fail(function(){
                         // Trigger save failed event 
-                        $(Bolt).trigger('fail.bolt.editcontent.save');
+                        $(Bolt).trigger('fail.bolt.content.save');
 
                         $('p.lastsaved').text(msgNotSaved);
                     })
                     .always(function(){
                         // Trigger save always event
-                        $(Bolt).trigger('always.bolt.editcontent.save');
+                        $(Bolt).trigger('always.bolt.content.save');
 
                         // Re-enable buttons
                         window.setTimeout(function(){
