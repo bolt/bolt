979e800958b25ffcea6eb3ecce6af80b3910ac4b
Ross Riley <riley.ross@gmail.com>
2016-01-30 08:48:10 +0000

    add an invert switch for bi-directional relations

--- src/Storage/Entity/Relations.php
+++ src/Storage/Entity/Relations.php
@@ -11,4 +11,20 @@ class Relations extends Entity
     protected $from_id;
     protected $to_contenttype;
     protected $to_id;
+
+    private $invert = false;
+
+    public function actAsInverse()
+    {
+        $this->invert = true;
+    }
+
+    public function getToId()
+    {
+        if ($this->invert === true) {
+            return $this->from_id;
+        }
+
+        return $this->to_id;
+    }
 }
