7be8de761f2cd1a69c8d9024e969356104e3b697
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-14 14:58:26 +0100

    Explicit getters/setters for \Bolt\Storage\Entity\Cron

--- src/Storage/Entity/Cron.php
+++ src/Storage/Entity/Cron.php
@@ -3,17 +3,61 @@ namespace Bolt\Storage\Entity;
 
 /**
  * Entity for cron jobs.
- *
- * @method integer   getId()
- * @method string    getInterim()
- * @method \DateTime getLastrun()
- * @method setId($id)
- * @method setInterim($interim)
- * @method setLastrun($lastrun)
  */
 class Cron extends Entity
 {
+    /** @var integer */
     protected $id;
+    /** @var string */
     protected $interim;
+    /** @var \DateTime */
     protected $lastrun;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getInterim()
+    {
+        return $this->interim;
+    }
+
+    /**
+     * @param string $interim
+     */
+    public function setInterim($interim)
+    {
+        $this->interim = $interim;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getLastrun()
+    {
+        return $this->lastrun;
+    }
+
+    /**
+     * @param \DateTime $lastrun
+     */
+    public function setLastrun($lastrun)
+    {
+        $this->lastrun = $lastrun;
+    }
 }
