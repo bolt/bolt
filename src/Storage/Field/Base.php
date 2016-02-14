0cffc03266a1046127f850d6ff2f797ba98d2b9f
Gawain Lynch <gawain.lynch@gmail.com>
2016-02-14 16:20:21 +0100

    Update PHPDoc for \Bolt\Storage\Field\Base

--- src/Storage/Field/Base.php
+++ src/Storage/Field/Base.php
@@ -1,33 +1,53 @@
 <?php
-namespace Bolt\Storage\Field;
 
+namespace Bolt\Storage\Field;
 
 class Base implements FieldInterface
 {
+    /** @var string */
     public $name;
+    /** @var string */
     public $template;
 
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     * @param string $template
+     */
     public function __construct($name, $template)
     {
         $this->name = $name;
         $this->template = $template;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getTemplate()
     {
         return $this->template;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getStorageType()
     {
         return 'text';
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getStorageOptions()
     {
         return [];
