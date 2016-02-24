29ee4beb008cc92e36ea6b964776ca1a3cb5c394
Bob den Otter <bob@twokings.nl>
2016-02-24 20:53:20 +0100

    Clarify who is shown what in docblocks.

--- src/Logger/FlashLoggerInterface.php
+++ src/Logger/FlashLoggerInterface.php
@@ -18,42 +18,42 @@ interface FlashLoggerInterface
     const CONFIGURATION = 'configuration';
 
     /**
-     * Display a 'danger' message.
+     * Display a 'danger' message. Shown only to authenticated users.
      *
      * @param string $message
      */
     public function danger($message);
 
     /**
-     * Display a 'error' message.
+     * Display a 'error' message. Shown to all users, even those who aren't authenticated.
      *
      * @param string $message
      */
     public function error($message);
 
     /**
-     * Display a 'info' message.
+     * Display a 'info' message. Shown to all users, even those who aren't authenticated.
      *
      * @param string $message
      */
     public function info($message);
 
     /**
-     * Display a 'success' message.
+     * Display a 'success' message. Shown to all users, even those who aren't authenticated.
      *
      * @param string $message
      */
     public function success($message);
 
     /**
-     * Display a 'warning' message.
+     * Display a 'warning' message. Shown only to authenticated users.
      *
      * @param string $message
      */
     public function warning($message);
 
     /**
-     * Display a 'configuration needed' message.
+     * Display a 'configuration needed' message. Shown only to authenticated users.
      *
      * @param string $message
      */
