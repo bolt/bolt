b0baf15733eae68395e260b57153d8d2368ce9de
Carson Full <carsonfull@gmail.com>
2016-02-24 23:31:01 -0600

    Revert form csrf_provider to use $app['session']

--- src/Provider/FormServiceProvider.php
+++ src/Provider/FormServiceProvider.php
@@ -5,8 +5,6 @@ namespace Bolt\Provider;
 use Silex\Application;
 use Silex\Provider\FormServiceProvider as SilexFormServiceProvider;
 use Silex\ServiceProviderInterface;
-use Symfony\Component\Security\Csrf\CsrfTokenManager;
-use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
 
 /**
  * Register form services
@@ -20,14 +18,6 @@ class FormServiceProvider implements ServiceProviderInterface
         if (!isset($app['form.factory'])) {
             $app->register(new SilexFormServiceProvider());
         }
-
-        $app['form.csrf_provider'] = $app->share(
-            function ($app) {
-                $storage = new SessionTokenStorage($app['sessions']['csrf']);
-
-                return new CsrfTokenManager(null, $storage);
-            }
-        );
     }
 
     public function boot(Application $app)
