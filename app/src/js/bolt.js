5893d7b94e3d72749f49ffbd73f83a6b5ae93906
rarila <magnet@gmx.net>
2016-02-27 16:14:13 +0100

    Add bolt events module stub

--- app/src/js/bolt.js
+++ app/src/js/bolt.js
@@ -9,6 +9,7 @@
  * @mixes Bolt.conf
  * @mixes Bolt.data
  * @mixes Bolt.datetime
+ * @mixes Bolt.events
  * @mixes Bolt.extend
  * @mixes Bolt.files
  * @mixes Bolt.omnisearch
---
+++ app/src/js/modules/events.js
@@ -0,0 +1,20 @@
+/**
+ * Events.
+ *
+ * @mixin
+ * @namespace Bolt.events
+ *
+ * @param {Object} bolt - The Bolt module.
+ */
+(function (bolt) {
+    'use strict';
+
+    /*
+     * Bolt.events mixin container.
+     */
+    var events = {};
+
+    // Apply mixin container
+    bolt.events = events;
+
+})(Bolt || {});
