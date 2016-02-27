acd9bf4562d9542ea30f0c0edbcda11ae6ea4655
rarila <magnet@gmx.net>
2016-02-27 16:16:19 +0100

    Remove old unused Bolt.fields stuff

--- app/src/js/bolt.js
+++ app/src/js/bolt.js
@@ -16,23 +16,5 @@
  * @mixes Bolt.secmenu
  * @mixes Bolt.stack
  * @mixes Bolt.utils
- *
- * @mixes Bolt.fields
- * @mixes Bolt.fields.categories
- * @mixes Bolt.fields.file
- * @mixes Bolt.fields.filelist
- * @mixes Bolt.fields.image
- * @mixes Bolt.fields.imagelist
- * @mixes Bolt.fields.geolocation
- * @mixes Bolt.fields.html
- * @mixes Bolt.fields.markdown
- * @mixes Bolt.fields.meta
- * @mixes Bolt.fields.relationship
- * @mixes Bolt.fields.repeater
- * @mixes Bolt.fields.select
- * @mixes Bolt.fields.slug
- * @mixes Bolt.fields.tags
- * @mixes Bolt.fields.templateselect
- * @mixes Bolt.fields.video
  */
 var Bolt = {};
