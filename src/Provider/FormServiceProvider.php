a221a10120f5873b6ee433a7eaaaf574ed26a7e4
Carson Full <carsonfull@gmail.com>
2016-02-27 18:47:39 -0600

    Moved csrf classes into their own DI keys

--- src/Provider/FormServiceProvider.php
+++ src/Provider/FormServiceProvider.php
@@ -5,6 +5,9 @@ namespace Bolt\Provider;
 use Silex\Application;
 use Silex\Provider\FormServiceProvider as SilexFormServiceProvider;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\Security\Csrf\CsrfTokenManager;
+use Symfony\Component\Security\Csrf\TokenGenerator\UriSafeTokenGenerator;
+use Symfony\Component\Security\Csrf\TokenStorage\SessionTokenStorage;
 
 /**
  * Register form services
@@ -18,6 +21,32 @@ class FormServiceProvider implements ServiceProviderInterface
         if (!isset($app['form.factory'])) {
             $app->register(new SilexFormServiceProvider());
         }
+
+        $app['form.csrf_provider'] = $app->share(
+            function ($app) {
+                return $app['csrf'];
+            }
+        );
+
+
+        $app['csrf'] = $app->share(
+            function ($app) {
+                return new CsrfTokenManager($app['csrf.generator'], $app['csrf.storage']);
+            }
+        );
+
+        $app['csrf.generator'] = $app->share(
+            function ($app) {
+                return new UriSafeTokenGenerator($app['csrf.generator.entropy']);
+            }
+        );
+        $app['csrf.generator.entropy'] = 256;
+
+        $app['csrf.storage'] = $app->share(
+            function ($app) {
+                return new SessionTokenStorage($app['session']);
+            }
+        );
     }
 
     public function boot(Application $app)
