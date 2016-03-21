cf6f1bb928d9ab97d555a50fc9caa5acd6666d00
Gawain Lynch <gawain.lynch@gmail.com>
2016-03-21 09:24:23 +0100

    Add a repository mapping

--- src/Extension/StorageTrait.php
+++ src/Extension/StorageTrait.php
@@ -11,6 +11,37 @@ use Pimple as Container;
  */
 trait StorageTrait
 {
+    /**
+     * Return a list of entities to map to repositories.
+     *
+     * <pre>
+     *  return [
+     *      'alias' => [\Entity\Class\Name => \Repository\Class\Name],
+     *  ];
+     * </pre>
+     *
+     * @return array
+     */
+    protected function registerRepositoryMappings()
+    {
+        return [];
+    }
+
+    /**
+     * Call this in register method.
+     *
+     * @internal
+     */
+    final protected function extendRepositoryMapping()
+    {
+        $app = $this->getContainer();
+        foreach ($this->registerRepositoryMappings() as $alias => $map) {
+            $app['storage.repositories'] += $map;
+            $app['storage.metadata']->setDefaultAlias($app['schema.prefix'] . $alias, key($map));
+            $app['storage']->setRepository(key($map), current($map));
+        }
+    }
+
     /** @return Container */
     abstract protected function getContainer();
 }
