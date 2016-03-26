d94302139681bc86fcbbe5e23421cee4f6846c3f
Carson Full <carsonfull@gmail.com>
2016-03-25 21:02:25 -0500

    Move logic for bootstrapping web requests to web.php. Individual installs don't have to duplicate logic for php version check or checking for files with built-in web server.

--- index.php
+++ index.php
@@ -1,47 +1,14 @@
 <?php
-/**
- * Bolt entry script.
- *
- * Here we'll require in the first stage load script, which handles all the
- * tasks needed to return the app.  Once we get the app, we simply tell it
- * to run, building a beautiful web page for you and other visitors.
- *
- * We get things started by ensuring the PHP version we're running on
- * is supported by the app.  We'll display a friendly error page if not.
- *
- * Next, we'll check if the script is being run with PHP's built in web
- * server, and make sure static assets are handled gracefully.
- */
 
-/**
- * Version must be greater than 5.5.9.
- *
- * Note, we use `dirname(__FILE__)` instead of `__DIR__`. The latter was
- * introduced "only" in PHP 5.3, and we need to be able to show the notice to
- * the poor souls who are still on PHP 5.2.
- *
- * @see: https://github.com/bolt/bolt/issues/1531
- * @see: https://github.com/bolt/bolt/issues/3371
+/*
+ * `dirname(__FILE__)` is intentional to support PHP 5.2 until legacy.php can be shown.
  */
-if (version_compare(PHP_VERSION, '5.5.9', '<')) {
-    require dirname(__FILE__) . '/app/legacy.php';
+/** @var Silex\Application|false $app */
+$app = require dirname(__FILE__) . '/app/web.php';
 
+// If web.php returns false, meaning the path is a file, pass it along.
+if ($app === false) {
     return false;
 }
 
-/**
- * Return false if the requested file is available on the filesystem.
- *
- * @see: http://silex.sensiolabs.org/doc/web_servers.html#php-5-4
- */
-if (php_sapi_name() === 'cli-server') {
-    $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-
-    if (is_file($filename)) {
-        return false;
-    }
-}
-
-/** @var \Silex\Application $app */
-$app = require __DIR__ . '/app/bootstrap.php';
 $app->run();
