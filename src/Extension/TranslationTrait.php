44e7e44e1d2b4fe4bf5e40dc8a014787de0f7419
Aaron Valandra <avaland@woopta.com>
2016-05-09 11:12:53 -0500

    Added the ability to automatically register translations for extensions if they are contained in the translation directory. (cherry picked from commit a9d9017)

---
+++ src/Extension/TranslationTrait.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Bolt\Extension;
+
+use Bolt\Filesystem\Handler\DirectoryInterface;
+use Pimple as Container;
+
+/**
+ * Twig function/filter addition and interface functions for an extension.
+ *
+ * @author Carson Full <carsonfull@gmail.com>
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+trait TranslationTrait
+{
+
+    /**
+     * Call this in register method.
+     *
+     * @internal
+     */
+    final protected function addTranslations()
+    {
+        $app = $this->getContainer();
+
+        $translationDirectory = $this->getBaseDirectory()->getDir('translations');
+        if ($translationDirectory->exists())
+        {
+            foreach ($translationDirectory->getContents(true) as $fileInfo) {
+                $filename = explode('.', $fileInfo->getFilename());
+                if ($fileInfo->isFile())
+                {
+                    if (isset($filename[0])) {
+                        $translation = $filename[0];
+                    } else {
+                        $translation = '';
+                    }
+                    $app['translator']->addResource('yml', $app['paths']['extensionspath'] . DIRECTORY_SEPARATOR . $fileInfo->getPath(), $translation);
+                }
+            }
+        }
+
+    }
+
+    /** @return Container */
+    abstract protected function getContainer();
+
+    /** @return string */
+    abstract public function getName();
+
+    /** @return DirectoryInterface */
+    abstract protected function getBaseDirectory();
+}
