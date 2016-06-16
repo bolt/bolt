b32025627edba8d1d4a5a5b964cb4818c92c873f
Niklas Goebel <git@niklas-goebel.de>
2016-06-16 14:21:43 +0200

    Improve authorized error to be user friendly

--- src/Controller/Backend/FileManager.php
+++ src/Controller/Backend/FileManager.php
@@ -150,8 +150,12 @@ class FileManager extends BackendBase
         $filesystem = $this->filesystem()->getFilesystem($namespace);
 
         if (!$filesystem->authorized($path)) {
-            $error = Trans::__('general.phrase.access-denied-permissions-view-file-directory', ['%s' => $path]);
-            $this->abort(Response::HTTP_FORBIDDEN, $error);
+            if (empty($path)) {
+                $path = $namespace;
+            }
+            
+            $this->flashes()->error(Trans::__('general.phrase.access-denied-permissions-view-file-directory', ['%s' => $path]));
+            return new RedirectResponse($this->generateUrl('dashboard'));
         }
 
         if (!$this->isAllowed('files:uploads')) {
