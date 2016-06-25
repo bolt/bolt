a735b4da3ccc1045a52d9df13673cff890875a0d
Gawain Lynch <gawain.lynch@gmail.com>
2016-06-25 05:01:24 +0200

    Add --enable option to Nut user:create

--- src/Nut/UserAdd.php
+++ src/Nut/UserAdd.php
@@ -5,6 +5,7 @@ namespace Bolt\Nut;
 use Bolt\Storage\Entity;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -20,11 +21,12 @@ class UserAdd extends BaseCommand
         $this
             ->setName('user:add')
             ->setDescription('Add a new user.')
-            ->addArgument('username', InputArgument::REQUIRED, 'The username (loginname) you wish to add a role to.')
+            ->addArgument('username', InputArgument::REQUIRED, 'The user name (login name) for the new user.')
             ->addArgument('displayname', InputArgument::REQUIRED, 'The display name for the new user.')
             ->addArgument('email', InputArgument::REQUIRED, 'The email address for the new user.')
             ->addArgument('password', InputArgument::REQUIRED, 'The password for the new user.')
             ->addArgument('role', InputArgument::REQUIRED, 'The role you wish to give them.')
+            ->addOption('enable', 'e', InputOption::VALUE_NONE, 'Enable the new user.')
         ;
     }
 
@@ -41,6 +43,7 @@ class UserAdd extends BaseCommand
             'email'       => $input->getArgument('email'),
             'displayname' => $input->getArgument('displayname'),
             'roles'       => (array) $input->getArgument('role'),
+            'enabled'     => (bool) $input->getOption('enable'),
         ]);
 
         $message = [];
