94a346efd6647f244860b211b5d4997ddee11d13
Gawain Lynch <gawain.lynch@gmail.com>
2016-07-05 19:19:31 +0200

    PHPDoc and type hints

--- src/Composer/Action/CheckPackage.php
+++ src/Composer/Action/CheckPackage.php
@@ -31,34 +31,44 @@ final class CheckPackage extends BaseAction
 
         // Get the packages that a set as "required" in the JSON file
         $json = $jsonFile->parse();
-        $jsonpack = $json['require'];
+        $jsonRequires = $json['require'];
 
         // Find the packages that are NOT part of the root install yet and mark
         // them as pending installs
-        if (!empty($jsonpack)) {
-            foreach ($jsonpack as $packageName => $targetPackageVersion) {
-                if (!array_key_exists($packageName, $rootPackage)) {
-                    try {
-                        $remote = $this->findBestVersionForPackage($packageName, $targetPackageVersion, true);
-                    } catch (\Exception $e) {
-                        $msg = sprintf('%s recieved an error from Composer: %s in %s::%s', __METHOD__, $e->getMessage(), $e->getFile(), $e->getLine());
-                        $this->app['logger.system']->critical($msg, ['event' => 'exception', 'exception' => $e]);
+        if (!empty($jsonRequires)) {
+            /**
+             * @var string $packageName
+             * @var string $packageVersion
+             */
+            foreach ($jsonRequires as $packageName => $packageVersion) {
+                if (array_key_exists($packageName, $rootPackage)) {
+                    continue;
+                }
+                try {
+                    $remote = $this->findBestVersionForPackage($packageName, $packageVersion, true);
+                } catch (\Exception $e) {
+                    $msg = sprintf('%s recieved an error from Composer: %s in %s::%s', __METHOD__, $e->getMessage(), $e->getFile(), $e->getLine());
+                    $this->app['logger.system']->critical($msg, ['event' => 'exception', 'exception' => $e]);
 
-                        throw new PackageManagerException($e->getMessage(), $e->getCode(), $e);
-                    }
+                    throw new PackageManagerException($e->getMessage(), $e->getCode(), $e);
+                }
 
-                    // If a 'best' version is found, and there is a version mismatch then
-                    // propose as an update. Making the assumption that Composer isn't
-                    // going to offer us an older version.
-                    if (is_array($remote)) {
-                        $packages['installs'][] = $remote;
-                    }
+                // If a 'best' version is found, and there is a version mismatch then
+                // propose as an update. Making the assumption that Composer isn't
+                // going to offer us an older version.
+                if (is_array($remote)) {
+                    $packages['installs'][] = $remote;
                 }
             }
         }
 
-        // For installed packages, see if there is a valid update
-        foreach ($rootPackage as $packageName => $targetPackageVersion) {
+        /**
+         * For installed packages, see if there is a valid update.
+         *
+         * @var string $packageName
+         * @var array  $targetPackageVersion
+         */
+        foreach ($rootPackage as $packageName => $packageVersion) {
             try {
                 $remote = $this->findBestVersionForPackage($packageName, $targetPackageVersion, true);
             } catch (\Exception $e) {
