91171e944d2b404dbb7bc0df0b5036cb86489091
Svante Richter <svante.richter@gmail.com>
2016-07-31 12:01:17 +0200

    Fix backend status page quirks (#5085)
    This does two things:
    1. check if the request is safe (just status change) or unsafe (delete)
    and only ask the user to confirm if it is unsafe.
    2. Allow unpublished records in the async zone, since all async routes
    are on the backend and should therefore show unpublished too.

--- app/src/js/widgets/buic/buicListing.js
+++ app/src/js/widgets/buic/buicListing.js
@@ -37,21 +37,25 @@
          */
         modifyRecords: function (action, ids, buttonText) {
             var self = this,
-                modifications = {},
+                safeQuery = true,
                 actions = {
                     'delete': {
+                        'safe': false,
                         'name': bolt.data('recordlisting.action.delete', {'%CTNAME%': this.contentTypeName}),
                         'cmd': {'delete': null}
                     },
                     'publish': {
+                        'safe': true,
                         'name': bolt.data('recordlisting.action.publish', {'%CTNAME%': this.contentTypeName}),
                         'cmd': {'modify': {'status': 'published'}}
                     },
                     'depublish': {
+                        'safe': true,
                         'name': bolt.data('recordlisting.action.depublish'),
                         'cmd': {'modify': {'status': 'held'}}
                     },
                     'draft': {
+                        'safe': true,
                         'name': bolt.data('recordlisting.action.draft'),
                         'cmd': {'modify': {'status': 'draft'}}
                     }
@@ -59,9 +63,13 @@
                 msg;
 
             // Build POST data.
-            modifications[self.contentType] = {};
+            self.modifications = {};
+            self.modifications[self.contentType] = {};
             $(ids).each(function () {
-                modifications[self.contentType][this] = actions[action].cmd;
+                if(!actions[action].safe){
+                    safeQuery = false;
+                }
+                self.modifications[self.contentType][this] = actions[action].cmd;
             });
 
             // Build message:
@@ -72,57 +80,65 @@
             }
             msg = msg + '<br><br><b>' + bolt.data('recordlisting.confirm.no-undo') + '</b>';
 
-            bootbox.dialog({
-                message: msg,
-                title: actions[action].name,
-                buttons: {
-                    cancel: {
-                        label: bolt.data('recordlisting.action.cancel'),
-                        className: 'btn-default'
-                    },
-                    ok: {
-                        label: buttonText,
-                        className: 'btn-primary',
-                        callback: function () {
-                            var url = bolt.conf('paths.async') + 'content/action' + window.location.search;
-
-                            $.ajax({
-                                url: url,
-                                type: 'POST',
-                                data: {
-                                    'bolt_csrf_token': self.csrfToken,
-                                    'contenttype': self.contentType,
-                                    'actions': modifications
-                                },
-                                success: function (data) {
-                                    self.element.html(data);
-                                    self.element.find('table.listing tbody').buicListingPart();
-
-                                    /*
-                                     Commented out for now - it has to be decided if functionality is wanted
-                                    // Restore selection state.
-                                    $(table).find('td input:checkbox[name="checkRow"]').each(function () {
-                                        var id = $(this).parents('tr').attr('id').substr(5);
-
-                                        if (id && ids.indexOf(id) >= 0) {
-                                            this.checked = true;
-                                            self._rowSelection(this);
-                                        }
-                                    });
-                                    $(table).find('tbody').each(function () {
-                                        self._handleSelectionState(this);
-                                    });
-                                    */
-                                },
-                                error: function (jqXHR, textStatus, errorThrown) {
-                                    console.log(jqXHR.status + ' (' + errorThrown + '):');
-                                    console.log(JSON.parse(jqXHR.responseText));
-                                },
-                                dataType: 'html'
-                            });
+            if(!safeQuery){
+                bootbox.dialog({
+                    message: msg,
+                    title: actions[action].name,
+                    buttons: {
+                        cancel: {
+                            label: bolt.data('recordlisting.action.cancel'),
+                            className: 'btn-default'
+                        },
+                        ok: {
+                            label: buttonText,
+                            className: 'btn-primary',
+                            callback: function(){
+                                self.sendQuery();
+                            }
                         }
                     }
-                }
+                });
+            }else{
+                self.sendQuery();
+            }
+        },
+        sendQuery: function () {
+            var self = this,
+                url = bolt.conf('paths.async') + 'content/action' + window.location.search;
+
+            $.ajax({
+                url: url,
+                type: 'POST',
+                data: {
+                    'bolt_csrf_token': self.csrfToken,
+                    'contenttype': self.contentType,
+                    'actions': self.modifications
+                },
+                success: function (data) {
+                    self.element.html(data);
+                    self.element.find('table.listing tbody').buicListingPart();
+
+                    /*
+                        Commented out for now - it has to be decided if functionality is wanted
+                    // Restore selection state.
+                    $(table).find('td input:checkbox[name="checkRow"]').each(function () {
+                        var id = $(this).parents('tr').attr('id').substr(5);
+
+                        if (id && ids.indexOf(id) >= 0) {
+                            this.checked = true;
+                            self._rowSelection(this);
+                        }
+                    });
+                    $(table).find('tbody').each(function () {
+                        self._handleSelectionState(this);
+                    });
+                    */
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log(jqXHR.status + ' (' + errorThrown + '):');
+                    console.log(JSON.parse(jqXHR.responseText));
+                },
+                dataType: 'html'
             });
         }
     });
