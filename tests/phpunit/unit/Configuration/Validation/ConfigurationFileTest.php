88a4ffa2e87d7cbf5aca473d1c12d33275b4fa16
Gawain Lynch <gawain.lynch@gmail.com>
2016-07-31 14:07:34 +0200

    [Tests] Configuration file validation tests

---
+++ tests/phpunit/unit/Configuration/Validation/ConfigurationFileTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Bolt\Tests\Configuration\Validation;
+
+/**
+ * Configuration file validation tests.
+ *
+ * @runTestsInSeparateProcesses
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+class ConfigurationFileTest extends AbstractValidationTest
+{
+    public function test()
+    {
+    }
+}
