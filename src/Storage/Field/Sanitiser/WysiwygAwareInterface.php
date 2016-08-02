dbaf20a980897621d9f1c9492fd9d5caf278f315
Gawain Lynch <gawain.lynch@gmail.com>
2016-08-02 15:49:40 +0200

    WYSIWYG aware interface

---
+++ src/Storage/Field/Sanitiser/WysiwygAwareInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Bolt\Storage\Field\Sanitiser;
+
+/**
+ * WYSIWYG aware interface.
+ *
+ * @author Gawain Lynch <gawain.lynch@gmail.com>
+ */
+interface WysiwygAwareInterface
+{
+}
