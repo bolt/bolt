04ea20d661114a463a36d27ef278764f6957390d
rarila <magnet@gmx.net>
2016-08-06 12:47:40 +0200

    Add ESLint configuration for bolt js files autoconverted from .jshintrc

---
+++ app/src/js/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "browser": true
+    },
+    "globals": {
+        "$": true,
+        "Bolt": true,
+        "CKEDITOR": true,
+        "CodeMirror": true,
+        "init": true,
+        "JSON": true,
+        "Modernizr": true,
+        "bootbox": true,
+        "google": true,
+        "jQuery": true,
+        "moment": true,
+        "UIkit": true
+    },
+    "rules": {
+        "curly": "error",
+        "eqeqeq": "error",
+        "guard-for-in": "error",
+        "wrap-iife": ["error","any"],
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        "no-use-before-define": "error",
+        "max-len": ["error", {"code": 120, "ignoreComments": true}],
+        "no-caller": "error",
+        "no-sequences": "error",
+        "no-irregular-whitespace": "error",
+        "no-new": "error",
+        "no-extra-parens": "off",
+        "no-undef": "error",
+        "no-unused-vars": "error"
+    }
+};
