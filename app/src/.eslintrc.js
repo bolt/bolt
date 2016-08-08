1e50fcede48160b8b842d01703606bddde2ddba4
rarila <magnet@gmx.net>
2016-08-08 06:15:49 +0200

    Reorder rules and comment them

--- app/src/.eslintrc.js
+++ app/src/.eslintrc.js
@@ -4,20 +4,53 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": {
-        "curly": "error",
-        "eqeqeq": "error",
-        "guard-for-in": "error",
-        "wrap-iife": ["error", "any"],
-        "indent": ["error", 4, {"SwitchCase": 1}],
-        "no-use-before-define": "error",
-        "max-len": ["warn", {"code": 120, "ignoreComments": true}],
-        "no-caller": "error",
-        "no-sequences": "error",
+        /*** Possible Errors ***/
+
+        // Disallow irregular whitespace outside of strings and comments
         "no-irregular-whitespace": "error",
-        "no-new": "error",
+        // Disallow unnecessary parentheses
         "no-extra-parens": "off",
+        // Disallow the use of console
+        "no-console": "warn",
+
+        /*** Best Practices ***/
+
+        // Enforce consistent brace style for all control statements
+        "curly": "error",
+        // Require the use of === and !==
+        "eqeqeq": "error",
+        //equire for-in loops to include an if statement
+        "guard-for-in": "error",
+        // Require parentheses around immediate function invocations
+        "wrap-iife": ["error", "any"],
+        // Disallow the use of arguments.caller or arguments.callee
+        "no-caller": "error",
+        // Disallow comma operators
+        "no-sequences": "error",
+        // Disallow new operators outside of assignments or comparisons
+        "no-new": "error",
+
+        /*** Strict Mode ***/
+
+        // Require or disallow strict mode directives
         "strict": ["error", "function"],
+
+        /*** Variables ***/
+
+        // Disallow the use of variables before they are defined
+        "no-use-before-define": "error",
+        // Disallow the use of undeclared variables unless mentioned in /*global */ comments
         "no-undef": "error",
-        "no-unused-vars": "error"
+        // Disallow unused variables
+        "no-unused-vars": "error",
+
+        /*** Node.js and CommonJS ***/
+
+        /*** Stylistic Issues ***/
+
+        // Enforce consistent indentation
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        // Enforce a maximum line length
+        "max-len": ["warn", {"code": 120, "ignoreComments": true}]
     }
 };
--- app/src/js/.eslintrc.js
+++ app/src/js/.eslintrc.js
@@ -16,18 +16,48 @@ module.exports = {
     },
     "extends": "eslint:recommended",
     "rules": {
-        "curly": "error",
-        "eqeqeq": "error",
-        "guard-for-in": "error",
-        "wrap-iife": ["error", "any"],
-        "indent": ["error", 4, {"SwitchCase": 1}],
-        "no-use-before-define": "error",
-        "max-len": ["warn", {"code": 120, "ignoreComments": true}],
-        "no-caller": "error",
-        "no-sequences": "error",
-        "no-new": "error",
+        /*** Possible Errors ***/
+
+        // Disallow irregular whitespace outside of strings and comments
+        "no-irregular-whitespace": "error",
+        // Disallow unnecessary parentheses
         "no-extra-parens": "off",
+        // Disallow the use of console
+        "no-console": "warn",
+
+        /*** Best Practices ***/
+
+        // Enforce consistent brace style for all control statements
+        "curly": "error",
+        // Require the use of === and !==
+        "eqeqeq": "error",
+        //equire for-in loops to include an if statement
+        "guard-for-in": "error",
+        // Require parentheses around immediate function invocations
+        "wrap-iife": ["error", "any"],
+        // Disallow the use of arguments.caller or arguments.callee
+        "no-caller": "error",
+        // Disallow comma operators
+        "no-sequences": "error",
+        // Disallow new operators outside of assignments or comparisons
+        "no-new": "error",
+
+        /*** Strict Mode ***/
+
+        /*** Variables ***/
+
+        // Disallow the use of variables before they are defined
+        "no-use-before-define": "error",
+        // Disallow the use of undeclared variables unless mentioned in /*global */ comments
         "no-undef": "error",
-        "no-unused-vars": ["error", {"varsIgnorePattern": "^(Bolt|init)$"}]
+        // Disallow unused variables
+        "no-unused-vars": ["error", {"varsIgnorePattern": "^(Bolt|init)$"}],
+
+        /*** Stylistic Issues ***/
+
+        // Enforce consistent indentation
+        "indent": ["error", 4, {"SwitchCase": 1}],
+        // Enforce a maximum line length
+        "max-len": ["warn", {"code": 120, "ignoreComments": true}],
     }
 };
