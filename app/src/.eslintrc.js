79c07daced9845cdd34ba2631bb991ec2ce52dc1
rarila <magnet@gmx.net>
2016-08-08 09:22:31 +0200

    Use single quotes in eslint config to be consistent

--- app/src/.eslintrc.js
+++ app/src/.eslintrc.js
@@ -1,66 +1,66 @@
 module.exports = {
-    "env": {
-        "node": true    // Define Node.js global variables and Node.js scoping
+    'env': {
+        'node': true    // Define Node.js global variables and Node.js scoping
     },
-    "extends": "eslint:recommended",
-    "rules": {
+    'extends': 'eslint:recommended',
+    'rules': {
         /*** Possible Errors ***/
 
         // Disallow the use of console
-        "no-console": "warn",
+        'no-console': 'warn',
         // Disallow unnecessary parentheses
-        "no-extra-parens": "off",
+        'no-extra-parens': 'off',
 
         /*** Best Practices ***/
 
         // Enforce consistent brace style for all control statements
-        "curly": "error",
+        'curly': 'error',
         // Require the use of === and !==
-        "eqeqeq": "error",
+        'eqeqeq': 'error',
         //equire for-in loops to include an if statement
-        "guard-for-in": "error",
+        'guard-for-in': 'error',
         // Disallow the use of arguments.caller or arguments.callee
-        "no-caller": "error",
+        'no-caller': 'error',
         // Disallow the use of eval()
-        "no-eval": "error",
+        'no-eval': 'error',
         // Disallow new operators outside of assignments or comparisons
-        "no-new": "error",
+        'no-new': 'error',
         // Disallow comma operators
-        "no-sequences": "error",
+        'no-sequences': 'error',
         // Require parentheses around immediate function invocations
-        "wrap-iife": ["error", "any"],
+        'wrap-iife': ['error', 'any'],
         // Disallow "Yoda" conditions
-        "yoda": "error",
+        'yoda': 'error',
 
         /*** Strict Mode ***/
 
         // Require or disallow strict mode directives
-        "strict": ["error", "function"],
+        'strict': ['error', 'function'],
 
         /*** Variables ***/
 
         // Disallow the use of undeclared variables unless mentioned in /*global */ comments
-        "no-undef": "error",
+        'no-undef': 'error',
         // Disallow unused variables
-        "no-unused-vars": "error",
+        'no-unused-vars': 'error',
         // Disallow the use of variables before they are defined
-        "no-use-before-define": "error",
+        'no-use-before-define': 'error',
 
         /*** Node.js and CommonJS ***/
 
         /*** Stylistic Issues ***/
 
         // Enforce consistent indentation
-        "indent": ["error", 4, {"SwitchCase": 1}],
+        'indent': ['error', 4, {'SwitchCase': 1}],
         // Enforce a maximum line length
-        "max-len": ["warn", {"code": 120, "ignoreComments": true}],
+        'max-len': ['warn', {'code': 120, 'ignoreComments': true}],
         // Enforce consistent spacing before and after semicolons
-        "semi-spacing": "error",
+        'semi-spacing': 'error',
         // Require or disallow semicolons instead of ASI
-        "semi": ["error", "always"],
+        'semi': ['error', 'always'],
         // Enforce consistent spacing before function definition opening parenthesis
-        "space-before-function-paren": ["error", {"anonymous": "always", "named": "never"}],
+        'space-before-function-paren': ['error', {'anonymous': 'always', 'named': 'never'}],
         // Require spacing around operators
-        "space-infix-ops": "error"
+        'space-infix-ops': 'error'
     }
 };
