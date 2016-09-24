ac5e50bdd7e89fa5cdadf9460e80ad61129de670
Carson Full <carsonfull@gmail.com>
2016-09-23 21:42:59 -0500

    Added themes asset package. Fixes #5759 long term.

--- src/Provider/AssetServiceProvider.php
+++ src/Provider/AssetServiceProvider.php
@@ -28,6 +28,7 @@ class AssetServiceProvider implements ServiceProviderInterface
                 $packages->addPackage('extensions', new PathPackage('', $app['asset.version_strategy']('web'), $app['asset.context']));
                 $packages->addPackage('files', $app['asset.package_factory']('files'));
                 $packages->addPackage('theme', $app['asset.package_factory']('theme'));
+                $packages->addPackage('themes', $app['asset.package_factory']('themes'));
 
                 return $packages;
             }
