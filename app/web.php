f41fc02101b54551433e528697758d89c3da1420
Carson Full <carsonfull@gmail.com>
2016-09-30 17:52:36 -0500

    Fix base path for file-like urls (i.e. thumbnails, file viewing/editing on backend UI, etc.) for built-in php server.

--- app/web.php
+++ app/web.php
@@ -14,6 +14,14 @@ if (PHP_SAPI === 'cli-server') {
     if (is_file($_SERVER['DOCUMENT_ROOT'] . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']))) {
         return false;
     }
+
+    // Fix server variables for PHP built-in server so base path is correctly determined.
+    $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+    $frame = end($trace);
+    // Absolute path to entry file
+    $_SERVER['SCRIPT_FILENAME'] = $frame['file'];
+    // Relative path to entry file from document root (dir the server is point to)
+    $_SERVER['SCRIPT_NAME'] = preg_replace("#^{$_SERVER['DOCUMENT_ROOT']}#", '', $_SERVER['SCRIPT_FILENAME']);
 }
 
 return require dirname(__FILE__) . '/bootstrap.php';
