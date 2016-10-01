52a8856b4746be7aae9a23f81c496130e407899a
Gawain Lynch <gawain.lynch@gmail.com>
2016-10-01 09:24:16 +0200

    Remove unuse imports

--- src/Storage/Field/Type/HtmlType.php
+++ src/Storage/Field/Type/HtmlType.php
@@ -6,7 +6,6 @@ use Bolt\Storage\Field\Sanitiser\SanitiserAwareInterface;
 use Bolt\Storage\Field\Sanitiser\SanitiserAwareTrait;
 use Bolt\Storage\Field\Sanitiser\WysiwygAwareInterface;
 use Doctrine\DBAL\Types\Type;
-use Twig_Markup;
 
 /**
  * This is one of a suite of basic Bolt field transformers that handles
@@ -17,7 +16,6 @@ use Twig_Markup;
 class HtmlType extends FieldTypeBase implements SanitiserAwareInterface, WysiwygAwareInterface
 {
     use SanitiserAwareTrait;
-
 
     public function hydrate($data, $entity)
     {
