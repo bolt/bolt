73b6a0bd92c6bbc0f3f16be676fe28e3cde32f4c
Carson Full <carsonfull@gmail.com>
2016-10-05 23:28:53 -0500

    Added Arr:replaceRecursive, isIndexed, and isAssociative. Deprecated mergeRecursiveDistinct and isIndexedArray. Reasons in code.

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -5,6 +5,72 @@ namespace Bolt\Helpers;
 class Arr
 {
     /**
+     * Replaces values from second array into first array recursively.
+     *
+     * This differs from {@see array_replace_recursive} in a couple ways:
+     *  - Lists (indexed arrays) from second array completely replace list in first array.
+     *  - Null values from second array do not replace lists or associative arrays in first
+     *    (they do still replace scalar values).
+     *
+     * @param array $array1
+     * @param array $array2
+     *
+     * @return array The combined array
+     */
+    public static function replaceRecursive(array $array1, array $array2)
+    {
+        $merged = $array1;
+
+        foreach ($array2 as $key => $value) {
+            if (is_array($value) && static::isAssociative($value) && isset($merged[$key]) && is_array($merged[$key])) {
+                $merged[$key] = static::replaceRecursive($merged[$key], $value);
+            } elseif ($value === null && isset($merged[$key]) && is_array($merged[$key])) {
+                continue;
+            } else {
+                $merged[$key] = $value;
+            }
+        }
+
+        return $merged;
+    }
+
+    /**
+     * Returns whether the given item is an indexed array - zero indexed and sequential.
+     *
+     * Note: Empty arrays are.
+     *
+     * @param array $array
+     *
+     * @return bool
+     */
+    public static function isIndexed($array)
+    {
+        if (!is_array($array)) {
+            return false;
+        }
+
+        return !static::isAssociative($array);
+    }
+
+    /**
+     * Returns whether the given item is an associative array.
+     *
+     * Note: Empty arrays are not.
+     *
+     * @param array $array
+     *
+     * @return bool
+     */
+    public static function isAssociative($array)
+    {
+        if (!is_array($array) || $array === []) {
+            return false;
+        }
+
+        return array_keys($array) !== range(0, count($array) - 1);
+    }
+
+    /**
      * Make a simple array consisting of key=>value pairs, that can be used
      * in select-boxes in forms.
      *
@@ -32,32 +98,15 @@ class Arr
     }
 
     /**
-     * array_merge_recursive does indeed merge arrays, but it converts values with duplicate
-     * keys to arrays rather than overwriting the value in the first array with the duplicate
-     * value in the second array, as array_merge does. I.e., with array_merge_recursive,
-     * this happens (documented behavior):
-     *
-     * array_merge_recursive(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('org value', 'new value'));
-     *
-     * array_merge_recursive_distinct does not change the datatypes of the values in the arrays.
-     * Matching keys' values in the second array overwrite those in the first array, as is the
-     * case with array_merge, i.e.:
-     *
-     * array_merge_recursive_distinct(array('key' => 'org value'), array('key' => 'new value'));
-     *     => array('key' => array('new value'));
-     *
-     * Parameters are passed by reference, though only for performance reasons. They're not
-     * altered by this function.
+     * This is the same as {@see array_replace_recursive}.
+     * Use that instead or the smarter {@see replaceRecursive}.
      *
      * @param array $array1
      * @param array $array2
      *
      * @return array
      *
-     * @author Daniel <daniel (at) danielsmedegaardbuus (dot) dk>
-     * @author Gabriel Sobrinho <gabriel (dot) sobrinho (at) gmail (dot) com>
-     * @author Bob den Otter for Bolt-specific excludes
+     * @deprecated since 3.3, to be removed in 4.0.
      */
     public static function mergeRecursiveDistinct(array &$array1, array &$array2)
     {
@@ -81,11 +130,16 @@ class Arr
     }
 
     /**
-     * Check if an array is indexed or associative.
+     * Use {@see isIndexed} or {@see isAssociative} instead.
+     *
+     * This is the same as isIndexed but it does not check if
+     * array is zero-indexed and has sequential keys.
      *
      * @param array $arr
      *
      * @return boolean True if indexed, false if associative
+     *
+     * @deprecated since 3.3, to be removed in 4.0.
      */
     public static function isIndexedArray(array $arr)
     {
