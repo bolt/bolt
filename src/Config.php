18b14061d7ca6c588a1a9a4d5684fe1116440a7d
Carson Full <carsonfull@gmail.com>
2016-10-05 23:36:23 -0500

    Replaced mergeRecursiveDistinct usage with replaceRecursive.

--- src/Config.php
+++ src/Config.php
@@ -322,7 +322,7 @@ class Config
         // "Only variables should be passed by reference")
         $tempconfig = $this->parseConfigYaml('config.yml');
         $tempconfiglocal = $this->parseConfigYaml('config_local.yml');
-        $general = Arr::mergeRecursiveDistinct($tempconfig, $tempconfiglocal);
+        $general = Arr::replaceRecursive($tempconfig, $tempconfiglocal);
 
         // Make sure old settings for 'accept_file_types' are not still picked up. Before 1.5.4 we used to store them
         // as a regex-like string, and we switched to an array. If we find the old style, fall back to the defaults.
@@ -331,7 +331,7 @@ class Config
         }
 
         // Merge the array with the defaults. Setting the required values that aren't already set.
-        $general = Arr::mergeRecursiveDistinct($this->defaultConfig, $general);
+        $general = Arr::replaceRecursive($this->defaultConfig, $general);
 
         // Make sure the cookie_domain for the sessions is set properly.
         if (empty($general['cookies_domain'])) {
