fa4d45f0609c22bf46f06c039e90d06b048b9c15
Gawain Lynch <gawain.lynch@gmail.com>
2016-10-07 10:19:42 +0200

    Move registration of filesystem provider up the chain … 'cause loading still needs work

--- src/Application.php
+++ src/Application.php
@@ -98,6 +98,7 @@ class Application extends Silex\Application
     {
         $this
             ->register(new Provider\BootServiceProvider($this))
+            ->register(new Provider\FilesystemServiceProvider())
             ->register(new Provider\DatabaseSchemaServiceProvider())
             ->register(new Provider\ConfigServiceProvider())
         ;
@@ -242,7 +243,6 @@ class Application extends Silex\Application
             ->register(new Provider\FilePermissionsServiceProvider())
             ->register(new Provider\MenuServiceProvider())
             ->register(new Provider\UploadServiceProvider())
-            ->register(new Provider\FilesystemServiceProvider())
             ->register(new Provider\ThumbnailsServiceProvider())
             ->register(new Provider\NutServiceProvider())
             ->register(new Provider\GuzzleServiceProvider())
