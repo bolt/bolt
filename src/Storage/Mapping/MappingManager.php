16208d675822b0470f2bb651b690f1dcfc25a16f
Ross Riley <riley.ross@gmail.com>
2016-10-12 13:48:48 +0100

    add a manager class to lookup and load types

---
+++ src/Storage/Mapping/MappingManager.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Bolt\Storage\Mapping;
+
+use InvalidArgumentException;
+
+/**
+ * This class takes care of looking up a mapping class from the registered handlers.
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+
+class MappingManager
+{
+    protected $handlers;
+    protected $defaultHandler;
+
+    public function __construct(array $handlers, $defaultHandler = null)
+    {
+        $this->handlers = $handlers;
+        $this->defaultHandler = $defaultHandler;
+    }
+
+    public function setHandlers(array $handlers)
+    {
+        $this->handlers = $handlers;
+    }
+
+    public function addHandler($type, $handler)
+    {
+        $this->handlers[$type] = $handler;
+    }
+
+    public function getHandler($type)
+    {
+        if (!array_key_exists($type, $this->handlers)) {
+            return $this->defaultHandler;
+        }
+        return $this->handlers[$type];
+    }
+
+    public function setDefaultHandler($handler)
+    {
+        $this->defaultHandler = $handler;
+    }
+
+    public function load($name, array $parameters)
+    {
+        if (!array_key_exists('type', $parameters) || empty($parameters['type'])) {
+            $error = sprintf('Field "%s" has no "type" set.', $name);
+
+            throw new InvalidArgumentException($error);
+        }
+
+        $handler = $this->getHandler($parameters['type']);
+
+        return new $handler($name, $parameters);
+    }
+
+}
