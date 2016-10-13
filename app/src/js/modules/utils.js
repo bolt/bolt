5e46dc040ca91233d4642305a210b0e964368501
Gawain Lynch <gawain.lynch@gmail.com>
2016-10-13 18:54:11 +0200

    Fix JS Hint errors

--- app/src/js/modules/utils.js
+++ app/src/js/modules/utils.js
@@ -83,12 +83,16 @@
                 args = arguments;
             var later = function () {
                 timeout = null;
-                if (!immediate) func.apply(context, args);
+                if (!immediate) {
+                    func.apply(context, args);
+                }
             };
             var callNow = immediate && !timeout;
             clearTimeout(timeout);
             timeout = setTimeout(later, wait);
-            if (callNow) func.apply(context, args);
+            if (callNow) {
+                func.apply(context, args);
+            }
         };
     };
 
