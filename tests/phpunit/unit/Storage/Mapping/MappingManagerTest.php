90931027cc42243803d106df24f8e6a507b87ef9
Ross Riley <riley.ross@gmail.com>
2016-10-13 10:16:05 +0100

    add more tests to make sure manager loads correct instance

--- tests/phpunit/unit/Storage/Mapping/MappingManagerTest.php
+++ tests/phpunit/unit/Storage/Mapping/MappingManagerTest.php
@@ -12,12 +12,21 @@ use Bolt\Tests\BoltUnitTest;
  */
 class MappingManagerTest extends BoltUnitTest
 {
-    public function testCaseTransform()
+    public function testLookup()
     {
         $app = $this->getApp();
-
         $manager = new MappingManager($app['mapping.definitions'], $app['mapping.default']);
 
         $this->assertEquals('Bolt\Storage\Mapping\Definition\Slug', $manager->getHandler('slug'));
+        $this->assertEquals('Bolt\Storage\Mapping\Definition', $manager->getHandler('nonexistent'));
+    }
+
+    public function testLoad()
+    {
+        $app = $this->getApp();
+        $manager = new MappingManager($app['mapping.definitions'], $app['mapping.default']);
+
+        $this->assertInstanceOf('Bolt\Storage\Mapping\Definition\Slug', $manager->load('slug', ['type' => 'slug']));
+        $this->assertInstanceOf('Bolt\Storage\Mapping\Definition', $manager->load('title', ['type' => 'text']));
     }
 }
