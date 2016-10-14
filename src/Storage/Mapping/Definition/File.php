06e8a1e846b8a9102d3aff470b2484bd78fb1673
Ross Riley <riley.ross@gmail.com>
2016-10-14 16:40:57 +0100

    add new classes for images and files

---
+++ src/Storage/Mapping/Definition/File.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bolt\Storage\Mapping\Definition;
+
+use Bolt\Storage\Mapping\Definition;
+
+/**
+ * Adds specific functionality for the file and filelist definition
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+
+class File extends Definition
+{
+
+}
---
+++ src/Storage/Mapping/Definition/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Bolt\Storage\Mapping\Definition;
+
+use Bolt\Storage\Mapping\Definition;
+
+/**
+ * Adds specific functionality for the image and imagelist definition
+ *
+ * @author Ross Riley <riley.ross@gmail.com>
+ */
+
+class Image extends Definition
+{
+
+}
