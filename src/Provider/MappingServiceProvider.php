a01883c9b9b56d2f6928d836265b013390039e10
Ross Riley <riley.ross@gmail.com>
2016-10-15 14:30:10 +0100

    setup mapping to new classes

--- src/Provider/MappingServiceProvider.php
+++ src/Provider/MappingServiceProvider.php
@@ -22,13 +22,18 @@ class MappingServiceProvider implements ServiceProviderInterface
         );
 
         $app['mapping.definitions'] = [
+            'date'                         => 'Bolt\Storage\Mapping\Definition\Date',
+            'datetime'                     => 'Bolt\Storage\Mapping\Definition\Date',
             'file'                         => 'Bolt\Storage\Mapping\Definition\File',
             'filelist'                     => 'Bolt\Storage\Mapping\Definition\File',
+            'float'                        => 'Bolt\Storage\Mapping\Definition\Number',
             'image'                        => 'Bolt\Storage\Mapping\Definition\Image',
             'imagelist'                    => 'Bolt\Storage\Mapping\Definition\Image',
+            'integer'                      => 'Bolt\Storage\Mapping\Definition\Number',
+            'repeater'                     => 'Bolt\Storage\Mapping\Definition\Repeater',
             'select'                       => 'Bolt\Storage\Mapping\Definition\Select',
             'slug'                         => 'Bolt\Storage\Mapping\Definition\Slug',
-            'repeater'                     => 'Bolt\Storage\Mapping\Definition\Repeater',
+            'textarea'                     => 'Bolt\Storage\Mapping\Definition\Textarea',
         ];
 
         $app['mapping.default'] = 'Bolt\Storage\Mapping\Definition';
