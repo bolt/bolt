d3fc40ec1bc5c3476b2cba157dce0c7cbed0be77
Ross Riley <riley.ross@gmail.com>
2016-10-16 17:14:32 +0100

    add new methods to get contenttype values

--- src/Storage/Mapping/ContentType.php
+++ src/Storage/Mapping/ContentType.php
@@ -2,14 +2,16 @@
 
 namespace Bolt\Storage\Mapping;
 
+use ArrayAccess;
+
 /**
- * Legacy bridge for ContentType array access.
+ * Mapping class to represent a ContentType with array access.
  *
- * @deprecated Deprecated since 3.0, to be removed in 4.0.
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
+ * @author Ross Riley <riley.ross@gmail.com>
  */
-class ContentType implements \ArrayAccess
+class ContentType implements ArrayAccess
 {
     /** @var string */
     protected $boltname;
@@ -28,11 +30,110 @@ class ContentType implements \ArrayAccess
         $this->contentType = $contentType;
     }
 
+    public function setup()
+    {
+
+    }
+
+    public function validate()
+    {
+
+    }
+
+    public function getDefaultStatus()
+    {
+        return $this->get('default_status', 'draft');
+    }
+
+    public function getFields()
+    {
+        return $this->get('fields', []);
+    }
+
+    public function getIconMany()
+    {
+        return $this->get('icon_many', false);
+    }
+
+    public function getIconOne()
+    {
+        return $this->get('icon_one', false);
+    }
+
+    public function getName()
+    {
+        return $this->get('name');
+    }
+
+    public function getRelations()
+    {
+        return $this->get('relations', []);
+    }
+
+    public function getSearchable()
+    {
+        return $this->get('searchable', true);
+    }
+
+    public function getShowOnDashboard()
+    {
+        return $this->get('show_on_dashboard', true);
+    }
+
+    public function getSingularName()
+    {
+        return $this->get('singular_name');
+    }
+
+    public function getSingularSlug()
+    {
+        return $this->get('singular_slug');
+    }
+
+    public function getSlug()
+    {
+        return $this->get('slug');
+    }
+
+    public function getTaxonomy()
+    {
+        return $this->get('taxonomy', []);
+    }
+
+    public function getViewless()
+    {
+        return $this->get('viewless', false);
+    }
+
+
+
+    protected function get($param, $default = null)
+    {
+        if ($this->has($param)) {
+            return $this->contentType[$param];
+        }
+
+        return $default;
+    }
+
+    protected function has($param)
+    {
+        if (array_key_exists($param, $this->contentType) && !empty($this->contentType[$param])) {
+            return true;
+        }
+
+        return false;
+    }
+
     public function __toString()
     {
         return $this->boltname;
     }
 
+    /**
+     *  ArrayAccess interface methods
+     *
+     */
     public function offsetSet($offset, $value)
     {
         $this->contentType[$offset] = $value;
