42ab85962431ab6878a46fbb278a1eb6cf5ea454
Ross Riley <riley.ross@gmail.com>
2016-10-17 14:14:42 +0100

    all the validation and setup is handled in the class, so can be removed from Config

--- src/Config.php
+++ src/Config.php
@@ -14,7 +14,7 @@ use Bolt\Filesystem\Handler\ParsableInterface;
 use Bolt\Helpers\Deprecated;
 use Bolt\Helpers\Html;
 use Bolt\Helpers\Str;
-use Bolt\Storage\Database;
+use Bolt\Storage\Mapping\ContentType;
 use Bolt\Translation\Translator;
 use Bolt\Translation\Translator as Trans;
 use Cocur\Slugify\Slugify;
@@ -392,6 +392,7 @@ class Config
         foreach ($tempContentTypes as $key => $contentType) {
             try {
                 $contentType = $this->parseContentType($key, $contentType, $generalConfig);
+                $key = $contentType->getSlug();
                 $contentTypes[$key] = $contentType;
             } catch (InvalidArgumentException $e) {
                 $this->exceptions[] = $e->getMessage();
@@ -476,211 +477,13 @@ class Config
      */
     protected function parseContentType($key, $contentType, $generalConfig)
     {
-        // If the slug isn't set, and the 'key' isn't numeric, use that as the slug.
-        if (!isset($contentType['slug']) && !is_numeric($key)) {
-            $contentType['slug'] = Slugify::create()->slugify($key);
-        }
 
-        // If neither 'name' nor 'slug' is set, we need to warn the user. Same goes for when
-        // neither 'singular_name' nor 'singular_slug' is set.
-        if (!isset($contentType['name']) && !isset($contentType['slug'])) {
-            $error = sprintf("In contenttype <code>%s</code>, neither 'name' nor 'slug' is set. Please edit <code>contenttypes.yml</code>, and correct this.", $key);
-            throw new InvalidArgumentException($error);
-        }
-        if (!isset($contentType['singular_name']) && !isset($contentType['singular_slug'])) {
-            $error = sprintf("In contenttype <code>%s</code>, neither 'singular_name' nor 'singular_slug' is set. Please edit <code>contenttypes.yml</code>, and correct this.", $key);
-            throw new InvalidArgumentException($error);
-        }
-
-        // Contenttypes without fields make no sense.
-        if (!isset($contentType['fields'])) {
-            $error = sprintf("In contenttype <code>%s</code>, no 'fields' are set. Please edit <code>contenttypes.yml</code>, and correct this.", $key);
-            throw new InvalidArgumentException($error);
-        }
-
-        if (!isset($contentType['slug'])) {
-            $contentType['slug'] = Slugify::create()->slugify($contentType['name']);
-        }
-        if (!isset($contentType['name'])) {
-            $contentType['name'] = ucwords(preg_replace('/[^a-z0-9]/i', ' ', $contentType['slug']));
-        }
-        if (!isset($contentType['singular_slug'])) {
-            $contentType['singular_slug'] = Slugify::create()->slugify($contentType['singular_name']);
-        }
-        if (!isset($contentType['singular_name'])) {
-            $contentType['singular_name'] = ucwords(preg_replace('/[^a-z0-9]/i', ' ', $contentType['singular_slug']));
-        }
-        if (!isset($contentType['show_on_dashboard'])) {
-            $contentType['show_on_dashboard'] = true;
-        }
-        if (!isset($contentType['show_in_menu'])) {
-            $contentType['show_in_menu'] = true;
-        }
-        if (!isset($contentType['sort'])) {
-            $contentType['sort'] = false;
-        }
-        if (!isset($contentType['default_status'])) {
-            $contentType['default_status'] = 'draft';
-        }
-        if (!isset($contentType['viewless'])) {
-            $contentType['viewless'] = false;
-        }
-        if (!isset($contentType['liveeditor'])) {
-            $contentType['liveeditor'] = true;
-        }
-        // Override contenttype setting with view and config settings
-        if (($contentType['viewless']) || (!$generalConfig['liveeditor'])) {
-            $contentType['liveeditor'] = false;
-        }
-        // Allow explicit setting of a Contenttype's table name suffix. We default
-        // to slug if not present as it has been this way since Bolt v1.2.1
-        if (!isset($contentType['tablename'])) {
-            $contentType['tablename'] = Slugify::create()->slugify($contentType['slug'], '_');
-        } else {
-            $contentType['tablename'] = Slugify::create()->slugify($contentType['tablename'], '_');
-        }
-        if (!isset($contentType['allow_numeric_slugs'])) {
-            $contentType['allow_numeric_slugs'] = false;
-        }
-
-        list($fields, $groups) = $this->parseFieldsAndGroups($contentType['fields'], $generalConfig);
-        $contentType['fields'] = $fields;
-        $contentType['groups'] = $groups;
-
-        // Make sure taxonomy is an array.
-        if (isset($contentType['taxonomy'])) {
-            $contentType['taxonomy'] = (array) $contentType['taxonomy'];
-        }
-
-        // when adding relations, make sure they're added by their slug. Not their 'name' or 'singular name'.
-        if (!empty($contentType['relations']) && is_array($contentType['relations'])) {
-            foreach (array_keys($contentType['relations']) as $relkey) {
-                if ($relkey != Slugify::create()->slugify($relkey)) {
-                    $contentType['relations'][Slugify::create()->slugify($relkey)] = $contentType['relations'][$relkey];
-                    unset($contentType['relations'][$relkey]);
-                }
-            }
-        }
+        $contentType = new ContentType($key, $contentType, $generalConfig);
+        $contentType->setup();
+        $contentType->validate();
+        $contentType->setupFields($this->app['mapping']);
 
         return $contentType;
-    }
-
-    /**
-     * Parse a Contenttype's filed and determine the grouping.
-     *
-     * @param array $fields
-     * @param array $generalConfig
-     *
-     * @return array
-     */
-    protected function parseFieldsAndGroups(array $fields, array $generalConfig)
-    {
-        $acceptableFileTypes = $generalConfig['accept_file_types'];
-
-        $currentGroup = 'ungrouped';
-        $groups = [];
-        $hasGroups = false;
-
-        foreach ($fields as $key => $field) {
-            unset($fields[$key]);
-            $key = str_replace('-', '_', strtolower(Str::makeSafe($key, true)));
-            if (!isset($field['type']) || empty($field['type'])) {
-                $error = sprintf('Field "%s" has no "type" set.', $key);
-
-                throw new InvalidArgumentException($error);
-            }
-
-            // If field is a "file" type, make sure the 'extensions' are set, and it's an array.
-            if ($field['type'] == 'file' || $field['type'] == 'filelist') {
-                if (empty($field['extensions'])) {
-                    $field['extensions'] = $acceptableFileTypes;
-                }
-
-                $field['extensions'] = (array) $field['extensions'];
-            }
-
-            // If field is an "image" type, make sure the 'extensions' are set, and it's an array.
-            if ($field['type'] == 'image' || $field['type'] == 'imagelist') {
-                if (empty($field['extensions'])) {
-                    $field['extensions'] = array_intersect(
-                        Image\Type::getExtensions(),
-                        $acceptableFileTypes
-                    );
-                }
-
-                $field['extensions'] = (array) $field['extensions'];
-            }
-
-            // Make indexed arrays into associative for select fields
-            // e.g.: [ 'yes', 'no' ] => { 'yes': 'yes', 'no': 'no' }
-            if ($field['type'] === 'select' && isset($field['values']) && Arr::isIndexed($field['values'])) {
-                $field['values'] = array_combine($field['values'], $field['values']);
-            }
-
-            if (!empty($field['group'])) {
-                $hasGroups = true;
-            }
-
-            // Make sure we have these keys and every field has a group set.
-            $field = array_replace(
-                [
-                    'class'   => '',
-                    'default' => '',
-                    'group'   => $currentGroup,
-                    'label'   => '',
-                    'variant' => '',
-                ],
-                $field
-            );
-
-            // Collect group data for rendering.
-            // Make sure that once you started with group all following have that group, too.
-            $currentGroup = $field['group'];
-            $groups[$currentGroup] = 1;
-
-            $fields[$key] = $field;
-
-            // Repeating fields checks
-            if ($field['type'] === 'repeater') {
-                $fields[$key] = $this->parseFieldRepeaters($fields, $key);
-                if ($fields[$key] === null) {
-                    unset($fields[$key]);
-                }
-            }
-        }
-
-        // Make sure the 'uses' of the slug is an array.
-        if (isset($fields['slug']) && isset($fields['slug']['uses'])) {
-            $fields['slug']['uses'] = (array) $fields['slug']['uses'];
-        }
-
-        return [$fields, $hasGroups ? array_keys($groups) : []];
-    }
-
-    /**
-     * Basic validation of repeater fields.
-     *
-     * @param array  $fields
-     * @param string $key
-     *
-     * @return array
-     */
-    private function parseFieldRepeaters(array $fields, $key)
-    {
-        $blacklist = ['repeater', 'slug', 'templatefield'];
-        $repeater = $fields[$key];
-
-        if (!isset($repeater['fields']) || !is_array($repeater['fields'])) {
-            return;
-        }
-
-        foreach ($repeater['fields'] as $repeaterKey => $repeaterField) {
-            if (!isset($repeaterField['type']) || in_array($repeaterField['type'], $blacklist)) {
-                unset($repeater['fields'][$repeaterKey]);
-            }
-        }
-
-        return $repeater;
     }
 
     /**
