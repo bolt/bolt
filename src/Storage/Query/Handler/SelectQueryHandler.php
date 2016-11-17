1df7bcf5dc155b3084c67b9868b842bfe72e0d91
Ross Riley <riley.ross@gmail.com>
2016-11-17 08:05:02 +0000

    handle whitelisting of fields in the select handler

--- src/Storage/Query/Handler/SelectQueryHandler.php
+++ src/Storage/Query/Handler/SelectQueryHandler.php
@@ -25,18 +25,16 @@ class SelectQueryHandler
             $query->setQueryBuilder($repo->createQueryBuilder($contenttype));
             $query->setContentType($contenttype);
 
-            /** This block is added to deal with the possibility that a requested filter is not an allowable option on the
-             * database table. If the requested field filter is not a valid field on this table then we completely skip
-             * the query because no results will be expected if the field does not exist.
+            /** Run the parameters through the whitelister. If we get a false back from this method it's because there
+             * is no need to continue with the query.
              */
-            $metadata = $repo->getClassMetadata();
-            $allowedParams = array_keys($metadata->getFieldMappings());
-            $queryParams = array_keys($contentQuery->getParameters());
-            if (array_diff($queryParams, $allowedParams)) {
+            $params = $this->whitelistParameters($contentQuery->getParameters(), $repo);
+            if (!$params) {
                 continue;
             }
 
-            $query->setParameters($contentQuery->getParameters());
+            /** Continue and run the query add the results to the set */
+            $query->setParameters($params);
             $contentQuery->runDirectives($query);
 
             $result = $repo->queryWith($query);
@@ -50,5 +48,38 @@ class SelectQueryHandler
         } else {
             return $set;
         }
+    }
+
+    /** This block is added to deal with the possibility that a requested filter is not an allowable option on the
+     * database table. If the requested field filter is not a valid field on this table then we completely skip
+     * the query because no results will be expected if the field does not exist. The exception to this is if the field
+     * is part of an OR query then we remove the missing field from the stack but still allow the other fields through.
+     * @param array $queryParams
+     * @param $repo
+     * @return bool|array $cleanParams
+     */
+    public function whitelistParameters(array $queryParams, $repo)
+    {
+        $metadata = $repo->getClassMetadata();
+        $allowedParams = array_keys($metadata->getFieldMappings());
+        $cleanParams = [];
+        foreach ($queryParams as $fieldSelect => $valueSelect) {
+            $stack = preg_split('/ *(\|\|\|) */', $fieldSelect);
+            if (count($stack) > 1) {
+                $allowed = array_intersect($stack, $allowedParams);
+                if (!count($allowed)) {
+                    return false;
+                }
+                $allowed = join(" ||| ", $allowed);
+                $cleanParams[$allowed] = $valueSelect;
+            } else {
+                if (!in_array($fieldSelect, $allowedParams)) {
+                    return false;
+                }
+                $cleanParams[$fieldSelect] = $valueSelect;
+            }
+        }
+
+        return $cleanParams;
     }
 }
