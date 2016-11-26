7867be19b467317d5b1406dc6398495dc1d3765e
Ross Riley <riley.ross@gmail.com>
2016-11-26 08:59:26 +0000

    remove mapping of underscores to dots

--- src/Storage/CaseTransformTrait.php
+++ src/Storage/CaseTransformTrait.php
@@ -28,6 +28,6 @@ trait CaseTransformTrait
      */
     public function underscore($id)
     {
-        return strtolower(preg_replace(['/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'], ['\\1_\\2', '\\1_\\2'], str_replace('_', '.', $id)));
+        return strtolower(preg_replace(['/([A-Z]+)([A-Z][a-z])/', '/([a-z\d])([A-Z])/'], ['\\1_\\2', '\\1_\\2'], $id));
     }
 }
