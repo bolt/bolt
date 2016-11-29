c6d6d234c0408edab1859c4df18148e4e355b1cb
Carson Full <carsonfull@gmail.com>
2016-11-28 19:02:07 -0600

    Added more test cases to CanonicalTest

--- tests/phpunit/unit/Canonical/CanonicalTest.php
+++ tests/phpunit/unit/Canonical/CanonicalTest.php
@@ -9,16 +9,31 @@ use Symfony\Component\HttpFoundation\Request;
  * Test for Canonical class.
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
+ * @author Carson Full <carsonfull@gmail.com>
  */
 class CanonicalTest extends BoltUnitTest
 {
-    public function testCanonical()
+    public function provider()
+    {
+        return [
+            'default'                  => [null, '/drop/bear', 'http://bolt.dev/drop/bear'],
+            'override host'            => ['koala.org.au', '/drop/bear', 'http://koala.org.au/drop/bear'],
+            'override host and https'  => ['https://koala.org.au', '/drop/bear', 'https://koala.org.au/drop/bear'],
+            'https does not downgrade' => ['koala.org.au', 'https://koala.org.au/drop/bear', 'https://koala.org.au/drop/bear'],
+        ];
+    }
+
+    /**
+     * @dataProvider provider
+     */
+    public function testCanonical($override, $uri, $expected)
     {
         $app = $this->getApp(false);
         $app->match('/drop/bear', function () { return '<html><head></head><body>Koala!</body></html>'; });
-        $canonical = $app['config']->get('general/canonical');
-        $app['config']->set('general/canonical', 'koala.org.au');
-        $request = Request::create('https://koala.org.au/drop/bear');
+        if ($override !== null) {
+            $app['config']->set('general/canonical', $override);
+        }
+        $request = Request::create($uri);
 
         $url = null;
         $app->after(function() use (&$url, $app) {
@@ -26,8 +41,7 @@ class CanonicalTest extends BoltUnitTest
         });
 
         $app->handle($request);
-        $app['config']->set('general/canonical', $canonical);
 
-        $this->assertEquals('https://koala.org.au/drop/bear', $url);
+        $this->assertEquals($expected, $url);
     }
 }
