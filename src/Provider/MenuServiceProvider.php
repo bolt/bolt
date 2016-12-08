3e1f4e588eeaa9734eeac7ae69d9f98443efa974
Carson Full <carsonfull@gmail.com>
2016-12-07 23:51:05 -0600

    Determine menu base path from request base path and backend mount prefix instead of generating url.
    Fixes #6139 The base url generated could have query params or fragment which breaks urls that are concatenated to it.

--- src/Provider/MenuServiceProvider.php
+++ src/Provider/MenuServiceProvider.php
@@ -29,8 +29,13 @@ class MenuServiceProvider implements ServiceProviderInterface
         $app['menu.admin'] = $app->share(
             function ($app) {
                 // This service should not be invoked until request cycle since it depends
-                // on url generation. Probably should be refactored somehow.
-                $baseUrl = $app['url_generator']->generate('dashboard');
+                // on url generation and request base path. Probably should be refactored somehow.
+                $baseUrl = '';
+                if (($request = $app['request_stack']->getCurrentRequest()) !== null) {
+                    $baseUrl = $request->getBasePath();
+                }
+                $baseUrl .= '/' . trim($app['controller.backend.mount_prefix'], '/');
+
                 $adminMenu = new AdminMenuBuilder(new MenuEntry('root', $baseUrl));
                 $rootEntry = $adminMenu->build($app);
 
