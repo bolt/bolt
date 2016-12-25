b5141a53e20473c910191b55614726caab40a2ca
Ross Riley <riley.ross@gmail.com>
2016-12-25 19:48:56 +0700

    add new field to legacy manager

--- src/Storage/Field/Manager.php
+++ src/Storage/Field/Manager.php
@@ -16,7 +16,7 @@ class Manager
     protected $defaults = [
         'text', 'integer', 'float', 'geolocation', 'imagelist', 'image', 'file', 'filelist', 'video', 'hidden', 'html',
         'textarea', 'datetime', 'date', 'select', 'templateselect', 'templatefields', 'markdown', 'checkbox', 'slug',
-        'repeater',
+        'repeater', 'repeaterblock'
     ];
 
     protected $dummyFields = ['repeater'];
