bcadc74345e39c3984032dfa71bcaf0b7b9005fd
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-13 16:37:30 +0100

    Add profiler debug handler listener

--- src/Provider/DebugServiceProvider.php
+++ src/Provider/DebugServiceProvider.php
@@ -5,6 +5,7 @@ namespace Bolt\Provider;
 use Bolt\Debug\ShutdownHandler;
 use Silex\Application;
 use Silex\ServiceProviderInterface;
+use Symfony\Component\HttpKernel\EventListener\DebugHandlersListener;
 
 class DebugServiceProvider implements ServiceProviderInterface
 {
@@ -30,6 +31,21 @@ class DebugServiceProvider implements ServiceProviderInterface
 
         // Register PHP shutdown functions to catch fatal errors & exceptions
         ShutdownHandler::register();
+
+        $app['debug.debug_handlers_listener'] = $app->share(
+            function ($app) {
+                return new DebugHandlersListener(
+                    null,
+                    $app['logger'],
+                    ShutdownHandler::$errorLevels, // Levels
+                    ShutdownHandler::$errorLevels, // Throw at
+                    true, // Scream
+                    $app['debug.file_link_formatter']
+                );
+            }
+        );
+
+        $app['debug.file_link_formatter'] = null;
     }
 
     /**
@@ -39,6 +55,8 @@ class DebugServiceProvider implements ServiceProviderInterface
     {
         if (!$app['debug']) {
             ShutdownHandler::register(false);
+        } else {
+            $app['dispatcher']->addSubscriber($app['debug.debug_handlers_listener']);
         }
 
         $errorLevel = $app['config']->get($app['debug'] ? 'general/debug_error_level' : 'production_error_level');
