ed1f8243137aa8e9071321caeb2ca8b641e74b5e
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-16 10:05:14 +0100

    Fix "Undefined index: repeater" in ContentValuesTrait

--- src/Storage/Entity/ContentValuesTrait.php
+++ src/Storage/Entity/ContentValuesTrait.php
@@ -125,6 +125,9 @@ trait ContentValuesTrait
         // add the fields for this contenttype,
         if (is_array($contenttype)) {
             foreach ($contenttype['fields'] as $field => $property) {
+                if (!isset($this->values[$field])) {
+                    continue;
+                }
                 switch ($property['type']) {
                     // Set the slug, while we're at it
                     case 'slug':
