7d5e9b62dd4e11fe070861b1a00e08dccb19df11
kouz75 <kouz2@free.fr>
2017-01-19 10:36:42 +0100

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -322,8 +322,7 @@ class Config
         // Read the config and merge it. (note: We use temp variables to prevent
         // "Only variables should be passed by reference")
         $tempconfig = $this->parseConfigYaml('config.yml');
-        if(isset($_SERVER['ENVIRONMENT']))
-        {
+        if (isset($_SERVER['ENVIRONMENT'])) {
             $tempconfigenvironment = $this->parseConfigYaml('config_'.$_SERVER['ENVIRONMENT'].'.yml');
             $tempconfig = Arr::mergeRecursiveDistinct($tempconfig, $tempconfigenvironment);
         }
