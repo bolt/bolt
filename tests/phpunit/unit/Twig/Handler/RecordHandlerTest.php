be9623d976d3953aefbb8dd28af2a487f45ec365
Gawain Lynch <gawain.lynch@gmail.com>
2017-01-21 14:52:28 +0100

    [Tests] Update expectations for theme.yml

--- tests/phpunit/unit/Twig/Handler/RecordHandlerTest.php
+++ tests/phpunit/unit/Twig/Handler/RecordHandlerTest.php
@@ -438,15 +438,17 @@ GRINGALET;
             ],
             'clippy' => [
                 'name'     => 'Clippy',
-                'filename' => 'anotherextrafields.twig',
+                'filename' => 'styleguide.twig',
             ],
         ]);
         $handler = new RecordHandler($app);
 
         $result = $handler->listTemplates('*extra*', false);
         $this->assertArrayHasKey('extrafields.twig', $result);
-        $this->assertArrayHasKey('anotherextrafields.twig', $result);
         $this->assertContains('Koala', $result);
+
+        $result = $handler->listTemplates('*styleguide*', false);
+        $this->assertArrayHasKey('styleguide.twig', $result);
         $this->assertContains('Clippy', $result);
 
         $this->assertArrayNotHasKey('entry.twig', $result);
