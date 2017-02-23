f3b4aff8365d558f5ce2e8a0b7c185ceb662a850
Carson Full <carsonfull@gmail.com>
2017-02-23 16:14:24 -0600

    Added "site" path alias, which acts as the root directory for all site files (app, web, extensions). By default it is the same as root, but this allows for all paths to be moved to a subdir at once.
    "root" cannot be used for this as it needs to be the root directory
    containing the "vendor" dir, so that paths can be made relative correctly.

--- src/Configuration/PathResolver.php
+++ src/Configuration/PathResolver.php
@@ -27,13 +27,14 @@ class PathResolver
     public static function defaultPaths()
     {
         return [
-            'app'               => 'app',
+            'site'              => '.',
+            'app'               => '%site%/app',
             'cache'             => '%app%/cache',
             'config'            => '%app%/config',
             'database'          => '%app%/database',
-            'extensions'        => 'extensions',
+            'extensions'        => '%site%/extensions',
             'extensions_config' => '%config%/extensions',
-            'web'               => 'public',
+            'web'               => '%site%/public',
             'files'             => '%web%/files',
             'themes'            => '%web%/theme',
             'bolt_assets'       => '%web%/bolt-public/view',
