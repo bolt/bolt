e3544b18dbc3430779009709fbb684572a841ea8
Gawain Lynch <gawain.lynch@gmail.com>
2017-03-03 16:35:25 +1200

    Throw an exception when a collection is created missing a handler

--- src/Storage/Collection/CollectionManager.php
+++ src/Storage/Collection/CollectionManager.php
@@ -3,6 +3,7 @@
 namespace Bolt\Storage\Collection;
 
 use Bolt\Storage\EntityManager;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Collection Manager class
@@ -33,10 +34,19 @@ class CollectionManager
         $this->em = $em;
     }
 
+    /**
+     * @param string $class
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return ArrayCollection
+     */
     public function create($class)
     {
-        if (isset($this->collections[$class])) {
-            return call_user_func($this->collections[$class]);
+        if (!isset($this->collections[$class])) {
+            throw new \InvalidArgumentException(sprintf('No collection handler exists for %s', $class));
         }
+
+        return call_user_func($this->collections[$class]);
     }
 }
