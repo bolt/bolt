e74b62d6a8c8d6d920558180f5090335b6f0723e
Gawain Lynch <gawain.lynch@gmail.com>
2017-03-27 07:42:23 +0200

    [Tests] Coverage for Bolt\Extension\TwigTrait

--- tests/phpunit/unit/Extension/TwigTraitTest.php
+++ tests/phpunit/unit/Extension/TwigTraitTest.php
@@ -3,6 +3,8 @@
 namespace Bolt\Tests\Extension;
 
 use Bolt\Tests\BoltUnitTest;
+use Bolt\Tests\Extension\Mock\TwigExtension;
+use Twig_Loader_Array as ArrayLoader;
 
 /**
  * Class to test Bolt\Extension\TwigTrait
@@ -11,7 +13,45 @@ use Bolt\Tests\BoltUnitTest;
  */
 class TwigTraitTest extends BoltUnitTest
 {
-    public function test()
+    public function testTwigExtension()
     {
+        $app = $this->getApp(false);
+        $ext = new TwigExtension();
+        $baseDir = $app['filesystem']->getDir('extensions://');
+        $baseDir->setPath('local/bolt/twig');
+        $ext->setBaseDirectory($baseDir);
+        $ext->setContainer($app);
+        $ext->register($app);
+        $app->boot();
+
+        $this->addToAssertionCount(1);
+    }
+
+    public function testRenderTemplate()
+    {
+        $app = $this->getApp();
+        $template = <<< TWIG
+Function koala {{ koala(name) }}
+Function dropbear {{ dropbear(name) }}
+Filter koala {{ name|koala }}
+Filter dropbear {{ name|dropbear }}
+TWIG;
+        $loader = new ArrayLoader(['marsupial.twig' => $template]);
+        $app['twig']->setLoader($loader);
+
+        $ext = new TwigExtension();
+        $baseDir = $app['filesystem']->getDir('extensions://');
+        $baseDir->setPath('local/bolt/twig');
+        $ext->setBaseDirectory($baseDir);
+        $ext->setContainer($app);
+        $ext->register($app);
+
+        $context = ['name' => 'Kenny Koala'];
+        $html = $ext->getTestTemplateOutput('marsupial.twig', $context);
+
+        $this->assertRegExp('/Function koala kenny koala/', $html);
+        $this->assertRegExp('/Function dropbear kenny koala/', $html);
+        $this->assertRegExp('/Filter koala KENNY KOALA/', $html);
+        $this->assertRegExp('/Filter dropbear KENNY KOALA/', $html);
     }
 }
