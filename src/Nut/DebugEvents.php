dca3f42b8f9ed41d83809faced7f6c4ab7baa5b1
Gawain Lynch <gawain.lynch@gmail.com>
2017-04-04 20:09:19 +0200

    Add sorting options to DebugEvents

--- src/Nut/DebugEvents.php
+++ src/Nut/DebugEvents.php
@@ -3,7 +3,9 @@
 namespace Bolt\Nut;
 
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Helper\TableCell;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -20,7 +22,8 @@ class DebugEvents extends BaseCommand
     {
         $this
             ->setName('debug:events')
-            ->setDescription('System events, and target callable debug dumper.')
+            ->setDescription('Events, and target callable, debug dumper.')
+            ->addOption('sort-callable', null, InputOption::VALUE_NONE, 'Sort events in order of callable name.')
         ;
     }
 
@@ -29,33 +32,33 @@ class DebugEvents extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->displayListeners($output);
-    }
-
-    /**
-     * Output a table of listeners, and their callbacks.
-     *
-     * @param OutputInterface $output
-     */
-    private function displayListeners(OutputInterface $output)
-    {
         $table = new Table($output);
-        $table->setHeaders(array(
-            'Event Name',
-            'Priority',
-            'Listener',
-        ));
+        $table->setHeaders([
+            [new TableCell('Events, and target callable', ['colspan' => 3])],
+            ['Event Name', 'Priority', 'Callable'],
+        ]);
         $dispatcher = $this->app['dispatcher'];
         $listeners = $dispatcher->getListeners();
 
         foreach ($listeners as $eventName => $eventListeners) {
+            if ($input->getOption('sort-callable')) {
+                uasort($eventListeners, function ($a, $b) {
+                    $a = is_array($a) ? get_class($a[0]) : get_class($a);
+                    $b = is_array($b) ? get_class($b[0]) : get_class($b);
+                    if ($a === $b) {
+                        return 0;
+                    }
+
+                    return ($a < $b) ? -1 : 1;
+                });
+            }
             foreach ($eventListeners as $callable) {
                 $priority = $dispatcher->getListenerPriority($eventName, $callable);
                 if (is_array($callable)) {
                     $table->addRow([
                         $eventName,
                         $priority,
-                        sprintf('%s::%s', get_class($callable[0]), $callable[1]),
+                        sprintf('%s::%s()', get_class($callable[0]), $callable[1]),
                     ]);
                 } else {
                     $table->addRow([$eventName, $priority, get_class($callable)]);
