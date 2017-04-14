b184d978479b42e7dd3193a7019f9437845b69e3
Carson Full <carsonfull@gmail.com>
2017-04-14 10:11:09 -0500

    Deprecated Bolt\Helpers\Arr class.

--- src/Helpers/Arr.php
+++ src/Helpers/Arr.php
@@ -4,6 +4,9 @@ namespace Bolt\Helpers;
 
 use Bolt\Collection;
 
+/**
+ * @deprecated since 3.3, to be removed in 4.0. Use {@see Bolt\Collection\Arr} instead.
+ */
 class Arr extends Collection\Arr
 {
     /**
