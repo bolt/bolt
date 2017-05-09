fa3a3d312e47bb2aa8811d23eee41cfd6c035094
Gawain Lynch <gawain.lynch@gmail.com>
2017-05-09 09:45:00 +0200

    Embed service provider

--- src/Application.php
+++ src/Application.php
@@ -257,6 +257,7 @@ class Application extends Silex\Application
             ->register(new Provider\MailerServiceProvider())
             ->register(new Provider\PagerServiceProvider())
             ->register(new Provider\CanonicalServiceProvider())
+            ->register(new Provider\EmbedServiceProvider())
         ;
 
         $this['paths'] = $this['resources']->getPaths();
