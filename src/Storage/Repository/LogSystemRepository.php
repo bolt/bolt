207c8a062836eefe44c865a0b7a2c35f2616de46
Bob den Otter <bobdenotter@gmail.com>
2017-05-11 20:36:58 +0200

    Implement missing `LogSystemRepository::countSystemLog()`

--- src/Storage/Repository/LogSystemRepository.php
+++ src/Storage/Repository/LogSystemRepository.php
@@ -7,4 +7,28 @@ namespace Bolt\Storage\Repository;
  */
 class LogSystemRepository extends BaseLogRepository
 {
+    /**
+     * Get a count of system log entries.
+     *
+     * @return integer
+     */
+    public function countSystemLog()
+    {
+        $query = $this->countSystemLogQuery();
+
+        return $this->getCount($query->execute()->fetch());
+    }
+
+    /**
+     * Build the query to get a count of system log entries.
+     *
+     * @return QueryBuilder
+     */
+    public function countSystemLogQuery()
+    {
+        $qb = $this->createQueryBuilder();
+        $qb->select('COUNT(id) as count');
+
+        return $qb;
+    }
 }
