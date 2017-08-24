503c186b2bc318b55ea702f251f83eef124797f7
Gawain Lynch <gawain.lynch@gmail.com>
2017-08-24 14:09:23 +0200

    [Tests] Add a registration test for \Bolt\Extension\Manager

--- tests/phpunit/unit/Extension/ManagerTest.php
+++ tests/phpunit/unit/Extension/ManagerTest.php
@@ -2,16 +2,37 @@
 
 namespace Bolt\Tests\Extension;
 
-use Bolt\Tests\BoltUnitTest;
+use Bolt\Config;
+use Bolt\Extension\Manager;
+use Bolt\Filesystem\Adapter\Local;
+use Bolt\Filesystem\Filesystem;
+use Bolt\Logger\FlashLogger;
+use PHPUnit\Framework\TestCase;
+use Silex\Application;
 
 /**
- * Class to test Bolt\Extension\Manager.
+ * @covers \Bolt\Extension\Manager
  *
  * @author Gawain Lynch <gawain.lynch@gmail.com>
  */
-class ManagerTest extends BoltUnitTest
+class ManagerTest extends TestCase
 {
-    public function test()
+    /**
+     * @expectedException \RuntimeException
+     * @expectedExceptionMessage Can not re-register extensions.
+     */
+    public function testRegister()
     {
+        $filesystem = new Filesystem(new Local(__DIR__));
+        $flash = new FlashLogger();
+        $config = $this->getMockBuilder(Config::class)
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+        $app = new Application();
+        $manager = new Manager($filesystem, $filesystem, $flash, $config);
+        $manager->register($app);
+
+        $manager->register($app);
     }
 }
