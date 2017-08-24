6ac497c32556fbf6ee1685f2e7fa21153828d375
Gawain Lynch <gawain.lynch@gmail.com>
2017-08-24 14:29:56 +0200

    [Tests] Add base assertion for EntityBuilderTest

--- tests/phpunit/unit/Storage/EntityBuilderTest.php
+++ tests/phpunit/unit/Storage/EntityBuilderTest.php
@@ -2,18 +2,28 @@
 
 namespace Bolt\Tests\Storage;
 
+use Bolt\Storage\Entity\Builder;
 use Bolt\Tests\BoltUnitTest;
 
 /**
  * Class to test src/Storage/Repository and field transforms for load and hydrate.
  *
+ * @covers \Bolt\Storage\Entity\Builder
+ *
  * @author Ross Riley <riley.ross@gmail.com>
  */
 class EntityBuilderTest extends BoltUnitTest
 {
-    public function testBuilderCreate()
+    /**
+     * @expectedException \Bolt\Exception\StorageException
+     * @expectedExceptionMessage Attempted to load mapping data for unmapped class Bolt\Storage\Entity\Content
+     */
+    public function testBuilderCreateUnmapped()
     {
         $app = $this->getApp();
+        /** @var Builder $builder */
         $builder = $app['storage.entity_builder'];
+
+        $builder->create(['id' => 42, 'title' => 'Kenny drops the bear']);
     }
 }
