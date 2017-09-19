7dede6b0ea94e71e61c1b7d5522c595c10fca53c
Xiao Hu Tai <xiao@twokings.nl>
2017-09-19 11:15:33 +0200

    Add `is_numerical` check - See #6995

--- src/Config.php
+++ src/Config.php
@@ -350,6 +350,9 @@ class Config
             if (!empty($taxonomy['options']) && is_array($taxonomy['options'])) {
                 $options = [];
                 foreach ($taxonomy['options'] as $optionKey => $optionValue) {
+                    if (is_numeric($optionKey)) {
+                        $optionKey = $optionValue;
+                    }
                     $optionKey = Slugify::create()->slugify($optionKey);
                     $options[$optionKey] = $optionValue;
                 }
