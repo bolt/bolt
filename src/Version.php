798c0f6bf3d0339e140476b34591a6ebbebce1ca
Bob den Otter <bobdenotter@gmail.com>
2017-09-26 19:25:00 +0200

    Bump version to 3.4.0 beta 1

--- src/Version.php
+++ src/Version.php
@@ -19,7 +19,7 @@ final class Version
      *   Stable      — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    const VERSION = '3.4.0 beta 0';
+    const VERSION = '3.4.0 beta 1';
 
     /**
      * Whether this release is a stable one.
