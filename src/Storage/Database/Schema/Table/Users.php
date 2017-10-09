e3312e971245f07fc89538a7e587070d42adae5b
Ivan Masár <helix84@centrum.sk>
2017-10-09 15:54:22 +0200

    decrease column length to fit the utf8mb4 limit (closes: #7060)

--- src/Storage/Database/Schema/Table/Users.php
+++ src/Storage/Database/Schema/Table/Users.php
@@ -18,7 +18,7 @@ class Users extends BaseTable
         $this->table->addColumn('id',             'integer',    ['autoincrement' => true]);
         $this->table->addColumn('username',       'string',     ['length' => 32]);
         $this->table->addColumn('password',       'string',     ['length' => 128]);
-        $this->table->addColumn('email',          'string',     ['length' => 254]);
+        $this->table->addColumn('email',          'string',     ['length' => 191]);
         $this->table->addColumn('lastseen',       'datetime',   ['notnull' => false]);
         $this->table->addColumn('lastip',         'string',     ['length' => 45, 'notnull' => false]);
         $this->table->addColumn('displayname',    'string',     ['length' => 32]);
