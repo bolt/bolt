a8262462ecc0fe976e7d9501a7f9507e8769543a
Alex Marucci <alex.marucci@blockex.com>
2018-09-05 13:40:56 +0100

    Better naming for test function.

--- tests/phpunit/unit/Extension/SimpleExtensionTest.php
+++ tests/phpunit/unit/Extension/SimpleExtensionTest.php
@@ -71,7 +71,7 @@ class SimpleExtensionTest extends BoltUnitTest
         $this->assertSame($expected, $events);
     }
 
-    public function testRegisterRepositoryMappings()
+    public function testStorageTraitHasBeenImported()
     {
         $ext = new NormalExtension();
         $storageTraitHasBeenImported = method_exists($ext, 'extendRepositoryMapping');
