e9c2c520089af4d260bc526b66e37156ec713d75
Jarek Jakubowski <jarjak@tsh.io>
2018-09-11 22:18:27 +0200

    Add AppAwareTrait to new Content

---
+++ src/Storage/Entity/AppAwareTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bolt\Storage\Entity;
+
+use Bolt\Legacy\AppSingleton;
+use Silex\Application;
+
+trait AppAwareTrait
+{
+    protected $app;
+
+    public function __get($name)
+    {
+        if ($name === 'app') {
+            $this->getApp();
+        }
+        return is_callable(['parent', '__get']) ? parent::__get($method, $parameters) : null;
+    }
+
+    protected function getApp()
+    {
+        if (!$this->app) {
+            $this->setApp();
+        }
+        return $this->app;
+    }
+
+    protected function setApp(Application $app = null)
+    {
+        $this->app = $app ?: AppSingleton::get();
+    }
+}
