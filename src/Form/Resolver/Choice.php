aeb34e552a65effd93d5f751d5304406b5b36c26
Rômulo Guimarães <romulo1984@gmail.com>
2019-02-18 09:43:25 -0300

    Set default limit const

--- src/Form/Resolver/Choice.php
+++ src/Form/Resolver/Choice.php
@@ -17,6 +17,8 @@ use Bolt\Storage\Query\QueryResultset;
  */
 final class Choice
 {
+    const DEFAULT_LIMIT = 500;
+    
     /** @var Query */
     private $query;
 
@@ -113,7 +115,7 @@ final class Choice
      */
     private function getYamlValues(Bag $field)
     {
-        $values = array_slice($field->get('values', []), 0, $field->get('limit', 500), true);
+        $values = array_slice($field->get('values', []), 0, $field->get('limit', DEFAULT_LIMIT), true);
         if ($field->get('sortable')) {
             asort($values, SORT_REGULAR);
         }
@@ -146,7 +148,7 @@ final class Choice
 
         $filter = $field->get('filter');
         $filter['order'] = $field->get('sort');
-        $filter['limit'] = $field->get('limit', 500);
+        $filter['limit'] = $field->get('limit', DEFAULT_LIMIT);
         /** @var QueryResultset $entities */
         $entities = $this->query->getContent($contentType, $filter);
         if (!$entities) {
