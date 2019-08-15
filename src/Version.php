9131e134072f5cabc5ed8048cc948d212a5adb82
Bob den Otter <bob@twokings.nl>
2019-08-15 16:50:36 +0200

    Preparing release 3.6.10

--- src/Version.php
+++ src/Version.php
@@ -19,7 +19,7 @@ final class Version
      *   Stable      — 3.0.0
      *   Development — 3.1.0 alpha 1
      */
-    const VERSION = '3.6.9';
+    const VERSION = '3.6.10';
 
     /**
      * Whether this release is a stable one.
