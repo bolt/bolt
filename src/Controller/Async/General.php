19a49850c7ca1426a3976824cef99c86fe2a971c
SahAssar <svante.richter@gmail.com>
2019-11-04 17:08:54 +0100

    Fix tags in non-english languages and firefox caching selected options

--- src/Controller/Async/General.php
+++ src/Controller/Async/General.php
@@ -260,7 +260,7 @@ class General extends AsyncBase
         $table .= 'taxonomy';
 
         $query = $this->createQueryBuilder()
-            ->select('name, COUNT(slug) AS count')
+            ->select('slug, name, COUNT(slug) AS count')
             ->from($table)
             ->where('taxonomytype = :taxonomytype')
             ->groupBy('name')
@@ -275,11 +275,11 @@ class General extends AsyncBase
         usort(
             $results,
             function ($a, $b) {
-                if ($a['name'] == $b['name']) {
+                if ($a['slug'] == $b['slug']) {
                     return 0;
                 }
 
-                return ($a['name'] < $b['name']) ? -1 : 1;
+                return ($a['slug'] < $b['slug']) ? -1 : 1;
             }
         );
 
@@ -333,7 +333,7 @@ class General extends AsyncBase
         $table .= 'taxonomy';
 
         $query = $this->createQueryBuilder()
-            ->select("DISTINCT $table.name")
+            ->select("DISTINCT slug, name")
             ->from($table)
             ->where('taxonomytype = :taxonomytype')
             ->orderBy('name', 'ASC')
